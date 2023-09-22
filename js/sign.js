3a3d1de92a7c6892bd07da06f79c9e063a1834ea
Haneul-Kim <4188989@naver.com>
2023-09-22 18:44:45 +0900

    Refactor: 이미지 관련 HTML 태그 구조 변경
    이미지에 직접 효과를 넣는 것보다
    효과가 적용된 스냅샷을 사용하는 것이 반응형 이미지 구현에 좋을 것 같아서
    교체를 진행했음.

--- js/sign.js
+++ js/sign.js
@@ -1,13 +1,27 @@
-const $eyes = document.getElementsByClassName("form__pw-reveal-click");
+const $eyes = document.getElementsByClassName("form__input-btn");
+const $input = document.querySelectorAll("input");
+console.log($input);
 for (let i = 0; i < $eyes.length; i++) {
   $eyes[i].addEventListener("click", (event) => {
     showpw(event, i);
   });
+  $eyes[i].addEventListener("keypress", (event) => {
+    if (event.key === "Enter" || event.key === "Space") {
+    showpw(event, i);
+    }
+  });
+}
+for (let i=0; i < $input.length; i++) {
+  $input[i].addEventListener("keypress", (event) => {
+    if (event.key === "Enter") {
+      event.preventDefault();
+    }
+  })
 }
 
 function showpw(event, i) {
   event.preventDefault();
-  const $input = event.target.parentNode.previousSibling.previousSibling;
+  const $input = event.currentTarget.previousSibling.previousSibling;
   $input.classList.toggle("active");
   if ($input.classList.contains("active")) {
     $eyes[i].setAttribute("src", "assets/sign-eye-on.svg");
