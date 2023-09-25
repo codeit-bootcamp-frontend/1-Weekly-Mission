00c5e7b9932a933e302a2fb3dd4d9e4b76a23114
ayden <wpfekdml@naver.com>
2023-09-25 19:42:30 +0900

    fix(sign.js) : 조금 더 간결하게 이벤트 핸들러 수정

--- sign.js
+++ sign.js
@@ -2,14 +2,14 @@
 const sign_form = document.querySelector(".sign_form")
 
 function eyes_on_off(e) {
-    if (e.target.classList.contains("input__eyes--on")) { // 눈이 떠있는 = 비밀번호가 보일 때 동작
+    if (e.target.classList.contains("input__eyes--on")) {
+        // 눈이 떠있는 = 비밀번호가 보일 때 동작
         e.target.classList.remove("input__eyes--on");
-        e.target.classList.add("input__eyes--off");
 
         // span 바로 앞의 input type password로 변경
         e.target.previousElementSibling.type = "password"
-    } else if (e.target.classList.contains("input__eyes--off")) { // 눈이 감겨있는 = 비밀번호가 안 보일 때 동작
-        e.target.classList.remove("input__eyes--off");
+    } else if (e.target.classList.contains("input__eyes--off")) {
+        // 눈이 감겨있는 = 비밀번호가 안 보일 때 동작
         e.target.classList.add("input__eyes--on");
 
         // span 바로 앞의 input type 비우기
