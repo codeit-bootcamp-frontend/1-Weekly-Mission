f192fcf2098bf70b1f204eed88d1f9cf56aaedc6
ayden <wpfekdml@naver.com>
2023-09-25 21:36:02 +0900

    feat(sign) : input에 focus가 in out 될 때 적용할 이벤트 완성

--- sign.js
+++ sign.js
@@ -18,3 +18,50 @@ function eyes_on_off(e) {
 }
 
 sign_form.addEventListener('click', eyes_on_off);
+
+// focusout 때 input이 비어있으면 error 뱉는 이벤트
+const sign_input = document.querySelectorAll(".input__container");
+const error_message_null = ["이메일을 입력해주세요", "비밀번호를 입력해주세요", "확인할 비밀번호를 입력해주세요"];
+
+function create_Error_message(e) {
+    // input이 비어있을 때만 동작하도록 조건문 구성
+    if (e.target.value == "") {
+        // 새로운 span tag 하나 생성하고
+        const newDivTag = document.createElement("div");
+
+        // 생성한 span tag에 필요한 메시지를 넣어주고
+        for (let line in sign_input) {
+            if (e.target == sign_input[line]) {
+                newDivTag.textContent = error_message_null[line];
+                break
+            }
+        }
+
+        newDivTag.classList.add("null_error_message")
+
+        // 준비된 span tag를 div의 막내 노드가 되도록 한다
+        let currnetDivTag = e.target.parentElement
+        currnetDivTag.after(newDivTag);
+
+        currnetDivTag.classList.add("null_error_border")
+    }
+}
+
+    // 반복문을 통해 각각의 노드에 이벤트 핸들러를 달아준다
+for (let line of sign_input) {
+    line.addEventListener("focusout", create_Error_message)
+}
+
+// focusin 때 error 가 있으면 지우는 이벤트
+function remove_Error_message(e) {
+    let currentDivTag = e.target.parentElement;
+
+    if (currentDivTag.classList.contains("null_error_border")) {
+        currentDivTag.classList.remove("null_error_border")
+        currentDivTag.nextElementSibling.remove();
+    }
+}
+
+for (let line of sign_input) {
+    line.addEventListener("focusin", remove_Error_message)
+}
