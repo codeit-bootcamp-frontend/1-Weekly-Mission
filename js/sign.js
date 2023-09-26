58888a70c967f3850cd3ecb235f0830d43c1ce5a
Haneul-Kim <4188989@naver.com>
2023-09-26 13:39:43 +0900

    Feat: 로그인 페이지 입력값 확인 기능 추가
    input 태그의 value가 없을때 확인 메시지 표시.
    email형식의 value가 아닐때 확인 메시지 표시.

--- js/sign.js
+++ js/sign.js
@@ -1,5 +1,66 @@
-const $eyes = document.getElementsByClassName("form__pw-reveal-click");
-const $input = document.querySelectorAll("input");
+const $eyes = document.querySelectorAll(".eye");
+const $inputs = document.querySelectorAll(".form__input");
+const [$errEm, $errPw, $errPwCh] = document.querySelectorAll(".form__err")
+const [$labelEm, $labelPw, $labelPwCh] = document.querySelectorAll(".form__label")
+// RFC 5322 방식을 이용한 이메일 형식 정규표현식
+const regEx = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\[[\t -Z^-~]*])");
+
+// 기본 : 빈 입력값, 입력값 형태 확인, /folder로 이동하기
+for (let i = 0; i < $inputs.length; i++) {
+  $inputs[i].addEventListener("keypress", preventEnter)
+  $inputs[i].addEventListener("focusout", certify)
+  $inputs[i].addEventListener("focusin", certify)
+}
+
+function preventEnter(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+  }
+}
+
+function certify({ currentTarget, type }) {
+  // focusout 메시지 띄우기
+  if (type === "focusout") {
+    const val = currentTarget.value;
+    insertErr(val, currentTarget);
+    return;
+  }
+  // focusin 메시지 지우기
+  if (currentTarget.classList.contains("err-input")) {
+    const type = currentTarget.type;
+    const $err = type === 'email' ? $errEm : $errPw;
+    const $label = type === 'email' ? $labelEm : $labelPw;
+    $err.textContent = '';
+    $label.classList.toggle("err");
+    currentTarget.classList.toggle("err-input");
+  }
+}
+
+function insertErr(val, currentTarget) {
+  const type = currentTarget.type;
+  const $err = type === 'email' ? $errEm : $errPw;
+  const $label = type === 'email' ? $labelEm : $labelPw;
+  if (!val) {
+    // 메시지 띄우기
+    const msg = type === 'email' ? "이메일을 입력해주세요." : "비밀번호를 입력해주세요.";
+    // 메시지를 미리 추가해놓은 p태그로 전송
+    $err.textContent = msg;
+    $label.classList.toggle("err");
+    currentTarget.classList.toggle("err-input");
+  } else if (type === 'email' && !regEx.test(val)) {
+    const msg = "올바른 이메일 주소가 아닙니다."
+    $err.textContent = msg;
+    $label.classList.toggle("err");
+    currentTarget.classList.toggle("err-input");
+  }
+}
+
+// 값을 확인하고 맞으면 /folder로 이동
+function checkValue() {
+
+}
+
+// 심화 : 비밀번호 숨기기 기능
 for (let i = 0; i < $eyes.length; i++) {
   $eyes[i].addEventListener("click", (event) => {
     showpw(event, i);
@@ -10,25 +71,17 @@ for (let i = 0; i < $eyes.length; i++) {
     }
   });
 }
-for (let i = 0; i < $input.length; i++) {
-  $input[i].addEventListener("keypress", (event) => {
-    if (event.key === "Enter") {
-      event.preventDefault();
-    }
-  })
-}
-
 function showpw(event, i) {
   event.preventDefault();
-  const $input = event.currentTarget.parentNode.previousSibling.previousSibling;
-  $input.classList.toggle("active");
-  if ($input.classList.contains("active")) {
+  const $pw = document.querySelectorAll(".password")[i];
+  $pw.classList.toggle("active");
+  if ($pw.classList.contains("active")) {
     $eyes[i].setAttribute("src", "assets/sign-eye-on.svg");
-    $input.setAttribute("type", "text");
+    $pw.setAttribute("type", "text");
   } else {
     $eyes[i].setAttribute("src", "assets/sign-eye-off.svg");
-    $input.setAttribute("type", "password");
+    $pw.setAttribute("type", "password");
   }
-  $input.focus();
+  $pw.focus();
 }
 
