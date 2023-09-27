1758f2546026c4e2633b80661919831f6dc0d436
ayden <wpfekdml@naver.com>
2023-09-27 17:36:02 +0900

    fix(sign.js) : 하나의 이벤트 흐름을 맡은 바에 따라 쪼개는 연습

--- sign.js
+++ sign.js
@@ -5,7 +5,7 @@ function eyes_on_off(e) {
     if (e.target.classList.contains("input__eyes--on")) {
         // 눈이 떠있는 = 비밀번호가 보일 때 동작
         e.target.classList.remove("input__eyes--on");
-
+        
         // span 바로 앞의 input type password로 변경
         e.target.previousElementSibling.type = "password"
     } else if (e.target.classList.contains("input__eyes--off")) {
@@ -19,46 +19,69 @@ function eyes_on_off(e) {
 
 sign_form.addEventListener('click', eyes_on_off);
 
+
 // focusout 때 (input이 비어있거나 이메일이 잘못되었으면) error 뱉는 이벤트
 const sign_input = document.querySelectorAll(".input__container");
 const error_message_null = ["이메일을 입력해주세요", "비밀번호를 입력해주세요", "확인할 비밀번호를 입력해주세요"];
-const emailVerification = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
+const emailVerification = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-function create_Error_message(e) {
-    // 새로운 div tag 하나 생성하고
-    let newDivTag = document.createElement("div");
+// div 노드 생성하는 함수
+const createDivElement = () => { return document.createElement("div"); }
 
-    // input이 비어있을 때만 동작하도록 조건문 구성
-    if (e.target.value == "") {
-        // 생성한 div tag에 필요한 메시지를 넣어주고
+// input이 비어있으면 정해진 텍스트 노드 붙여주는 함수
+const validateInputNull = (arg, e) => {
+    if (e.target.value === "") {
         for (let line in sign_input) {
-            if (e.target == sign_input[line]) {
-                newDivTag.textContent = error_message_null[line];
+            if (e.target === sign_input[line]) {
+                arg.textContent = error_message_null[line];
                 break
             }
         }
-    } else if (e.target == sign_input[0]) {
-        if (e.target.value !== emailVerification) { // 이메일 검증 실패시
-            newDivTag.textContent = "올바른 이메일 주소가 아닙니다."
-        }
-    } else { // input이 비어있지도, 이메일이 잘못되지도 않았을 때 노드 추가 방지
-        braak
     }
 
-    // div tag에 에러 메세지 class 추가
-    newDivTag.classList.add("null_error_message")
+    return arg
+}
 
-    // 준비된 div tag를 div의 막내 노드가 되도록 한다
-    let currnetDivTag = e.target.parentElement
-    currnetDivTag.after(newDivTag);
+// input의 이메일이 맞지 않으면 정해진 텍스트 노드 붙여주는 함수
+const validateInputEmail = (arg, e) => {
+       if (e.target === sign_input[0]) {
+        if (e.target.value.match(emailVerification) == null) {
+            arg.textContent = "올바른 이메일 주소가 아닙니다.";
+        }
+    }
 
-    currnetDivTag.classList.add("null_error_border")
+    return arg
+}
+
+// 정해진 노드를 찾아서 에러 메세지 붙여줌
+const addErrorMessageAfterPointNode = (errorMassageNode, pointNode, e) => {
+    pointNode.after(errorMassageNode);
+    pointNode.classList.add("null_error_border");
+}
+
+// 인풋이 비어있거나, 인풋의 이메일이 이상하면 튀어나오는 이벤트 핸들러
+const handleInputError = (e) => {
+    let errorMessageNode = createDivElement();
+
+    errorMessageNode.classList.add("null_error_message");
+
+    validateInputNull(errorMessageNode, e);
+    validateInputEmail(errorMessageNode, e);
+
+    console.log(errorMessageNode.textContent)
+
+    if (errorMessageNode.textContent !== "") {
+        let replaceErrorMassagePoint = e.target.parentElement;
+        addErrorMessageAfterPointNode(errorMessageNode, replaceErrorMassagePoint, e);
+    }
 }
 
     // 반복문을 통해 각각의 노드에 이벤트 핸들러를 달아준다
 for (let line of sign_input) {
-    line.addEventListener("focusout", create_Error_message)
+    line.addEventListener("focusout", handleInputError)
 }
+
+
 
 // focusin 때 error 가 있으면 지우는 이벤트
 function remove_Error_message(e) {
