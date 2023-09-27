f50918b10e71306b4bd67ccdb01a3cf6f326e631
oauch <lgm9515@naver.com>
2023-09-27 12:42:17 +0900

    Feat: 에러 클래스 삭제 분리, 로그인 함수 구현

--- js/singin.js
+++ js/singin.js
@@ -2,6 +2,7 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const emailError = document.createElement("div");
 const passwordError = document.createElement("div");
+const loginButton = document.querySelector("form");
 
 /**
  * 이메일 체크 함수
@@ -9,11 +10,8 @@ const passwordError = document.createElement("div");
  * @returns boolean
  */
 function emailCheck(email) {
-  if (email.indexOf("@") >= 0 && email.indexOf(".") >= 0) {
-    return true;
-  } else {
-    return false;
-  }
+  var pattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+  return pattern.test(email);
 }
 
 /**
@@ -30,7 +28,14 @@ function emailErrorMsg() {
     emailError.classList.add("error-msg");
     emailError.textContent = "올바른 이메일 주소가 아닙니다.";
     email.parentElement.append(emailError);
-  } else if (email.parentElement.lastElementChild === emailError) {
+  }
+}
+
+/**
+ * 이메일 에러 클래스 삭제 함수
+ */
+function emailErrorMsgDel() {
+  if (email.parentElement.lastElementChild === emailError) {
     email.classList.remove("error");
     email.parentElement.lastElementChild.remove();
   }
@@ -45,11 +50,45 @@ function passwordErrorMsg() {
     passwordError.classList.add("error-msg");
     passwordError.textContent = "비밀번호를 입력하세요.";
     password.parentElement.append(passwordError);
-  } else if (password.parentElement.lastElementChild === passwordError) {
+  }
+}
+
+/**
+ * 비밀번호 에러 클래스 삭제 함수
+ */
+function passwordErrorMsgDel() {
+  if (password.parentElement.lastElementChild === passwordError) {
     password.classList.remove("error");
     password.parentElement.lastElementChild.remove();
   }
 }
 
+/**
+ * 로그인 함수
+ * @param {event} event
+ */
+function login(event) {
+  event.preventDefault();
+  let link = "../pages/folder.html";
+  const testEmail = "test@codeit.com";
+  const testPassword = "codeit101";
+
+  if (email.value !== testEmail && password.value !== testPassword) {
+    email.classList.add("error");
+    emailError.classList.add("error-msg");
+    emailError.textContent = "이메일을 확인해주세요.";
+    email.parentElement.append(emailError);
+    password.classList.add("error");
+    passwordError.classList.add("error-msg");
+    passwordError.textContent = "비밀번호를 확인해주세요.";
+    password.parentElement.append(passwordError);
+  } else if (email.value === testEmail && password.value === testPassword) {
+    window.location.replace(link);
+  }
+}
+
 email.addEventListener("focusout", emailErrorMsg);
+email.addEventListener("focusin", emailErrorMsgDel);
 password.addEventListener("focusout", passwordErrorMsg);
+password.addEventListener("focusin", passwordErrorMsgDel);
+loginButton.addEventListener("submit", login);
