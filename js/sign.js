c4ff512e0f6ad4888a122193ce093585e8df9f75
Haneul-Kim <4188989@naver.com>
2023-09-27 22:09:41 +0900

    Feat: 회원가입 페이지 입력값 확인 기능 추가
    입력값의 존재, 이메일 양식을 확인하는 자바스크립트 추가.
    비밀번호 확인값도 비밀번호와 일치하는 지 확인함.
    엔터키와 버튼클릭 모두 작동하게 하여 사용자 접근성 높임.
    test@codeit.com, codeit101 입력시 /folder로 이동함.
    
    비밀번호 확인 기능도 구현, 키보드 focus 가능한 버튼으로 만들어서
    웹 접근성을 높이려고 했음. button 태그 cursor도 수정함.

--- js/sign.js
+++ js/sign.js
@@ -26,31 +26,39 @@ function certify({ currentTarget, type }) {
 
 function insertErr(currentTarget) {
   const val = currentTarget.value;
-  const type = currentTarget.type;
-  const $err = type === 'email' ? $errEm : $errPw;
-  const $label = type === 'email' ? $labelEm : $labelPw;
+  const name = currentTarget.name;
+  const $err = name === 'email' ? $errEm : name === 'password' ? $errPw : $errPwCh;
+  const $label = name === 'email' ? $labelEm : name === 'password' ? $labelPw : $labelPwCh;
+  const $inputPw = $inputs[1];
+  const $inputPwCh = $inputs[2];
   if (!val) {
     // 메시지 띄우기
-    const msg = type === 'email' ? "이메일을 입력해주세요." : "비밀번호를 입력해주세요.";
+    const msg = name === 'email' ? "이메일을 입력해주세요." : "비밀번호를 입력해주세요.";
     // 메시지를 미리 추가해놓은 p태그로 전송
     $err.textContent = msg;
-    $label.classList.toggle("err");
-    currentTarget.classList.toggle("err-input");
-  } else if (type === 'email' && !regEx.test(val)) {
+    $label.classList.add("err");
+    currentTarget.classList.add("err-input");
+  } else if (name === 'email' && !regEx.test(val)) {
     const msg = "올바른 이메일 주소가 아닙니다."
     $err.textContent = msg;
-    $label.classList.toggle("err");
-    currentTarget.classList.toggle("err-input");
+    $label.classList.add("err");
+    currentTarget.classList.add("err-input");
+    // 입력한 비밀번호와 확인값이 같은지 검증
+  } else if ($inputPwCh && $inputPw.value !== $inputPwCh.value) {
+    const msg = "비밀번호가 일치하지 않습니다."
+    $errPwCh.textContent = msg;
+    $labelPwCh.classList.add("err");
+    $inputPwCh.classList.add("err-input");
   }
 }
 
 function resetErr(currentTarget) {
-  const type = currentTarget.type;
-  const $err = type === 'email' ? $errEm : $errPw;
-  const $label = type === 'email' ? $labelEm : $labelPw;
+  const name = currentTarget.name;
+  const $err = name === 'email' ? $errEm : name === 'password' ? $errPw : $errPwCh;
+  const $label = name === 'email' ? $labelEm : name === 'password' ? $labelPw : $labelPwCh;
   $err.textContent = '';
-  $label.classList.toggle("err");
-  currentTarget.classList.toggle("err-input");
+  $label.classList.remove("err");
+  currentTarget.classList.remove("err-input");
 }
 
 // 값을 확인하고 맞으면 /folder로 이동
@@ -60,14 +68,22 @@ function checkValue(event) {
   event.preventDefault();
   const emVal = $inputs[0].value;
   const pwVal = $inputs[1].value;
+  const pwChVal = $inputs[2]?.value;
   const emCheck = emVal === 'test@codeit.com';
   const pwCheck = pwVal === 'codeit101';
-  // 일치하면 이동, 값이 불일치하면 값 제거;
-  if (emCheck && pwCheck) {
-    location.href = "./folder.html"
-  } else if (emVal && pwVal) {
+  // 일치하면 이동, 값이 불일치하면 값 제거; 회원가입 페이지에서는 이동 X
+  if (!$inputs[2]) {
+    if (emCheck && pwCheck) {
+      location.href = "./folder.html"
+    } else if (emVal && pwVal) {
+      $inputs[0].value = "";
+      $inputs[1].value = "";
+    }
+    // 회원가입 페이지에서 비밀번호 === 비밀번호확인
+  } else if (emCheck && pwVal === pwChVal) {
     $inputs[0].value = "";
     $inputs[1].value = "";
+    $inputs[2].value = "";
   }
 }
 
@@ -88,8 +104,8 @@ function eyeEvent(event) {
 
 function showpw(event) {
   event.preventDefault();
-  const $eye = event.currentTarget
-  const $pw = document.querySelector(".password");
+  const $eye = event.currentTarget.firstElementChild;
+  const $pw = event.currentTarget.previousElementSibling;
   $pw.classList.toggle("active");
   if ($pw.classList.contains("active")) {
     $eye.setAttribute("src", "assets/sign-eye-on.svg");
