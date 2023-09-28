9dc6b132d5912d3d6873438eac2be4bf12e0417e
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-09-28 17:04:09 +0900

    feat(login.js): 이메일유효성검사기능구현

--- script.js
+++ script.js
@@ -8,21 +8,33 @@ function showMessage(inputText) {
   return `${inputText}을 입력해주세요`;
 }
 
-// 빈값일때
+// 빈값일때 handling
 function IsEmpty(input, inputLabel) {
   const errorMsgs = inputLabel.querySelector("small");
   if (input.value.trim().length === 0 && errorMsgs.innerText.length === 0) {
     // inpuLabel.textContent는 공백을 포함하기 떄문에 공백포함해서 나와서 이상함
     let errorMessage = showMessage(inputLabel.innerText);
-    inputLabel.querySelector("small").innerText = errorMessage;
+    errorMsgs.innerText = errorMessage;
   } else if (input.value.trim().length !== 0) {
-    inputLabel.querySelector("small").innerText = "";
+    errorMsgs.innerText = "";
+  }
+}
+
+// 유효한 이메일이 아닐때 handling
+function IsValidEmail(input, inputLabel, message) {
+  const re = /^[a-z0-9]+@[a-z]+\.[a-z]{3,5}/;
+  const texts = input.value.trim();
+
+  if (!re.test(texts) && texts.length) {
+    const errorMsgs = inputLabel.querySelector("small");
+    errorMsgs.innerText = message;
   }
 }
 
 email.addEventListener("focusout", function (event) {
   event.preventDefault();
   IsEmpty(email, emailLabel);
+  IsValidEmail(email, emailLabel, "유효하지않은이메일입니다");
 });
 
 password.addEventListener("focusout", function (event) {
