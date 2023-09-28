14bbc9136d64210c77aa81c03a0ffcb9812c6b0e
ParkSohyunee <sohyunee016@gmail.com>
2023-09-28 12:34:18 +0900

    feat: signin - 이메일 형식 유효성검사 기능 추가

--- Linkbrary/src/signin.js
+++ Linkbrary/src/signin.js
@@ -6,11 +6,26 @@ const password = document.getElementById("password");
 const emailError = document.getElementById("emailError");
 const passwordError = document.getElementById("passwordError");
 
+// 이메일 input에서 focus out할 때, 이메일 형식에 맞지 않는 값이 있는 경우 에러메세지
+const regExp = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/i;
+
+const emailValidation = (value) => {
+  if (!regExp.test(value)) {
+    emailError.textContent = "올바른 이메일 주소가 아닙니다.";
+    email.style.borderColor = "var(--color-red)";
+  } else {
+    emailError.textContent = "";
+    email.style.borderColor = "var(--color-gray-20)";
+  }
+};
+
 // 이메일, 비밀번호 input에서 focus out할 때, 값이 없을 경우 에러메세지
 const checkEmailValue = () => {
   if (!email.value) {
     emailError.textContent = "이메일을 입력해주세요";
     email.style.borderColor = "var(--color-red)";
+  } else {
+    emailValidation(email.value);
   }
 };
 
@@ -25,6 +40,5 @@ email.addEventListener("focusout", checkEmailValue);
 password.addEventListener("focusout", checkPasswordValue);
 
 // To-do
-// 이메일 input에서 focus out할 때, 이메일 형식에 맞지 않는 값이 있는 경우 에러메세지
 // 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동
 // 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지
