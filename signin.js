c40def0ca3f31b5b54e6c7bfc02fe22ff47da656
youdame <112458620+youdame@users.noreply.github.com>
2023-09-28 23:57:26 +0900

    Feat(signin): 에러 메세지 부분 구현 최종 완료 but 로그인 기능이 이상함

--- signin.js
+++ signin.js
@@ -3,63 +3,64 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const pattern = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-za-z0-9\\-]+/;
 
+const loginButton = document.querySelector(".login-button");
 const pEmail = document.createElement("p");
 const pPassword = document.createElement("p");
 
 function emailValidChk(email) {
-  if (pattern.test(email) === false) { return false; }
-  else { return true; }
+  return pattern.test(email);
 }
 
+function displayError(inputBox, errorMessage, p) {  
+  p.textContent = errorMessage;
+  p.classList.add("error");
+  inputBox.append(p);
+}
+
+function clearErrors(e, p){
+  e.target.parentElement.lastElementChild.textContent = "";
+  
+}
 function showErrorMessage(e) {
   const inputBox = e.target.parentElement;
-  // Create a new <p> element for each error message
-  let errorMessage = '';
 
+  
+  
   switch (e.target.id) {
+    
     case "email":
       if (e.target.value === "") {
-        errorMessage = "이메일을 입력해주세요.";
+        displayError(inputBox, "이메일을 입력해주세요.", pEmail);
       } else if (!emailValidChk(email.value)) {
-        errorMessage = "올바른 이메일 주소가 아닙니다.";
-      }
-      pEmail.textContent = errorMessage;
-      pEmail.classList.add("error");
-      inputBox.append(pEmail);
+        displayError(inputBox, "올바른 이메일 주소가 아닙니다.", pEmail);
+      } 
+
       break;
 
     case "password":
       if (e.target.value === "") {
-        errorMessage = "비밀번호를 입력해주세요.";
-      }
-      pPassword.textContent = errorMessage;
-      pPassword.classList.add("error");
-      inputBox.append(pPassword);
+        displayError(inputBox, "비밀번호를 입력해주세요.",pPassword);
+      } 
       break;
 
     default:
       return;
   }
-
-
-  
 }
 
 function userAuthentication(e) {
   const inputEmail = document.querySelector(".email");
   const inputPassword = document.querySelector(".password");
-
-  if (email.value === "test@codeit.com" && password.value === "codeit101") {
+  console.log(e.type);
+  if (email.value == "test@codeit.com" && password.value == "codeit101") {
     window.location.href = "/folder";
   } else {
-    pEmail.textContent = "이메일을 확인해주세요.";
-    pPassword.textContent = "비밀번호를 확인해주세요.";
-    pEmail.classList.add("error");
-    pPassword.classList.add("error");
-    inputEmail.append(pEmail);
-    inputPassword.append(pPassword);
+    // e.preventDefault();
+    displayError(inputEmail, "이메일을 확인해주세요.", pEmail);
+    displayError(inputPassword, "비밀번호를 확인해주세요.", pPassword);
   }
 }
 
 inputFrame.addEventListener("focusout", showErrorMessage);
-password.addEventListener("change", userAuthentication);
+inputFrame.addEventListener("focusin", clearErrors);
+loginButton.addEventListener("click", userAuthentication);
