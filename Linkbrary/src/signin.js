8f906956e0ea95e58adeabd22f3749bd74e3310d
ParkSohyunee <sohyunee016@gmail.com>
2023-09-28 15:37:02 +0900

    feat: signin - 로그인 기능 추가

--- Linkbrary/src/signin.js
+++ Linkbrary/src/signin.js
@@ -1,4 +1,5 @@
 const input = document.querySelector(".form__input");
+const form = document.querySelector(".form");
 
 const email = document.getElementById("email");
 const password = document.getElementById("password");
@@ -36,9 +37,27 @@ const checkPasswordValue = () => {
   }
 };
 
+// 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동
+const onClickSubmit = (e) => {
+  e.preventDefault();
+
+  if (!email.value || !password.value) {
+    checkEmailValue();
+    checkPasswordValue();
+    return;
+  }
+  if (email.value === "test@codeit.com" && password.value === "codeit101") {
+    form.submit();
+  } else {
+    // 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지
+    emailError.textContent = "이메일을 확인해주세요";
+    email.style.borderColor = "var(--color-red)";
+    passwordError.textContent = "비밀번호를 확인해주세요";
+    password.style.borderColor = "var(--color-red)";
+  }
+  // console.log(e);
+};
+
 email.addEventListener("focusout", checkEmailValue);
 password.addEventListener("focusout", checkPasswordValue);
-
-// To-do
-// 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동
-// 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지
+form.addEventListener("submit", onClickSubmit);
