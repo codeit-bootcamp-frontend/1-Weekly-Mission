893b4ee00e1f5e3de7393ca0020babbb11f06c47
olseul <seul9085@naver.com>
2023-09-28 10:54:36 +0900

    feat: 로그인 버튼 클릭 시 동작하는 이벤트 구현

--- signin/signin.js
+++ signin/signin.js
@@ -1,5 +1,6 @@
 const emailInput = document.querySelector("#username");
 const passwordInput = document.querySelector("#password");
+const loginBtn = document.querySelector("#loginBtn");
 
 const emailErrorText = document.querySelector("#emailError");
 const passwordErrorText = document.querySelector("#passwordError");
@@ -26,6 +27,26 @@ passwordInput.addEventListener("blur", function () {
   } else {
     passwordErrorText.textContent = "";
     passwordErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
+  }
+});
+
+loginBtn.addEventListener("click", function () {
+  const email = emailInput.value;
+  const password = passwordInput.value;
+
+  if (email === "test@codeit.com" && password === "codeit101") {
+    location.href = "/folder";
+  } else if (!email && !password) {
+    emailErrorText.textContent = "이메일을 확인해주세요.";
+    emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    passwordErrorText.textContent = "비밀번호를 확인해주세요.";
+    passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
+  } else if (!email) {
+    emailErrorText.textContent = "이메일을 확인해주세요.";
+    emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
+  } else if (!password) {
+    passwordErrorText.textContent = "비밀번호를 확인해주세요.";
+    passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
   }
 });
 
