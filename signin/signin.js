ea5866cb4d21a85b5366e11fc90da842df5f02c7
olseul <seul9085@naver.com>
2023-09-28 11:45:12 +0900

    feat: input에 에러 케이스 디자인 적용

--- signin/signin.js
+++ signin/signin.js
@@ -10,12 +10,15 @@ emailInput.addEventListener("blur", function () {
   if (!email) {
     emailErrorText.textContent = "이메일을 입력해주세요.";
     emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   } else if (!isValidEmail(email)) {
     emailErrorText.textContent = "올바른 이메일 주소가 아닙니다.";
     emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   } else {
     emailErrorText.textContent = "";
     emailErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
+    emailInput.classList.remove("error"); // 정상 시 .error 클래스 제거
   }
 });
 
@@ -24,9 +27,11 @@ passwordInput.addEventListener("blur", function () {
   if (!password) {
     passwordErrorText.textContent = "비밀번호를 입력해주세요.";
     passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    passwordInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   } else {
     passwordErrorText.textContent = "";
     passwordErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
+    passwordInput.classList.remove("error"); // 정상 시 .error 클래스 제거
   }
 });
 
@@ -41,12 +46,16 @@ loginBtn.addEventListener("click", function () {
     emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
     passwordErrorText.textContent = "비밀번호를 확인해주세요.";
     passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    passwordInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   } else if (!email) {
     emailErrorText.textContent = "이메일을 확인해주세요.";
     emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   } else if (!password) {
     passwordErrorText.textContent = "비밀번호를 확인해주세요.";
     passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
+    passwordInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
   }
 });
 
