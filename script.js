7ab42551d23ed2caa185a0b7906e3f8844e478c3
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-09-28 16:32:25 +0900

    feat(login.js): email,password에 인풋을 넣었을때 오류메시지 초기화 기능 개발

--- script.js
+++ script.js
@@ -4,23 +4,19 @@ const password = document.getElementById("password");
 const emailLabel = document.querySelector(".email-label");
 const passwordLabel = document.querySelector(".password-label");
 
-// function showError(event) {
-//   console.log(event.target.classList);
-// }
-
-// form.addEventListener("focusout", showError);
-
 function showMessage(inputText) {
   return `${inputText}을 입력해주세요`;
 }
 
+// 빈값일때
 function IsEmpty(input, inputLabel) {
   const errorMsgs = inputLabel.querySelector("small");
   if (input.value.trim().length === 0 && errorMsgs.innerText.length === 0) {
     // inpuLabel.textContent는 공백을 포함하기 떄문에 공백포함해서 나와서 이상함
     let errorMessage = showMessage(inputLabel.innerText);
     inputLabel.querySelector("small").innerText = errorMessage;
-    console.log(inputLabel);
+  } else if (input.value.trim().length !== 0) {
+    inputLabel.querySelector("small").innerText = "";
   }
 }
 
