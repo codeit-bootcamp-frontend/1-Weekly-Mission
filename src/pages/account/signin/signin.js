cd91db8420cc61407cecc88380c581c1b43b01cf
Archer <sp38013@naver.com>
2023-09-29 17:33:38 +0900

    Feat : signin submit event 기능 추가

--- src/pages/account/signin/signin.js
+++ src/pages/account/signin/signin.js
@@ -2,10 +2,23 @@ import * as account from "../account.js";
 
 const inputEmail = document.querySelector(".email_input");
 const inputPwd = document.querySelector(".pwd_input");
+const $form = document.querySelector(".sign_form");
 
 inputEmail.addEventListener("focusout", (e) => {
   account.validationForm(e);
 });
 inputPwd.addEventListener("focusout", (e) => {
   account.validationForm(e);
+});
+
+$form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  let $Email = inputEmail.value;
+  let $Pwd = inputPwd.value;
+  if ($Email === "test@codeit.com" && $Pwd === "codeit101") {
+    alert("로그인 성공!");
+    location.href = "/folder";
+  } else {
+    alert("로그인 실패!");
+  }
 });
