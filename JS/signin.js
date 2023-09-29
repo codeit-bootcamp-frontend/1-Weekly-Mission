32a67af338c4917bfac924bb3f5e57cd72a93c68
Nacho-Kang <tarea202001@gmail.com>
2023-09-29 19:35:17 +0900

    Feat(signin) : inputbox 유효성 조건 추가 및 eyeOn / eyeoff  이벤트 구현
    -input 에러 케이스 red border-line 추가
    -비밀번호 eyeOn / eyeOff 이벤트 핸들러 구현 (makeEyeOn : 비밀번호
    텍스트화, makeEyeOff : 비밀번호 토큰화)
    -input email 유효성 검사 조건 수정
    -images eye-off.png 파일 추가
    -수정사항 관련 태그 내 id 및 class명 수정
    -input placeholder 속성 추가

--- JS/signin.js
+++ JS/signin.js
@@ -1,3 +1,5 @@
+// form input box 유효성 검사
+
 const $inputEmail = document.querySelector(`[name="email"]`);
 const $inputPassword = document.querySelector(`[name="password"]`)
 const spanErrorEmail = document.querySelector('#error_email');
@@ -5,17 +7,20 @@ const spanErrorPassword = document.querySelector('#error_password');
 const $submitBtn = document.querySelector('#submit');
 let flag = true;
 
-function verificationEmail (){
+function verificationEmail (){ 
   if ( $inputEmail.value.length === 0){
     spanErrorEmail.textContent = "이메일을 입력해주세요.";
     spanErrorEmail.classList.add('show');
+    spanErrorEmail.previousElementSibling.classList.add('red_border');
     flag = false;
-  } else if ($inputEmail.value.indexOf('@') === -1 || $inputEmail.value.indexOf('@') === $inputEmail.value.length - 1) {
+  } else if ($inputEmail.value.indexOf('@', 1) <= 0 || $inputEmail.value.lastIndexOf('.') === $inputEmail.value.length -1 ||$inputEmail.value.lastIndexOf('.') < 0 || $inputEmail.value.lastIndexOf('.') < $inputEmail.value.lastIndexOf('@')) {
     spanErrorEmail.textContent = "올바른 이메일 주소가 아닙니다.";
     spanErrorEmail.classList.add('show');
+    spanErrorEmail.previousElementSibling.classList.add('red_border');
     flag = false;
   } else {
     spanErrorEmail.classList.remove('show')
+    spanErrorEmail.previousElementSibling.classList.remove('red_border');
   }
 }
 
@@ -23,9 +28,11 @@ function verificationPassword (){
   if ( $inputPassword.value.length === 0){
     spanErrorPassword.textContent = "비밀번호를 입력해주세요.";
     spanErrorPassword.classList.add('show');
+    spanErrorPassword.previousElementSibling.classList.add('red_border');
     flag = false;
   } else {
     spanErrorPassword.classList.remove('show')
+    spanErrorPassword.previousElementSibling.classList.remove('red_border');
   }
 }
 
@@ -60,3 +67,23 @@ function submitVerification (e){
 $inputPassword.addEventListener('focusout', verificationPassword);
 $inputEmail.addEventListener('focusout', verificationEmail);
 $submitBtn.addEventListener('click', submitVerification);
+
+// eye icon toggle 이벤트
+
+const $eyeOn = document.querySelector('#eye_on');
+const $eyeOff = document.querySelector('#eye_off');
+
+function makeEyeOn (){
+  $eyeOn.classList.toggle('show');
+  $eyeOff.classList.toggle('show');
+  $inputPassword.type = "text";
+}
+
+function makeEyeOff (){
+  $eyeOn.classList.toggle('show');
+  $eyeOff.classList.toggle('show');
+  $inputPassword.type = "password";
+}
+
+$eyeOff.addEventListener('click', makeEyeOn);
+$eyeOn.addEventListener('click', makeEyeOff);
