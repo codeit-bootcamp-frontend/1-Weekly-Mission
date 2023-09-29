8ee122543ea7dd51b284decee2cdbb6124d787a9
Archer <sp38013@naver.com>
2023-09-29 16:54:12 +0900

    Feat : account Pages 이메일 정규표현식 검사 및 에러 처리기능 추가

--- src/pages/account/account.js
+++ src/pages/account/account.js
@@ -5,8 +5,9 @@ const inputPwdCheck = document.querySelector(".pwd_check");
 
 // 정규 표현삭
 const expEmail =
-  "/^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i";
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
+//유효성 검사 함수
 function validationForm(e) {
   let node = e.target;
   let nodeInfo = {
@@ -14,6 +15,9 @@ function validationForm(e) {
     className: node.className,
     value: node.value,
   };
+  let errorGb = false; //Error 구분값
+  let errType = 0;
+
   /** 기존 에러 이벤트 초기화 */
   if (node.classList.contains("errorInput")) {
     node.classList.remove("errorInput");
@@ -25,26 +29,40 @@ function validationForm(e) {
     node.nextElementSibling.remove("error_msg");
   }
 
-  let msg = "";
-  //value check
   if (nodeInfo.value === "") {
-    msg = "입력해주세요.";
-    let errMsg = setErrorMsg(nodeInfo, msg); //error 메세지 생성
-    let errTag = nodeUtils.createNode("span", "error_msg", errMsg); //tag 생성
-    node.classList.toggle("errorInput"); //error class 추가 및 삭제
-    node.after(errTag);
-    // toggleGb ? node.after(errTag) : node.nextElementSibling.remove();
+    errorGb = true;
+    errType = 1;
   } else {
     //정규표현식 체크
+    if (nodeInfo.type === "email") {
+      errorGb = !expEmail.test(nodeInfo.value);
+      errType = 2;
+    }
+  }
+
+  //Error 이벤트 동작
+  if (errorGb) {
+    let errMsg = setErrorMsg(nodeInfo, errType); //error 메세지 생성
+    let errTag = nodeUtils.createNode("span", "error_msg", errMsg); //tag 생성
+    node.classList.add("errorInput"); //error class 추가 및 삭제
+    node.after(errTag);
   }
 }
 
-function setErrorMsg(nodeInfo, msg) {
+/***
+ * 에러 메세지 생성 함수
+ * errType
+ *    1 : value 없음
+ *    2 : 유효성 검사 실패
+ *
+ *
+ *
+ */
+function setErrorMsg(nodeInfo, errType) {
   let type = nodeInfo.type;
   let className = nodeInfo.className;
-
-  let errorMsg = { type: "", msg: msg };
-
+  let errorMsg = { type: "", errType: errType };
+  let result = "";
   if (type === "email") {
     errorMsg.type = "이메일";
   } else if (
@@ -55,7 +73,13 @@ function setErrorMsg(nodeInfo, msg) {
     errorMsg.type = "비밀번호";
   }
 
-  return `${errorMsg.type}을 ${errorMsg.msg}`;
+  if (errType === 1) {
+    result = `${errorMsg.type}을 입력해주세요.`;
+  } else if (errType === 2) {
+    result = `올바른 ${errorMsg.type} 주소가 아닙니다.`;
+  }
+
+  return result;
 }
 
 inputEmail.addEventListener("focusout", (e) => {
