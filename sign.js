3381c8a76b0e5f7196c41ed44dd66a08a93864ca
ayden <wpfekdml@naver.com>
2023-09-30 15:03:33 +0900

    feat(sign) : 잘못된 로그인 시도가 있을 때, 에러 메세지 발생

--- sign.js
+++ sign.js
@@ -35,23 +35,29 @@ for (let line of sign_input) {
     // focusout 때 (input이 비어있거나 이메일이 잘못되었으면) error 뱉는 이벤트
 const emailVerification = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
 
-const errorMessageId = {
+const errorMessageInput = {
     signinEmail: "이메일을 입력해주세요",
     signinPassword: "비밀번호를 입력해주세요",
     signinPasswordCheck: "확인할 비밀번호를 입력해주세요",
 }
 
+const renderErrorMessageNode = () => {
+    const errorMessageNode = document.createElement("div");
+    errorMessageNode.classList.add("null_error_message");
+    return errorMessageNode
+}
+
 const validateInput = (arg) => {
     if (arg.value === "") {
-        return errorMessageId[arg.id]
+        return errorMessageInput[arg.id]
     } else if (arg.value.match(emailVerification) === null && arg.id === "signinEmail") {
         return "올바른 이메일 주소가 아닙니다.";
     }
 }
 
-const renderErrorMessage = (arg, e) => {
+const setErrorMessage = (arg, target) => {
     if (arg.textContent !== "") {
-        let pointNode = e.target.parentElement;
+        let pointNode = target.parentElement;
         pointNode.after(arg);
         pointNode.classList.add("null_error_border");
     }
@@ -59,13 +65,11 @@ const renderErrorMessage = (arg, e) => {
 
     // 인풋이 비어있거나, 인풋의 이메일이 이상하면 튀어나오는 이벤트 핸들러
 const handleInputError = (e) => {
-    const errorMessageNode = document.createElement("div");
-
-    errorMessageNode.classList.add("null_error_message");
+    const errorMessageNode = renderErrorMessageNode();
 
     errorMessageNode.textContent = validateInput(e.target);
 
-    renderErrorMessage(errorMessageNode, e);
+    setErrorMessage(errorMessageNode, e.target);
 }
 
     // 반복문을 통해 각각의 노드에 이벤트 핸들러를 달아준다
@@ -77,14 +81,27 @@ for (let line of sign_input) {
 // 버튼 눌렀을 때 맞으면 로그인 아니면 에러 뱉기
 const loginButtonNode = document.querySelector(".sign_form__btn");
 
+const errorMessageLogin = {
+    signinEmail: "이메일을 확인해주세요",
+    signinPassword: "비밀번호를 확인해주세요",
+}
+
 const validateLoginInput = () => sign_input[0].value === "test@codeit.com" && sign_input[1].value === "codeit101";
 
 const handleLoginButton = (e) => {
-    if (validateLoginInput) {
-        e.preventDefault()
-        location.href = "/folder.html"
+    if (validateLoginInput === true) {
+        e.preventDefault();
+        location.href = "/folder.html";
     } else {
+        for (let line of sign_input) {
+            e.preventDefault();
 
+            const errorMessageNode = renderErrorMessageNode();
+
+            errorMessageNode.textContent = errorMessageLogin[line.id];
+
+            setErrorMessage(errorMessageNode, line)
+        }
     }
 }
 
