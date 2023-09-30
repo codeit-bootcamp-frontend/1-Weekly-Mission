105569b3a8a6a4b96ce6d7a5a6a9c4cf2281670f
James Song <skdmlrh12@gmail.com>
2023-09-30 22:10:24 +0900

    feat(signin page): add toggle_pw_visibility function

--- src/pages/signin/signin.js
+++ src/pages/signin/signin.js
@@ -2,6 +2,7 @@
 const $email = document.querySelector('#email');
 const $password = document.querySelector('#password');
 const $form = document.querySelector('form');
+const $pw_invisible =document.querySelector('.password-invisible');
 
 const $email_error_msg = document.createElement('div');
 const $password_error_msg = document.createElement('div');
@@ -9,6 +10,8 @@ $email_error_msg.classList.add('email_error_msg');
 $password_error_msg.classList.add('password_error_msg');
 $email.after($email_error_msg);
 $password.after($password_error_msg);
+
+let toggle_pw = 0;
 
 const checking_email_input = (event) => {
   const repExp_Korean = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;  // 한글 정규표현식
@@ -39,6 +42,7 @@ const checking_password_input = (event) => {
 const admin_login = (event) => {
   event.preventDefault();
   if ($email.value === "test@codeit.com" && $password.value === "codeit101") {
+    // admin 계정으로 로그인 시 'folder/' 로 이동
     window.location.href = '../folder/index.html';
   } else {
     $email_error_msg.textContent = '이메일을 확인해주세요.';
@@ -48,6 +52,21 @@ const admin_login = (event) => {
   }
 }
 
+const toggle_pw_visibility = () => {
+  if (toggle_pw === 0) {
+    // 비밀번호 보이게 하기
+    $pw_invisible.setAttribute('src', '../../assets/images/svg/eye-on.svg');
+    $password.setAttribute('type', 'text');
+    toggle_pw += 1;
+  } else {
+    // 비밀번호 가리기
+    $pw_invisible.setAttribute('src', '../../assets/images/svg/eye-off.svg');
+    $password.setAttribute('type', 'password');
+    toggle_pw -= 1;
+  }
+}
+
 $email.addEventListener('focusout', checking_email_input);
 $password.addEventListener('focusout', checking_password_input);
 $form.addEventListener('submit', admin_login);
+$pw_invisible.addEventListener('click', toggle_pw_visibility);
