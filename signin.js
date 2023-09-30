830c28ad4118613170cc75464a95dd184ea06ae5
박종민 <qooktree@naver.com>
2023-09-30 23:26:18 +0900

    feat: signin page password error code 작성

--- signin.js
+++ signin.js
@@ -5,21 +5,21 @@ const password = document.getElementById("password")
 const inputs = document.getElementsByClassName("inner-input")
 const [TESTEMAIL, TESTPWD] = ["test@codeit.com", "codeit101"]
 const EXPTEXT = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
-let noEmail, emailErrorNoText, noPwd
+let noEmail, noPwd
 
 const emailError = (bool, verify, check) => {
     console.log(check)
     if (bool && verify === undefined){
         noEmail = document.createElement("p")
         noEmail.classList.add("inner-input-error-message")
-        noEmail.textContent = "이메일을 입력해주세요"
+        noEmail.textContent = "이메일을 입력해주세요."
         email.parentNode.parentNode.append(noEmail)
         inputs[0].classList.add("inner-input-error")
     } else if (!bool && verify !== undefined && !check) {
         if (!noEmail){
             noEmail = document.createElement("p")
             noEmail.classList.add("inner-input-error-message")
-            noEmail.textContent = "이메일을 입력해주세요"
+            noEmail.textContent = "이메일을 입력해주세요."
             email.parentNode.parentNode.append(noEmail)
             inputs[0].classList.add("inner-input-error")
         }
@@ -33,6 +33,21 @@ const emailError = (bool, verify, check) => {
     }
 }
 
+const pwdError = (bool, verify) => {
+    if (bool && verify === undefined){
+        noPwd = document.createElement("p")
+        noPwd.classList.add("inner-input-error-message")
+        noPwd.textContent = "비밀번호를 입력해주세요."
+        password.parentNode.parentNode.append(noPwd)
+        inputs[1].classList.add("inner-input-error")
+    }
+    else if (!bool && verify !== undefined){
+        noPwd.remove()
+        noPwd = undefined
+        inputs[1].classList.remove("inner-input-error")
+    }
+}
+
 email.addEventListener("focusout", e => {
     e.preventDefault()
     if (e.target.value === "") emailError(true, noEmail, true)
@@ -41,4 +56,9 @@ email.addEventListener("focusout", e => {
         if (EXPTEXT.test(e.target.value) === false) emailError(false, noEmail, false)
         else emailError(false, noEmail, true)
     }
+})
+
+password.addEventListener("focusout", e => {
+    if (e.target.value === "") pwdError(true, noPwd)
+    else pwdError(false, noPwd)
 })
