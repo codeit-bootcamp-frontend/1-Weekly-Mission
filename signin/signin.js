fd3c88cb3a0d50ed19fbeeae11ae7511f36f7b26
wecaners <wecaners@naver.com>
2023-09-30 21:12:07 +0900

    Refactor: modify variable name input -> emailInput

--- signin/signin.js
+++ signin/signin.js
@@ -1,4 +1,4 @@
-const input = document.querySelector(".email-input");
+const emailInput = document.querySelector(".email-input");
 const pwInput = document.querySelector(".password-input");
 const emailEmptyMsg = document.createElement("span");
 const emailInvalidMsg = document.createElement("span");
@@ -9,10 +9,10 @@ function inputEmptyValueHandler(e) {
   if (!e.target.value) {
     emailEmptyMsg.textContent = "이메일을 입력해주세요.";
     emailEmptyMsg.classList.add("input-error-msg");
-    input.after(emailEmptyMsg);
-    input.classList.add("input-error");
+    emailInput.after(emailEmptyMsg);
+    emailInput.classList.add("input-error");
   } else {
-    input.classList.remove("input-error");
+    emailInput.classList.remove("input-error");
     emailEmptyMsg.remove();
   }
 }
@@ -21,13 +21,12 @@ function inputInvalidEmailHandler(e) {
   if (!regex.test(e.target.value) && e.target.value.length > 0) {
     emailInvalidMsg.textContent = "올바른 이메일 주소가 아닙니다.";
     emailInvalidMsg.classList.add("input-error-msg");
-    input.after(emailInvalidMsg);
-    input.classList.add("input-error");
+    emailInput.after(emailInvalidMsg);
+    emailInput.classList.add("input-error");
   } else {
-    input.classList.remove("input-error");
     emailInvalidMsg.remove();
   }
 }
 
-input.addEventListener("focusout", inputEmptyValueHandler);
-input.addEventListener("focusout", inputInvalidEmailHandler);
+emailInput.addEventListener("focusout", inputEmptyValueHandler);
+emailInput.addEventListener("focusout", inputInvalidEmailHandler);
