f8f7fd9c05019f0951f777235a541d59ba90ec14
HellenHong <hongjw030@naver.com>
2023-10-01 00:55:34 +0900

    feat(sign): Create login function

--- js/sign.js
+++ js/sign.js
@@ -5,6 +5,26 @@ const signinBtn = document.querySelector(".signin-button");
 const emailError = document.querySelector(".email-message");
 const passwordError = document.querySelector(".password-message");
 
-signinBtn.addEventListener("click", function () {
-  location.href = "./folder.html";
+signinBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (email.value == "test@codeit.com" && password.value == "codeit101") {
+    location.href = "./folder.html";
+    emailError.classList.remove("error-appear");
+    passwordError.classList.remove("error-appear");
+  } else if (
+    email.value !== "test@codeit.com" &&
+    password.value == "codeit101"
+  ) {
+    emailError.classList.add("error-appear");
+    passwordError.classList.remove("error-appear");
+  } else if (
+    email.value == "test@codeit.com" &&
+    password.value !== "codeit101"
+  ) {
+    emailError.classList.remove("error-appear");
+    passwordError.classList.add("error-appear");
+  } else {
+    emailError.classList.add("error-appear");
+    passwordError.classList.add("error-appear");
+  }
 });
