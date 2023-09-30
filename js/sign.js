01dac20ba9bc1d56ab9d8dfcdf49d0e72a7e0226
HellenHong <hongjw030@naver.com>
2023-10-01 00:40:03 +0900

    feat(sign): Create error message

--- js/sign.js
+++ js/sign.js
@@ -1,3 +1,10 @@
 const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const signinBtn = document.querySelector(".signin-button");
+
+const emailError = document.querySelector(".email-message");
+const passwordError = document.querySelector(".password-message");
+
+signinBtn.addEventListener("click", function () {
+  location.href = "./folder.html";
+});
