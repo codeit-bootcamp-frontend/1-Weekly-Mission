705b087807abbbb7caa14c7df6afc618e0cba1aa
HellenHong <hongjw030@naver.com>
2023-10-01 01:28:42 +0900

    feat(sign): Create email input functions

--- js/sign.js
+++ js/sign.js
@@ -2,6 +2,8 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const signinBtn = document.querySelector(".signin-button");
 
+const eyeBtn = document.querySelector(".eye-slashed");
+
 const emailError = document.querySelector(".email-message");
 const passwordError = document.querySelector(".password-message");
 
@@ -28,3 +30,22 @@ signinBtn.addEventListener("click", function (e) {
     passwordError.classList.add("error-appear");
   }
 });
+
+email.addEventListener("blur", function () {
+  if (!email.value) {
+    emailError.classList.add("error-appear");
+  } else if (!isValidEmail(email.value)) {
+    emailError.classList.add("error-appear");
+  } else {
+    emailError.classList.remove("error-appear");
+  }
+});
+
+function isValidEmail(email) {
+  const pattern = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-za-z0-9\\-]+/;
+  if (pattern.test(email) === false) {
+    return false;
+  } else {
+    return true;
+  }
+}
