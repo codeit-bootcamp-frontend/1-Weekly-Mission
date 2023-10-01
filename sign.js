845907c12b6be5d45bcadcbb9e478e4732b0a684
Accept77 <accept_agreed@kakao.com>
2023-10-01 17:16:14 +0900

    feat: css style fix, make sign.jsd

---
+++ sign.js
@@ -0,0 +1,89 @@
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const emailBox = email.parentElement;
+const passwordBox = password.parentElement.parentElement;
+const login = document.querySelector('.login').parentElement;
+
+function emailValueChecker () {
+    const includeAt = email.value.indexOf('@');
+    const includeFullStop = email.value.indexOf('.');
+    const includeSpace = email.value.indexOf(' ');
+    if (email.value === '') {
+        return (`이메일을 입력해주세요.`);
+    }
+    else if (includeAt === -1 || includeFullStop === -1 || includeSpace !== -1
+        || (Math.abs(includeAt - includeFullStop) === 1 || includeFullStop === email.value.length - 1)) {
+        return (`올바른 이메일 주소가 아닙니다.`)
+    }
+    else {
+        return (false);
+    }
+}
+
+function emailCheck () {
+    const emailValue = emailValueChecker();
+
+    if (emailBox.lastElementChild.classList.contains(`wrong-text`)) {
+        emailBox.lastElementChild.remove();
+    }
+    
+    if (emailValue){
+        email.className = 'wrong';
+        const alertWord = document.createElement('div');
+        alertWord.textContent = `${emailValue}`;
+        alertWord.setAttribute('class','wrong-text');
+        emailBox.append(alertWord);
+    }
+    else {
+        email.className = 'normal';
+    }
+}
+
+function passwordCheck () {
+    if (passwordBox.lastElementChild.classList.contains(`wrong-text`)) {
+        passwordBox.lastElementChild.remove();
+    }
+
+    if (password.value === ''){
+        password.className = 'wrong';
+        const alertWord = document.createElement('div');
+        alertWord.textContent = `비밀번호를 입력해주세요`;
+        alertWord.setAttribute('class','wrong-text');
+        passwordBox.append(alertWord);
+    }
+    else {
+        password.className = 'normal';
+    }
+}
+
+function loginCheck () {
+    if (email.value === '' || password.value === '') {
+        if (email.value === '') {
+            if (emailBox.lastElementChild.classList.contains(`wrong-text`)) {
+                emailBox.lastElementChild.remove();
+            }
+            email.className = 'wrong';
+            const alertEmail = document.createElement('div');
+            alertEmail.textContent = `이메일을 확인해주세요`;
+            alertEmail.setAttribute('class','wrong-text');
+            emailBox.append(alertEmail);
+        }
+        if (password.value === '') {
+            if (passwordBox.lastElementChild.classList.contains(`wrong-text`)) {
+                passwordBox.lastElementChild.remove();
+            }
+            password.className = 'wrong';
+            const alertPassword = document.createElement('div');
+            alertPassword.textContent = `비밀번호를 확인해주세요`;
+            alertPassword.setAttribute('class','wrong-text');
+            passwordBox.append(alertPassword);
+        }
+    }
+    else if (email.classList.contains('normal') && password.classList.contains('normal')){
+        login.setAttribute('href','/folder');
+    }
+}
+
+email.addEventListener('focusout', emailCheck);
+password.addEventListener('focusout', passwordCheck);
+login.addEventListener('click', loginCheck);
