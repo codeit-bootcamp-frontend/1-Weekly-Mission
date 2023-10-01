80375af7bef8b3296b77cd85130e0fde092cff05
Gyuho Lee <j980303@naver.com>
2023-10-01 18:11:09 +0900

    refactor(mentor): 로그인 페이지 js 추가

---
+++ js/signin.js
@@ -0,0 +1,86 @@
+const idInput = document.querySelector('#id-label');
+const pwInput = document.querySelector('#password-label');
+const idErrorMsg = document.createElement('span');
+const pwErrorMsg = document.createElement('span');
+const inputBox = document.querySelector('.input-box:nth-child(2)');
+const idInvalidMsg = document.createElement('span');
+const loginBtn = document.querySelector('.login-btn');
+const eyeBtn = document.querySelector('.eye-off');
+
+let reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+
+function idEmpty(e) {
+  if(!e.target.value){
+    idErrorMsg.textContent = '이메일을 입력해주세요.';
+    idErrorMsg.classList.add('input-error-msg');
+    idInput.after(idErrorMsg);
+    idInput.style.borderColor = '#FF5B56';
+    }else{
+      idInput.style.borderColor = '#CCD5E3';
+      idErrorMsg.remove();
+    }
+}
+
+function idInvalid(e) {
+  if(!reg.test(e.target.value) && e.target.value.length > 0){
+    idInvalidMsg.textContent = '올바른 이메일 주소가 아닙니다.';
+    idInvalidMsg.classList.add('input-error-msg');
+    idInput.after(idInvalidMsg);
+    idInput.style.borderColor = '#FF5B56';
+  }else{
+    idInvalidMsg.remove();
+  }
+}
+
+function pwEmpty(e) {
+  if(!e.target.value){
+    pwErrorMsg.textContent = '비밀번호를 입력해주세요.';
+    pwErrorMsg.classList.add('input-error-msg');
+    inputBox.after(pwErrorMsg);
+    pwInput.style.borderColor = '#FF5B56';
+    }else{
+      pwInput.style.borderColor = '#CCD5E3';
+      pwErrorMsg.remove();
+    }
+}
+
+function clickSigninBtn(e){
+  if(idInput.value !== "test@codeit.com"){
+    idErrorMsg.textContent = '이메일을 확인해주세요.';
+    idErrorMsg.classList.add('input-error-msg');
+    idInput.after(idErrorMsg);
+    idInput.style.borderColor = '#FF5B56';
+  }else{
+    idErrorMsg.remove();
+  }
+
+  if(pwInput.value !== "codeit101"){
+    pwErrorMsg.textContent = '비밀번호를 확인해주세요.';
+    pwErrorMsg.classList.add('input-error-msg');
+    inputBox.after(pwErrorMsg);
+    pwInput.style.borderColor = '#FF5B56';
+    }else{
+      pwErrorMsg.remove();
+  }
+
+  if(idInput.value === "test@codeit.com" && pwInput.value === "codeit101"){
+    location.href = "../folder.html";
+  }
+}
+
+function clickEyeBtn(e){
+  if(pwInput.getAttribute("type") === "password"){
+    pwInput.setAttribute("type", "text");
+    eyeBtn.firstElementChild.setAttribute("src", "../assets/png/eye-on.png");
+  }else{
+    pwInput.setAttribute("type", "password");
+    eyeBtn.firstElementChild.setAttribute("src", "../assets/png/eye-off.png");
+  }
+}
+
+
+idInput.addEventListener("focusout", idEmpty);
+idInput.addEventListener("focusout", idInvalid);
+pwInput.addEventListener("focusout", pwEmpty);
+loginBtn.addEventListener("click", clickSigninBtn);
+eyeBtn.addEventListener("click", clickEyeBtn);
