3ab3b18c41c293451bd49b22ca330059c7295410
kanglocal <develocal25@gmail.com>
2023-10-01 23:28:17 +0900

    refactor : login.js의 주석 첨삭, 불필요한 코드 삭제

--- pages/login/login.js
+++ pages/login/login.js
@@ -5,6 +5,7 @@ const eyes = document.querySelectorAll('.eye');
 
 form.addEventListener('focusout', validate);
 form.addEventListener('keydown', resetValidation);
+form.addEventListener('change', resetValidation);
 
 if(signinButton){
     signinButton.addEventListener('click', login);
@@ -33,37 +34,33 @@ function signup(){
 }
 
 function login(){
-    // 에러 여부 확인 후 로그인
-    const error = document.querySelectorAll('.error');
-    if(error.length === 0){
+    // 에러 여부 확인 후 로그인.
+    const errors = document.querySelectorAll('.error');
+    if(errors.length === 0){
         const email = document.querySelector('#email');
         const password = document.querySelector('#password');
 
         if(email.value === 'test@codeit.com' && password.value === 'codeit101'){
+            // 로그인 성공.
             location.href = "/pages/folder";
 
         }else{
-            // 에러메세지 출력
+            // 에러메세지 출력.
             printMessage(email.id, 'login');
             printMessage(password.id, 'login');
-            email.classList.add('error');
-            password.classList.add('error');
         }
     }
 }
 
 function validate(e){
-    // 값의 유효성을 체크 후 문제가 있다면 printMessage 함수 호출
+    // 값의 유효성을 체크 후 문제가 있다면 printMessage 함수를 호출한다.
 
     const id = e.target.id;
     const value = e.target.value;
-    const target = e.target;
-    let result = true;
 
     if(!value){
         // 비어있는 값인지 체크
         printMessage(id, 'empty');
-        result = false;
 
     }else if(id === 'email'){
         // 이메일 형식인지 체크
@@ -71,24 +68,20 @@ function validate(e){
 
         if(exp.test(value) === false){
             printMessage(id, 'validation');
-            result = false;
         }
+
     }else if(id === 'password-check'){
         // 비밀번호가 일치하는지 체크
         const password = document.querySelector('#password');
         if(value !== password.value){
             printMessage(id, 'coincidence');
-            result = false;
         }
-    }
-
-    if(!result){
-        target.classList.add('error');
     }
 }
 
 function printMessage(id, type){
-    // 메세지를 생성 후 messageDelivery 함수를 호출
+    // 메세지를 생성 후 messageDelivery 함수를 호출한다.
+
     let message = "";
 
     if(type === 'empty'){
@@ -103,12 +96,14 @@ function printMessage(id, type){
                 message = "비밀번호를 한 번 더 입력해주세요.";
                 break;
         }
+
     }else if(type === 'validation'){
         switch(id){
             case 'email':
                 message = "올바른 이메일 주소가 아닙니다.";
                 break;
         }
+
     }else if(type === 'login'){
         switch(id){
             case 'email':
@@ -118,6 +113,7 @@ function printMessage(id, type){
                 message = "비밀번호를 확인해주세요.";
                 break;
         }
+
     }else if(type === 'coincidence'){
         switch(id){
             case 'password-check':
@@ -130,7 +126,7 @@ function printMessage(id, type){
 }
 
 function messageDelivery(id, message){
-    // 메세지를 화면에 출력
+    // 메세지를 화면에 출력하고, 대상에 error클래스를 추가한다.
 
     // 에러메세지가 이미 있다면 return
     const target = document.getElementById(id);
@@ -138,14 +134,18 @@ function messageDelivery(id, message){
         return;
     }
 
-    // id로 메세지를 넣을 곳 탐색
+    // 메세지를 넣을 곳 탐색 후 출력.
     const p = document.createElement('p');
-
     p.textContent = message;
     document.getElementById(id).parentElement.append(p);
+
+    // 에러메세지를 넣은 후 target에 error클래스를 추가.
+    target.classList.add('error');
 }
 
 function resetValidation(e){
+    // 값이 변경되었을 경우, 변경대상의 error클래스와 에러메세지를 삭제한다.
+
     if(e.target.classList.contains('error')){
         e.target.classList.remove('error');
 
