049a2f8573f299893ca13b64ddfaa7ecf938fcdb
ayden <wpfekdml@naver.com>
2023-10-02 17:02:20 +0900

    fix(sign.js) : dom 종속, validate 함수, 파라미터 이름 외 수정

--- sign.js
+++ sign.js
@@ -1,56 +1,43 @@
-// 눈알 깜빡이는 이벤트
-const sign_form = document.querySelectorAll("[type=button]");
+// DOM 요소 집어오기
+const eyeButtons = document.querySelectorAll(".input__eyes");
+const signInputs = document.querySelectorAll(".input__container");
+const loginButtonNode = document.querySelector(".sign_form__btn");
 
-const togglePasswordVisibility = (e) => {
-    e.target.classList.toggle("input__eyes--on");
+// class 선택자 변수화
+const nullErrMsg = "null_error_message";
+const nullErrBorder = "null_error_border";
+const eyesOn = "input__eyes--on";
 
-    if (!e.target.classList.contains("input__eyes--on")) {
-        e.target.previousElementSibling.type = "password"
-    } else {
-        e.target.previousElementSibling.type = "";
-    }
-}
-
-for (let line of sign_form) {
-    line.addEventListener('click', togglePasswordVisibility);
-}
-
-
-    // focusin 때 error 가 있으면 지우는 이벤트
-const sign_input = document.querySelectorAll(".input__container");
-
-const removeErrorMessage = (e) => {
-    let currentDivTag = e.target.parentElement;
-
-    if (currentDivTag.classList.contains("null_error_border")) {
-        currentDivTag.classList.remove("null_error_border")
-        currentDivTag.nextElementSibling.remove();
-    }
-}
-
-for (let line of sign_input) {
-    line.addEventListener("focusin", removeErrorMessage)
-}
-
-    // focusout 때 (input이 비어있거나 이메일이 잘못되었으면) error 뱉는 이벤트
-const emailVerification = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-
+// 에러 메세지 관리
 const errorMessageInput = {
     signinEmail: "이메일을 입력해주세요",
     signinPassword: "비밀번호를 입력해주세요",
     signinPasswordCheck: "확인할 비밀번호를 입력해주세요",
 }
 
-const renderErrorMessageNode = () => {
-    const errorMessageNode = document.createElement("div");
-    errorMessageNode.classList.add("null_error_message");
-    return errorMessageNode
+const errorMessageLogin = {
+    signinEmail: "이메일을 확인해주세요",
+    signinPassword: "비밀번호를 확인해주세요",
 }
+
+// 각종 함수
+const renderErrorMessageNode = () => {
+    const divTag = document.createElement("div");
+    divTag.classList.add(nullErrMsg);
+    return divTag;
+}
+
+const isEmail = (input) => {
+    const emailRegex = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+  
+    return emailRegex.test(input);
+};
+
 
 const validateInput = (arg) => {
     if (arg.value === "") {
         return errorMessageInput[arg.id]
-    } else if (arg.value.match(emailVerification) === null && arg.id === "signinEmail") {
+    } else if (!isEmail(arg.value) && arg.id === "signinEmail") {
         return "올바른 이메일 주소가 아닙니다.";
     }
 }
@@ -59,11 +46,26 @@ const setErrorMessage = (arg, target) => {
     if (arg.textContent !== "") {
         let pointNode = target.parentElement;
         pointNode.after(arg);
-        pointNode.classList.add("null_error_border");
+        pointNode.classList.add(nullErrBorder);
     }
 }
 
-    // 인풋이 비어있거나, 인풋의 이메일이 이상하면 튀어나오는 이벤트 핸들러
+const validateLoginInput = () => signInputs[0].value === "test@codeit.com" && signInputs[1].value === "codeit101";
+
+// 이벤트 핸들러들
+const togglePasswordVisibility = ({ target }) => {
+    const isVisible = target.classList.contains(eyesOn);
+    const targetType = target.previousElementSibling.type;
+
+    target.classList.toggle(eyesOn);
+
+    if (!isVisible) {
+        targetType = "password"
+    } else {
+        targetType = "";
+    }
+}
+
 const handleInputError = (e) => {
     const errorMessageNode = renderErrorMessageNode();
 
@@ -72,30 +74,22 @@ const handleInputError = (e) => {
     setErrorMessage(errorMessageNode, e.target);
 }
 
-    // 반복문을 통해 각각의 노드에 이벤트 핸들러를 달아준다
-for (let line of sign_input) {
-    line.addEventListener("focusout", handleInputError)
+const removeErrorMessage = (e) => {
+    let currentDivTag = e.target.parentElement;
+
+    if (currentDivTag.classList.contains(nullErrBorder)) {
+        currentDivTag.classList.remove(nullErrBorder)
+        currentDivTag.nextElementSibling.remove();
+    }
 }
-
-
-// 버튼 눌렀을 때 맞으면 로그인 아니면 에러 뱉기
-const loginButtonNode = document.querySelector(".sign_form__btn");
-
-const errorMessageLogin = {
-    signinEmail: "이메일을 확인해주세요",
-    signinPassword: "비밀번호를 확인해주세요",
-}
-
-const validateLoginInput = () => sign_input[0].value === "test@codeit.com" && sign_input[1].value === "codeit101";
 
 const handleLoginButton = (e) => {
     e.preventDefault();
 
-    if (validateLoginInput === true) {
+    if (validateLoginInput()) {
         location.href = "/folder.html";
     } else {
-        for (let line of sign_input) {
-
+        for (const line of signInputs) {
             const errorMessageNode = renderErrorMessageNode();
 
             errorMessageNode.textContent = errorMessageLogin[line.id];
@@ -105,4 +99,16 @@ const handleLoginButton = (e) => {
     }
 }
 
-loginButtonNode.addEventListener("click", handleLoginButton)
+// 눈알 토글 클릭 이벤트 바인딩
+for (const line of eyeButtons) {
+    line.addEventListener('click', togglePasswordVisibility);
+}
+
+// 인풋 포커스 아웃 이벤트 & 인풋 포커스 아웃 이벤트 바인딩
+for (const line of signInputs) {
+    line.addEventListener("focusout", handleInputError);
+    line.addEventListener("focusin", removeErrorMessage);
+}
+
+// 로그인 버튼 폴더 이동 클릭 이벤트 바인딩
+loginButtonNode.addEventListener("click", handleLoginButton);
