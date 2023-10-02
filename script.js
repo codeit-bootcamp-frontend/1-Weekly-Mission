a1a4c106d4f8d9c6257db04c8f56d7b08ed80b4a
Hyeyoon <foryou9097@gmail.com>
2023-10-03 01:40:28 +0900

    feat: 비밀번호 비어있는 값 입력시  오류메시지 출력

--- script.js
+++ script.js
@@ -1,38 +1,38 @@
 window.onload=function(){
   
-  // <눈모양 아이콘 적용, 비밀번호 입력타입 변경> // 왜 눌러도 반응이 없을까요? ㅠㅠ
+ 
   const eyeImagePasswordEl = document.querySelector('#eyeImage-password')
   const eyeImagePasswordReEl = document.querySelector('#eyeImage-password-re')
   const eyeImagePassword = eyeImagePasswordEl.children[0]
   const eyeImagePasswordRe = eyeImagePasswordReEl.children[0]
-  const passWord = document.querySelector('#password')
-  const passWordRe = document.querySelector('#password-re')
+  const inputEmail= document.querySelector('#sign-up')
+  const inputPassWord = document.querySelector('#password')
+  const inputPassWordRe = document.querySelector('#password-re')
 
-  function toggleImage(image, password) {
-    if (image.src.includes('eye-off')) {
-      image.setAttribute('src','./images/signin/eye-on.png');
-      password.setAttribute('type', '');
-    } else {
-      image.setAttribute('src','./images/signin/eye-off.svg');
-      password.setAttribute('type', 'password');
-    }
-  }
-  eyeImagePassword.addEventListener('click', ()=> {
-    toggleImage(eyeImagePassword, passWord)});
-
-  eyeImagePasswordRe.addEventListener('click', ()=> {
-    toggleImage(eyeImagePasswordRe, passWordRe)});
-
-  //오류메시지 출력
+    //오류메시지 출력
   const emailErrorMessage = document.querySelector('.email-error-message')
   const passwordErrorMessage = document.querySelector('.password-error-message')
   const passwordReErrorMessage = document.querySelector('.password-re-error-message')
 
+ // <눈모양 아이콘 적용, 비밀번호 입력타입 변경>
+  function toggleImage(image, inputPassword) {
+    if (image.src.includes('eye-off')) {
+      image.setAttribute('src','./images/signin/eye-on.png');
+      inputPassword.setAttribute('type', '');
+    } else {
+      image.setAttribute('src','./images/signin/eye-off.svg');
+      inputPassword.setAttribute('type', 'password');
+    }
+  }
+  eyeImagePassword.addEventListener('click', ()=> {
+    toggleImage(eyeImagePassword, inputPassWord)});
+
+  eyeImagePasswordRe.addEventListener('click', ()=> {
+    toggleImage(eyeImagePasswordRe, inputPassWordRe)});
+
   //인풋
-  const inputEmail= document.querySelector('#sign-up')
- 
-  const inputPassword = document.querySelector('#password')
-  const inputPasswordRe = document.querySelector('#password-re')
+  // const inputPassword = document.querySelector('#password')
+  // const inputPasswordRe = document.querySelector('#password-re')
   
   //<이메일을 입력하는 동안에 에러메시지 안 보이게 하기 >
   //1) 이메일을 입력하는 동안 에러메시지를 가려주는 함수
@@ -42,7 +42,7 @@ window.onload=function(){
   //2) 이벤트 적용
   inputEmail.addEventListener('input', errorMessageStop)
 
-  // <이메일이 유효한지 검사하기>
+  // <이메일 형식검증, 오류메시지 출력 >
   //1) 사용자가 입력한 입력값이 유효한 형식인지 불린값으로 판단하는 함수
   function testEmail(email){
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
@@ -50,18 +50,34 @@ window.onload=function(){
   }
   //2) 불린값으로 반환된 결과를 기준으로 오류메세지를 출력하고 CSS속성을 변경
   function checkEmail(){
-    const email = inputEmail.value;
+    const email = inputEmail.value.trim();    //사용자가 인풋란에 입력한 값을 JS에서 email로 선언
     const inputCss = document.querySelector('input')
-     //사용자가 인풋란에 입력한 값을 JS에서 email로 선언
-    if (testEmail(email)){
-      emailErrorMessage.style.display = 'none';
-    } else {
+    if (email === ''){
+      emailErrorMessage.style.display = 'block';
+      emailErrorMessage.textContent = '이메일을 입력해주세요.';
+    }
+    else if (!testEmail(email)){
       emailErrorMessage.style.display = 'block';
       emailErrorMessage.textContent = '올바른 이메일 주소가 아닙니다.';
       inputCss.style.border = '1px solid red';
+    } else {
+      emailErrorMessage.style.display = 'none';
     }
   };
   //3) 이벤트 적용
   inputEmail.addEventListener('focusout', checkEmail)
+
+  //<비밀번호 오류메시지 출력>
+  function checkPassword(){
+    const password = inputPassWord.value;
+    if (password === ''){
+      passwordErrorMessage.style.display = 'block';      
+    }else {
+      passwordErrorMessage.style.display = 'none';
+    };
+  }
+  
+  inputPassWord.addEventListener('focusout', checkPassword)
+
 
 } //onload end
