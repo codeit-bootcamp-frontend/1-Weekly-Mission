a0be1e1a48738a8db66345440ee70859b76b5724
djiny7296 <djiny7296@gmail.com>
2023-10-02 18:05:09 +0900

    refactor(mentor): 테스트이메일/비밀번호 구분

--- js/signin.js
+++ js/signin.js
@@ -1,34 +1,37 @@
-const emailInput = document.querySelector('#email-input')
-const emailError = document.querySelector('#email-error')
+// 요소노드 선택
+const emailInput = document.querySelector('#email-input');
+const emailError = document.querySelector('#email-error');
+const passwordInput = document.querySelector('#password-input');
+const passwordError = document.querySelector('#password-error');
+const loginButton = document.querySelector('#login-button');
+const eyeButton = document.querySelector('.form--eye-button');
 
-const passwordInput = document.querySelector('#password-input')
-const passwordError = document.querySelector('#password-error')
+const borderRed = "border-red";
+const eyeOn = "on";
 
-const loginButton = document.querySelector('#login-button')
-const eyeButton = document.querySelector('.form--eye-button')
-
-const borderRed = "border-red"
-const eyeOn = "on"
+// 유저 정보
+const userEmail = "test@codeit.com";
+const userePassword = "codeit101";
 
 // 이메일 유효성 검사 함수
 function checkEmail(email) {  
   let regExp = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i;
   
   return regExp.test(email);
-}
+};
 
 function valiDateEmail() {
   const emailValue = emailInput.value;
 
   if (!emailValue) {
     emailError.textContent = '이메일을 입력해주세요.';
-    emailInput.classList.add(borderRed)
+    emailInput.classList.add(borderRed);
   } else if (!checkEmail(emailValue)) {
     emailError.textContent = '올바른 이메일 주소가 아닙니다.';
-    emailInput.classList.add(borderRed)
+    emailInput.classList.add(borderRed);
   } else {
     emailError.textContent = '';
-    emailInput.classList.remove(borderRed)
+    emailInput.classList.remove(borderRed);
   }
 }
 
@@ -37,12 +40,12 @@ function valiDatePassword() {
 
   if (!passwordValue) {
     passwordError.textContent = '비밀번호를 입력해주세요.';
-    passwordInput.classList.add(borderRed)
+    passwordInput.classList.add(borderRed);
   } else {
     passwordError.textContent = '';
-    passwordInput.classList.remove(borderRed)
-  }
-}
+    passwordInput.classList.remove(borderRed);
+  };
+};
 
 emailInput.addEventListener('focusout', valiDateEmail);
 passwordInput.addEventListener('focusout', valiDatePassword);
@@ -51,28 +54,28 @@ function getLogin() {
   const emailValue = emailInput.value;
   const passwordValue = passwordInput.value;
 
-  if (emailValue === 'test@codeit.com' && passwordValue === 'codeit101') {
+  if (emailValue === userEmail && passwordValue === userePassword) {
     location.href = '../pages/folder.html';
-  } else if (emailValue !== 'test@codeit.com') {
+  } else if (emailValue !== userEmail) {
     emailError.textContent = '이메일을 확인해주세요.';
     passwordError.textContent = '비밀번호를 확인해주세요.';
     emailInput.classList.add(borderRed);
-  } else if (passwordValue !== 'codeit101') {
+  } else if (passwordValue !== userePassword) {
     passwordError.textContent = '비밀번호를 확인해주세요.';
-    passwordInput.classList.add(borderRed)
-  }
-}
+    passwordInput.classList.add(borderRed);
+  };
+};
 
 loginButton.addEventListener('click', getLogin);
 
 function showPassword() {
   if (passwordInput.type === "password") {
-    eyeButton.classList.toggle(eyeOn)
+    eyeButton.classList.toggle(eyeOn);
     passwordInput.type = "text";
   } else {
-    eyeButton.classList.toggle(eyeOn)
+    eyeButton.classList.toggle(eyeOn);
     passwordInput.type = "password"
-  }
-}
+  };
+};
 
 eyeButton.addEventListener('click', showPassword);
