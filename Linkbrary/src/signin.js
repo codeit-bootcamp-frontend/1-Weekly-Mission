e0a2fb761998bec43b5b830f6b412e9a6041b6ae
ParkSohyunee <sohyunee016@gmail.com>
2023-10-03 19:00:10 +0900

    refactor: signin 리팩토링 - focusout 이벤트 로직 개선 - 프리티어 재적용

--- Linkbrary/src/signin.js
+++ Linkbrary/src/signin.js
@@ -12,7 +12,7 @@ const passwordError = document.getElementById("passwordError");
 const addErrorClass = (el) => el.classList.add("error");
 const removeErrorClass = (el) => el.classList.remove("error");
 
-// 이메일 input에서 focus out할 때, 이메일 형식에 맞지 않는 값이 있는 경우 에러메세지
+/** 이메일 형식 검증 */
 const emailRegExp = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/i;
 
 const emailValidation = (value) => {
@@ -23,37 +23,43 @@ const emailValidation = (value) => {
   isEmailValid ? removeErrorClass(email) : addErrorClass(email);
 };
 
-// 이메일, 비밀번호 input에서 focus out할 때, 값이 없을 경우 에러메세지
-const checkEmailValue = () => {
+/** 비밀번호 있으면 에러메세지 초기화 */
+const passwordInputReset = () => {
+  passwordError.textContent = "";
+  removeErrorClass(password);
+};
+
+/** 이메일, 비밀번호 focus out할 때, value 체크 */
+const checkFormValue = (e) => {
+  if (!e.target.classList.contains("form__input")) return;
+
+  const { value, id } = e.target;
+  const context = id === "email" ? "이메일을" : "비밀번호를";
+
+  if (!value) {
+    document.getElementById(`${id}Error`).textContent = `${context} 입력해주세요`;
+    addErrorClass(e.target);
+    return;
+  }
+  id === "email" ? emailValidation(e.target.value) : passwordInputReset();
+};
+
+/** 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동  */
+const onClickSubmit = (e) => {
+  e.preventDefault();
+
   if (!email.value) {
     emailError.textContent = "이메일을 입력해주세요";
     addErrorClass(email);
     return;
   }
-  emailValidation(email.value);
-};
-
-const checkPasswordValue = () => {
   if (!password.value) {
     passwordError.textContent = "비밀번호를 입력해주세요";
     addErrorClass(password);
     return;
   }
-  passwordError.textContent = "";
-  removeErrorClass(password);
-};
 
-// 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동
-const onClickSubmit = (e) => {
-  e.preventDefault();
-
-  if (!email.value || !password.value) {
-    checkEmailValue();
-    checkPasswordValue();
-    return;
-  }
-
-  // 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지
+  /** 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지 */
   if (email.value !== USER_EMAIL || password.value !== USER_PASSWORD) {
     emailError.textContent = "이메일을 확인해주세요";
     passwordError.textContent = "비밀번호를 확인해주세요";
@@ -64,6 +70,5 @@ const onClickSubmit = (e) => {
   form.submit();
 };
 
-email.addEventListener("focusout", checkEmailValue);
-password.addEventListener("focusout", checkPasswordValue);
+form.addEventListener("focusout", checkFormValue);
 form.addEventListener("submit", onClickSubmit);
