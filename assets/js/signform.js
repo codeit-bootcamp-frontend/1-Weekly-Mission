bb1590a7e923901719be09beb22d69db9433e725
arky02 <kyean07@naver.com>
2023-10-03 19:54:11 +0900

    modify(signform): fix form submit error, and a few substantial errors modify(signin): fix form submit error

--- assets/js/signform.js
+++ assets/js/signform.js
@@ -1,21 +1,33 @@
 // ===============================================
 // js code for both signup.html and signin.html
 // ===============================================
+// debugger;
 
+// input text element들 class name으로 가져오기
 const email = document.querySelector('.email');
-const pwd = document.querySelector('.password');
+const pwd = document.querySelector('.password'); 
 const check_pwd = document.querySelector('.check_password');
 
-email.addEventListener('focusout',emailTextEmptyValidation); // 이메일 입력칸 비어있는지 확인 후 비어있는지 검사, 경우에 따라 에러문구 handle
-email.addEventListener('keydown',() => {
-    deleteInputErrorEffect("email","check_email"); // "이메일을 확인해주세요" 에러문구 떠있으면 지우기
-    if(email.classList.contains("error--inputText")) emailTextEmptyValidation();} // "이메일을 입력해주세요" 에러문구 떠있으면 이메일 입력칸 비워있는지 검사, 경우에 따라 에러문구 handle
-);
-pwd.addEventListener('keyup',()=>{
-    deleteInputErrorEffect("password","check_pwd"); // "비밀번호를 확인해주세요" 에러문구 떠있으면 지우기
-    pwdSameValidaton(check_pwd.value,pwd.value);   // 비밀번호 입력칸과 비밀번호 입력 확인 칸의 입력 값 같은지 검사, 경우에 따라 에러문구 handle
+email.addEventListener('focusout', handleEmailFormatError); // 이메일 입력칸 비어있는지 확인 후 비어있는지 검사, 경우에 따라 에러문구 handle
+email.addEventListener('keydown', () => {
+    deleteErrorEffect(email, "errorNoEmailTxt"); // resolve Email Validation #1(isEmailNotEmpty) - "이메일을 입력해주세요" 에러문구 떠있으면 지우기
+    deleteErrorEffect(email, "errorNotEmail");   // resolve Email Validation #2(isEmail) - "올바른 이메일 주소가 아닙니다." 에러문구 떠있으면 지우기
+    deleteErrorEffect(email, "errorCheckEmail"); // resolve Login Fail Event - "이메일을 확인해주세요" 에러문구 떠있으면 지우기
 });
-check_pwd.addEventListener('keyup',() => pwdSameValidaton(check_pwd.value,pwd.value));   // 비밀번호 입력칸과 비밀번호 입력 확인 칸의 입력 값 같은지 검사, 경우에 따라 에러문구 handle
+pwd.addEventListener('keyup', ()=>{
+    deleteErrorEffect(pwd, "errorCheckPwd"); // "비밀번호를 확인해주세요" 에러문구 떠있으면 지우기
+    if(check_pwd)
+        pwdSameValidaton(check_pwd.value,pwd.value);   // 비밀번호 입력칸과 비밀번호 입력 확인 칸의 입력 값 같은지 검사, 경우에 따라 에러문구 handle
+});
+if(check_pwd)
+    check_pwd.addEventListener('keyup',() => pwdSameValidaton(check_pwd.value, pwd.value));   // 비밀번호 입력칸과 비밀번호 입력 확인 칸의 입력 값 같은지 검사, 경우에 따라 에러문구 handle
+
+const $formEl = document.querySelector('.form');
+$formEl.addEventListener('submit', (e)=>{
+    // 마우스로 button을 클릭하든, 엔터키를 치든 submit이벤트 발생
+    e.preventDefault(); // form 제출로 override됨 방지
+    login(); // 로그인 시도
+});
 
 // 회원 클래스
 class Member{
@@ -23,146 +35,130 @@ class Member{
         this.email = email;
         this.pwd = pwd;
     }
+    getEmail() { return this.email; }
+    getPwd() { return this.pwd; }
+}
 
-    getEmail(){
-        return this.email;
+// 회원 리스트
+const memberList = [];
+const admin = new Member('test@codeit.com', 'codeit101');
+
+memberList.push(admin);
+
+//============================================
+// ======== Handing Email Validation =========
+
+//각 함수의 결과에 따라 에러 메세지 띄우는 힘수
+function handleEmailFormatError(){
+    const emailValue = email.value;
+
+    if(isEmailNotEmpty(emailValue)){
+        if(!isEmail(emailValue))
+            createErrorEffect(email, "errorNotEmail", "올바른 이메일 주소가 아닙니다.");
     }
-    getPwd(){
-        return this.pwd;
-    }
+    else
+        createErrorEffect(email, "errorNoEmailTxt", "이메일을 입력해주세요"); // [Error!!] Email inputText에 값 없음
 }
 
-var MemberList = [];
-const admin = new Member('test@codeit.com','1');
-
-MemberList.push(admin);
-
-function handleInputErrorEffect(className,appearenceType){
-    const obj = document.querySelector(`.${className}`);
-    if(appearenceType){     // appearenceType = 1 : add ver.
-        obj.classList.add("error--inputText");
-    }else{                  // appearenceType = 2 : remove ver.
-        if(obj.classList.contains("error--inputText")){
-            obj.classList.remove("error--inputText");
-        }
-    }
+// Email Validation #1 - is Email text Empty?
+function isEmailNotEmpty(emailValue){  // isEmailNotEmpty: 이메일 입력칸의 입력값 존재 여부 검사
+    return (emailValue!=="");
 }
 
-function emailTextEmptyValidation(){    // emailTextEmptyValidation: 이메일 입력칸의 입력값 존재 여부 확인
-    if((email.nextElementSibling == null)) { // errorTxt 없음
-        if(email.value == ""){  //email input-text에 값 없음
-            // [ Error occurred! ] : no input value on email input text
-            console.log("[Error occurred! (type1)] : no input value on email input text");                       
-            createInputErrorEffect(email, "email", "no_emailTxt", "이메일을 입력해주세요");
-        }            
-    }else{  // 이미 errorTxt가 있음 
-        if(email.nextElementSibling != null && email.value != "") { // email input-text애 값 있음
-            // [ Error solved! ]
-            console.log("[Error solved! (type1)] : no input value on email input text");
-            deleteInputErrorEffect("email", "no_emailTxt");                              
-        }                                       
-    }
+// Email Validation #2 - is Email?
+function isEmail(emailValue){            // is Email: 이메일 형식 올바른지 검사
+    const pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i; // 이메일 형식 검사 정규식
+    return pattern.test(emailValue);
+} 
+//============================================
+
+//============================================
+// ======== Handing Error Effect =========
+
+// Create an Error effect
+function createErrorEffect(inputTargetEl, errorClassName, errorTxt){
+    deleteErrorEffect(inputTargetEl, errorClassName); // 이미 errorClassName가진 error effect가 화면에 떠있다면 지운 후 새로 띄우기 (중복 띄움 방지)
+    const ErrorMessage = createErrorMessageEl(errorClassName, errorTxt);  // 1. create error messsage element - createErrorMessageEl
+    inputTargetEl.after(ErrorMessage);            // 2. add errorText
+    handleInputErrorEffect(inputTargetEl, true);  // 3. make inputText Error effect on it's border - handleInputErrorEffect
 }
 
-function validEmailFormality(emailValue){
-    if(validEmailCheck(emailValue)==false){
-        return false;
-    }else return true;
+// Delete an Error effect - 먼저 해당 error effect가 화면 상에 있는지 검사 후, 있다면 지우기
+function deleteErrorEffect(inputTargetEl, errorClassName){
+    const delElement = document.querySelector("." + errorClassName);
+    if(delElement) {  // check if an error Effect "errorClassName" is exist
+        delElement.remove();                           // 1. if an errorTxt exist => remove errorTxt
+        handleInputErrorEffect(inputTargetEl, false);  // 2. remove error effect on input-text border
+    }            
 }
 
-function validEmailFormalityCheck(emailValue){
-    var pattern = /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
-    return (emailValue.match(pattern)!=null)
-}
-
-function createErrorMessage(className, message){ 
+// Creating Error effect - create ErrorMessageElement
+function createErrorMessageEl(className, message){ 
     const errorTxt = document.createElement("span"); 
     errorTxt.classList.add(className);
     errorTxt.classList.add("error--text");           
     errorTxt.textContent = message;      
     return errorTxt;
-}
+} 
 
-function createInputErrorEffect(inputElement, inputClassName, errorTxtClassName, errorTxt){
-    const ErrorMessage = createErrorMessage(errorTxtClassName, errorTxt);  // [show errorEffect]: 1) create errorText
-    inputElement.after(ErrorMessage);                   // [show errorEffect]: 2) add errorText
-    handleInputErrorEffect(inputClassName,true);        // [show errorEffect]: 3) add error effect on input-text border
+// Handle InputText Error effect - create & delete InputText's ErrorEffect
+function handleInputErrorEffect(targetInputEl, isAddErrorVer){
+    if(isAddErrorVer){  // isAddErrorVer: 1 => add error effect
+        targetInputEl.classList.add("error--inputText");
+    }else{             // isAddErrorVer: 2 => remove error effect
+        if(targetInputEl.classList.contains("error--inputText")) 
+            targetInputEl.classList.remove("error--inputText");
+    }
 }
+//============================================
 
-function deleteInputErrorEffect(inputClassName, errorTxtClassName){
-    const delElement = document.querySelector("."+errorTxtClassName);
-    if(delElement) {  // check if an errorEffect which class name is "inputClassName" is exist
-        delElement.remove();                           // [remove errorEffect]: 1) if an errorTxt exist => remove errorTxt
-        handleInputErrorEffect(inputClassName,false);  // [remove errorEffect]: 2) remove error effect on input-text border
-    }            
-}
 
 // ===============================================
 // js code for only signup.html (회원가입)
 // ===============================================
-function pwdSameValidaton(checkPwd_txtValue, userPwd_txtValue){    // pwdSameValidaton: "비밀번호"와 "비밀번호 확인" 입력칸의 입력값 같음 확인
-    console.log("[Error occurred! (type2)] : input value is diff between 2 pwd inputs");
-    const eitherInputisEmpty = (userPwd_txtValue == "" || checkPwd_txtValue == "");
-    console.log(eitherInputisEmpty);
+function pwdSameValidaton(checkPwdValue, pwdValue){    // pwdSameValidaton: "비밀번호"와 "비밀번호 확인" 입력칸의 입력값 같음 확인
+    const isEitherInputEmpty = (pwdValue === "" || checkPwdValue === "");
 
-    if(check_pwd.nextElementSibling == null){  // errorTxt 없음 
-        if(checkPwd_txtValue != userPwd_txtValue && !eitherInputisEmpty) {  // 두개의 pwd input 값 diff & 두개의 inputText 모두 value가 있음
-            // [ Error occurred! ] : input value is diff between 2 pwd inputs
-            createInputErrorEffect(check_pwd,"check_password","pwd_diff", "비밀번호가 다릅니다")
-        }
-    }else{  // 이미 errorTxt 있음
-        if(userPwd_txtValue == checkPwd_txtValue || eitherInputisEmpty){ // 두개의 pwd input 값 same or 둘 중 하나 이상의 inputText value가 없음
-            // [ Error solved! ]
-            console.log("[Error solved! (type2)] : no input value on email input text");
-            deleteInputErrorEffect("check_password", "pwd_diff");
-        }
-    } 
+    if(checkPwdValue != pwdValue && !isEitherInputEmpty)
+        createErrorEffect(check_pwd, "errorPwdDiff", "비밀번호가 다릅니다"); // [Error!!]: 두개의 pwd input 값 diff
+    else if(pwdValue === checkPwdValue || isEitherInputEmpty)
+        deleteErrorEffect(check_pwd, "errorPwdDiff");  // [Error solve!!]: 두개의 pwd input 값 같거나, 적어도 하나의 pwd input값이 비어있음
 } 
 
 // ===============================================
 // js code for only signin.html (로그인)
 // ===============================================
-// const signBtn = document.querySelector('#btn_signin');
-// signBtn.addEventListener('click',()=>{window.location.replace('/index.html');});
+function login(){
+    const emailValue =  document.getElementById('email_signin').value;
+    const pwdValue =  document.getElementById('password_signin').value;
+    const isMemberRight = isMember(emailValue,pwdValue);
+    const isEmailFormatValid = isEmail(emailValue) && isEmailNotEmpty(emailValue);  // isEmailFormatValid: 이메일 유효성 검사 (이메일 입력값 존재 여부, 이메일 형식 일치 여부 판단)
 
-function Login(){
-    // debugger;
-    const emailValue =  document.querySelector('#email_signin').value;
-    const pwdValue =  document.querySelector('#password_signin').value;
-
-    setTimeout(() => console.log("after"), 13000);
-
-    if(isMember(emailValue,pwdValue)){   //isMember == true; 로그인 성공!!!
-        debugger;
-        console.log("로그인 성공!");
-        window.open('/folder.html');
-        // window.location = '/index.html';
-        // preventDefault();
-         // window.location.replace('/folder.html');
-        // debugger;
-    }else{                              //isMember == false; 로그인 실패
-        console.log("로그인 실패");
-        if(validEmailFormality(emailValue)){
-            createInputErrorEffect(email,"email","no_formality_email","올바른 이메일 주소가 아닙니다.");
-        }else{
-            createInputErrorEffect(email,"email","check_email","이메일을 확인해주세요.");
-            createInputErrorEffect(pwd,"password","check_pwd","비밀번호를 확인해주세요.");
-        }
-   }
-
+    if(isEmailFormatValid && isMemberRight === 1) // 둘 다 1일 때만 유효성 검사 통과, 회원임.
+        window.location.replace("/folder.html"); // isMemberRight = 1: 회원임. => 로그인 성공!!!
+    else
+        handleLoginFailEvent(isEmailFormatValid, isMemberRight); // isMemberRight = 0 or 2: 비회원이거나 비번 틀림. => 로그인 실패
 }
 
-function isMember(email,pwd){
-    for(let temp of MemberList){
-        if(temp.getEmail() == email){
-            if(temp.getPwd() == pwd){
-                console.log("isMember");
-                debugger;
-                return true;
-            }else{
-                console.log("비밀번호가 맞지 않습니다!");
-            }
-        }
+function isMember(email,pwd){ // 로그인 시도 시 회원 리스트에 속해있는지 확인, isMember인지 boolean 값 return
+    if(memberList.some((member) => member.getEmail() === email && member.getPwd() === pwd))
+        return 1;  // 회원임
+    else if(memberList.some((member) => member.getEmail() === email))
+        return 2;  // 회원인데 비밀번호 틀림
+    return 0;      // 회원 아님
+}
+
+function handleLoginFailEvent(isEmailFormatValid, isMemberRight){
+    // isEmailFormatValid = 1(이메일 형식 맞음) or 0(이메일 형식이 아님)
+    // isMemberRight = 0(회원 아님) or 2(회원인데 비번틀림)
+
+    if(!isEmailFormatValid) 
+        handleEmailFormatError(); // isEmailFormatValid = 0, 이메일 형식이 아님.
+    else{
+        if(!isMemberRight){ // isMemberRight = 0
+            createErrorEffect(email, "errorCheckEmail", "이메일을 확인해주세요."); // 회원 아님
+            createErrorEffect(pwd, "errorCheckPwd", "비밀번호를 확인해주세요."); 
+        }else // isMemberRight = 2
+            createErrorEffect(pwd, "errorCheckPwd", "비밀번호를 확인해주세요."); // 회원인데 비밀번호 틀림
     }
-    return false;
 }
