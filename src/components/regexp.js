cca082e686e8c623d1b9951b093c83fec00616ba
NIX <lsc58461@naver.com>
2023-10-04 18:10:20 +0900

    feat: Password regex 패턴 추가

--- src/components/regexp.js
+++ src/components/regexp.js
@@ -1,3 +1,4 @@
 const EMAIL_PATTERN = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+const PASSWORD_PATTERN = /^(?=.*[A-Za-z])(?=.*\d).{8,}$/;
 
-export { EMAIL_PATTERN };
+export { EMAIL_PATTERN, PASSWORD_PATTERN };
