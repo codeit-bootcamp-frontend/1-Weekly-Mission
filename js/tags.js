9f89ccf0df1bca1faa87bf443b8932d9cfc6c5f0
oauch <lgm9515@naver.com>
2023-10-04 10:50:11 +0900

    Refactor: Tag, Function js파일로 분리

---
+++ js/tags.js
@@ -0,0 +1,8 @@
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const passwordVisible = document.querySelector(".toogle-eye");
+const emailError = document.createElement("div");
+const passwordError = document.createElement("div");
+const loginButton = document.querySelector(".signin-form");
+
+export { email, password, passwordVisible, emailError, passwordError, loginButton };
