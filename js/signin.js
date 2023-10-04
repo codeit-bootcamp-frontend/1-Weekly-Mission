df850df228f3c6bed3ad9a05c075aca3f86fac98
Gyuho Lee <j980303@naver.com>
2023-10-04 16:03:43 +0900

    refactor(mentor): DOM Element 인지 시키기 위해 변수명 수정

--- js/signin.js
+++ js/signin.js
@@ -1,9 +1,9 @@
 const idInput = document.querySelector('#id-label');
 const pwInput = document.querySelector('#password-label');
-const idErrorMsg = document.createElement('span');
-const pwErrorMsg = document.createElement('span');
+const idErrorMsgEl = document.createElement('span');
+const pwErrorMsgEl = document.createElement('span');
 const inputBox = document.querySelector('.input-box:nth-child(2)');
-const idInvalidMsg = document.createElement('span');
+const idInvalidMsgEl = document.createElement('span');
 const loginBtn = document.querySelector('.login-btn');
 const eyeImg = document.querySelector('.eye-off');
 
@@ -11,56 +11,56 @@ let reg = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[
 
 function idEmpty(e) {
   if(e.target.value.length === 0){
-    idErrorMsg.textContent = '이메일을 입력해주세요.';
-    idErrorMsg.classList.add('input-error-msg');
-    idInput.after(idErrorMsg);
+    idErrorMsgEl.textContent = '이메일을 입력해주세요.';
+    idErrorMsgEl.classList.add('input-error-msg');
+    idInput.after(idErrorMsgEl);
     idInput.style.borderColor = 'var(--red)';
     }else{
       idInput.style.borderColor = 'var(--gray-40)';
-      idErrorMsg.remove();
+      idErrorMsgEl.remove();
     }
 }
 
 function idInvalid(e) {
   if(!reg.test(e.target.value) && e.target.value.length > 0){
-    idInvalidMsg.textContent = '올바른 이메일 주소가 아닙니다.';
-    idInvalidMsg.classList.add('input-error-msg');
-    idInput.after(idInvalidMsg);
+    idInvalidMsgEl.textContent = '올바른 이메일 주소가 아닙니다.';
+    idInvalidMsgEl.classList.add('input-error-msg');
+    idInput.after(idInvalidMsgEl);
     idInput.style.borderColor = 'var(--red)';
   }else{
-    idInvalidMsg.remove();
+    idInvalidMsgEl.remove();
   }
 }
 
 function pwEmpty(e) {
   if(e.target.value.length === 0){
-    pwErrorMsg.textContent = '비밀번호를 입력해주세요.';
-    pwErrorMsg.classList.add('input-error-msg');
-    inputBox.after(pwErrorMsg);
+    pwErrorMsgEl.textContent = '비밀번호를 입력해주세요.';
+    pwErrorMsgEl.classList.add('input-error-msg');
+    inputBox.after(pwErrorMsgEl);
     pwInput.style.borderColor = 'var(--red)';
     }else{
       pwInput.style.borderColor = 'var(--gray-40)';
-      pwErrorMsg.remove();
+      pwErrorMsgEl.remove();
     }
 }
 
 function clickSigninBtn(e){
   if(idInput.value !== "test@codeit.com"){
-    idErrorMsg.textContent = '이메일을 확인해주세요.';
-    idErrorMsg.classList.add('input-error-msg');
-    idInput.after(idErrorMsg);
+    idErrorMsgEl.textContent = '이메일을 확인해주세요.';
+    idErrorMsgEl.classList.add('input-error-msg');
+    idInput.after(idErrorMsgEl);
     idInput.style.borderColor = 'var(--red)';
   }else{
-    idErrorMsg.remove();
+    idErrorMsgEl.remove();
   }
 
   if(pwInput.value !== "codeit101"){
-    pwErrorMsg.textContent = '비밀번호를 확인해주세요.';
-    pwErrorMsg.classList.add('input-error-msg');
-    inputBox.after(pwErrorMsg);
+    pwErrorMsgEl.textContent = '비밀번호를 확인해주세요.';
+    pwErrorMsgEl.classList.add('input-error-msg');
+    inputBox.after(pwErrorMsgEl);
     pwInput.style.borderColor = 'var(--red)';
     }else{
-      pwErrorMsg.remove();
+      pwErrorMsgEl.remove();
   }
 
   if(idInput.value === "test@codeit.com" && pwInput.value === "codeit101"){
