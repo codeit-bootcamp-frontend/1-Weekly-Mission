e61627df7885298468957c1eb6ac3106a04b9dce
이승연 <orrhrr@gamil.com>
2023-10-04 15:40:00 +0900

    refactor : add init function

--- signin.js
+++ signin.js
@@ -1,10 +1,9 @@
 
 const inputEmailEl = document.querySelector('.input-email');
 const inputPswEl = document.querySelector('.input-psw');
-const loginBtn = document.querySelector('.sign-link');
 const showMessageByEmailEl = document.querySelector('.by-email');
 const showMessageByPswEl = document.querySelector('.by-psw')
-const ShowPswEyeIconEl = document.querySelector('.psw-box img');
+const eyeOffIconEl = document.querySelector('.psw-box img');
 
 const TEST_EMAIL = 'test@codeit.com';
 const TEST_PSW = 'codeit101';
@@ -12,14 +11,18 @@ const TEST_PSW = 'codeit101';
 function checkEmail() {
   const VALID_EMAIL_REG = /^[A-Za-z0-9_\.\-]+@[A-Za-z0-9\-]+\.[A-Za-z0-9\-]+/;
   const VALID_EMAIL_CHECK = VALID_EMAIL_REG .test(inputEmailEl.value);
-  // early return 기법 사용
+
   if(!inputEmailEl.value) {
     showMessageByEmailEl.textContent = '이메일을 입력해주세요.';
     inputEmailEl.classList.add('input-wrong');
-    } else if (!VALID_EMAIL_CHECK) {
-      showMessageByEmailEl.textContent = '올바른 이메일 주소가 아닙니다.';
-      inputEmailEl.classList.add('input-wrong');
+    return;
     }
+
+  if (!VALID_EMAIL_CHECK) {
+    showMessageByEmailEl.textContent = '올바른 이메일 주소가 아닙니다.';
+    inputEmailEl.classList.add('input-wrong');
+    return;
+  }
 }
 
 function removeEmailCheckMessage () {
@@ -29,14 +32,14 @@ function removeEmailCheckMessage () {
 
 function removePswCheckMessage () {
     showMessageByPswEl.textContent = '';
-    ShowPswEyeIconEl.classList.remove('psw-wrong');
+    eyeOffIconEl.classList.remove('psw-wrong');
     inputPswEl.classList.remove('input-wrong');
 }
 
 function checkPassword () {
   if(!inputPswEl.value) {
     showMessageByPswEl.textContent = '비밀번호를 입력해주세요.';
-    ShowPswEyeIconEl.classList.add('psw-wrong');
+    eyeOffIconEl.classList.add('psw-wrong');
     inputPswEl.classList.add('input-wrong');
   }
 }
@@ -49,21 +52,39 @@ function checkUser(e) {
     showMessageByEmailEl.textContent = '이메일을 확인해주세요.';
     showMessageByPswEl.textContent = '비밀번호를 확인해주세요.';
 
-    ShowPswEyeIconEl.classList.add('psw-wrong');
+    eyeOffIconEl.classList.add('psw-wrong');
     inputPswEl.classList.add('input-wrong');
     inputEmailEl.classList.add('input-wrong');
-
     inputEmailEl.setAttribute('onfocus', 'this.select()');
     inputPswEl.setAttribute('onfocus', 'this.select()');
   }
 }
 
+function showPsw () {
+  if(eyeOffIconEl.getAttribute('src') === "images/icon/eye-off.svg") {
+    eyeOffIconEl.setAttribute('src', 'images/icon/eye-on.svg');
+    inputPswEl.removeAttribute('type');
+  } else {
+    eyeOffIconEl.setAttribute('src', 'images/icon/eye-off.svg');
+    inputPswEl.setAttribute('type','password');
+  }
+}
 
-inputEmailEl.addEventListener('focusout', checkEmail);
-inputEmailEl.addEventListener('focusin', removeEmailCheckMessage);
+function init() {
+  const loginBtn = document.querySelector('.sign-link');
 
-inputPswEl.addEventListener('focusout', checkPassword);
-inputPswEl.addEventListener('focusin', removePswCheckMessage);
+  inputEmailEl.addEventListener('focusout', checkEmail);
+  inputEmailEl.addEventListener('focusin', removeEmailCheckMessage);
 
-loginBtn.addEventListener('click',checkUser);
+  inputPswEl.addEventListener('focusout', checkPassword);
+  inputPswEl.addEventListener('focusin', removePswCheckMessage);
+
+  loginBtn.addEventListener('click', checkUser);
+
+  eyeOffIconEl.addEventListener('click', showPsw);
+}
+
+init();
+
+
 
