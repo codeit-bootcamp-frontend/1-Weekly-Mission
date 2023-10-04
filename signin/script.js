021d9ccfcf3d273a9472a07a92e9d2b94df79c3e
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-04 12:43:41 +0900

    Fix: email input 값에 따른 에러메시지 발생 기능 수정

--- signin/script.js
+++ signin/script.js
@@ -1,27 +1,39 @@
-// 이메일 input에서 focus out할 때
-// 값이 없을 경우 '이메일을 입력해주세요' 에러메세지
-// 이메일 형식에 맞지 않는 값이 있는 경우 '올바른 이메일 주소가 아닙니다' 에러메시지
-const email = document.querySelector('#email-input');
-const password = document.querySelector('#password')
-const errorMessage = document.querySelector(".error-message");
-// email.onclick = function() {
-//   let x = email.value;
-//   if (x == null || x == ''){
-//       errorMessage.classList.remove('hide');
-//   }
-// }
+const email = document.querySelector('#email');
 
-function isEmptyValue(e) {
-  if (!e.target.value) {
-    const div = document.createElement('div');
-    email.style.borderColor = "red";
-    div.classList.add('error-message');
-    div.innerHTML = '이메일을 입력해주세요';
-    email.after(div);
+
+const setError = (element, message) => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.textContent = message;
+  inputControl.classList.add('error');
+  inputControl.classList.remove('success');
+}
+
+const setSuccess = element => {
+  const inputControl = element.parentElement;
+  const errorDisplay = inputControl.querySelector('.error');
+
+  errorDisplay.textContent = '';
+  inputControl.classList.add('success');
+  inputControl.classList.remove('error');
+}
+
+const isValidEmail = email => {
+  const regExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return regExp.test(email);
+}
+
+const checkEmailValidation = () => {
+  const emailValue = email.value.trim();
+  
+  if (emailValue === '') {
+    setError(email, '이메일을 입력하세요.');
+  } else if (!isValidEmail(emailValue)) {
+    setError(email, '올바른 이메일 주소가 아닙니다.')
   } else {
-    email.remove();
-    div.classList.remove('error-message');
+    setSuccess(email);
   }
 }
 
-email.addEventListener('focusout', isEmptyValue);
+email.addEventListener("focusout", checkEmailValidation);
