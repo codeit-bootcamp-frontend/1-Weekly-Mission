c90e89576472b10d6054bf39c92e05785e7c021a
NIX <lsc58461@naver.com>
2023-10-05 17:40:27 +0900

    chore: utils 폴더로 이동

--- src/js/tags.js
+++
@@ -1,9 +0,0 @@
-const loginForm = document.querySelector(".form");
-const emailInput = document.querySelector("#email");
-const passwordInput = document.querySelector("#password");
-const emailErrorMessage = document.querySelector("#email-error");
-const passwordErrorMessage = document.querySelector("#password-error");
-const eyeButton = document.querySelector(".items__eye-button");
-const eyeIcon = document.querySelector(".eye-button");
-
-export { loginForm, emailInput, passwordInput, emailErrorMessage, passwordErrorMessage, eyeButton, eyeIcon };
