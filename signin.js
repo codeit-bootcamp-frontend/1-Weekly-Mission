2a4eb78e7940f7793de9798c3cb602ed83650baf
K-Y-Hoo <bookopen1@gmail.com>
2023-10-05 19:06:10 +0900

    4주차 눈 모양 이미지 변경 함수 수정

--- signin.js
+++ signin.js
@@ -10,24 +10,24 @@ const passwordInput = document.getElementById('passwordInput');
 const passwordError = document.getElementById('passwordError');
 const loginButton = document.getElementById('loginButton');
 
-let cnt = 1;
+
+
 function changeEyeButton() {
   let img = document.getElementById('eyeImg');
 
-  if (cnt%2==1) {
+  if (img.getAttribute('src') === './images/eye-off.png') {
     //사선 없는 눈 모양 이미지로 변경
-    img.src="./images/eye-on.png";
+    img.setAttribute('src', './images/eye-on.png');
 
     //비밀번호 입력값 보이게 만들기
     password.type="text";
   } else {
     //사선 있는 눈 모양 이미지로 변경
-    img.src="./images/eye-off.png";
+    img.setAttribute('src', './images/eye-off.png');
 
     //비밀번호 입력값 가려지게 만들기
     password.type="password";
   }
-  cnt++;
 }
 
 loginButton.addEventListener('click', () => {
