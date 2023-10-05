ac1a2beb2a1bab042c66f85e93e70d268c2f11f1
손지은 <wiseag@naver.com>
2023-10-05 14:36:06 +0900

    feat : Create signup.js

---
+++ script/signup.js
@@ -0,0 +1,72 @@
+const $pwdErrorMessage = document.querySelector('.pwd_error_message');
+
+const $pwd = document.querySelector('.pwd_input');
+const $signBtn = document.querySelector('.btn-sign');
+const $pwdEye = document.querySelector('.password-eye');
+
+let emailValid = false;
+let pwdValid = false;
+
+const REGEMAIL = /^[A-Za-z0-9\-]+@[A-Za-z0-9]+\.[a-z]/;
+function emailErrorMessage(e){
+    emailValid = false;
+    if(e.target.value === ""){
+        $emailErrorMessage.textContent = "이메일을 입력해주세요."
+        $emailErrorMessage.style.display ="block";
+        $email.classList.add('border-red');
+    }
+   else if(!REGEMAIL.test(e.target.value)){
+        $emailErrorMessage.textContent = "올바른 이메일 주소가 아닙니다."
+        $emailErrorMessage.style.display ="block";
+        $email.classList.add('border-red');
+   }
+   else{
+    $emailErrorMessage.style.display ="none";
+    $email.classList.remove("border-red");
+    emailValid = true;
+   }
+}
+
+function pwdErrorMessage(e){
+    pwdValid=false;
+    if(e.target.value === ""){
+        $pwdErrorMessage.textContent = "비밀번호를 입력해주세요."
+        $pwdErrorMessage.style.display ="block";
+        $pwd.classList.add('border-red');
+    }
+    else{
+        $pwdErrorMessage.style.display ="none";
+        $pwd.classList.remove("border-red");
+        pwdValid = true;
+    }
+}
+$email.addEventListener("focusout",emailErrorMessage);
+$pwd.addEventListener("focusout",pwdErrorMessage);
+
+function signinValidCheck(e){
+    if(!emailValid){
+        $emailErrorMessage.textContent = "이메일을 확인해주세요."
+        $emailErrorMessage.style.display ="block";
+        e.preventDefault();
+    }
+    if(!pwdValid){
+        $pwdErrorMessage.textContent = "비밀번호를 확인해주세요."
+        $pwdErrorMessage.style.display ="block";
+        e.preventDefault();
+    }
+}
+document.querySelector('.form__sign').addEventListener('submit',signinValidCheck);
+
+let eyeOn = $pwdEye.src.includes('eye-on');
+function pwdEyeOnOff(e){
+    eyeOn = !eyeOn;
+    if(eyeOn){
+        e.target.src = "/img/eye-on.svg";
+        $pwd.type ="text";
+    }
+    else{
+        e.target.src = "/img/eye-off.svg";
+        $pwd.type ="password";
+    }
+}
+$pwdEye.addEventListener('click',pwdEyeOnOff);
