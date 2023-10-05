54be77f9d0a40fab85834e38c75a21aaea94ad9a
namminimi <tbvhdaos@naver.com>
2023-10-05 20:22:27 +0900

    refactor(mentor): 로그인, 눈 아이콘 코드 수정

--- javascript/member/password.js
+++ javascript/member/password.js
@@ -1,15 +1,17 @@
-const eye = document.querySelector('.fa-solid'); /* 눈 아이콘 */
-const passwordType = document.querySelector('#password'); /* 패스워드속성 */
+import {$, $all} from './utils.js';
+
+const eyeIcon = $('.fa-solid'); /* 눈 아이콘 */
+const passwordInput = $('#password'); /* 패스워드 */
 
 /* 눈 아이콘 & 패스워드 속성 변경 */
 function replaceIcon() {
-    if(eye.classList.contains('fa-eye-slash')){
-        eye.classList.replace('fa-eye-slash', 'fa-eye');
-        passwordType.setAttribute('type','text');
-    } else {
-        eye.classList.replace('fa-eye', 'fa-eye-slash');
-        passwordType.setAttribute('type','password');
-    }
+    if(eyeIcon.classList.contains('fa-eye-slash')){
+        eyeIcon.classList.replace('fa-eye-slash', 'fa-eye');
+        passwordInput.setAttribute('type','text');
+        return;
+    } 
+    eyeIcon.classList.replace('fa-eye', 'fa-eye-slash');
+    passwordInput.setAttribute('type','password');
 }
 
-eye.addEventListener('click', replaceIcon);
+eyeIcon.addEventListener('click', replaceIcon);
---
+++ javascript/member/utils.js
@@ -0,0 +1,11 @@
+/* 요소 하나 선택 함수 */
+function $(selector) {
+    return document.querySelector(selector);
+}
+
+/* 요소 여러개 선택 함수 */
+function $all(selector) {
+    return document.querySelectorAll(selector);
+}
+
+export {$, $all};
