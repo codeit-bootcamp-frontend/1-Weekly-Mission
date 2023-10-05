6b4d99b02ce262ba6c4989494548b902b5e761c8
이승연 <orrhrr@gamil.com>
2023-10-05 14:02:58 +0900

    refactor : element variables separately in variables.js

---
+++ variablesEl.js
@@ -0,0 +1,8 @@
+
+const inputEmailEl = document.querySelector('.input-email');
+const inputPswEl = document.querySelector('.input-psw');
+const showMessageByEmailEl = document.querySelector('.by-email');
+const showMessageByPswEl = document.querySelector('.by-psw')
+const eyeOffIconEl = document.querySelector('.psw-box img');
+
+export {inputEmailEl, inputPswEl, showMessageByEmailEl, showMessageByPswEl, eyeOffIconEl};
