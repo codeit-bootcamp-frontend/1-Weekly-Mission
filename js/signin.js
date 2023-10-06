89e0f9b2e19b3373433afea48fd607e35fdf8a11
박종민 <qooktree@naver.com>
2023-10-06 15:09:10 +0900

    fix: signin.js 이메일 형식 체크 REGEXP_TEXT 오류 수정

--- js/signin.js
+++ js/signin.js
@@ -12,7 +12,7 @@ const pElementPasswordError = document.createElement("p")
 
 // 이메일 형식 체크 함수
 const checkEmailForm = email => {
-    const REGEXP_TEXT = new RegExp("[a-z0-9]+@[a-z]+\.[a-z]{2,3}")
+    const REGEXP_TEXT = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
     return REGEXP_TEXT.test(email)
 }
 
