3d2ed74e038d01534ccf22289b81e4eef2468f7d
NIX <lsc58461@naver.com>
2023-10-06 14:26:22 +0900

    refactor: ERROR_MESSAGES 분리

---
+++ src/utils/error-message.js
@@ -0,0 +1,18 @@
+const ERROR_MESSAGES = {
+  email: {
+    empty: "이메일을 입력해주세요.",
+    invalid: "올바른 이메일 주소가 아닙니다.",
+    duplicate: "이미 사용 중인 이메일입니다.",
+  },
+  password: {
+    empty: "비밀번호를 입력해주세요.",
+    invalid: "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
+    mismatch: "비밀번호가 일치하지 않아요.",
+  },
+  submit: {
+    email: "이메일을 확인해주세요.",
+    password: "비밀번호를 확인해주세요.",
+  },
+};
+
+export default ERROR_MESSAGES;
