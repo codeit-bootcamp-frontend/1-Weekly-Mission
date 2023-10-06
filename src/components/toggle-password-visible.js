b7e15e90269aa50f1349f1702a74c00d00758b0c
NIX <lsc58461@naver.com>
2023-10-06 13:40:21 +0900

    refactor: isPasswordVisible 로직 개선

--- src/components/toggle-password-visible.js
+++ src/components/toggle-password-visible.js
@@ -1,9 +1,10 @@
-let isPasswordVisible = false;
-
 const togglePasswordVisible = (inputElement, iconElement, eyeIconPath) => {
+  let isPasswordVisible = inputElement.type === "text";
+
   const pwIconPath = isPasswordVisible ? eyeIconPath.eye_off : eyeIconPath.eye_on;
 
   isPasswordVisible = !isPasswordVisible;
+
   iconElement.setAttribute("src", pwIconPath);
   inputElement.type = isPasswordVisible ? "text" : "password";
 };
