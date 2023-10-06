f55a9902f72b7a00e91adc00ab1d79ebb0c2c90b
hanseulhee <3021062@gmail.com>
2023-10-07 08:46:56 +0900

    refactor: warning에서 error로 변경

--- src/js/auth.js
+++ src/js/auth.js
@@ -5,8 +5,8 @@ const passwordInput = document.querySelector('.passwordInput')
 const VERIFIED_EMAIL = 'test@codeit.com'
 const VERIFIED_PASSWORD = 'codeit101'
 
-const WARNING_INPUT_STYLE = 'warningInput'
-const WARNING_CONTENT_STYLE = 'warningContent'
+const ERROR_INPUT_STYLE = 'errorInput'
+const ERROR_CONTENT_STYLE = 'errorContent'
 
 form.addEventListener('submit', function (e) {
   e.preventDefault()
@@ -14,25 +14,25 @@ form.addEventListener('submit', function (e) {
 })
 
 // 경고 스타일
-function warningStyle(input, message) {
+function setErrorStyle(input, message) {
   const eachParent = input.parentElement
-  const warningContent = eachParent.querySelector('span')
-  const warningInput = eachParent.querySelector('input')
+  const errorContent = eachParent.querySelector('span')
+  const errorInput = eachParent.querySelector('input')
 
-  warningInput.classList.add(WARNING_INPUT_STYLE)
-  warningContent.className = WARNING_CONTENT_STYLE
-  warningContent.innerText = message
+  errorInput.classList.add(ERROR_INPUT_STYLE)
+  errorContent.className = ERROR_CONTENT_STYLE
+  errorContent.innerText = message
 }
 
 // 경고 스타일 삭제
-function noneWarningStyle(input) {
+function deleteErrorStyle(input) {
   const eachParent = input.parentElement
-  const warningContent = eachParent.querySelector('span')
-  const warningInput = eachParent.querySelector('input')
+  const errorContent = eachParent.querySelector('span')
+  const errorInput = eachParent.querySelector('input')
 
-  warningInput.classList.remove(WARNING_INPUT_STYLE)
-  warningContent.className = WARNING_CONTENT_STYLE
-  warningContent.innerText = ''
+  errorInput.classList.remove(ERROR_INPUT_STYLE)
+  errorContent.className = ERROR_CONTENT_STYLE
+  errorContent.innerText = ''
 }
 
 // 이메일 유효성 검사
@@ -40,22 +40,22 @@ function emailValidation() {
   const standard = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
   standard.test(emailInput.value)
-    ? noneWarningStyle(emailInput)
-    : warningStyle(emailInput, '올바른 이메일 주소가 아닙니다.')
+    ? deleteErrorStyle(emailInput)
+    : setErrorStyle(emailInput, '올바른 이메일 주소가 아닙니다.')
 }
 
 // 이메일 값이 없을 경우
 function emailCheck() {
   if (emailInput.value.length === 0) {
-    warningStyle(emailInput, '이메일을 입력해주세요.')
+    setErrorStyle(emailInput, '이메일을 입력해주세요.')
   }
 }
 
 // 비밀번호 값이 없을 경우
 function passwordCheck() {
   passwordInput.value.length === 0
-    ? warningStyle(passwordInput, '비밀번호를 입력해주세요.')
-    : noneWarningStyle(passwordInput)
+    ? setErrorStyle(passwordInput, '비밀번호를 입력해주세요.')
+    : deleteErrorStyle(passwordInput)
 }
 
 // 정해진 이메일과 패스워드
@@ -63,8 +63,8 @@ function loginCheck() {
   emailInput.value === VERIFIED_EMAIL &&
   passwordInput.value === VERIFIED_PASSWORD
     ? (location.href = '/folder.html')
-    : warningStyle(emailInput, '이메일을 확인해주세요.')
-  warningStyle(passwordInput, '비밀번호를 확인해주세요.')
+    : setErrorStyle(emailInput, '이메일을 확인해주세요.')
+  setErrorStyle(passwordInput, '비밀번호를 확인해주세요.')
 }
 
 emailInput.addEventListener('focusout', emailValidation)
