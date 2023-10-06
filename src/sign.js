83c4adf348d75479fc833a8055a0e5b61db3f4de
박경서 <zerm0907@gmail.com>
2023-10-06 17:34:08 +0900

    Refactor(sing.js) : pw input 오류 함수 따로 구현

--- src/sign.js
+++ src/sign.js
@@ -1,6 +1,7 @@
-const emailDiv = document.querySelector('.content1_div2_div1');
+const emailDiv = document.querySelector('.content1_div2_div1'); //이메일 상위 div
+const pwDiv = document.querySelector('.content1_div2_div2_div-input'); //비밀번호 상위 div
 const emailInput = document.querySelector('.content1_div2_div1_input'); //이메일 input
-const passwordInput = document.querySelector('.content1_div2_div2_input'); //비밀번호 input
+const pwInput = document.querySelector('.content1_div2_div2_input'); //비밀번호 input
 const eye = document.querySelector('.content1_div2_div2_img');
 const login = document.querySelector('.content1_div3_div');
 const emailCheck = document.querySelector('.content1_div2_div1_span2');
@@ -9,8 +10,8 @@ const passwordCheck = document.querySelector('.content1_div2_div2_span2');
 const VERIFYEAMIL = 'test@codeit.com';
 const VERIFYPW = 'codeit101';
 
-//입력 안했을때 경고
-function errorMsg(e) {
+//이메일 오류 함수
+function emailErrorMsg(e) {
   const newMsg = emailInput.value;
   if (!newMsg && !document.querySelector('.emailErrorSpan')) {
     //이메일칸 빈칸일때 경고창
@@ -35,29 +36,24 @@ function errorMsg(e) {
     span.remove();
     emailInput.style.border = '1px solid #ccd5e3';
   }
-  // const emailSpan = e.target.nextElementSibling;
-  // const emailInput = e.target;
-  // const emailSpan2 = e.target.parentElement.lastElementChild;
-  // const newMsg = emailInput.value;
-  // if (!newMsg) {
-  //   emailSpan.style.display = 'block';
-  //   emailInput.style.border = '1px solid red';
-  //   emailSpan2.style.display = 'none'; //이메일 형식
-  //   emailCheck.style.display = 'none'; //이메일 확인
-  //   passwordCheck.style.display = 'none'; //비밀번호 확인
-  // } else {
-  //   emailSpan.style.display = 'none'; //이메일 입력
-  //   emailCheck.style.display = 'none'; //이메일 확인
-  //   passwordCheck.style.display = 'none'; //비밀번호 확인
-  //   emailInput.style.border = '1px solid #ccd5e3';
-  //   if (e.target == emailInput && verify(newMsg) !== true) {
-  //     emailSpan2.style.display = 'block'; //이메일 형식
-  //     emailInput.style.border = '1px solid red';
-  //   } else {
-  //     emailSpan2.style.display = 'none'; //이메일 형식
-  //     emailInput.style.border = '1px solid #ccd5e3';
-  //   }
-  // }
+}
+
+//비밀번호 오류 함수
+function pwErrorMsg(e) {
+  const newMsg = pwInput.value;
+  if (!newMsg && !document.querySelector('.pwErrorSpan')) {
+    //비밀번호칸 빈칸일때 경고창
+    const emptyPwSpan = document.createElement('span');
+    emptyPwSpan.classList.add('pwErrorSpan');
+    emptyPwSpan.innerText = '이메일을 입력해주세요.';
+    pwDiv.append(emptyPwSpan);
+    pwInput.style.border = '1px solid red';
+  } else if (newMsg) {
+    //비밀번호칸 입력됐을 때
+    const emptyPwSpan = document.querySelector('.pwErrorSpan');
+    emptyPwSpan.remove();
+    pwInput.style.border = '1px solid #ccd5e3';
+  }
 }
 
 //비밀번호 표시
@@ -82,24 +78,25 @@ function verify(text) {
 
 //아이디 비번 일치
 function loginCheck(e) {
-  // if (
-  //   emailInput.value === VERIFYEAMIL &&
-  //   passwordInput.value === VERIFYPW
-  // ) {
-  //   console.log('hello');
-  //   location.replace('./folder.html');
-  // } else {
-  //   emailInput.nextElementSibling.style.display = 'none'; //이메일 입력
-  //   emailInput.parentElement.lastElementChild.style.display = 'none'; //올바른 이메일 입력
-  //   passwordInput.nextElementSibling.style.display = 'none'; //비밀번호 입력
-  //   emailCheck.style.display = 'block';
-  //   emailInput.style.border = '1px solid red';
-  //   passwordCheck.style.display = 'block';
-  //   passwordInput.style.border = '1px solid red';
-  // }
+  if (emailInput.value === VERIFYEAMIL && pwInput.value === VERIFYPW) {
+    location.replace('./folder.html');
+  } else {
+    const noEmailspan = document.createElement('span');
+    noEmailspan.classList.add('emailErrorSpan');
+    noEmailspan.innerText = '올바른 이메일 주소가 아닙니다.';
+    emailDiv.append(noEmailspan);
+    emailInput.style.border = '1px solid red';
+    // emailInput.nextElementSibling.style.display = 'none'; //이메일 입력
+    // emailInput.parentElement.lastElementChild.style.display = 'none'; //올바른 이메일 입력
+    // passwordInput.nextElementSibling.style.display = 'none'; //비밀번호 입력
+    // emailCheck.style.display = 'block';
+    // emailInput.style.border = '1px solid red';
+    // passwordCheck.style.display = 'block';
+    // passwordInput.style.border = '1px solid red';
+  }
 }
 
-emailInput.addEventListener('focusout', errorMsg);
-passwordInput.addEventListener('focusout', errorMsg);
+emailInput.addEventListener('focusout', emailErrorMsg);
+pwInput.addEventListener('focusout', pwErrorMsg);
 eye.addEventListener('click', viewPassword);
 login.addEventListener('click', loginCheck);
