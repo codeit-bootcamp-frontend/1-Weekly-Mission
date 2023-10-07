a00c402693f085283cece061514ede14048506c6
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-07 17:09:53 +0900

    password형식맞을때부분border색상초기화

--- common.js
+++ common.js
@@ -28,7 +28,7 @@ export function isEmailValid(input) {
 
 // 값이 8자 미만으로 있거나 문자열만 있거나 숫자만 있는 경우, “비밀번호는 영문, 숫자 조합 8자 이상
 export function isPasswordValid(input) {
-  const texts = input;
+  const texts = input.value.trim();
   var regex = /^[A-Za-z]+$/; // 문자로만 이루어진거
   var regex2 = /^[0-9]+$/; // 숫자로만 이루어진거
   return !regex2.test(texts) && !regex.test(texts) && texts.length > 8;
@@ -40,6 +40,8 @@ export function showValidPasswordErrorMessage(input, inputLabel) {
     errorMsgs.innerText = "올바른 패스워드 형식이 아닙니다";
     errorMsgs.style.color = "red";
     input.style.border = "3px solid red";
+  } else {
+    input.style.border = "";
   }
 }
 export function showValidEmailErrorMessage(input, inputLabel) {
