4aba4abf226ee49ac888db7fa0e29c1530f87df1
박종민 <qooktree@naver.com>
2023-10-07 13:19:46 +0900

    style: prettier 적용

--- js/member.js
+++ js/member.js
@@ -5,60 +5,71 @@ const pElementEmailError = document.createElement("p")
 const pElementPasswordError = document.createElement("p")
 const pElementPasswordConfirmError = document.createElement("p")
 const passwordConfirm = document.getElementById("password-confirm")
-const [emailInnerInput, passwordInnerInput, passwordConfirmInnerInput] = document.getElementsByClassName("inner-input")
-
+const [emailInnerInput, passwordInnerInput, passwordConfirmInnerInput] =
+  document.getElementsByClassName("inner-input")
 
 // 에러 메시지 삭제 함수
 const clearErrorMessage = (createdTag) => {
-    if (createdTag === email.parentElement.nextElementSibling){
-        emailInnerInput.classList.remove("inner-input-error")
-    } else if (createdTag === password.parentElement.nextElementSibling){
-        passwordInnerInput.classList.remove("inner-input-error")
-        passwordConfirmInnerInput?.classList.remove("inner-input-error")
-    } else if (createdTag === passwordConfirm?.parentElement.nextElementSibling){
-        passwordConfirmInnerInput?.classList.remove("inner-input-error")
-    }
-    createdTag.remove()
+  if (createdTag === email.parentElement.nextElementSibling) {
+    emailInnerInput.classList.remove("inner-input-error")
+  } else if (createdTag === password.parentElement.nextElementSibling) {
+    passwordInnerInput.classList.remove("inner-input-error")
+    passwordConfirmInnerInput?.classList.remove("inner-input-error")
+  } else if (createdTag === passwordConfirm?.parentElement.nextElementSibling) {
+    passwordConfirmInnerInput?.classList.remove("inner-input-error")
+  }
+  createdTag.remove()
 }
-
 
 // 에러 메시지 추가 함수
 const addErrorMessage = (createdTag, errorText) => {
-    createdTag.classList.add("inner-input-error-message")
-    createdTag.textContent = errorText
+  createdTag.classList.add("inner-input-error-message")
+  createdTag.textContent = errorText
 
-    if (createdTag === pElementEmailError){
-        email.parentNode.parentNode.append(createdTag)
-        emailInnerInput.classList.add("inner-input-error")
-    } else if (createdTag === pElementPasswordError){
-        password.parentNode.parentNode.append(createdTag)
-        passwordInnerInput.classList.add("inner-input-error")
-    } else if (createdTag === pElementPasswordConfirmError){
-        passwordConfirm?.parentNode.parentNode.append(createdTag)
-        passwordConfirmInnerInput?.classList.add("inner-input-error")
-    }
+  if (createdTag === pElementEmailError) {
+    email.parentNode.parentNode.append(createdTag)
+    emailInnerInput.classList.add("inner-input-error")
+  } else if (createdTag === pElementPasswordError) {
+    password.parentNode.parentNode.append(createdTag)
+    passwordInnerInput.classList.add("inner-input-error")
+  } else if (createdTag === pElementPasswordConfirmError) {
+    passwordConfirm?.parentNode.parentNode.append(createdTag)
+    passwordConfirmInnerInput?.classList.add("inner-input-error")
+  }
 }
-
 
 // 이메일 형식 체크 함수
-let checkEmailForm = email => {
-    const REGEXP_EMAIL_TEXT = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
-    return REGEXP_EMAIL_TEXT.test(email)
+let checkEmailForm = (email) => {
+  const REGEXP_EMAIL_TEXT =
+    /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/i
+  return REGEXP_EMAIL_TEXT.test(email)
 }
-
 
 // 비밀번호 눈 이미지 Toggle
-const changeEyeImg = e => {
-    e.preventDefault()
-    if (e.target.src.includes("/images/eye-on.png")){
-        e.target.src = "/images/eye-off.png"
-        e.target.parentNode.parentNode.firstElementChild.setAttribute("type", "password")
-    } else {
-        e.target.src = "/images/eye-on.png"
-        e.target.parentNode.parentNode.firstElementChild.setAttribute("type", "text")
-    }
+const changeEyeImg = (e) => {
+  e.preventDefault()
+  if (e.target.src.includes("/images/eye-on.png")) {
+    e.target.src = "/images/eye-off.png"
+    e.target.parentNode.parentNode.firstElementChild.setAttribute(
+      "type",
+      "password"
+    )
+  } else {
+    e.target.src = "/images/eye-on.png"
+    e.target.parentNode.parentNode.firstElementChild.setAttribute(
+      "type",
+      "text"
+    )
+  }
 }
 
-
-export { checkEmailForm, changeEyeImg, clearErrorMessage, addErrorMessage }  // export functions
-export { email, password, passwordConfirm, loginForm, pElementEmailError, pElementPasswordError, pElementPasswordConfirmError }  // export variables
+export { checkEmailForm, changeEyeImg, clearErrorMessage, addErrorMessage } // export functions
+export {
+  email,
+  password,
+  passwordConfirm,
+  loginForm,
+  pElementEmailError,
+  pElementPasswordError,
+  pElementPasswordConfirmError,
+} // export variables
--- js/signin.js
+++ js/signin.js
@@ -1,57 +1,68 @@
-import { email, password, loginForm, pElementEmailError, pElementPasswordError } from "./member.js"
-import { changeEyeImg, checkEmailForm, clearErrorMessage, addErrorMessage } from "./member.js"
+import {
+  email,
+  password,
+  loginForm,
+  pElementEmailError,
+  pElementPasswordError,
+} from "./member.js" // import variables
+
+import {
+  changeEyeImg,
+  checkEmailForm,
+  clearErrorMessage,
+  addErrorMessage,
+} from "./member.js" // import functions
 
 const passwordEye = document.getElementsByClassName("password-eye")[0]
 
 // 이메일 확인
-const setEmailErrorMessage = e => {
-    e.preventDefault()
-    if (!email.value){
-        addErrorMessage(pElementEmailError, "이메일을 입력해주세요.")
-    } else if (!checkEmailForm(email.value)){
-        addErrorMessage(pElementEmailError, "올바른 이메일 주소가 아닙니다.")
-    } else {
-        clearErrorMessage(pElementEmailError)
-    }
+const setEmailErrorMessage = (e) => {
+  e.preventDefault()
+  if (!email.value) {
+    addErrorMessage(pElementEmailError, "이메일을 입력해주세요.")
+  } else if (!checkEmailForm(email.value)) {
+    addErrorMessage(pElementEmailError, "올바른 이메일 주소가 아닙니다.")
+  } else {
+    clearErrorMessage(pElementEmailError)
+  }
 }
-
 
 // 비밀번호 확인
-const setPasswordErrorMessage = e => {
-    e.preventDefault()
-    if (!password.value){
-        addErrorMessage(pElementPasswordError, "비밀번호를 입력해주세요.")
-    } else {
-        clearErrorMessage(pElementPasswordError)
-    }
+const setPasswordErrorMessage = (e) => {
+  e.preventDefault()
+  if (!password.value) {
+    addErrorMessage(pElementPasswordError, "비밀번호를 입력해주세요.")
+  } else {
+    clearErrorMessage(pElementPasswordError)
+  }
 }
 
-// const pressEnter = e => {
-//     if (e.keyCode === 13) {
-//         e.preventDefault()
-//         submitLoginForm()
-//     }
-// }
-
+const pressEnter = (e) => {
+  if (e.key === "Enter") {
+    submitLoginForm(e)
+  }
+}
 
 // Form 전송 이벤트 함수
-const submitLoginForm = e => {
-    e.preventDefault()
-    console.log(1)
-    if (email.value === "test@codeit.com" && password.value === "codeit101"){
-        location.href = "/pages/folder.html"
-    } else {
-        addErrorMessage(pElementEmailError, "이메일을 확인해주세요.")
-        addErrorMessage(pElementPasswordError, "비밀번호를 확인해주세요.")
+const submitLoginForm = (event) => {
+  event?.preventDefault()
+  if (email.value === "test@codeit.com" && password.value === "codeit101") {
+    try {
+      location.href = "/pages/folder.html"
+    } catch (err) {
+      throw new Error("Can't redirect to folder page")
     }
+  } else {
+    addErrorMessage(pElementEmailError, "이메일을 확인해주세요.")
+    addErrorMessage(pElementPasswordError, "비밀번호를 확인해주세요.")
+  }
 }
-
 
 password.addEventListener("focusout", setPasswordErrorMessage)
 email.addEventListener("focusout", setEmailErrorMessage)
 
 loginForm.addEventListener("submit", submitLoginForm)
-// loginForm.addEventListener("keydown", pressEnter)
+loginForm.addEventListener("keydown", pressEnter)
 
-email.addEventListener("invalid", e => e.preventDefault())
+email.addEventListener("invalid", (e) => e.preventDefault())
 passwordEye.addEventListener("click", changeEyeImg)
