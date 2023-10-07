07463b1c739cfcb1dd12029aa5c1a7f31345648f
olseul <seul9085@naver.com>
2023-10-07 17:51:31 +0900

    refactor: 중복 코드 최소화를 위해 에러 메시지 출력 함수 분리 후 리팩토링

--- signup/signup.js
+++ signup/signup.js
@@ -10,88 +10,79 @@ const passwordCheckErrorText = document.querySelector("#password-check-error");
 
 const toggleVisibility = document.querySelector("#toggleVisibility");
 
-emailInput.addEventListener("blur", function () {
+function displayError(element, message) {
+  element.textContent = message;
+  element.style.visibility = message ? "visible" : "hidden";
+}
+
+function checkEmailValidity() {
   const email = emailInput.value;
   if (!email) {
-    emailErrorText.textContent = "이메일을 입력해주세요.";
-    emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    displayError(emailErrorText, "이메일을 입력해주세요.");
+    return false;
   } else if (!isValidEmail(email)) {
-    emailErrorText.textContent = "올바른 이메일 주소가 아닙니다.";
-    emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    displayError(emailErrorText, "올바른 이메일 주소가 아닙니다.");
+    return false;
   } else if (email === "test@codeit.com") {
-    emailErrorText.textContent = "이미 사용 중인 이메일입니다.";
-    emailErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    emailInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    displayError(emailErrorText, "이미 사용 중인 이메일입니다.");
+    return false;
   } else {
-    emailErrorText.textContent = "";
-    emailErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
-    emailInput.classList.remove("error"); // 정상 시 .error 클래스 제거
+    displayError(emailErrorText, "");
+    return true;
   }
-});
+}
 
-passwordInput.addEventListener("blur", function () {
+function checkPasswordValidity() {
   const password = passwordInput.value;
   if (!password) {
-    passwordErrorText.textContent = "비밀번호를 입력해주세요.";
-    passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    passwordInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    displayError(passwordErrorText, "비밀번호를 입력해주세요.");
+    return false;
   } else if (password.length < 8 || !isValidPassword(password)) {
-    passwordErrorText.textContent =
-      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.";
-    passwordErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    passwordInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
+    displayError(
+      passwordErrorText,
+      "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요."
+    );
+    return false;
   } else {
-    passwordErrorText.textContent = "";
-    passwordErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
-    passwordInput.classList.remove("error"); // 정상 시 .error 클래스 제거
+    displayError(passwordErrorText, "");
+    return true;
+  }
+}
+
+function checkPasswordMatch() {
+  if (passwordInput.value !== passwordCheckInput.value) {
+    displayError(passwordCheckErrorText, "비밀번호가 일치하지 않아요.");
+    return false;
+  } else {
+    displayError(passwordCheckErrorText, "");
+    return true;
+  }
+}
+
+emailInput.addEventListener("blur", checkEmailValidity);
+passwordInput.addEventListener("blur", checkPasswordValidity);
+passwordCheckInput.addEventListener("blur", checkPasswordMatch);
+
+loginBtn.addEventListener("click", submitForm);
+document.addEventListener("keydown", function (event) {
+  if (event.key === "Enter") {
+    submitForm();
   }
 });
 
-passwordCheckInput.addEventListener("blur", function () {
-  const passwordCheck = passwordCheckInput.value;
-  if (!passwordCheck) {
-    passwordCheckErrorText.textContent = "비밀번호를 입력해주세요.";
-    passwordCheckErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    passwordCheckInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
-  } else if (passwordInput.value !== passwordCheckInput.value) {
-    passwordCheckErrorText.textContent = "비밀번호가 일치하지 않아요.";
-    passwordCheckErrorText.style.visibility = "visible"; // 에러 메시지 표시
-    passwordCheckInput.classList.add("error"); // 에러 발생 시 .error 클래스 추가
-  } else {
-    passwordCheckErrorText.textContent = "";
-    passwordCheckErrorText.style.visibility = "hidden"; // 에러 메시지 숨김
-    passwordCheckInput.classList.remove("error"); // 정상 시 .error 클래스 제거
+function submitForm() {
+  if (checkEmailValidity() && checkPasswordValidity() && checkPasswordMatch()) {
+    window.location.href = "/folder";
   }
-});
-
-loginBtn.addEventListener("click", function () {
-  const email = emailInput.value;
-  const password = passwordInput.value;
-
-  if (email === "test@codeit.com" && password === "codeit101") {
-    location.href = "/folder";
-  } else {
-    // 잘못된 계정과 비밀번호에 대한 처리
-    emailErrorText.textContent = "이메일을 확인해주세요.";
-    emailErrorText.style.visibility = "visible";
-    passwordErrorText.textContent = "비밀번호를 확인해주세요.";
-    passwordErrorText.style.visibility = "visible";
-    emailInput.classList.add("error");
-    passwordInput.classList.add("error");
-  }
-});
+}
 
 toggleVisibility.addEventListener("click", function () {
   if (passwordInput.type === "password") {
-    // 현재 비밀번호 타입이 'password'인 경우
-    passwordInput.type = "text"; // 타입을 'text'로 변경하여 비밀번호를 표시
-    toggleVisibility.src = "/assets/eye-on.svg"; // 이미지 변경
+    passwordInput.type = "text";
+    toggleVisibility.src = "/assets/eye-on.svg";
   } else {
-    // 현재 비밀번호 타입이 'text'인 경우
-    passwordInput.type = "password"; // 타입을 다시 'password'로 변경하여 숨김
-    toggleVisibility.src = "/assets/eye-off.svg"; // 이미지 변경
+    passwordInput.type = "password";
+    toggleVisibility.src = "/assets/eye-off.svg";
   }
 });
 
