1d0d812dd26692503a33833813c5c1a9bd0edacf
안성재 <asusia@naver.com>
2023-10-07 09:14:34 +0900

    refactor: 회원가입 시도 시 일괄적인 유효성 검사를 위해 이벤트 콜백 함수에서 분리된 입력값 유효성 검사 함수 작성

--- js/signup.js
+++ js/signup.js
@@ -20,32 +20,44 @@ const editErrorStatus = (element, message='') => {
   }
 };
 
-const checkEmailInput = (e) => {
-  if (!e.target.value) {
-    editErrorStatus(e.target, '이메일을 입력해주세요.');
-  } else if (!emailRegex.test(e.target.value)) {
-    editErrorStatus(e.target, '올바른 이메일 주소가 아닙니다.');
-  } else if (e.target.value === correctMail) {
-    editErrorStatus(e.target, '이미 사용 중인 이메일입니다.');
+const checkEmail = (email) => {
+  if (!email) {
+    editErrorStatus(emailInput, '이메일을 입력해주세요.');
+  } else if (!emailRegex.test(email)) {
+    editErrorStatus(emailInput, '올바른 이메일 주소가 아닙니다.');
+  } else if (email === correctMail) {
+    editErrorStatus(emailInput, '이미 사용 중인 이메일입니다.');
   } else {
-    editErrorStatus(e.target);
+    editErrorStatus(emailInput);
   }
+};
+
+const checkPassword = (password) => {
+  if (!passwordRegex.test(password)) {
+    editErrorStatus(passwordInput, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
+  } else {
+    editErrorStatus(passwordInput);
+  }
+};
+
+const checkPasswordConfirm = (password) => {
+  if (password !== passwordInput.value) {
+    editErrorStatus(passwordConfirmInput, '비밀번호가 일치하지 않아요.');
+  } else {
+    editErrorStatus(passwordConfirmInput);
+  }
+};
+
+const checkEmailInput = (e) => {
+  checkEmail(e.target.value);
 };
 
 const checkPasswordInput = (e) => {
-  if (!passwordRegex.test(e.target.value)) {
-    editErrorStatus(e.target, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
-  } else {
-    editErrorStatus(e.target);
-  }
+  checkPassword(e.target.value);
 };
 
 const checkPasswordConfirmInput = (e) => {
-  if (e.target.value !== passwordInput.value) {
-    editErrorStatus(e.target, '비밀번호가 일치하지 않아요.');
-  } else {
-    editErrorStatus(e.target);
-  }
+  checkPasswordConfirm(e.target.value);
 };
 
 const trySignUp = (e) => {
@@ -54,19 +66,9 @@ const trySignUp = (e) => {
   if (emailRegex.test(emailInput.value) && passwordRegex.test(passwordInput.value) && passwordConfirmInput.value === passwordInput.value) {
     location.href = '/html/folder.html';
   } else {
-      if (!emailInput.value) {
-        editErrorStatus(emailInput, '이메일을 입력해주세요.');
-      } else if (!emailRegex.test(emailInput.value)) {
-        editErrorStatus(emailInput, '올바른 이메일 주소가 아닙니다.');
-      } else if (e.target.value === correctMail) {
-        editErrorStatus(emailInput, '이미 사용 중인 이메일입니다.');
-      }
-      if (!passwordRegex.test(passwordInput.value)) {
-        editErrorStatus(passwordInput, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
-      }
-      if (passwordConfirmInput.value !== passwordInput.value) {
-        editErrorStatus(passwordConfirmInput, '비밀번호가 일치하지 않아요.');
-      }
+    checkEmail(emailInput);
+    checkPassword(passwordInput);
+    checkPasswordConfirm(passwordConfirmInput);
   }
 };
 
