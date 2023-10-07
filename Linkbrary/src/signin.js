abbced519906bfec2a2c1b16d6d8d0df9a50946b
ParkSohyunee <sohyunee016@gmail.com>
2023-10-07 10:19:13 +0900

    refactor: signin - 공통으로 사용하는 로직 리팩토링

--- Linkbrary/src/signin.js
+++ Linkbrary/src/signin.js
@@ -1,3 +1,5 @@
+import { checkEmailValidation } from "./common/emailPasswordValidation.js";
+
 const USER_EMAIL = "test@codeit.com";
 const USER_PASSWORD = "codeit101";
 
@@ -12,21 +14,36 @@ const passwordError = document.getElementById("passwordError");
 const addErrorClass = (el) => el.classList.add("error");
 const removeErrorClass = (el) => el.classList.remove("error");
 
-/** 이메일 형식 검증 */
-const emailRegExp = /^[a-zA-Z0-9]+@[a-zA-Z]+\.[a-zA-Z]{2,3}$/i;
-
+/* 이메일 유효성 검사 */
 const emailValidation = (value) => {
-  const isEmailValid = emailRegExp.test(value);
-  const context = isEmailValid ? "" : "올바른 이메일 주소가 아닙니다.";
+  if (!value) {
+    emailError.textContent = "이메일을 입력해주세요";
+    addErrorClass(email);
+    return;
+  }
 
-  emailError.textContent = context;
-  isEmailValid ? removeErrorClass(email) : addErrorClass(email);
+  const isEmailValid = checkEmailValidation(value);
+  if (!isEmailValid) {
+    emailError.textContent = "올바른 이메일 주소가 아닙니다.";
+    addErrorClass(email);
+    return;
+  }
+
+  emailError.textContent = "";
+  removeErrorClass(email);
+  return true;
 };
 
-/** 비밀번호 있으면 에러메세지 초기화 */
-const passwordInputReset = () => {
+/** 비밀번호 유효성 검사 */
+const passwordValidation = (value) => {
+  if (!value) {
+    passwordError.textContent = "비밀번호를 입력해주세요";
+    addErrorClass(password);
+    return;
+  }
   passwordError.textContent = "";
   removeErrorClass(password);
+  return true;
 };
 
 /** 이메일, 비밀번호 focus out할 때, value 체크 */
@@ -34,30 +51,17 @@ const checkFormValue = (e) => {
   if (!e.target.classList.contains("form__input")) return;
 
   const { value, id } = e.target;
-  const context = id === "email" ? "이메일을" : "비밀번호를";
 
-  if (!value) {
-    document.getElementById(`${id}Error`).textContent = `${context} 입력해주세요`;
-    addErrorClass(e.target);
-    return;
-  }
-  id === "email" ? emailValidation(e.target.value) : passwordInputReset();
+  if (id === "email") emailValidation(value);
+  if (id === "password") passwordValidation(value);
 };
 
 /** 이메일: test@codeit.com, 비밀번호: codeit101 으로 로그인 시도할 경우, “/folder” 페이지로 이동  */
 const onClickSubmit = (e) => {
   e.preventDefault();
 
-  if (!email.value) {
-    emailError.textContent = "이메일을 입력해주세요";
-    addErrorClass(email);
-    return;
-  }
-  if (!password.value) {
-    passwordError.textContent = "비밀번호를 입력해주세요";
-    addErrorClass(password);
-    return;
-  }
+  if (!emailValidation(email.value)) return;
+  if (!passwordValidation(password.value)) return;
 
   /** 이외의 로그인 시도의 경우, 이메일 input, 비밀번호 input 아래에 해당 에러 메세지 */
   if (email.value !== USER_EMAIL || password.value !== USER_PASSWORD) {
