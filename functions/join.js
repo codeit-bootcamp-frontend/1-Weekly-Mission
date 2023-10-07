5ed000a4ac3a6ba43300a973f1bee541cbe343f5
이승연 <orrhrr@gamil.com>
2023-10-07 16:01:57 +0900

    fix : Remove unnecessary conditions for checkUserForJoin function

--- functions/join.js
+++ functions/join.js
@@ -1,12 +1,13 @@
-import { inputEmailEl, inputPswEl, showMessageByEmailEl, showMessageByPswEl, eyeOffIconEls, showMessageByPswCheckEl, inputPswCheckEl ,TEST_EMAIL } from './variablesEl.js'
+import { inputEmailEl, inputPswEl, showMessageByEmailEl, showMessageByPswEl, eyeOffIconEls, inputPswCheckEl,TEST_EMAIL } from './variablesEl.js'
 
 function checkUserForJoin(e) {
-  if (inputEmailEl.value && !showMessageByPswCheckEl.textContent && inputEmailEl.value !== TEST_EMAIL) 
+  if (inputEmailEl.value && 
+    inputEmailEl.value !== TEST_EMAIL) 
   return e.target.setAttribute('href', '/folder')
   
     showMessageByEmailEl.textContent = '이메일을 확인해주세요.';
     showMessageByPswEl.textContent = '비밀번호를 확인해주세요.';
-    showMessageByPswCheckEl.textContent = '비밀번호를 확인해주세요.'
+    // showMessageByPswCheckEl.textContent = '비밀번호를 확인해주세요.'
 
     eyeOffIconEls.forEach((eyeOffIconEl) => eyeOffIconEl.classList.add('psw-wrong'));
     inputPswEl.classList.add('input-wrong');
