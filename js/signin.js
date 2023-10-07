d6896b29dd05500a91f56fc272c1de5ce1f07e36
Gyuho Lee <j980303@naver.com>
2023-10-08 08:30:01 +0900

    refactor(mentor): 리팩토링

--- js/signin.js
+++ js/signin.js
@@ -1,95 +1,117 @@
-const idInput = document.querySelector('#id-label');
-const pwInput = document.querySelector('#password-label');
-const idErrorMsgEl = document.createElement('span');
-const pwErrorMsgEl = document.createElement('span');
-const inputBox = document.querySelector('.input-box:nth-child(2)');
-const idInvalidMsgEl = document.createElement('span');
-const loginBtn = document.querySelector('.login-btn');
-const eyeImg = document.querySelector('.eye-off');
+const idInput = $('#id-label');
+const passwordInput = $('#password-label');
+const loginButton = $('.login-btn');
+const eyeImage = $('.eye-off');
 
 const REG_ID = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
-function idEmpty(e) {
-  if(e.target.value.length === 0){
-    idErrorMsgEl.textContent = '이메일을 입력해주세요.';
-    idErrorMsgEl.classList.add('input-error-msg');
-    idInput.after(idErrorMsgEl);
-    idInput.style.borderColor = 'var(--red)';
-    }else{
-      idInput.style.borderColor = 'var(--gray-40)';
-      idErrorMsgEl.remove();
-    }
+function $(selector){
+  return document.querySelector(selector);
 }
 
-function idInvalid(e) {
-  if(!reg.test(e.target.value) && e.target.value.length > 0){
-    idInvalidMsgEl.textContent = '올바른 이메일 주소가 아닙니다.';
-    idInvalidMsgEl.classList.add('input-error-msg');
-    idInput.after(idInvalidMsgEl);
-    idInput.style.borderColor = 'var(--red)';
-  }else{
-    idInvalidMsgEl.remove();
+// 클래스 추가 함수
+function addError(element, className = 'input-error-msg'){
+  element.classList.add(className);
+}
+
+// 에러 메세지 생성 함수
+function displayUserInputError(element, input, message){
+  input.after(element);
+  element.textContent = message;
+  addError(element);
+  input.style.borderColor = 'var(--red)';
+}
+
+// 옳으면 에러 메세지 제거하는 함수
+function clearUserInputError(element, input){
+  element.textContent = '';
+  input.style.borderColor = 'var(--gray-40)';
+}
+
+
+function handleIdError(event) {
+  const idInput = $('#id-label');
+  const idErrorMassageElem = document.createElement('span');
+
+  if(!event.target.value){
+    displayUserInputError(idErrorMassageElem, idInput, '이메일을 입력해주세요.');
+    return;
+  }else if(!REG_ID.test(event.target.value) && event.target.value.length > 0){
+    displayUserInputError(idErrorMassageElem, idInput, '올바른 이메일 주소가 아닙니다.');
+    return;
   }
+
+  clearUserInputError(idInput, idErrorMassageElem);
 }
 
-function pwEmpty(e) {
-  if(e.target.value.length === 0){
-    pwErrorMsgEl.textContent = '비밀번호를 입력해주세요.';
-    pwErrorMsgEl.classList.add('input-error-msg');
-    inputBox.after(pwErrorMsgEl);
-    pwInput.style.borderColor = 'var(--red)';
-    }else{
-      pwInput.style.borderColor = 'var(--gray-40)';
-      pwErrorMsgEl.remove();
-    }
+function handlePasswordEmptyError(event) {
+  const passwordInput = $('#password-label');
+  const pwErrorMassageElem = document.createElement('span');
+
+  if(!event.target.value){
+    displayUserInputError(pwErrorMassageElem, passwordInput, '비밀번호를 입력해주세요.');
+    return;
+  }
+
+  clearUserInputError(passwordInput, pwErrorMassageElem);
 }
 
-function clickSigninBtn(e){
+
+
+function handleToggleEmail() {
+  const idErrorMassageElem = document.createElement('span');
+
   if(idInput.value !== "test@codeit.com"){
-    idErrorMsgEl.textContent = '이메일을 확인해주세요.';
-    idErrorMsgEl.classList.add('input-error-msg');
-    idInput.after(idErrorMsgEl);
-    idInput.style.borderColor = 'var(--red)';
-  }else{
-    idErrorMsgEl.remove();
+    displayUserInputError(idErrorMassageElem, idInput, '이메일을 입력해주세요.');
+    return;
   }
 
-  if(pwInput.value !== "codeit101"){
-    pwErrorMsgEl.textContent = '비밀번호를 확인해주세요.';
-    pwErrorMsgEl.classList.add('input-error-msg');
-    inputBox.after(pwErrorMsgEl);
-    pwInput.style.borderColor = 'var(--red)';
-    }else{
-      pwErrorMsgEl.remove();
+  clearUserInputError(idInput, idErrorMassageElem);
+}
+
+function handleTogglePassword() {
+  const pwErrorMassageElem = document.createElement('span');
+
+  if(passwordInput.value !== "codeit101"){
+    displayUserInputError(pwErrorMassageElem, passwordInput, '비밀번호를 확인해주세요.');
+    return;
   }
 
-  if(idInput.value === "test@codeit.com" && pwInput.value === "codeit101"){
-    e.preventDefault();
-    location.href = "../folder.html";
+  clearUserInputError(passwordInput, pwErrorMassageElem);
+}
+
+function loginSuccess(event){
+  event.preventDefault();
+  alert('로그인 되었습니다.');
+  location.href = "../folder.html";
+}
+
+function handleSignIn(event){
+  if(idInput.value === "test@codeit.com" && passwordInput.value === "codeit101"){
+    return loginSuccess();
   }
+
+  //존재하지 않는 이메일일 때
+  handleToggleEmail();
+
+  //존재하지 않는 비밀번호일 때
+  handleTogglePassword()
 }
 
 
+function handleToggleEye(){
+  const passwordInput = $('#password-label');
+  const eyeImage = $('.eye-off');
 
-function clickEyeImg(e){
-  const pwType = pwInput.getAttribute("type") === "password";
-  const [inputType, eyeOnOff] = pwType ? ["text", "eye-on"] : ["password", "eye-off"];
+  const passwordType = passwordInput.getAttribute("type") === "password";
+  const [inputType, eyeOnOff] = passwordType ? ["text", "eye-on"] : ["password", "eye-off"];
 
-  pwInput.setAttribute("type", inputType);
-  eyeImg.setAttribute("src", `../assets/png/${eyeOnOff}.png`)
-
-  // if(pwInput.getAttribute("type") === "password"){
-  //   pwInput.setAttribute("type", "text");
-  //   eyeImg.setAttribute("src", "../assets/png/eye-on.png");
-  // }else{
-  //   pwInput.setAttribute("type", "password");
-  //   eyeImg.setAttribute("src", "../assets/png/eye-off.png");
-  // }
+  passwordInput.setAttribute("type", inputType);
+  eyeImage.setAttribute("src", `../assets/png/${eyeOnOff}.png`);
 }
 
 
-idInput.addEventListener("focusout", idEmpty);
-idInput.addEventListener("focusout", idInvalid);
-pwInput.addEventListener("focusout", pwEmpty);
-loginBtn.addEventListener("click", clickSigninBtn);
-eyeImg.addEventListener("click", clickEyeImg);
+idInput.addEventListener("focusout", handleIdError);
+passwordInput.addEventListener("focusout", handlePasswordEmptyError);
+loginButton.addEventListener("click", handleSignIn);
+eyeImage.addEventListener("click", handleToggleEye);
