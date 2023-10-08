b79bc4687577c1a045209a92b195e34112eefe49
Gyuho Lee <j980303@naver.com>
2023-10-08 13:11:53 +0900

    signin span태그 안 없어지는 에러, 눈모양 위치 변경되는 에러 수정

--- js/signin.js
+++ js/signin.js
@@ -1,28 +1,36 @@
-const idInput = $('#id-label');
+const emailInput = $('#id-label');
 const passwordInput = $('#password-label');
 const loginButton = $('.login-btn');
 const eyeImage = $('.eye-off');
 
-const idErrorMassageElem = document.createElement('span');
+const emailErrorMassageElem = document.createElement('span');
 const pwErrorMassageElem = document.createElement('span');
 
-const REG_ID = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+const REG_EMAIL = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
 
 function $(selector){
   return document.querySelector(selector);
 }
 
 // 클래스 추가 함수
-function addError(element, className = 'input-error-msg'){
+function addClass(element, className = 'input-error-msg'){
   element.classList.add(className);
 }
 
 // 에러 메세지 생성 함수
 function displayUserInputError(element, input, message){
-  input.after(element);
   element.textContent = message;
-  addError(element);
   input.style.borderColor = 'var(--red)';
+  addClass(element);
+
+  if(input.id === 'password-label') {
+    const passwordWrapper = $('.input-password-wrapper')
+    passwordWrapper.after(element)
+    passwordWrapper.style.marginBottom = '10px'
+    return;
+  }
+
+  input.after(element);
 }
 
 // 옳으면 에러 메세지 제거하는 함수
@@ -32,19 +40,19 @@ function clearUserInputError(element, input){
 }
 
 // 로그인 에러 다루는 함수
-function handleIdError(event) {
-  const idInput = $('#id-label');
+function handleEmailError(event) {
+  const emailInput = $('#id-label');
   
 
   if(!event.target.value){
-    displayUserInputError(idErrorMassageElem, idInput, '이메일을 입력해주세요.');
+    displayUserInputError(emailErrorMassageElem, emailInput, '이메일을 입력해주세요.');
     return;
-  }else if(!REG_ID.test(event.target.value) && event.target.value.length > 0){
-    displayUserInputError(idErrorMassageElem, idInput, '올바른 이메일 주소가 아닙니다.');
+  }else if(!REG_EMAIL.test(event.target.value) && event.target.value.length > 0){
+    displayUserInputError(emailErrorMassageElem, emailInput, '올바른 이메일 주소가 아닙니다.');
     return;
   }
 
-  clearUserInputError(idErrorMassageElem, idInput);
+  clearUserInputError(emailErrorMassageElem, emailInput);
 }
 
 // 비밀번호 오류시 에러 출력, 삭제 함수
@@ -63,14 +71,14 @@ function handlePasswordEmptyError(event) {
 
 //존재하지 않는 이메일일 떄
 function handleToggleEmail() {
-  const idInput = $('#id-label');
+  const emailInput = $('#id-label');
 
-  if(idInput.value !== "test@codeit.com"){
-    displayUserInputError(idErrorMassageElem, idInput, '이메일을 확인해주세요.');
+  if(emailInput.value !== "test@codeit.com"){
+    displayUserInputError(emailErrorMassageElem, emailInput, '이메일을 확인해주세요.');
     return;
   }
 
-  clearUserInputError(idErrorMassageElem, idInput);
+  clearUserInputError(emailErrorMassageElem, emailInput);
 }
 
 
@@ -87,18 +95,19 @@ function handleTogglePassword() {
 }
 
 //로그인 성공했을 때
-function loginSuccess(event){
-  event.preventDefault();
+function loginSuccess(){
   alert('로그인 되었습니다.');
   location.href = "../folder.html";
 }
 
 // 로그인 성공, 실패 다루는 함수
-function handleSignIn(){
-  const idInput = $('#id-label');
+function handleSignIn(event){
+  event.preventDefault();
+  
+  const emailInput = $('#id-label');
   const passwordInput = $('#password-label');
 
-  if(idInput.value === "test@codeit.com" && passwordInput.value === "codeit101"){
+  if(emailInput.value === "test@codeit.com" && passwordInput.value === "codeit101"){
     return loginSuccess();
   }
 
@@ -120,19 +129,19 @@ function handleToggleEye(){
 }
 
 
-idInput.addEventListener("focusout", handleIdError);
+emailInput.addEventListener("focusout", handleEmailError);
 passwordInput.addEventListener("focusout", handlePasswordEmptyError);
 loginButton.addEventListener("click", handleSignIn);
 eyeImage.addEventListener("click", handleToggleEye);
 
 export { 
-  idInput,
+  emailInput,
   passwordInput,
   loginButton,
   eyeImage,
-  REG_ID,
+  REG_EMAIL,
   $,
-  addError,
+  addClass,
   displayUserInputError,
   clearUserInputError,
   handleToggleEye,
