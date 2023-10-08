89b18f97d1d2e24c5005646a4d7fcafe39a89e0d
namminimi <111848336+namminimi@users.noreply.github.com>
2023-10-08 11:04:03 +0900

    [남민섭] Week4 (#134)
    * refactor(mentor):2주차 위클리미션입니다
    
    * 이미지 절대경로에서 상대경로로 수정
    
    * 피드백 수정
    
    * section2 이미지 변경
    
    * 테블릿 반응형, 너비변화에 따라 글자 줄바꿈 이벤트 함수 추가
    
    * 반응형 태블릿  이벤트 함수 제거, br 태그에 클레스명 추가
    
    * 반응형 모바일 추가
    
    * 공유 메타태그 추가
    
    * 메타태그 이미지 경로 수정
    
    * 공유 메타 이미지 수정
    
    * header 반응형 수정
    
    * refactor(mentor):weekly3 mission입니다
    
    * refector:(mentor):병합잘못하여 수정입니다
    
    * refactor(mentor): 로그인 에러메세지 추가
    
    * refector(mentor): 눈 아이콘 변경에 따른 패스워드 속성 변경함수 추가
    
    * refactor(mentor): 로그인, 눈 아이콘 코드 수정
    
    * refector(mentor):메세지 수정 안되는 부분 수정

--- javascript/member/signin.js
+++ javascript/member/signin.js
@@ -1,7 +1,12 @@
-const inputs = document.querySelectorAll('input'); /* 이메일, 비밀번호 input */
-const login_btn = document.querySelector('form') /* form */
-const emailBox = document.querySelector('.email-box'); /* 이메일 메세지*/
-const passwordBox = document.querySelector('.password-box'); /* 비밀번호 메세지 */
+import {$, $all} from './utils.js';
+
+const inputs = $all('input'); /* 이메일, 비밀번호 input */
+const form = $('form') /* form */
+
+
+/* 정규표현식 */
+const CHECK_EMAIL = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; /* 이메일 체크*/
+
 
 /* 테스트 아이디&비번 */
 const testAccount = {
@@ -15,61 +20,74 @@ const inputAccount = {
     password: ""
 };
 
-/* 정규표현식 */
-const check_email = /^[a-zA-Z0-9+-\_.]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/; /* 이메일 */
-
-
-function signInform(e) {
-    /* 아이디 입력확인 */
-    if(e.target.value === "" && e.target.name == 'email') {
-        emailBox.classList.remove('error');
-        emailBox.classList.remove('wrong');
+/* 이메일 에러 메세지 */
+function emailErrorMessage(email, emailBox) {
+    if(!email.value) {
         emailBox.classList.add('empty');
-    } else if(!check_email.test(e.target.value) && e.target.value && e.target.name == 'email') {
-        emailBox.classList.remove('empty');
-        emailBox.classList.remove('wrong');
-        emailBox.classList.add('error');
-    } else if(check_email.test(e.target.value) && e.target.value && e.target.name == 'email') {
-        emailBox.classList.remove('error');
-        emailBox.classList.remove('empty');
-        emailBox.classList.remove('wrong');
-        inputAccount.email = e.target.value;
+        return ;
     }
-    /* 비밀번호 입력확인 */
-    if(e.target.value === "" && e.target.name == 'password') {
+    if(!CHECK_EMAIL.test(email.value)) {
+        emailBox.classList.add('wrong');
+        return;
+    } 
+    
+}
+/* 비밀번호 에러 메세지 */
+function passwordErrorMessage(password, passwordBox) {
+    if(!password.value) {
         passwordBox.classList.add('empty');
-        passwordBox.classList.remove('wrong');
-    } else if (e.target.value && e.target.name == 'password') {
-        passwordBox.classList.remove('empty');
-        passwordBox.classList.remove('wrong');
-        inputAccount.password = e.target.value;
+        return;
+    } 
+    
+}
+
+/* focusout 될 시 입력값 저장 */
+function hendleFocusOut(event) {
+    const emailBox = $('.email-box'); /* 이메일 메세지*/
+    const passwordBox = $('.password-box'); /* 비밀번호 메세지 */
+
+    const {target = target.name} = event; /* 이메일과 비밀번호 분해 */
+
+    if(target.name === 'email') {
+        emailBox.classList.remove('disaccord', 'empty', 'wrong'); /* 제거 */
+        /* 아이디 입력확인 */
+        emailErrorMessage(email, emailBox)
+        inputAccount.email = email.value;
+    } else {
+        passwordBox.classList.remove('empty', 'disaccord'); /* 제거 */
+        /* 비밀번호 입력확인 */
+        passwordErrorMessage(password, passwordBox)
+        inputAccount.password = password.value;
     }
 }
+
+
 /* 로그인 확인 */
-function loginSubmit(e) {
-    e.preventDefault();
+function handleSubmit(event) {
+    const emailBox = $('.email-box'); /* 이메일 메세지*/
+    const passwordBox = $('.password-box'); /* 비밀번호 메세지 */
+    
+    event.preventDefault();
     if(inputAccount.email === testAccount.email && inputAccount.password === testAccount.password) {
         alert('환영합니다.');
-        login_btn.submit();
+        form.submit();
         location.href= './folder.html'; 
         /* form의 action 속성을 사용하면 이메일/비번 쿼리문으로 전달되어서 location.href 사용 */
     } else {
+        /* 이메일 최종확인 */
         if(inputAccount.email !== testAccount.email) {
-            emailBox.classList.add('wrong');
-        } else {
-            emailBox.classList.remove('wrong');
+            emailBox.classList.add('disaccord');
         }
+        /* 비밀번호 최종확인 */ 
         if(inputAccount.password !== testAccount.password) {
-            passwordBox.classList.add('wrong');
-        } else {
-            passwordBox.classList.remove('wrong');
+            passwordBox.classList.add('disaccord');
         }
     }
 };
 
 
 for (let input of inputs) {
-    input.addEventListener('focusout', signInform);
+    input.addEventListener('focusout', hendleFocusOut);
 }
 
-login_btn.addEventListener('submit',loginSubmit);
+form.addEventListener('submit',handleSubmit);
