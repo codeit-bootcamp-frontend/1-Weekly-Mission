6aac1752801a2303ec847abaca400ebf4603330d
Hyeyoon <foryou9097@gmail.com>
2023-10-08 12:34:12 +0900

    feat: 비밀번호 input테두리에 적용하는 속성 추가, 에러메시지 내용을 함수로 바꾸기

--- signup.js
+++ signup.js
@@ -5,10 +5,10 @@ window.onload=function(){
   const eyeImagePassword = eyeImagePasswordEl.children[0]
   // const eyeImagePasswordRe = eyeImagePasswordReEl.children[0]
   const inputEmail= document.querySelector('#sign-up')
-  const inputPassWord = document.querySelector('#password')
+  const inputPassword = document.querySelector('#password')
   // const inputPassWordRe = document.querySelector('#password-re')
   const loginButton = document.querySelector('button[type="submit"]');
-  const inputEmailCss = document.querySelector('input')
+  const [inputEmailCss, inputPasswordCss] = document.querySelectorAll('input')
 
   //오류메시지 출력
   const emailErrorMessage = document.querySelector('.email-error-message')
@@ -18,7 +18,7 @@ window.onload=function(){
  // <눈모양 아이콘 적용, 비밀번호 입력타입 변경>
   function toggleImage(image, inputPassword) {
     if (image.src.includes('eye-off')) {
-      image.setAttribute('src','./images/signin/eye-on.png');
+      image.setAttribute('src','./images/signin/eye-on.svg');
       inputPassword.setAttribute('type', '');
     } else {
       image.setAttribute('src','./images/signin/eye-off.svg');
@@ -26,17 +26,28 @@ window.onload=function(){
     }
   }
   eyeImagePassword.addEventListener('click', ()=> {
-    toggleImage(eyeImagePassword, inputPassWord)});
+    toggleImage(eyeImagePassword, inputPassword)});
   // eyeImagePasswordRe.addEventListener('click', ()=> {
   //   toggleImage(eyeImagePasswordRe, inputPassWordRe)});
 
-  //<이메일을 입력하는 동안에 에러메시지 안 보이게 하기 >
-  //1) 이메일을 입력하는 동안 에러메시지를 가려주는 함수
+  //<입력하는 동안에는 에러메시지 안 보이게 하기 >
   function errorMessageStop(){
     emailErrorMessage.style.display = 'none';
   }
-  //2) 이벤트 적용
   inputEmail.addEventListener('input', errorMessageStop)
+  inputPassword.addEventListener('input', errorMessageStop)
+
+  //<에러가 발생한 경우 CSS속성 정리>>
+  function addErrorStyle(inputElement, errorMessageElement, errorMessage){
+    inputElement.style.border = '1px solid red';
+    errorMessageElement.style.display = 'block';
+    errorMessageElement.textContent = errorMessage;
+  }
+  function removeErrorStyle(inputElement, errorMessageElement, errorMessage){
+    inputElement.style.border = '1px solid #ccd5e3';
+    errorMessageElement.style.display = 'none';
+    errorMessageElement.textContent = errorMessage;
+  }
 
   // <이메일 형식검증, 오류메시지 출력 >
   //1) 사용자가 입력한 입력값이 유효한 형식인지 불린값으로 판단하는 함수
@@ -48,14 +59,10 @@ window.onload=function(){
   function checkEmail(){
     const email = inputEmail.value.trim();    //사용자가 인풋란에 입력한 값을 JS에서 email로 선언
     if (email === ''){
-      emailErrorMessage.style.display = 'block';
-      inputEmailCss.style.border = '1px solid red';
-      emailErrorMessage.textContent = '이메일을 입력해주세요.';
+      addErrorStyle(inputEmailCss, emailErrorMessage, '이메일을 입력해주세요.');
     }
     else if (!testEmail(email)){
-      emailErrorMessage.style.display = 'block';
-      emailErrorMessage.textContent = '올바른 이메일 주소가 아닙니다.';
-      inputEmailCss.style.border = '1px solid red';
+      addErrorStyle(inputEmailCss, emailErrorMessage, '올바른 이메일 주소가 아닙니다.');
     } else {
       emailErrorMessage.style.display = 'none';
       inputEmailCss.style.border = '1px solid #ccd5e3';
@@ -66,32 +73,32 @@ window.onload=function(){
 
   //<비밀번호 빈 값일때 오류메시지 출력>
   function checkPassword(){
-    const password = inputPassWord.value;
+    const password = inputPassword.value;
     if (password === ''){
-      passwordErrorMessage.style.display = 'block';      
+      addErrorStyle(inputPasswordCss, passwordErrorMessage, '비밀번호를 입력해주세요.');
     }else {
-      passwordErrorMessage.style.display = 'none';
+      removeErrorStyle(inputPasswordCss, passwordErrorMessage);
     };
   }
-  inputPassWord.addEventListener('focusout', checkPassword)
+  inputPassword.addEventListener('focusout', checkPassword)
 
   //<아이디&비밀번호 올바르게 입력했을 경우 /folder로 이동하고 아닌 경우 확인메시지 출력>
 function validAccount(email, password){
   if (email === 'test@codeit.com' && password === 'codeit101'){
     window.location.href = './index.html';
-  } else if (email !== 'test@codeit.com'){
-    emailErrorMessage.style.display = 'block';
-    inputEmailCss.style.border = '1px solid red';
-    emailErrorMessage.textContent = '이메일을 확인해주세요.';
-  } else if (password !== 'codeit101'){
-    passwordErrorMessage.style.display = 'block';
-    passwordErrorMessage.textContent = '비밀번호를 확인해주세요';    
+  } else if (email !== 'test@codeit.com' && password === 'codeit101'){
+    addErrorStyle(inputEmailCss, emailErrorMessage, '이메일을 확인해주세요.');
+  } else if (email === 'test@codeit.com' && password !== 'codeit101'){
+    addErrorStyle(inputPasswordCss, passwordErrorMessage, '비밀번호를 확인해주세요');
+  } else{
+    addErrorStyle(inputEmailCss, emailErrorMessage, '이메일을 확인해주세요.');
+    addErrorStyle(inputPasswordCss, passwordErrorMessage, '비밀번호를 확인해주세요');
   }
 }
 loginButton.addEventListener('click', function(event) {
   event.preventDefault();
   const email = inputEmail.value.trim();
-  const password = inputPassWord.value;
+  const password = inputPassword.value;
   validAccount(email, password);
 });
 
