aafe83fa95a83bd289c35ac8e4e65e5997a09ca4
HellenHong <hongjw030@naver.com>
2023-10-08 09:38:12 +0900

    refactor(signin.js): Refactor isValidEmail functions

--- js/signin.js
+++ js/signin.js
@@ -53,11 +53,7 @@ email.addEventListener("blur", function () {
 
 function isValidEmail(email) {
   const pattern = /^[A-Za-z0-9_\\.\\-]+@[A-Za-z0-9\\-]+\.[A-za-z0-9\\-]+/;
-  if (pattern.test(email) === false) {
-    return false;
-  } else {
-    return true;
-  }
+  return pattern.test(email);
 }
 
 password.addEventListener("blur", function () {
