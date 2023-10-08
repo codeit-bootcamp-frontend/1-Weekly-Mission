656d14e99b4a7be33230a0960b88ed1e69ad09aa
HellenHong <hongjw030@naver.com>
2023-10-08 13:21:29 +0900

    refactor(signup.js): Refactor signupBtn funtion

--- js/sign/signup.js
+++ js/sign/signup.js
@@ -17,57 +17,68 @@ import {
 const signupBtn = document.querySelector(".signup-button");
 const eyeBtn1 = document.querySelector("#eye-button1");
 const eyeBtn2 = document.querySelector("#eye-button2");
-signupBtn.addEventListener("click", function (e) {
-  e.preventDefault();
-  if (
-    !email.classList.contains("error-border") &&
-    !password.classList.contains("error-border") &&
-    !passwordRepeat.classList.contains("error-border")
-  ) {
-    location.href = "./folder.html";
-    disappearError(email, emailError);
-    disappearError(password, passwordError);
-    disappearError(passwordRepeat, passwordRepeatError);
-  }
-});
 
-email.addEventListener("blur", function () {
+function emailChecker() {
   if (!email.value) {
     appearError(email, emailError, "이메일을 입력해주세요.");
+    return false;
   } else if (!isValidEmail(email.value)) {
     appearError(email, emailError, "올바른 이메일 주소가 아닙니다.");
+    return false;
   } else if (email.value === "test@codeit.com") {
     appearError(email, emailError, "이미 사용 중인 이메일입니다.");
+    return false;
   } else {
     disappearError(email, emailError);
+    return true;
   }
-});
+}
 
-password.addEventListener("blur", function () {
+function passwordChecker() {
   if (!password.value) {
     appearError(password, passwordError, "비밀번호를 입력해주세요.");
+    return false;
   } else if (!isValidPassword(password.value)) {
     appearError(
       password,
       passwordError,
       "비밀번호는 영문, 숫자 조합 8자리 이상 입력해 주세요."
     );
+    return false;
   } else {
     disappearError(password, passwordError);
+    return true;
   }
-});
+}
 
-passwordRepeat.addEventListener("blur", function () {
-  if (passwordRepeat.value !== password.value) {
+function passwordRepeatChecker() {
+  if (passwordRepeat.value !== password.value || !passwordRepeat.value) {
     appearError(
       passwordRepeat,
       passwordRepeatError,
       "비밀번호가 일치하지 않아요."
     );
+    return false;
   } else {
     disappearError(passwordRepeat, passwordRepeatError);
+    return true;
   }
-});
+}
+
+email.addEventListener("blur", emailChecker);
+password.addEventListener("blur", passwordChecker);
+passwordRepeat.addEventListener("blur", passwordRepeatChecker);
 
 eyeBtn1.addEventListener("click", eyeCheck);
 eyeBtn2.addEventListener("click", eyeCheck);
+
+signupBtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  const flag = emailChecker() * passwordChecker() * passwordRepeatChecker();
+  if (flag) {
+    location.href = "./folder.html";
+    disappearError(email, emailError);
+    disappearError(password, passwordError);
+    disappearError(passwordRepeat, passwordRepeatError);
+  }
+});
