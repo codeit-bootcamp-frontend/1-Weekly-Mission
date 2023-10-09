da9c1e9f1e320200a69277eca1e64bbd225d5485
hanseulhee <3021062@gmail.com>
2023-10-09 11:20:27 +0900

    feat: auth 관련 상수 추가

--- src/constants/authMessage.js
+++ src/constants/authMessage.js
@@ -1,12 +1,12 @@
 // email
-const ERROR_EMAIL_EMPTY = '이메일을 입력해주세요.'
-const ERROR_EMAIL_VALIDATION = '올바른 이메일 주소가 아닙니다.'
-const ERROR_EMAIL_EXIST = '이미 사용 중인 이메일입니다.'
-const ERROR_EMAIL_CHECK = '이메일을 확인해주세요.'
+export const ERROR_EMAIL_EMPTY = '이메일을 입력해주세요.'
+export const ERROR_EMAIL_VALIDATION = '올바른 이메일 주소가 아닙니다.'
+export const ERROR_EMAIL_EXIST = '이미 사용 중인 이메일입니다.'
+export const ERROR_EMAIL_CHECK = '이메일을 확인해주세요.'
 
 // password
-const ERROR_PASSWORD_EMPTY = '비밀번호를 입력해주세요.'
-const ERROR_PASSWORD_VALIDATION =
-  '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.'
-const ERROR_PASSWORD_NOTCORRECT = '비밀번호가 일치하지 않아요.'
-const ERROR_PASSWORD_CHECK = '비밀번호를 확인해주세요.'
+export const ERROR_PASSWORD_EMPTY = '비밀번호를 입력해주세요.'
+export const ERROR_PASSWORD_VALIDATION =
+  '비밀번호는 영문, 숫자 조합 8자 이상 입력  해 주세요.'
+export const ERROR_PASSWORD_NOTCORRECT = '비밀번호가 일치하지 않아요.'
+export const ERROR_PASSWORD_CHECK = '비밀번호를 확인해주세요.'
