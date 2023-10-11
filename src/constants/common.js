9ae890084763b0f99f237af5d3dd1dbf381f0ed3
hanseulhee <3021062@gmail.com>
2023-10-11 17:18:41 +0900

    feat: api 주소 상수 설정

--- src/constants/common.js
+++ src/constants/common.js
@@ -5,3 +5,6 @@ export const VERIFIED_PASSWORD = 'codeit101'
 // error style
 export const ERROR_INPUT_STYLE = 'errorInput'
 export const ERROR_CONTENT_STYLE = 'errorContent'
+
+// api
+export const API_BASE_URL = 'https://bootcamp-api.codeit.kr'
