09455951b563a8e3f8be85e84059304cd60feb34
hanseulhee <3021062@gmail.com>
2023-10-14 03:57:08 +0900

    refactor: key, value 값 동일

--- src/libs/apis/email.js
+++ src/libs/apis/email.js
@@ -3,7 +3,7 @@ import { instance } from '../api.js'
 // 이메일 중복 확인
 export default async function emailDoubleCheck(email) {
   const statusCode = 409
-  await instance.post('/api/check-email', { email: email }).then((res) => {
+  await instance.post('/api/check-email', { email }).then((res) => {
     if (res.response?.status == statusCode) {
       alert('동일한 이메일이 존재합니다 😢')
     }
