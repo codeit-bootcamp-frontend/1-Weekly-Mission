a0eabbfc7cee2dfb603e9233f0bcebaee7ee57b8
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-14 17:59:58 +0900

    Style: 전체적인 코드 정리

--- sign/signup/index.js
+++ sign/signup/index.js
@@ -3,11 +3,26 @@ import { addErrorMessage, removeErrorMessage } from '/utils/error.js';
 import { toggleEyeIcon } from '/utils/toggle-icon.js';
 
 
-
+// Email
 const emailField = document.querySelector('.email-field');
 const emailInput = document.querySelector('#email');
 const emailErrorMessage = document.querySelector('.email-error-message');
 
+// Password
+const passwordField = document.querySelector('.password-field');
+const passwordInput = document.querySelector('#password');
+const passwordErrorMessage = document.querySelector('.password-error-message');
+const pwdConfirmInput = document.querySelector('#pwd-confirm');
+
+// Toggle
+const eyeIcon = document.querySelector('#eye-icon');
+const eyeIcon_Re = document.querySelector('#eye-icon-re');
+
+// LoginButton
+const loginBtn = document.querySelector('.btn-login');
+
+
+// 이메일 유효성 검사 
 function checkEmailValidation() {
   const email = emailInput.value.trim();
 
@@ -26,19 +41,11 @@ function checkEmailValidation() {
   }
 }
 
-emailInput.addEventListener("focusout", checkEmailValidation);
-
-
-
-const passwordField = document.querySelector('.password-field');
-const passwordInput = document.querySelector('#password');
-const passwordErrorMessage = document.querySelector('.password-error-message');
-
+// 비밀번호 유효성 검사 
 function checkPwdValidation() {
   const password = passwordInput.value.trim();
 
-  // 값이 8자 미만 or only 문자열 or only 숫자
-  if (!isPassword(password)) { 
+  if (!isPassword(password)) { // 값이 8자 미만 or only 문자열 or only 숫자
     addErrorMessage(passwordField, passwordErrorMessage, '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.');
     return false;
   } else {
@@ -47,11 +54,7 @@ function checkPwdValidation() {
   }
 }
 
-passwordInput.addEventListener("focusout", checkPwdValidation);
-
-
-const pwdConfirmInput = document.querySelector('#pwd-confirm');
-
+// 비밀번호 일치 확인 
 function confirmPwd() {
   const password = passwordInput.value;
   const pwdConfirmField = document.querySelector('.pwd-confirm-field');
@@ -67,20 +70,13 @@ function confirmPwd() {
   }
 }
 
-pwdConfirmInput.addEventListener("focusout", confirmPwd);
-
-
-const loginBtn = document.querySelector('.btn-login');
-
-function submitForm() { // 동작 X
+// 회원가입 시도 
+function submitForm() { 
 
   if (checkEmailValidation() && checkPwdValidation() && confirmPwd()) {
     window.location.href = "/folder/index.html";
   }
 }
-
-loginBtn.addEventListener("click", submitForm);
-
 
 
 function submitOnEnter(e) {
@@ -89,23 +85,16 @@ function submitOnEnter(e) {
   }
 }
 
-const eyeIcon = document.querySelector('#eye-icon');
-const eyeIcon_Re = document.querySelector('#eye-icon-re');
+emailInput.addEventListener("focusout", checkEmailValidation);
+passwordInput.addEventListener("focusout", checkPwdValidation);
+pwdConfirmInput.addEventListener("focusout", confirmPwd);
+loginBtn.addEventListener("click", submitForm);
 
+
+// 비밀번호 문자열 노출 
 eyeIcon.addEventListener("click", () => {
   toggleEyeIcon(passwordInput, eyeIcon)
 });
-
 eyeIcon_Re.addEventListener("click", () => {
   toggleEyeIcon(pwdConfirmInput, eyeIcon_Re)
 });
-
-
-
-// emailInput.addEventListener("focusout", checkEmailValidation);
-// pwdInput.addEventListener("focusout", checkPwdValidation);
-// pwdConfirmInput.addEventListener("focusout", confirmPwd);
-// emailInput.addEventListener("keyup", submitOnEnter);
-// pwdInput.addEventListener("keyup", submitOnEnter);
-// pwdConfirmInput.addEventListener("keyup", submitOnEnter);
-// loginBtn.addEventListener("click", submitForm);
