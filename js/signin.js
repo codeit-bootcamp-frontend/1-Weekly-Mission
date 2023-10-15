9a631d9be839e3cd5fa2d1443f7d82f38fd92424
BeMatthewsong <songww1997@naver.com>
2023-10-15 20:55:56 +0900

    feat: POST 요청으로 로그인 체크 기능 추가

--- js/signin.js
+++ js/signin.js
@@ -1,10 +1,10 @@
-const email = document.querySelector(".email-input");
-const password = document.querySelector(".password-input");
-const signButton = document.querySelector(".sign-form");
-const passwordEye = document.querySelector("#password-toggle");
-const emailErrorMessage = document.querySelector(".email-error-message");
-const passwordErrorMessage = document.querySelector(".password-error-message");
-const passwordConfirmErrorMessage = document.querySelector(
+const $email = document.querySelector(".email-input");
+const $password = document.querySelector(".password-input");
+const $signButton = document.querySelector(".sign-form");
+const $passwordEye = document.querySelector("#password-toggle");
+const $emailErrorMessage = document.querySelector(".email-error-message");
+const $passwordErrorMessage = document.querySelector(".password-error-message");
+const $passwordConfirmErrorMessage = document.querySelector(
   ".passwordCheck-error-message"
 );
 
@@ -15,48 +15,68 @@ const TEST_USER = {
   email: "test@codeit.com",
   password: "codeit101",
 };
+const API = "https://bootcamp-api.codeit.kr/api";
+
+// 토큰 존재하는 경우
+if (localStorage.getItem("accessToken")) {
+  location.href = "/pages/folder.html";
+}
 
 const checkEmailValidation = (event) => {
   if (event.target.value === "") {
-    emailErrorMessage.style.display = "block";
-    emailErrorMessage.textContent = "이메일을 입력해주세요";
-    email.classList.add("border-red");
+    $emailErrorMessage.style.display = "block";
+    $emailErrorMessage.textContent = "이메일을 입력해주세요";
+    $email.classList.add("border-red");
   } else if (!EMAIL_REGEX.test(event.target.value)) {
-    emailErrorMessage.style.display = "block";
-    emailErrorMessage.textContent = "올바른 이메일을 입력해주세요";
-    email.classList.add("border-red");
+    $emailErrorMessage.style.display = "block";
+    $emailErrorMessage.textContent = "올바른 이메일을 입력해주세요";
+    $email.classList.add("border-red");
   } else {
-    emailErrorMessage.style.display = "none";
-    email.classList.remove("border-red");
+    $emailErrorMessage.style.display = "none";
+    $email.classList.remove("border-red");
   }
 };
 
 const checkPasswordValidation = (event) => {
   if (event.target.value === "") {
-    passwordErrorMessage.style.display = "block";
-    passwordErrorMessage.textContent = "비밀번호를 입력해주세요";
-    password.classList.add("border-red");
+    $passwordErrorMessage.style.display = "block";
+    $passwordErrorMessage.textContent = "비밀번호를 입력해주세요";
+    $password.classList.add("border-red");
   } else if (!PASSWORD_REGEX.test(event.target.value)) {
-    passwordErrorMessage.style.display = "block";
-    passwordErrorMessage.textContent =
+    $passwordErrorMessage.style.display = "block";
+    $passwordErrorMessage.textContent =
       "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요";
-    password.classList.add("border-red");
+    $password.classList.add("border-red");
   } else {
-    passwordErrorMessage.style.display = "none";
-    passwordCheck.classList.remove("border-red");
+    $passwordErrorMessage.style.display = "none";
+    $passwordCheck.classList.remove("border-red");
   }
 };
 
-const submitForm = (event) => {
+const submitForm = async (event) => {
   event.preventDefault();
-  if (
-    email.value === TEST_USER.email &&
-    password.value === TEST_USER.password
-  ) {
-    location.href = "/pages/folder.html";
-  } else {
-    checkEmailError();
-    checkPasswordError();
+  try {
+    const response = await fetch(`${API}/sign-in`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: $email.value,
+        password: $password.value,
+      }),
+    });
+    const responseData = await response.json();
+
+    if (response.status === 200) {
+      // 토큰 추가
+      localStorage.setItem("accessToken", responseData.accessToken);
+      window.location.href = "/pages/folder.html";
+    } else if (response.status === 400) {
+      alert("아이디 혹은 비밀번호를 확인해주세요");
+    }
+  } catch (err) {
+    console.log(err);
   }
 };
 
@@ -74,9 +94,9 @@ const togglePassword = (input, toggleButton) => {
     .setAttribute("src", "/assets/eye-off.svg");
 };
 
-email.addEventListener("focusout", checkEmailValidation);
-password.addEventListener("focusout", checkPasswordValidation);
-passwordEye.addEventListener("click", () =>
-  togglePassword(password, passwordEye)
+$email.addEventListener("focusout", checkEmailValidation);
+$password.addEventListener("focusout", checkPasswordValidation);
+$passwordEye.addEventListener("click", () =>
+  togglePassword($password, $passwordEye)
 );
-signButton.addEventListener("submit", submitForm);
+$signButton.addEventListener("submit", submitForm);
--- js/signup.js
+++ js/signup.js
@@ -1,77 +1,67 @@
-// import {
-//   checkEmailError,
-//   checkPasswordError,
-//   checkPasswordConfirmError,
-//   checkSignin,
-//   togglePassword,
-//   TEST_USER,
-// } from "./utils.js";
-
-const email = document.querySelector(".email-input");
-const password = document.querySelector(".password-input");
-const passwordConfirm = document.querySelector(".passwordConfirm-input");
-const passwordEye = document.querySelector("#password-toggle");
-const passwordConfirmEye = document.querySelector("#password-check-toggle");
-const emailErrorMessage = document.querySelector(".email-error-message");
-const passwordErrorMessage = document.querySelector(".password-error-message");
-const passwordConfirmErrorMessage = document.querySelector(
+const $email = document.querySelector(".email-input");
+const $password = document.querySelector(".password-input");
+const $passwordConfirm = document.querySelector(".passwordConfirm-input");
+const $passwordEye = document.querySelector("#password-toggle");
+const $passwordConfirmEye = document.querySelector("#password-check-toggle");
+const $emailErrorMessage = document.querySelector(".email-error-message");
+const $passwordErrorMessage = document.querySelector(".password-error-message");
+const $passwordConfirmErrorMessage = document.querySelector(
   ".passwordConfirm-error-message"
 );
-const signForm = document.querySelector(".sign-form");
+const $signForm = document.querySelector(".sign-form");
+
 let eyeOn = false;
 const EMAIL_REGEX = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/g;
 const PASSWORD_REGEX = /^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/;
-const TEST_USER = {
-  email: "test@codeit.com",
-  password: "codeit101",
-};
+
+const API = "https://bootcamp-api.codeit.kr/api";
 
 const checkEmailValidation = (event) => {
   if (event.target.value === "") {
-    emailErrorMessage.style.display = "block";
-    emailErrorMessage.textContent = "이메일을 입력해주세요";
-    email.classList.add("border-red");
+    $emailErrorMessage.style.display = "block";
+    $emailErrorMessage.textContent = "이메일을 입력해주세요";
+    $email.classList.add("border-red");
     return false;
   } else if (!EMAIL_REGEX.test(event.target.value)) {
-    emailErrorMessage.style.display = "block";
-    emailErrorMessage.textContent = "올바른 이메일을 입력해주세요";
-    email.classList.add("border-red");
+    $emailErrorMessage.style.display = "block";
+    $emailErrorMessage.textContent = "올바른 이메일을 입력해주세요";
+    $email.classList.add("border-red");
     return false;
   } else {
-    emailErrorMessage.style.display = "none";
-    email.classList.remove("border-red");
+    $emailErrorMessage.style.display = "none";
+    $email.classList.remove("border-red");
     return true;
   }
 };
 
 const checkPasswordValidation = (event) => {
   if (event.target.value === "") {
-    passwordErrorMessage.style.display = "block";
-    passwordErrorMessage.textContent = "비밀번호를 입력해주세요";
-    password.classList.add("border-red");
+    $passwordErrorMessage.style.display = "block";
+    $passwordErrorMessage.textContent = "비밀번호를 입력해주세요";
+    $password.classList.add("border-red");
     return false;
   } else if (!PASSWORD_REGEX.test(event.target.value)) {
-    passwordErrorMessage.style.display = "block";
-    passwordErrorMessage.textContent =
+    $passwordErrorMessage.style.display = "block";
+    $passwordErrorMessage.textContent =
       "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요";
-    password.classList.add("border-red");
+    $password.classList.add("border-red");
     return false;
   } else {
-    passwordErrorMessage.style.display = "none";
-    passwordCheck.classList.remove("border-red");
+    $passwordErrorMessage.style.display = "none";
+    $passwordCheck.classList.remove("border-red");
     return true;
   }
 };
 
 const checkPasswordConfirmValidation = (event) => {
   if (event.target.value === password.value) {
-    passwordConfirmErrorMessage.style.display = "none";
-    passwordConfirm.classList.remove("border-red");
+    $passwordConfirmErrorMessage.style.display = "none";
+    $passwordConfirm.classList.remove("border-red");
     return true;
   } else {
-    passwordConfirmErrorMessage.style.display = "block";
-    passwordConfirmErrorMessage.textContent = "비밀번호가 일치하지 않아요";
-    passwordConfirm.classList.add("border-red");
+    $passwordConfirmErrorMessage.style.display = "block";
+    $passwordConfirmErrorMessage.textContent = "비밀번호가 일치하지 않아요";
+    $passwordConfirm.classList.add("border-red");
     return false;
   }
 };
@@ -100,20 +90,13 @@ const submitForm = (event) => {
   }
 };
 
-email.addEventListener("focusout", checkEmailValidation);
-password.addEventListener("focusout", checkPasswordValidation);
-passwordConfirm.addEventListener("focusout", checkPasswordConfirmValidation);
-passwordEye.addEventListener("click", () =>
+$email.addEventListener("focusout", checkEmailValidation);
+$password.addEventListener("focusout", checkPasswordValidation);
+$passwordConfirm.addEventListener("focusout", checkPasswordConfirmValidation);
+$passwordEye.addEventListener("click", () =>
   togglePassword(password, passwordEye)
 );
-passwordConfirmEye.addEventListener("click", () =>
+$passwordConfirmEye.addEventListener("click", () =>
   togglePassword(passwordConfirm, passwordEye)
 );
-signForm.addEventListener("submit", submitForm);
-
-// 아래 코드에 대해 실패해서 일일이 이벤트 등록
-// for (let passwordEye of passwordEyes) {
-//   passwordEye.addEventListener("click", () =>
-//     togglePassword(password, passwordEye)
-//   );
-// }
+$signForm.addEventListener("submit", submitForm);
