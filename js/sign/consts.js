024d86225ac87225046c5ccdbab22d9795b2f728
HellenHong <hongjw030@naver.com>
2023-10-15 14:43:12 +0900

    refactor(*): Change class to id

--- js/sign/consts.js
+++ js/sign/consts.js
@@ -2,9 +2,9 @@ const email = document.querySelector("#email");
 const password = document.querySelector("#password");
 const passwordRepeat = document.querySelector("#password-repeat");
 
-const emailError = document.querySelector(".email-message");
-const passwordError = document.querySelector(".password-message");
-const passwordRepeatError = document.querySelector(".password-repeat-message");
+const emailError = document.querySelector("#email-message");
+const passwordError = document.querySelector("#password-message");
+const passwordRepeatError = document.querySelector("#password-repeat-message");
 
 export {
   email,
