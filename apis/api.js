2c19c367f861217a4e645a074f492ce8a009858f
qooktree1 <qooktree@gmail.com>
2023-10-15 15:08:32 +0900

    feat: fetch api CRUD 함수 작성

--- apis/api.js
+++ apis/api.js
@@ -1,6 +1,5 @@
 const BASE_URL = "https://bootcamp-api.codeit.kr"
 
-// const fetchPost
 const fetchPost = async (url, body, headers = {}) => {
   const fullUrl = BASE_URL + url
   const options = {
@@ -15,4 +14,44 @@ const fetchPost = async (url, body, headers = {}) => {
   return await res.json()
 }
 
-export { fetchPost }
+const fetchGet = async (url, headers = {}) => {
+  const fullUrl = BASE_URL + url
+  const options = {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  }
+  const res = await fetch(fullUrl, options)
+  return await res.json()
+}
+
+const fetchPut = async (url, body, headers = {}) => {
+  const fullUrl = BASE_URL + url
+  const options = {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+    body: JSON.stringify(body),
+  }
+  const res = await fetch(fullUrl, options)
+  return await res.json()
+}
+
+const fetchDelete = async (url, headers = {}) => {
+  const fullUrl = BASE_URL + url
+  const options = {
+    method: "DELETE",
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  }
+  const res = await fetch(fullUrl, options)
+  return await res.json()
+}
+
+export { fetchPost, fetchGet, fetchPut, fetchDelete }
