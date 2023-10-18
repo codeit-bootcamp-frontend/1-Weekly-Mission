d9c827d06e48723c2218d4539977e0ca2b4ba373
hanseulhee <3021062@gmail.com>
2023-10-18 13:46:56 +0900

    style: global style, style 변수 설정

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "router/Router";
 import "styles/index.css";
+import "styles/variables.css";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
