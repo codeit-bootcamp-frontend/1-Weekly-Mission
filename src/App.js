d59b54173761109894cee37614f10956f1c7a190
olseul <seul9085@naver.com>
2023-10-18 17:20:42 +0900

    feat: 폴더 관련 데이터 api를 사용하여 가져온 후 배치

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from "react";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 import FolderOwnerInfo from "./FolderOwnerInfo";
+import CardList from "./CardList";
 
 const App = () => {
   return (
     <div>
       <Navbar />
       <FolderOwnerInfo />
+      <CardList />
       <Footer />
     </div>
   );
---
+++ src/Card.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "./CardStyles.css";
+
+const timeDifference = (current, previous) => {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return "1 minute ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    const years = Math.floor(elapsed / msPerYear);
+    if (years === 1) {
+      return "1 year ago";
+    } else {
+      return years + " years ago";
+    }
+  }
+};
+
+const Card = ({ link }) => {
+  const currentTime = new Date().getTime();
+  const createdAt = new Date(link.createdAt).getTime();
+
+  return (
+    <div className="card" onClick={() => window.open(link.url, "_blank")}>
+      <img src={link.imageSource} alt={link.title} />
+      <div className="card-text">
+        <span>{timeDifference(currentTime, createdAt)}</span>
+        <h3>{link.title}</h3>
+        <p>{link.description}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
