815c0a54ea1609a29e8be8b972bc7a100ca45f1f
olseul <seul9085@naver.com>
2023-10-19 15:55:49 +0900

    feat: 프로필 영역의 데이터를 api를 통해 가져온 후 디자인

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,18 +1,42 @@
-import React from "react";
-import "./Navbar.css"; // 기존 상단 바 css 규칙 가져오기!!
+import React, { useState, useEffect } from "react";
+import "./Navbar.css";
 import "./global.css";
 import Logo from "./Logo";
 
 function Navbar() {
+  const [userData, setUserData] = useState(null);
+
+  useEffect(() => {
+    fetch("https://bootcamp-api.codeit.kr/api/sample/user")
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.id) {
+          setUserData(data);
+        }
+      })
+      .catch((error) => console.error("Error:", error));
+  }, []);
+
   return (
     <header className="header">
       <div className="header-content">
         <a href="../public">
           <Logo className="logo" alt="logo" />
         </a>
-        <a href="signin">
-          <button className="loginBtn">로그인</button>
-        </a>
+        {userData ? (
+          <div className="profile">
+            <img
+              src={userData.profileImageSource}
+              alt="profile"
+              className="profile-image"
+            />
+            <span className="email">{userData.email}</span>
+          </div>
+        ) : (
+          <a href="signin">
+            <button className="loginBtn">로그인</button>
+          </a>
+        )}
       </div>
     </header>
   );
