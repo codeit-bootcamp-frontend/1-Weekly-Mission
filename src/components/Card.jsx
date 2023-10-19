b62685cef9aeebb3b7de46d057258f56ce14f5c4
sunny-son <thstkd92@gmail.com>
2023-10-19 14:52:33 +0900

    style : App 카드 card-image, star-mark layout
    Co-authored-by: 송민혁 <songww1997@naver.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -4,8 +4,8 @@ function Card() {
     <>
       <div className="card-wrapper">
         <div className="card-image-box">
-          <img src='' alt='카드 이미지' />
-          <span>star</span>
+          <img className="card-image" src='' alt='카드 이미지' />
+          <span className="star-mark">star</span>
         </div>
         <div className="card-info-box">
           <div>
