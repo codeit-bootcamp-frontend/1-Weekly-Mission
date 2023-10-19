43a55d88435afac8dc0bdcaa09a3ad1c7e8d3251
hanseulhee <3021062@gmail.com>
2023-10-19 16:38:21 +0900

    feat: react-query 적용

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,20 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "router/Router";
 import "styles/index.css";
 import "styles/variables.css";
 
+const queryClient = new QueryClient();
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 
 root.render(
-  <React.StrictMode>
-    <Router />
-  </React.StrictMode>
+  <QueryClientProvider client={queryClient}>
+    <React.StrictMode>
+      <Router />
+    </React.StrictMode>
+  </QueryClientProvider>
 );
