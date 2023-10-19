87fa4607766ce9b6f8f83db7427f4256cd3bdbd4
sunny-son <thstkd92@gmail.com>
2023-10-19 14:24:30 +0900

    style : App 카드 wrapper 설계
    Co-authored-by: 송민혁 <songww1997@naver.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './css/reset.css';
 import './css/App.css';
+import Card from './components/Card';
 
 function App() {
   return (
     <div className='App'>
-      app
+      <Card />
     </div>
   );
 }
--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,9 +2,22 @@ function Card() {
 
   return (
     <>
-      
+      <div className="card-wrapper">
+        <div>
+          <img src='' alt='카드 이미지' />
+          <span>star</span>
+        </div>
+        <div>
+          <div>
+            <div>nowDate - createdAt</div>
+            <span>점표(...)</span>
+          </div>
+          <p>Paragraph</p>
+          <div>createdAt</div>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
 export default Card;
