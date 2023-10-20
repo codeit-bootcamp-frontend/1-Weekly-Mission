f8e7a038fe2a1adb18833edb005faa2fcc978d17
전수빈 <soobin08011812@gmail.com>
2023-10-20 20:33:26 +0900

    Feat: 샘플 유저 조회 api 연동 및 프로필 데이터가 있는 경우 프로필 추가, axios

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,16 +1,48 @@
 import "./header.css";
 import LogoImg from "../../assets/common/img_logo.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Header = () => {
+  const [userData, setUserData] = useState({
+    email: null,
+    name: null,
+    id: null,
+    profileImageSource: null,
+  });
+  const handleProfile = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/api/sample/user`
+      );
+      if (response.status === 200) {
+        setUserData(response.data);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    handleProfile();
+  }, []);
+
   return (
     <header>
       <nav className="contentContainer">
         <img src={LogoImg} width="133" height="24" id="logoImg" alt="logoImg" />
-        <a href="./html/login.html">
-          <button className="defaultBtn" id="loginBtn">
-            로그인
-          </button>
-        </a>
+        {userData.email ? (
+          <div className="profileContainer">
+            <img src={userData.profileImageSource} alt="profileImg" />
+            <div className="profileEmail">{userData.email}</div>
+          </div>
+        ) : (
+          <a href="./html/login.html">
+            <button className="defaultBtn" id="loginBtn">
+              로그인
+            </button>
+          </a>
+        )}
       </nav>
     </header>
   );
