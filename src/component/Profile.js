8e42bd58441a15957794147576b06868e4009739
손지은 <wiseag@naver.com>
2023-10-20 19:54:28 +0900

    feat : Add Profile css

--- src/component/Profile.js
+++ src/component/Profile.js
@@ -2,9 +2,13 @@ function Profile({ folderInfo }) {
   if (folderInfo) {
     return (
       <div className="profile">
-        <img src={folderInfo.owner.profileImageSource} />
-        <div>{folderInfo.owner.name}</div>
-        <div>{folderInfo.name}</div>
+        <img
+          className="profile__img"
+          src={folderInfo.owner.profileImageSource}
+          alt="프로필 이미지"
+        />
+        <div className="profile__name">{folderInfo.owner.name}</div>
+        <div className="profile__folder-name">{folderInfo.name}</div>
       </div>
     );
   }
