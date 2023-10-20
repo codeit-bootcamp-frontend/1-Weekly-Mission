c80bc9778691caad646316caf9c3003d674d3afc
K-Y-Hoo <bookopen1@gmail.com>
2023-10-20 17:07:57 +0900

    fix: 프로필 영역 계정 이미지 css 수정

--- part2/src/Nav.js
+++ part2/src/Nav.js
@@ -29,11 +29,19 @@ const Nav = () => {
             {userEmail ? <p>{userEmail}</p> : <span>로그인</span>}
           </a> */}
           {userEmail ? (
-            <a className="cta cta-short-account">
-              <img src="images/Ellipse.svg" alt="계정 배경 이미지" />
-              <img src="images/myprofile.svg" alt="계정 이미지" />
+            <div className="cta cta-short-account">
+              <img
+                style={{ zIndex: "1", position: "absolute", left: "-4.5rem" }}
+                src="images/Ellipse.svg"
+                alt="계정 배경 이미지"
+              />
+              <img
+                style={{ zIndex: "2", position: "absolute", left: "-3.6rem" }}
+                src="images/myprofile.svg"
+                alt="계정 이미지"
+              />
               <p style={{ fontSize: "1.4rem" }}>{userEmail}</p>
-            </a>
+            </div>
           ) : (
             <a className="cta cta-short" href="signin.html">
               <span>로그인</span>
