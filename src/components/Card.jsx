9f1f431124e3bb7b5d85e4438a7c2994811b0e14
sunny-son <thstkd92@gmail.com>
2023-10-20 14:08:49 +0900

    feat : moment 활용하여 시간 계산 추가
    Co-authored-by: 송민혁 <songww1997@naver.com>

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,30 +1,22 @@
-function formatDate(value) {
-  const date = new Date(value);
-  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}.`;
-}
-
-function calculatePassedTime(time) {
-  return Date.now() - time;
-}
+import Moment from 'react-moment';
 
 function Card({ item }) {
-
   return (
     <>
-      <div className='card-wrapper' key={item.id}>
+      <div className='card-wrapper'>
         <div className='card-image-box'>
           <img className='card-image' src={item.imageSource} alt={item.title} />
           <span className='star-mark'>star</span>
         </div>
         <div className='card-info-box'>
           <div className='card-info-top'>
-            <div className='card-passed-time'>{calculatePassedTime(item.createdAt)}</div>
+            <Moment className='card-passed-time' fromNow>{item.createdAt}</Moment>
             <button>
-              <img src='/src/assets/image/kebab.svg' alt='' />
+              <img src='/assets/image/kebab.svg' alt='카드 설정 버튼' />
             </button>
           </div>
           <p>{item.description}</p>
-          <div>{formatDate(item.createdAt)}</div>
+          <Moment format='YYYY.MM.DD'>{item.createdAt}</Moment>
         </div>
       </div>
     </>
