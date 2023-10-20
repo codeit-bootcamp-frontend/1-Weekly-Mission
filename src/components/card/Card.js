d48307d5d790a0f64566b3c3269ddacf569794a0
전수빈 <soobin08011812@gmail.com>
2023-10-20 20:54:29 +0900

    Feat: 샘플 폴더 조회 api 연동

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -3,35 +3,39 @@ import moment from "moment";
 import { useEffect, useState } from "react";
 import LogoImg from "../../assets/common/img_logo.png";
 
-function calculateTimeAgo(createdAt) {
-  const createdDate = moment(createdAt, "YYYY-MM-DDTHH:mm:ss[Z]");
-  const currentDate = moment();
-  const diff = currentDate.diff(createdDate, "seconds");
-
-  if (diff < 120) {
-    return "1 minute ago";
-  } else if (diff <= 3540) {
-    return `${Math.floor(diff / 60)} minutes ago`;
-  } else if (diff < 3600) {
-    return "1 hour ago";
-  } else if (diff <= 82800) {
-    return `${Math.floor(diff / 3600)} hours ago`;
-  } else if (diff < 86400) {
-    return "1 day ago";
-  } else if (diff <= 2592000) {
-    return `${Math.floor(diff / 86400)} days ago`;
-  } else if (diff <= 28512000) {
-    return `${Math.floor(diff / 2592000)} months ago`;
-  } else if (diff <= 31536000) {
-    return "1 year ago";
-  } else {
-    return `${Math.floor(diff / 31536000)} years ago`;
-  }
-}
-
-const Card = ({ src, createdAt, content }) => {
+const Card = ({ src, createdAt, content, url }) => {
   const [ago, setAgo] = useState(0);
   const [createdAtFormat, setCreatedAtFormat] = useState(createdAt);
+
+  const calculateTimeAgo = (createdAt) => {
+    const createdDate = moment(createdAt, "YYYY-MM-DDTHH:mm:ss[Z]");
+    const currentDate = moment();
+    const diff = currentDate.diff(createdDate, "seconds");
+
+    if (diff < 120) {
+      return "1 minute ago";
+    } else if (diff <= 3540) {
+      return `${Math.floor(diff / 60)} minutes ago`;
+    } else if (diff < 3600) {
+      return "1 hour ago";
+    } else if (diff <= 82800) {
+      return `${Math.floor(diff / 3600)} hours ago`;
+    } else if (diff < 86400) {
+      return "1 day ago";
+    } else if (diff <= 2592000) {
+      return `${Math.floor(diff / 86400)} days ago`;
+    } else if (diff <= 28512000) {
+      return `${Math.floor(diff / 2592000)} months ago`;
+    } else if (diff <= 31536000) {
+      return "1 year ago";
+    } else {
+      return `${Math.floor(diff / 31536000)} years ago`;
+    }
+  };
+
+  const openUrl = () => {
+    window.open(url, "__blank");
+  };
 
   useEffect(() => {
     setAgo(calculateTimeAgo(createdAt));
@@ -39,10 +43,10 @@ const Card = ({ src, createdAt, content }) => {
   }, [createdAt]);
 
   return (
-    <div className="cardContainer">
+    <div className="cardContainer" onClick={openUrl}>
       {src ? (
         <div className="cardImageContainer">
-          <img className="cardImage" src={src} alt="고양이" />
+          <img className="cardImage" src={src} alt="cardImg" />
         </div>
       ) : (
         <div className="cardImageContainer cardImage">
--- src/page/folder/Folder.js
+++ src/page/folder/Folder.js
@@ -1,15 +1,45 @@
 import Card from "../../components/card/Card";
 import "./folder.css";
 import SearchImg from "../../assets/folder/img_search.png";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Folder = () => {
+  const [cardData, setCardData] = useState([]);
+  const [user, setUser] = useState();
+  const [name, setName] = useState("");
+
+  const handleFolder = async () => {
+    try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_API}/api/sample/folder`
+      );
+      if (response.status === 200) {
+        const result = response.data.folder;
+        setCardData(result.links);
+        setUser(result.owner);
+        setName(result.name);
+      }
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  useEffect(() => {
+    handleFolder();
+  }, []);
+
   return (
     <div className="wrapper">
       <div className="section">
         <div className="folderContentContainer">
-          <div id="userProfile"></div>
-          <div id="userName">@코드잇</div>
-          <div id="folderName">⭐️ 즐겨찾기</div>
+          <img
+            id="userProfile"
+            src={user?.profileImageSource}
+            alt="profileImg"
+          />
+          <div id="userName">{user?.name}</div>
+          <div id="folderName">{name}</div>
         </div>
       </div>
       <div className="section" style={{ background: "#fff" }}>
@@ -22,14 +52,17 @@ const Folder = () => {
             ></input>
           </div>
           <div id="cardContainer">
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
-            <Card />
+            {cardData?.map((e) => {
+              return (
+                <Card
+                  key={e.id}
+                  url={e.url}
+                  src={e.imageSource}
+                  content={e.description}
+                  createdAt={e.createdAt}
+                />
+              );
+            })}
           </div>
         </div>
       </div>
