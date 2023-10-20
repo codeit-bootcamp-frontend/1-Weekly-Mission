0d77a1c0a3f6d6e13ba97fb981e1367249b01b79
Nahyun-Kang <tarea202001@gmail.com>
2023-10-20 14:09:56 +0900

    Setting: 초기 세팅

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import App from "./App";
+import App from "../src/components/js/App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<App />);
