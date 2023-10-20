d92ee461e579bbbf2d566e4ca245e6dece448056
hanseulhee <3021062@gmail.com>
2023-10-20 22:51:24 +0900

    feat: date message 상수화

---
+++ src/constants/date.ts
@@ -0,0 +1,10 @@
+export const PERIOD_CONTENT = {
+  singular: {
+    minute: "minute ago",
+    month: "month ago",
+    year: "year ago",
+  },
+  minute: "minutes ago",
+  month: "months ago",
+  year: "years ago",
+};
