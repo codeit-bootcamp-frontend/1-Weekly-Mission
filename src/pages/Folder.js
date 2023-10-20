abc4b39975dc55d6ff0a1065d8348f714e2b677f
NIX <lsc58461@naver.com>
2023-10-20 15:37:42 +0900

    feat: footer 컴포넌트 추가

--- src/pages/Folder.js
+++ src/pages/Folder.js
@@ -1,9 +1,11 @@
 import NavBar from "../components/NavBar";
+import Footer from "../components/Footer";
 
 const Folder = () => {
   return (
     <>
       <NavBar />
+      <Footer />
     </>
   );
 };
