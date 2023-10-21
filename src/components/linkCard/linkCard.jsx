6e81f7b87fec228e9dcfd83bc5c565f977c75273
Hayden <hayden.dev.k@gmail.com>
2023-10-21 18:26:47 +0900

    feat: day-fns 적용하여 createDate, timeAgo 구현

--- src/components/linkCard/linkCard.jsx
+++ src/components/linkCard/linkCard.jsx
@@ -1,12 +1,16 @@
 import "./linkCard.css";
 import noImageSource from "../../assets/images/noLinkCardImg.png";
 import { NO_SPECIAL_CHA_PATTERN } from "../../utils/constants";
+import getTimeAgo from "../../utils/getTimeAgo.js";
+import { parseISO, format } from "date-fns";
 
 export default function LinkCardComponent({ cardData }) {
 	const { createdAt, url, description, title, imageSource } = cardData;
 
 	const imageAlt =
 		title.split(" ")[0].replace(NO_SPECIAL_CHA_PATTERN, "") + " 대표 이미지";
+
+	const parsedCreatedDate = format(parseISO(createdAt), "yyyy-MM-dd");
 
 	return (
 		<li className="card--container">
@@ -25,9 +29,9 @@ export default function LinkCardComponent({ cardData }) {
 					/>
 				</div>
 				<div className="card--info">
-					<div className="time-since-post">{createdAt}</div>
+					<div className="time-ago">{getTimeAgo(createdAt)}</div>
 					<div className="description">{description}</div>
-					<div className="created-date">{createdAt}</div>
+					<div className="created-date">{parsedCreatedDate}</div>
 				</div>
 			</a>
 		</li>
