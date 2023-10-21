2ac70564568de85b71b666413a3d3cafd2e03c6a
wecaners <wecaners@naver.com>
2023-10-22 01:56:56 +0900

    Design: modify sharedPage design

--- src/pages/share/SharedPage.js
+++ src/pages/share/SharedPage.js
@@ -1,16 +1,21 @@
 import React from 'react';
 import Card from '../../components/card/Card';
 import SearchBar from '../../components/searchBar/SearchBar';
+import './sharedpage.css';
 
 export default function SharedPage({ links }) {
   return (
-    <main>
-      <SearchBar />
-      {links.map((item) => (
-        <div key={item.id}>
-          <Card linkInfo={item} />
+    <main className="main">
+      <section className="content-section">
+        <SearchBar />
+        <div className="links-container">
+          {links.map((item) => (
+            <div key={item.id}>
+              <Card linkInfo={item} />
+            </div>
+          ))}
         </div>
-      ))}
+      </section>
     </main>
   );
 }
