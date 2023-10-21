c7bf02ddd70d0fdb2bff1a5d69aa3cfb0a85010c
qooktree1 <qooktree@gmail.com>
2023-10-21 16:45:27 +0900

    Co-authored-by: JEONDONGHUN1042 <JEONDONGHUN1042@users.noreply.github.com>

---
+++ src/components/Card/Card.js
@@ -0,0 +1,35 @@
+import "./Card.css";
+
+import noImage from "../../assets/no-image.svg";
+
+const Card = ({
+  className,
+  createdAt,
+  description,
+  id,
+  imgUrl,
+  title,
+  url,
+}) => {
+  function formatDate(value) {
+    const date = new Date(value);
+    return `${date.getFullYear()}. ${date.getMonth()}. ${date.getDate()}`;
+  }
+
+  return (
+    <div className="card">
+      <div class="card-img-container">
+        <img className="card-img" src={imgUrl || noImage} alt="카드" />
+      </div>
+      <div className="card-info">
+        <div>
+          <p className="card-time-difference">10 minutes ago</p>
+        </div>
+        <p className="card-description">{description}</p>
+        <p className="card-created-at">{formatDate(createdAt)}</p>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
