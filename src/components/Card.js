fac60231116389a5294c3a74bb33bd04888389ed
HellenHong <hongjw030@naver.com>
2023-10-21 23:29:40 +0900

    feat(card): Create time functions

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,53 @@
 import "../styles/card.css";
 import noImage from "../assets/no-img-card.svg";
+
+function formatDate(value) {
+  const date = new Date(value);
+  return `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
+}
+
+function calcDate(value) {
+  const now = new Date(); // 현재 날짜 및 시간
+  const createdDate = new Date(value);
+
+  let minutes = (now - createdDate) / 1000 / 60;
+  if (minutes < 2) {
+    return `1 minute ago`;
+  } else if (minutes < 60) {
+    return `${Math.floor(minutes)} minutes ago`;
+  }
+
+  let hours = minutes / 60;
+  if (hours < 2) {
+    return `1 hour ago`;
+  } else if (hours < 24) {
+    return `${Math.floor(hours)} hours ago`;
+  }
+
+  let days = hours / 24;
+  if (days < 2) {
+    return `1 day ago`;
+  } else if (days < 31) {
+    return `${Math.floor(days)} days ago`;
+  }
+
+  let months = days / 31;
+  if (months < 2) {
+    return `1 month ago`;
+  } else if (months < 12) {
+    return `${Math.floor(months)} months ago`;
+  }
+
+  let years = months / 12;
+  if (years < 2) {
+    return `1 year ago`;
+  } else {
+    return `${Math.floor(years)} years ago`;
+  }
+}
 function Card(card) {
   const { createdAt, url, title, description, imageSource } = card.card;
+  const str = calcDate(createdAt);
   return (
     <a href={url} className="card-link">
       <div className="card-img-section">
@@ -12,9 +58,9 @@ function Card(card) {
         />
       </div>
       <div className="card-text-section">
-        <p className="time-stamp">{createdAt}</p>
+        <p className="time-stamp">{str}</p>
         <p className="introduce-text">{description}</p>
-        <p className="created-date">{createdAt}</p>
+        <p className="created-date">{formatDate(createdAt)}</p>
       </div>
     </a>
   );
