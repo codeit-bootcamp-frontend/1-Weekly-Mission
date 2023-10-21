d2272dbdbb9b192b33f067073e0b7ee6b498a774
HellenHong <hongjw030@naver.com>
2023-10-21 18:00:22 +0900

    refactor(nav): Add user section in nav

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
+import { useEffect, useState } from "react";
 import Nav from "./components/Nav";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
+import { getUser } from "./api";
 
 function App() {
+  const [userName, setUserName] = useState("");
+  const [userEmail, setUserEmail] = useState("");
+  const [userProfileImage, setUserProfileImage] = useState("");
+  const loadUser = async () => {
+    const { name, email, profileImageSource } = await getUser();
+    setUserName(name);
+    setUserEmail(email);
+    setUserProfileImage(profileImageSource);
+  };
+
+  useEffect(() => {
+    loadUser();
+  }, []);
+
   return (
     <>
-      <Nav />
-      <Header />
+      <Nav userEmail={userEmail} userProfile={userProfileImage} />
+      <Header userName={userName} userProfile={userProfileImage} />
       <div></div>
       <Footer />
     </>
