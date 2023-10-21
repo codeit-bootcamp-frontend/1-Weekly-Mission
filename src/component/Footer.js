a3e7fd903e1dc3784c1fc2319d6415880a6b27b2
손지은 <wiseag@naver.com>
2023-10-21 13:10:07 +0900

    feat : Create Footer, Icons

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -1,7 +1,49 @@
+import FacebookIcon from "../icon/FacebookIcon";
+import InstagramIcon from "../icon/InstagramIcon";
+import TwitterIcon from "../icon/TwitterIcon";
+import YoutubeIcon from "../icon/YoutubeIcon";
 function Footer() {
   return (
     <div className="footer">
       <div className="since-codeit">©codeit - 2023</div>
+      <div class="extra_sites">
+        <a class="extra_sites__link" href="/html/privacy.html">
+          Privacy Policy
+        </a>
+        <a class="extra_sites__link" href="/html/faq.html">
+          FAQ
+        </a>
+      </div>
+      <div class="icons">
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <FacebookIcon />
+        </a>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <TwitterIcon />
+        </a>
+        <a
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <YoutubeIcon />
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InstagramIcon />
+        </a>
+      </div>
     </div>
   );
 }
