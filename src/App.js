c8a6c785dac872318fe8ca80eb2d45a2834ec4cc
HellenHong <hongjw030@naver.com>
2023-10-21 12:01:05 +0900

    feat(Header): Create Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Nav from "./components/Nav";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
       <Nav />
+      <Header />
       <div></div>
       <Footer />
     </>
---
+++ src/components/Header.js
@@ -0,0 +1,17 @@
+import "../styles/header.css";
+import "../styles/reset.css";
+
+function Header() {
+  return (
+    <header>
+      <div>
+        가나다
+        {/* 프사 */}
+        {/* 이름 */}
+        {/* 즐겨찾기 */}
+      </div>
+    </header>
+  );
+}
+
+export default Header;
