cd5d06d6eecf8724d5ba2f02bb510dcaae104de1
sunny-son <thstkd92@gmail.com>
2023-10-21 18:29:20 +0900

    style : 폴더 프로필 스타일 추가

--- src/components/Folder.jsx
+++ src/components/Folder.jsx
@@ -5,9 +5,11 @@ function Folder({ folderProfile }) {
 
   return (
     <>
-      <div>
-        <img className='folder-avatar' src={avatar} alt='폴더 주인 아바타' />
-        <h3 className='folder-ownername'>{ownerName}</h3>
+      <div className='folder-profile-container'>
+        <div className='folder-profile'>
+          <img className='folder-avatar' src={avatar} alt='폴더 주인 아바타' />
+          <h3 className='folder-owner-name'>{ownerName}</h3>
+        </div>
         <h2 className='folder-name'>{folderName}</h2>
       </div>
     </>
