5071c4d18be51360817c088b8a709e0a3eece90d
aowjarkwk <bulls1rose@naver.com>
2023-10-21 17:07:14 +0900

    Refactor: day.js 라이브러리 추가

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,6 +2,51 @@ import React, { useState } from "react";
 import noImg from "../images/no img.png";
 import starIcon from "../images/star.svg";
 import kebab from "../images/kebab.png";
+import dayjs from "dayjs";
+
+import timezone from "dayjs/plugin/timezone";
+import relativeTime from "dayjs/plugin/relativeTime";
+import utc from "dayjs/plugin/utc";
+import updateLocale from "dayjs/plugin/updateLocale";
+const timeThresholds = [
+	{ l: "s", r: 1 },
+	{ l: "m", r: 1 },
+	{ l: "mm", r: 59, d: "minute" },
+	{ l: "h", r: 1 },
+	{ l: "hh", r: 23, d: "hour" },
+	{ l: "d", r: 1 },
+	{ l: "dd", r: 30, d: "day" },
+	{ l: "M", r: 1 },
+	{ l: "MM", r: 11, d: "month" },
+	{ l: "y", r: 1 },
+	{ l: "yy", d: "year" },
+];
+
+const relativeTimeLocale = {
+	future: "in %s",
+	past: "%s ago",
+	s: "1 minute",
+	m: "1 minute",
+	mm: "%d minutes",
+	h: "1 hour",
+	hh: "%d hours",
+	d: "1 day",
+	dd: "%d days",
+	M: "1 month",
+	MM: "%d months",
+	y: "1 year",
+	yy: "%d years",
+};
+
+const extendDayjs = () => {
+	dayjs.extend(relativeTime, { thresholds: timeThresholds });
+	dayjs.extend(updateLocale);
+	dayjs.extend(utc);
+	dayjs.extend(timezone);
+	dayjs.updateLocale("en", { relativeTime: relativeTimeLocale });
+};
+
+extendDayjs();
 
 const Card = ({ link }) => {
 	const [isHover, setIsHover] = useState(false);
@@ -12,36 +57,10 @@ const Card = ({ link }) => {
 
 	const { createdAt, url, title, imageSource = noImg } = link;
 
-	const createdDateObject = (timeStamp) => {
-		const date = new Date(timeStamp);
-		const createDate = `${date.getFullYear()}. ${date.getMonth() + 1}. ${date.getDate()}`;
-		return {
-			createDate,
-			time: date.getTime(),
-		};
-	};
-
-	const timeInfo = createdDateObject(createdAt);
-	const diffMinutes = Math.floor((new Date().getTime() - timeInfo.time) / 1000 / 60);
-
-	const divideAndFloor = (value, divide) => Math.floor(value / divide);
-
-	const getTime = (minute) => {
-		if (minute < 2) return "1 minute ago";
-		if (minute < 60) return `${minute} minutes ago`;
-
-		const hour = divideAndFloor(minute, 60);
-		if (hour < 24) return hour === 1 ? "1 hour ago" : `${hour} hours ago`;
-
-		const day = divideAndFloor(hour, 24);
-		if (day < 31) return day === 1 ? "1 day ago" : `${day} days ago`;
-
-		const month = divideAndFloor(day, 30);
-		if (month < 12) return month === 1 ? "1 month ago" : `${month} months ago`;
-
-		const year = divideAndFloor(month, 12);
-		return year === 1 ? "1 year ago" : `${year} years ago`;
-	};
+	const now = dayjs().utc(true);
+	const utcDate = dayjs.utc(createdAt);
+	const fromNow = utcDate.from(now);
+	const formattedDate = dayjs(utcDate).format("YYYY. M. DD");
 
 	return (
 		<a
@@ -58,11 +77,11 @@ const Card = ({ link }) => {
 			</div>
 			<div className={cardTextClass}>
 				<div className="card__time">
-					<p className="card__time-diff">{getTime(diffMinutes)}</p>
+					<p className="card__time-diff">{fromNow}</p>
 					<img className="kebab" src={kebab} alt="more" />
 				</div>
 				<p className="card__title">{title}</p>
-				<p className="card__created-date">{timeInfo.createDate}</p>
+				<p className="card__created-date">{formattedDate}</p>
 			</div>
 		</a>
 	);
