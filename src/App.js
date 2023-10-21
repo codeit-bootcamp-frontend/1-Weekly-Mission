511b193ccd1ca9c4f2fac165b637bf8a073b6e71
HellenHong <hongjw030@naver.com>
2023-10-21 11:28:11 +0900

    feat(App.js): Add frame

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div></div>
+      {/* <Footer /> */}
+    </>
   );
 }
 
