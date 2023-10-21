0a12771dddc272e048b4b9da99fd42797af1f75c
wecaners <wecaners@naver.com>
2023-10-22 01:25:08 +0900

    Feat: add calcCreateTime function

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import './card.css';
+import calcCreateTime from '../../utils/calcCreateTime';
 
 function Card({ linkInfo }) {
+  const createdAt = new Date(linkInfo?.createdAt);
+  const formattedTime = calcCreateTime(createdAt);
+
   return (
     <div className="card">
       <a
-        href={`/link/detail/${linkInfo.url}`}
+        href={`${linkInfo.url}`}
         className="card-link"
         target="_blank"
         rel="noreferrer"
       >
         <div className="card-img-section">
-          <img src={linkInfo.imageSource} alt="cat" className="sample-img" />
+          <img src={linkInfo?.imageSource} alt="cat" className="sample-img" />
         </div>
         <div className="card-text-section">
-          <p className="time-stamp">몇 분 전...</p>
+          <p className="time-stamp">{formattedTime}</p>
           <p className="introduce-text">
             Lorem ipsum dolor sit amet consectetur. Metus amet habitant nunc
             consequat. Tldkd
