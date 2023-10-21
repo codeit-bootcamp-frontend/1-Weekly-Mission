abe6f4ab4edea0663d90fc2ca81370aef7154f5d
heewon <hw7666@sogang.ac.kr>
2023-10-22 04:50:10 +0900

    ✨ Feat(shared) : Profile 컴포넌트 모바일 반응형 적용
    - 모바일 버전일 때는 프로필 사진만 나타나도록 반응형 적용

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,10 +1,24 @@
+import { useState, useEffect } from "react";
+
 function Profile({user}){
     const {email, profileImageSource} = user;
+    const [resize, setResize] = useState();
+
+    const handleResize = () => {
+      setResize(window.innerWidth);
+    };
+  
+    useEffect(() => {
+      window.addEventListener("resize", handleResize);
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }, []);
 
     return (
         <div className = "profile">
             <img className = "profile_img" src = {profileImageSource} alt = "프로필 사진"/>
-            <div className = "profile_email">{email}</div>
+            {(resize < 768) ? null : <div className = "profile_email">{email}</div>}
         </div>
     );
 }
