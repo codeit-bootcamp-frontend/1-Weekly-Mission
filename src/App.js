fb3b72dbbbb466a0c777460c00a8859abe6c2819
sunny-son <thstkd92@gmail.com>
2023-10-21 15:47:36 +0900

    feat : 유저 프로필 기능 추가

--- src/App.js
+++ src/App.js
@@ -2,12 +2,30 @@ import './css/reset.css';
 import './css/App.css';
 import CardList from './components/CardList';
 import { useCallback, useEffect, useState } from 'react';
-import { getCards } from './api/api';
+import { getCards, getUserProfile } from './api/api';
 import useAsync from './hooks/useAsync';
+import UserProfile from './components/UserProfile';
 
 function App() {
+  const [userProfile, setUserProfile] = useState(null);
   const [cards, setCards] = useState([]);
-  const [isLoading, loadingError, getCardsAsync] = useAsync(getCards);
+  const [isLoadingUserProfile, userProfileLoadingError, getUserProfileAsync] = useAsync(getUserProfile);
+  const [isLoadingCards, cardsLoadingError, getCardsAsync] = useAsync(getCards);
+
+  const handleUserProfile = useCallback(
+    async () => {
+      const result = await getUserProfileAsync();
+      if (!result) {
+        return;
+      }
+
+      setUserProfile(result);
+    }, [getUserProfileAsync],
+  );
+
+  useEffect(() => {
+    handleUserProfile();
+  }, [handleUserProfile]);
 
   const handleLoad = useCallback(
     async () => {
@@ -30,7 +48,7 @@ function App() {
       <nav>
         <div className='gnb'>
           <img className='logo' src='/assets/image/logo.svg' alt='홈으로 연결된 Linkbrary 로고' />
-          <div>로그인</div>
+          <UserProfile userProfile={userProfile}/>
         </div>
       </nav>
       <header>
@@ -48,7 +66,7 @@ function App() {
           />
         </form>
         <CardList items={cards} />
-        {loadingError?.message && <span>{loadingError.message}</span>}
+        {cardsLoadingError?.message && <span>{cardsLoadingError.message}</span>}
       </main>
     </div>
   );
