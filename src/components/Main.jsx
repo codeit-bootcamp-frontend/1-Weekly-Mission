a97d0bbbea35c63dc7719cfb11ba08a4a4c54e69
asusia <asusia@naver.com>
2023-10-21 19:06:01 +0900

    feat: 기능 없는 검색바 구현

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,9 +1,11 @@
 import CardList from "./CardList";
 import './Main.css';
+import SearchBar from "./SearchBar";
 
 function Main() {
   return (
     <main>
+      <SearchBar />
       <CardList />
     </main>
   );
