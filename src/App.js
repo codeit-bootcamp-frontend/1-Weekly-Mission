0a3667f1e95317ce1ae1fec9000d3e5074f793f4
전수빈 <soobin08011812@gmail.com>
2023-10-22 14:28:04 +0900

    Remove: app.css, index.css, logo.svg 삭제

--- src/App.js
+++ src/App.js
@@ -1,6 +1,4 @@
-import "./App.css";
 import Folder from "./page/folder/Folder";
-import "./components/styles.css";
 import Header from "./components/common/Header";
 import Footer from "./components/common/Footer";
 
