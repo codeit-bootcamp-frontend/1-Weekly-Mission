52e06b243347349c91d2e84a9025d97656a6b893
BeMatthewsong <songww1997@naver.com>
2023-10-22 22:35:39 +0900

    feat: folder 컴포넌트 태그 초기 제작

---
+++ src/components/Folder.jsx
@@ -0,0 +1,17 @@
+import '../css/folder.css';
+
+function Folder() {
+  return (
+    <>
+      <div className="folder-info-wrapper">
+        <div className="folder-info">
+          <img className="avatar" alt="아바타" />
+          <h3 className="owner-name">Name</h3>
+        </div>
+        <h2 className="folder-name">folder-name</h2>
+      </div>
+    </>
+  );
+}
+
+export default Folder;
