471d9e5c55b041280be2a604a9d06f0cf77ee280
kanglocal <develocal25@gmail.com>
2023-10-22 18:17:56 +0900

    refactor : assets 폴더 생성 및 이미지들 추가

---
+++ src/components/landing/Images.js
@@ -0,0 +1,23 @@
+import LogoImg from '../../assets/logo.svg';
+import TitleImg from '../../assets/img_title.png';
+import SaveImg from '../../assets/img_save.png';
+import ManageImg from '../../assets/img_manage.png';
+import ShareImg from '../../assets/img_share.png';
+import SearchImg from '../../assets/img_search.png';
+import SnsIconF from '../../assets/snsIcon_f.svg';
+import SnsIconT from '../../assets/snsIcon_t.svg';
+import SnsIconY from '../../assets/snsIcon_y.svg';
+import SnsIconI from '../../assets/snsIcon_i.svg';
+
+export {
+  LogoImg,
+  TitleImg,
+  SaveImg,
+  ManageImg,
+  ShareImg,
+  SearchImg,
+  SnsIconF,
+  SnsIconT,
+  SnsIconY,
+  SnsIconI,
+};
