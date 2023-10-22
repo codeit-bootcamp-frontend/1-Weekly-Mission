2bdab2f087cb0da26c62a616e8004242388e0363
wecaners <wecaners@naver.com>
2023-10-22 17:11:01 +0900

    Refactor: modify search bar position

--- src/pages/share/SharedPage.js
+++ src/pages/share/SharedPage.js
@@ -7,14 +7,16 @@ export default function SharedPage({ links }) {
   return (
     <main className="main">
       <section className="content-section">
-        <SearchBar />
-        <div className="links-container">
-          {links &&
-            links.map((item) => (
-              <div key={item.id}>
-                <Card linkInfo={item} />
-              </div>
-            ))}
+        <div className="container">
+          <SearchBar />
+          <div className="links-container">
+            {links &&
+              links.map((item) => (
+                <div key={item.id}>
+                  <Card linkInfo={item} />
+                </div>
+              ))}
+          </div>
         </div>
       </section>
     </main>
