9162efa7d0371d8f0446641e89d68d3d7312462d
ParkSohyunee <sohyunee016@gmail.com>
2023-10-22 12:54:28 +0900

    feat: 날짜 형식 함수 구현

--- src/components/units/CardItem.js
+++ src/components/units/CardItem.js
@@ -1,8 +1,11 @@
 import "components/units/CardItem.css";
 import logo from "assets/logo.svg";
+import { getCreatedDate, getDiffTime } from "common/utils/getDate";
 
 export default function CardItem({ link }) {
   const { description, imageSource, createdAt, url } = link;
+  const { yyyy, mm, dd } = getCreatedDate(createdAt);
+
   return (
     <a href={url} className="item" target="_blank" rel="noreferrer">
       <div className="image-wrapper">
@@ -12,9 +15,11 @@ export default function CardItem({ link }) {
         />
       </div>
       <div className="info">
-        <p className="timeDiffInfo">10 minutes ago</p>
+        <p className="timeDiffInfo">{getDiffTime(createdAt)}</p>
         <p className="description">{description}</p>
-        <p className="createdAt">{createdAt}</p>
+        <p className="createdAt">
+          {yyyy}. {mm}. {dd}
+        </p>
       </div>
     </a>
   );
