c470d0ce328865db1704be612910520bd81a379b
손지은 <wiseag@naver.com>
2023-10-22 11:43:27 +0900

    "refactor : footer social link 컴포넌트 화
    멘토님께서 진호님 PR에 남긴 '반복되는 HTML처리하기'를 보고 리팩토링 했습니다.

--- src/component/Footer.js
+++ src/component/Footer.js
@@ -3,45 +3,43 @@ import youtubeImg from "../assets/img/youtube.svg";
 import instagramImg from "../assets/img/instagram.svg";
 import twitterImg from "../assets/img/twitter.svg";
 import facebookImg from "../assets/img/facebook.svg";
+import SocialLink from "./SocialLink";
 
 function Footer() {
+  const SOCIAL_LINKS = [
+    {
+      href: "https://www.facebook.com/",
+      iconUrl: facebookImg,
+      description: "facebook icon",
+    },
+    {
+      href: "https://twitter.com/",
+      iconUrl: twitterImg,
+      description: "twiiter icon",
+    },
+    {
+      href: "https://www.youtube.com/",
+      iconUrl: youtubeImg,
+      description: "youtube icon",
+    },
+    {
+      href: "https://www.instagram.com/",
+      iconUrl: instagramImg,
+      description: "instagram icon",
+    },
+  ];
+  const socialLinks = SOCIAL_LINKS.map((link) => {
+    return <SocialLink link={{ ...link }} />;
+  });
+
   return (
-    <div className={css.footer}>
+    <div className={css.root}>
       <div className={css.since}>©codeit - 2023</div>
       <div className={css.extraSites}>
         <a href="/html/privacy.html">Privacy Policy</a>
         <a href="/html/faq.html">FAQ</a>
       </div>
-      <div className={css.icons}>
-        <a
-          href="https://www.facebook.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={facebookImg} />
-        </a>
-        <a
-          href="https://twitter.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={twitterImg} />
-        </a>
-        <a
-          href="https://www.youtube.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={youtubeImg} />
-        </a>
-        <a
-          href="https://www.instagram.com/"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <img src={instagramImg} />
-        </a>
-      </div>
+      <div className={css.icons}>{socialLinks}</div>
     </div>
   );
 }
