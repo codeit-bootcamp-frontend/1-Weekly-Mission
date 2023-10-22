126b178ebc8d93e28140f046fff30d1c715c24a0
BeMatthewsong <songww1997@naver.com>
2023-10-22 22:19:07 +0900

    style: 스타 이미지, 케밥 아이콘 경로 변경

--- src/hooks/useAsync.jsx
+++ src/hooks/useAsync.jsx
@@ -4,17 +4,22 @@ function useAsync(asyncFunction) {
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(null);
 
-  const wrappedFunction = useCallback(async (...args) => {
-    setPending(true);
-    setError(null);
-    try {
-      return await asyncFunction(...args);
-    } catch (error) {
-      setError(error);
-    } finally {
-      setPending(false);
-    }
-  }, [asyncFunction]);
+  const wrappedFunction = useCallback(
+    async (...args) => {
+      setPending(true);
+      setError(null);
+      try {
+        return await asyncFunction(...args);
+      } catch (error) {
+        setError(error);
+      } finally {
+        setPending(false);
+      }
+    },
+    [asyncFunction],
+  );
 
   return [pending, error, wrappedFunction];
 }
+
+export default useAsync;
