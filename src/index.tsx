a6ea9d02f2bfe9e7443d4245f3052afe05619f64
hanseulhee <3021062@gmail.com>
2023-10-22 15:42:45 +0900

    feat: queryClient 공통 옵션

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,10 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "libs/apis/queryClient";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Router from "router/Router";
 import "styles/index.css";
 import "styles/variables.css";
-
-const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
