87d06834b72a6ab09262ef7d04bf9ca5e6321366
hanseulhee <3021062@gmail.com>
2023-10-22 15:45:16 +0900

    feat: shared page 개발 및 스타일 적용

---
+++ src/pages/Shared.tsx
@@ -0,0 +1,45 @@
+import { useQuery } from "@tanstack/react-query";
+import FolderCardList from "components/card/FolderCardList";
+import SearchBar from "components/search/SearchBar";
+import getUserFolder from "libs/apis/getUserFolder";
+import styles from "styles/modules/shared.module.css";
+import manageStatus from "utils/manageStatus";
+
+function Shared() {
+  const { isLoading, isError, data } = useQuery<Folder>({
+    queryKey: ["userInform"],
+    queryFn: getUserFolder
+  });
+
+  if (isLoading || isError) {
+    return manageStatus({ isLoading, isError });
+  }
+
+  return (
+    <div>
+      <header>
+        <div className={styles.gapWrapper}>
+          <div className={styles.profileImageWrapper}>
+            <img
+              src={data?.folder.owner.profileImageSource}
+              alt={data?.folder.owner.name}
+              className={styles.profileImage}
+            />
+          </div>
+          <span className={styles.name}>@{data?.folder.owner.name}</span>
+        </div>
+        <h1 className={styles.bookmark}>⭐️ 즐겨찾기</h1>
+      </header>
+      <div className={styles.itemWrapper}>
+        <div className={styles.itemInWrapper}>
+          <SearchBar />
+          <div className={styles.cardListWrapper}>
+            <FolderCardList />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Shared;
