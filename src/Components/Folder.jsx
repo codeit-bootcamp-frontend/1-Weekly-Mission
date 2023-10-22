7f0ae931f716c61e1ff759ebc5e63ca43c902e9a
Geon <geonulim@gmail.com>
2023-10-22 14:20:51 +0900

    feat: useAsync 구현

--- src/Components/Folder.jsx
+++ src/Components/Folder.jsx
@@ -1,15 +1,17 @@
 import { useCallback, useEffect, useState } from 'react';
 import { getFolder } from '../api/apiClient';
+import useAsync from '../hooks/useAsync';
 import Banner from './Banner';
 import SearchBar from './SearchBar';
 import CardList from './CardList';
 
 function Folder() {
+  const [isLoading, loadingError, getFolderAsync] = useAsync(getFolder);
   const [links, setLinks] = useState([]);
   const [folderInfo, setFolderInfo] = useState();
 
   const loadFolder = useCallback(async () => {
-    const { folder } = await getFolder();
+    const { folder } = await getFolderAsync();
     const { name, owner, links: linksData } = folder;
     const folderInfoData = { name, owner };
     setFolderInfo(folderInfoData);
