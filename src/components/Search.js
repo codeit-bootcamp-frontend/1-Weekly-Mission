f48d72af602a526e08a23e4611996b8d2fb743d9
NIX <lsc58461@naver.com>
2023-10-22 20:25:26 +0900

    feat: SearchBar 기능 구현

---
+++ src/components/Search.js
@@ -0,0 +1,10 @@
+import "../styles/Search.css";
+import SearchBar from "./SearchBar";
+
+const Search = () => (
+  <div className="Search">
+    <SearchBar />
+  </div>
+);
+
+export default Search;
