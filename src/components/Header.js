c7672a8d9b5382668dbe748ff60ebdb1754ff707
Gyuho Lee <j980303@naver.com>
2023-10-22 18:30:51 +0900

    refactor(mentor): Footer 추가

--- src/components/Header.js
+++ src/components/Header.js
@@ -6,24 +6,26 @@ function header() {
 
   return (
     <header>
-      <div className="header__logo">
-        <a href="/">
-          <img src={logoImage} alt="logo" />
-        </a>
-      </div>
-      <div className="header__profile">
-        {!account ? (
-          <button>로그인</button>
-        ) : (
-          <>
-            <img
-              className="header__profile--logo"
-              src={profileImageSource}
-              alt={name}
-            />
-            <span className="header__profile--email">{email}</span>
-          </>
-        )}
+      <div className="header__container">
+        <div className="header__logo">
+          <a href="/">
+            <img src={logoImage} alt="LinkBrary logo" />
+          </a>
+        </div>
+        <div className="header__profile">
+          {!account ? (
+            <button>로그인</button>
+          ) : (
+            <>
+              <img
+                className="header__profile--logo"
+                src={profileImageSource}
+                alt={name}
+              />
+              <span className="header__profile--email">{email}</span>
+            </>
+          )}
+        </div>
       </div>
     </header>
   );
