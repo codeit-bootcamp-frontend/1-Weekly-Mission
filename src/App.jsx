57bf20e44815cf07524a1e6df2d10702c8555224
Geon <geonulim@gmail.com>
2023-10-24 17:18:21 +0900

    refactor: applied time ago library

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,13 @@
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
+
 import Navigator from './Components/Navigator';
 import Folder from './Components/Folder';
 import Footer from './Components/Footer';
 
 import './scss/main.scss';
+
+TimeAgo.addDefaultLocale(en);
 
 function App() {
   return (
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,12 @@
-import { calculateTimeAgo, getDate } from '../functions/calculateDate';
+import React from 'react';
+import ReactTimeAgo from 'react-time-ago';
+
 import DEFAULT_IMAGE from '../assets/images/default-link-img.svg';
 
 function Card({ data }) {
   const { url, title, description, createdAt, imageSource } = data;
 
-  const timeAgo = calculateTimeAgo(createdAt);
-  const createdDate = getDate(createdAt);
+  const createdDate = new Date(createdAt);
 
   const reduceText = (text, length) => {
     if (text.length > length) {
@@ -25,10 +26,12 @@ function Card({ data }) {
         />
       </div>
       <div className='card__text'>
-        <p className='card__time-passed'>{timeAgo}</p>
+        <p className='card__time-passed'>
+          <ReactTimeAgo date={createdAt} locale='en-US' />
+        </p>
         <p className='card__title'>{reduceText(title, 70)}</p>
         <p className='card__description'>{reduceText(description, 100)}</p>
-        <p className='card__date'>{createdDate}</p>
+        <p className='card__date'>{createdDate.toLocaleDateString()}</p>
       </div>
     </a>
   );
