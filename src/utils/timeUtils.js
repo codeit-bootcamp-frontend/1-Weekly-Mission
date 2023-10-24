fd8ffc823d35e7d44935fc307d28761a2d2b39b3
olseul <seul9085@naver.com>
2023-10-24 15:31:43 +0900

    refactor: timeDifference 함수를 utils 폴더로 분리

---
+++ src/utils/timeUtils.js
@@ -0,0 +1,30 @@
+const timeDifference = (current, previous) => {
+  const msPerMinute = 60 * 1000;
+  const msPerHour = msPerMinute * 60;
+  const msPerDay = msPerHour * 24;
+  const msPerMonth = msPerDay * 30;
+  const msPerYear = msPerDay * 365;
+
+  const elapsed = current - previous;
+
+  if (elapsed < msPerMinute) {
+    return "1 minute ago";
+  } else if (elapsed < msPerHour) {
+    return Math.round(elapsed / msPerMinute) + " minutes ago";
+  } else if (elapsed < msPerDay) {
+    return Math.round(elapsed / msPerHour) + " hours ago";
+  } else if (elapsed < msPerMonth) {
+    return Math.round(elapsed / msPerDay) + " days ago";
+  } else if (elapsed < msPerYear) {
+    return Math.round(elapsed / msPerMonth) + " months ago";
+  } else {
+    const years = Math.floor(elapsed / msPerYear);
+    if (years === 1) {
+      return "1 year ago";
+    } else {
+      return years + " years ago";
+    }
+  }
+};
+
+export { timeDifference };
