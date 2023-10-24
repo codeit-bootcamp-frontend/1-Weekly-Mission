b3fcf23bcb1f8aaca705508b160ffcff8fd12a09
olseul <seul9085@naver.com>
2023-10-24 16:08:58 +0900

    refactor: timeDifference 함수의 결과를 변수에 할당하여 JSX 코드 정리

--- src/Card.js
+++ src/Card.js
@@ -5,6 +5,7 @@ import { timeDifference } from "./utils/timeUtils";
 const Card = ({ link }) => {
   const currentTime = new Date().getTime();
   const createdAt = new Date(link.createdAt).getTime();
+  const timeDiffText = timeDifference(currentTime, createdAt);
 
   return (
     <div className="card" onClick={() => window.open(link.url, "_blank")}>
@@ -14,7 +15,7 @@ const Card = ({ link }) => {
         <div className="no-image">이미지가 없습니다.</div>
       )}
       <div className="card-text">
-        <span className="date">{timeDifference(currentTime, createdAt)}</span>
+        <span className="date">{timeDiffText}</span>
         <h3 className="title">{link.title}</h3>
         <p className="description">{link.description}</p>
       </div>
