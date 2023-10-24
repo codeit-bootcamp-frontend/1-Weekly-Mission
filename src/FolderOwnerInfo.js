d839e7fafd8fd763b5dc382e76858b799c568850
olseul <seul9085@naver.com>
2023-10-24 16:34:29 +0900

    refactor: 폴더 정보 상태 관리를 객체로 통합

--- src/FolderOwnerInfo.js
+++ src/FolderOwnerInfo.js
@@ -2,26 +2,34 @@ import React, { useState, useEffect } from "react";
 import "./FolderOwnerInfo.css";
 
 const FolderOwnerInfo = () => {
-  const [ownerName, setOwnerName] = useState("");
-  const [folderName, setFolderName] = useState("");
-  const [ownerImage, setOwnerImage] = useState("");
+  const [folderInfo, setFolderInfo] = useState({
+    ownerName: "",
+    folderName: "",
+    ownerImage: "",
+  });
 
   useEffect(() => {
     fetch("https://bootcamp-api.codeit.kr/api/sample/folder")
       .then((response) => response.json())
       .then((data) => {
-        setFolderName(data.folder.name);
-        setOwnerName(data.folder.owner.name);
-        setOwnerImage(data.folder.owner.profileImageSource);
+        setFolderInfo({
+          ownerName: data.folder.owner.name,
+          folderName: data.folder.name,
+          ownerImage: data.folder.owner.profileImageSource,
+        });
       });
   }, []);
 
   return (
     <div className="owner-info">
-      <img src={ownerImage} alt="Folder Owner" className="owner-image" />
-      <div className="owner-name">@{ownerName}</div>
+      <img
+        src={folderInfo.ownerImage}
+        alt="Folder Owner"
+        className="owner-image"
+      />
+      <div className="owner-name">@{folderInfo.ownerName}</div>
       <div className="owner-favorites">
-        <span>{folderName}</span>
+        <span>{folderInfo.folderName}</span>
       </div>
     </div>
   );
