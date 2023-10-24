924ac9f0e358642eb032375c61c8c23d09817708
oauch <lgm9515@naver.com>
2023-10-24 10:29:12 +0900

    Refactor(mentor): 반복되는 소셜링크 배열로 만들기

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,6 +6,13 @@ import Youtube from '../assets/icon/youtube.svg';
 import './footer.css';
 
 function Footer() {
+  const socialList = [
+    { id: 0, href: 'https://www.facebook.com/', imgSrc: Facebook, alt: '페이스북 아이콘' },
+    { id: 1, href: 'https://twitter.com/', imgSrc: Twitter, alt: '트위터 아이콘' },
+    { id: 2, href: 'https://www.youtube.com/', imgSrc: Youtube, alt: '유튜브 아이콘' },
+    { id: 3, href: 'https://www.instagram.com/', imgSrc: Instagram, alt: '인스타그램 아이콘' },
+  ];
+
   return (
     <footer>
       <div className="footer-background">
@@ -16,18 +23,11 @@ function Footer() {
             <a href="pages/faq.html">FAQ</a>
           </div>
           <div className="social">
-            <a href="https://www.facebook.com/" target="noopener noreferrer">
-              <img src={Facebook} alt="페이스북 아이콘" />
-            </a>
-            <a href="https://twitter.com/" target="noopener noreferrer">
-              <img src={Twitter} alt="트위터 아이콘" />
-            </a>
-            <a href="https://www.youtube.com/" target="noopener noreferrer">
-              <img src={Youtube} alt="유튜브 아이콘" />
-            </a>
-            <a href="https://www.instagram.com/" target="noopener noreferrer">
-              <img src={Instagram} alt="인스타그램 아이콘" />
-            </a>
+            {socialList.map((social) => (
+              <a key={social.id} href={social.href} target="noopener noreferrer">
+                <img src={social.imgSrc} alt={social.alt} />
+              </a>
+            ))}
           </div>
         </div>
       </div>
