043c0109b0fbd10d1bea588274c9930fd79372d6
olseul <seul9085@naver.com>
2023-10-24 16:03:31 +0900

    refactor: timeDifference 함수 early return 기법 적용하여 가독성 향상

--- src/utils/timeUtils.js
+++ src/utils/timeUtils.js
@@ -9,21 +9,25 @@ const timeDifference = (current, previous) => {
 
   if (elapsed < msPerMinute) {
     return "1 minute ago";
-  } else if (elapsed < msPerHour) {
+  }
+  if (elapsed < msPerHour) {
     return Math.round(elapsed / msPerMinute) + " minutes ago";
-  } else if (elapsed < msPerDay) {
+  }
+  if (elapsed < msPerDay) {
     return Math.round(elapsed / msPerHour) + " hours ago";
-  } else if (elapsed < msPerMonth) {
+  }
+  if (elapsed < msPerMonth) {
     return Math.round(elapsed / msPerDay) + " days ago";
-  } else if (elapsed < msPerYear) {
+  }
+  if (elapsed < msPerYear) {
     return Math.round(elapsed / msPerMonth) + " months ago";
+  }
+
+  const years = Math.floor(elapsed / msPerYear);
+  if (years === 1) {
+    return "1 year ago";
   } else {
-    const years = Math.floor(elapsed / msPerYear);
-    if (years === 1) {
-      return "1 year ago";
-    } else {
-      return years + " years ago";
-    }
+    return years + " years ago";
   }
 };
 
