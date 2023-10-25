ec719275cbbd96c236375ce3863731ae30977940
NIX <lsc58461@naver.com>
2023-10-25 16:50:26 +0900

    refactor(api): 에러 핸들링 로직 제거

--- src/api/getFolder.js
+++ src/api/getFolder.js
@@ -1,8 +1,5 @@
 const getFolder = async () => {
   const response = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
-  if (!response.ok) {
-    throw new Error("폴더 데이터를 불러오는데 실패했습니다.");
-  }
   const body = await response.json();
   return body;
 };
--- src/api/getUser.js
+++ src/api/getUser.js
@@ -1,8 +1,5 @@
 const getUser = async () => {
   const response = await fetch("https://bootcamp-api.codeit.kr/api/sample/user");
-  if (!response.ok) {
-    throw new Error("유저 데이터를 불러오는데 실패했습니다.");
-  }
   const body = await response.json();
   return body;
 };
