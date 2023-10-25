9a6ef776c4d0a82e441b780985b2c142ef24a838
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-25 19:07:43 +0900

    skeleton컴퍼넌트

---
+++ src/components/skeleton/ItemSkeleton.js
@@ -0,0 +1,14 @@
+import React, { useContext } from "react";
+import SkeletonStructure from "./SkeletonStructure";
+import LocaleContext from "../../contexts/LocaleContext";
+export default function ItemSkeleton({ type }) {
+  // const locale = useContext(LocaleContext);
+  // console.log(locale);
+  return (
+    <div className="skeleton-wrapper">
+      <SkeletonStructure type="image" />
+      <SkeletonStructure type="avatar" />
+      <SkeletonStructure type="title" />
+    </div>
+  );
+}
