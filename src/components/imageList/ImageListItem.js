ea7c37e26935e19997838b451cb5654c49e022b6
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-25 19:05:21 +0900

    styles: css모듈로변경

---
+++ src/components/imageList/ImageListItem.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Card from "../card/Card";
+import styles from "./ImageListItem.module.css";
+import { changeDateFormat, compareNowAndAfter } from "../../utils";
+
+export default function ImageListItem({ item }) {
+  const { createdAt, url, title, description, imageSource } = item;
+  const currentData = changeDateFormat(new Date());
+  const targetData = changeDateFormat(createdAt);
+  //const result = compareNowAndAfter(currentData, targetData);
+  const { year, month, day } = targetData;
+
+  const navgiateToPage = (url) => {
+    window.location.href = `${url}`;
+  };
+
+  return (
+    <>
+      <Card onClickFunc={() => navgiateToPage(url)}>
+        <img
+          className={styles.card__image}
+          src={imageSource}
+          alt={title}
+          // width="100%"
+          // height="250"
+        />
+        <p>시간계산함수만들어야함 </p>
+        <p>{description}</p>
+        <p>
+          {year}. {month}. {day}
+        </p>
+      </Card>
+    </>
+  );
+}
