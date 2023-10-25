09d38f2096a06f5f14f5f189a4cafd71b7e7014f
Seoyoung Min <seoyoungiyo@gmail.com>
2023-10-26 02:38:41 +0900

    feat: 반응형 디자인 적용

--- src/component/Gnb.js
+++ src/component/Gnb.js
@@ -2,10 +2,12 @@ import { useState, useEffect } from "react";
 import { getUser } from "../api";
 import logo from "../assets/img/logo.svg";
 import ProfileImg from "./ProfileImg";
+import CTA from "./CTA";
 import "../assets/css/Gnb.css";
 
 function Gnb() {
   const [userData, setUserData] = useState({});
+  const [isLogin, setIsLogin] = useState(false);
 
   async function getUserData() {
     try {
@@ -16,8 +18,10 @@ function Gnb() {
         name,
         profile: profileImageSource,
       });
+      setIsLogin(true);
     } catch (error) {
       console.log(error);
+      setIsLogin(false);
     }
   }
 
@@ -28,10 +32,14 @@ function Gnb() {
   return (
     <div className="headerContainer">
       <img className="logo" src={logo} alt="logo" />
-      <div className="profile">
-        <ProfileImg src={userData.profile} />
-        <span>{userData.email}</span>
-      </div>
+      {isLogin ? (
+        <div className="profile">
+          <ProfileImg src={userData.profile} />
+          <span>{userData.email}</span>
+        </div>
+      ) : (
+        <CTA href="">로그인</CTA>
+      )}
     </div>
   );
 }
