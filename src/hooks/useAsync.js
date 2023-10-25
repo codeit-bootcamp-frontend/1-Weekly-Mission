2ef0c7fc9dae16cf8de28752c4e8344f056a53d8
NIX <lsc58461@naver.com>
2023-10-25 19:16:25 +0900

    refactor(useAsync): JSDoc 제공

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -1,5 +1,12 @@
 import { useCallback, useState } from "react";
 
+/**
+ * 비동기 함수를 처리하고 실행 상태를 추적하기 위한 훅입니다.
+ *
+ * @param {Function} asyncFunction - 실행할 비동기 함수.
+ * @returns {[boolean, Function]} - 배열로 반환되며 첫 번째 요소는 실행 상태(pending)를 나타내는 불리언 값이고,
+ * 두 번째 요소는 비동기 함수를 래핑한 함수입니다.
+ */
 const useAsync = (asyncFunction) => {
   const [pending, setPending] = useState(false);
 
