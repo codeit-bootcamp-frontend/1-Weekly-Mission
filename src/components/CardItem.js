f8aa055eaf4e8b50ce952c3b28af4a5958539361
NIX <lsc58461@naver.com>
2023-10-25 17:08:59 +0900

    refactor(CardItem): 시간 비교 함수 추상화

--- src/components/CardItem.js
+++ src/components/CardItem.js
@@ -10,52 +10,54 @@ const formatDate = (value) => {
   return formattedCreatedAt;
 };
 
-const timeDiff = (createdAt) => {
-  const currentDate = new Date();
-  const createdDate = new Date(createdAt);
+function getTimeDiff(target, base = new Date()) {
+  const targetDate = new Date(target);
+  return base - targetDate;
+}
 
-  const timeDiffInMilliseconds = currentDate - createdDate;
-  const timeDiffInMinutes = Math.floor(timeDiffInMilliseconds / (1000 * 60));
-  const timeDiffInHours = Math.floor(timeDiffInMinutes / 60);
-  const timeDiffInDays = Math.floor(timeDiffInHours / 24);
-  const timeDiffInMonths = Math.floor(timeDiffInDays / 30);
-  const timeDiffInyears = Math.floor(timeDiffInMonths / 12);
+const prettyFormatTimeDiff = (diff) => {
+  const minutes = Math.floor(diff / (1000 * 60));
+  const hours = Math.floor(minutes / 60);
+  const days = Math.floor(hours / 24);
+  const months = Math.floor(days / 30);
+  const years = Math.floor(months / 12);
 
-  if (timeDiffInMinutes < 2) {
+  if (minutes < 2) {
     return "1 minute ago";
   }
-  if (timeDiffInMinutes <= 59) {
-    return `${timeDiffInMinutes} minutes ago`;
+  if (minutes <= 59) {
+    return `${minutes} minutes ago`;
   }
-  if (timeDiffInMinutes <= 60) {
+  if (minutes <= 60) {
     return "1 hour ago";
   }
-  if (timeDiffInHours <= 23) {
-    return `${timeDiffInHours} hours ago`;
+  if (hours <= 23) {
+    return `${hours} hours ago`;
   }
-  if (timeDiffInHours <= 24) {
+  if (hours <= 24) {
     return "1 day ago";
   }
-  if (timeDiffInDays <= 30) {
-    return `${timeDiffInDays} days ago`;
+  if (days <= 30) {
+    return `${days} days ago`;
   }
-  if (timeDiffInDays <= 31) {
+  if (days <= 31) {
     return `1 month ago`;
   }
-  if (timeDiffInMonths <= 11) {
-    return `${timeDiffInMonths} months ago`;
+  if (months <= 11) {
+    return `${months} months ago`;
   }
-  if (timeDiffInDays <= 31) {
+  if (months <= 12) {
     return `1 year ago`;
   }
-  return `${timeDiffInyears} years ago`;
+  return `${years} years ago`;
 };
 
 function CardItem({ item }) {
   const { createdAt, url, title, description, imageSource } = item;
 
   const formattedCreatedAt = formatDate(createdAt);
-  const timeDifference = timeDiff(createdAt);
+  const timeDiff = getTimeDiff(createdAt);
+  const formatTimeDiff = prettyFormatTimeDiff(timeDiff);
 
   return (
     <a className="CardItem" href={url} target="_blank" rel="noopener noreferrer">
@@ -63,7 +65,7 @@ function CardItem({ item }) {
         <img className="CardItem__img" src={imageSource === undefined ? logo : imageSource} alt={title} />
       </div>
       <div className="CardItem__container">
-        <p className="CardItem__container__time-diff">{timeDifference}</p>
+        <p className="CardItem__container__time-diff">{formatTimeDiff}</p>
         <p className="CardItem__container__description">
           {title}
           <br />
