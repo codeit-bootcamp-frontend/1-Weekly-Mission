c9a8f6bc4ee7df2133e9c5e3a68bbfafafca38e0
gooma519 <gooma519@gmail.com>
2023-10-26 12:29:45 +0900

    style(Card): 케밥, 별 이미지 추가

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,28 @@
 import '../styles/Card.css';
 import defaultImg from '../assets/images/no-image.png';
+import kebabImg from '../assets/images/kebab.svg';
+import starImg from '../assets/images/star.svg';
+import styled from 'styled-components';
 
 const moment = require('moment');
+
+const Kebab = styled.img`
+  position: relative;
+  z-index: 2;
+`;
+
+const Star = styled.img`
+  position: absolute;
+  right: 15px;
+  top: 15px;
+  z-index: 1;
+`;
+
+const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
 
 export default function Card({ value }) {
   const { imageSource = defaultImg } = value;
@@ -10,6 +31,11 @@ export default function Card({ value }) {
   };
   const ago = moment(value.createdAt).fromNow();
   const date = moment(value.createdAt).format('YYYY. MM. DD');
+
+  const handleKebab = (e) => {
+    e.preventDefault();
+    console.log('Kebab!');
+  };
   return (
     <a
       href={value.url}
@@ -17,12 +43,13 @@ export default function Card({ value }) {
       target='_blank'
       rel='noreferrer noopener'
     >
+      <Star src={starImg} alt='즐겨찾기' />
       <div style={imageStyle} className='card-image'></div>
       <div className='card-text'>
-        <div className='posting-box'>
+        <Box className='posting-box'>
           <div className='posting-time'>{ago}</div>
-          <div className='kebob-button'></div>
-        </div>
+          <Kebab src={kebabImg} alt='케밥' onClick={handleKebab} />
+        </Box>
         <div className='posting-description'>{value.description}</div>
         <div className='posting-date'>{date}</div>
       </div>
