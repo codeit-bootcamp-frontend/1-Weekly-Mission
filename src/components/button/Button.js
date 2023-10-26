bf7482dc279c817fffc30d050a6b3dc31a4ef75e
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-26 20:56:42 +0900

    styles(Button.js): 기본적인 버튼 스타일음

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -1,5 +1,16 @@
 import React from "react";
+import styled from "styled-components";
+//
+const BaseButton = styled.button`
+  background: #fff;
+  border: none;
+  border: 1px solid var(--primary);
+  border-radius: 4px;
+  height: 3rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
+`;
 
 export default function Button({ children }) {
-  return <button>{children}</button>;
+  return <BaseButton>{children}</BaseButton>;
 }
