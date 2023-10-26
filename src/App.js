ac3311c1dadda7fc211156bee2d6bfa9b088a487
ayden <wpfekdml@naver.com>
2023-10-27 01:07:47 +0900

    fix(something) : 뭔가 잘못되어서 파일 구조를 다시 수정

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
-import Nav from './componant/Nav/Nav';
-import FolderInfo from './componant/FolderInfo';
-import Footer from './componant/Footer/Footer';
-import Search from './componant/Search';
-import Cards from './componant/Cards';
-import { getSampleFolder } from './api';
-import { useEffect, useState } from 'react';
+import { Outlet } from 'react-router';
+import Nav from './components/Nav/Nav';
+import Footer from './components/Footer/Footer';
 
 function App() {
-  const section = {
-    display: 'flex',
-    flexDirection: 'column',
-    gap: '4rem',
-    width: '100%',
-    maxWidth: '112.4rem',
-    padding: '0rem 3.2rem',
-    margin: '4rem auto',
-  };
-
-  const [name, setName] = useState('');
-  const [owner, setOwner] = useState({});
-  const [cards, setCards] = useState([]);
-
-  const loadFolderData = async () => {
-    const {
-      folder: { name, owner, links },
-    } = await getSampleFolder();
-
-    setName(name);
-    setOwner({ ...owner });
-    setCards((prevCards) => {
-      return [...prevCards, ...links];
-    });
-  };
-
-  useEffect(() => {
-    loadFolderData();
-  }, []);
-
   return (
     <>
       <Nav />
-      <FolderInfo name={name} owner={owner} />
-      <section style={section}>
-        <Search />
-        <Cards cards={cards} />
-      </section>
+      <div>
+        <Outlet />
+      </div>
       <Footer />
     </>
   );
