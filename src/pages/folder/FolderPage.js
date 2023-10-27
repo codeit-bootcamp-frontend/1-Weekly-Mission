a467cf6289eaf8cc1ac8c0001d4f249186c54919
wecaners <wecaners@naver.com>
2023-10-27 15:53:42 +0900

    Feat: add sort button feature

--- src/pages/folder/FolderPage.js
+++ src/pages/folder/FolderPage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './folderPage.css';
 import AddLinkInput from './components/addLinkInput/AddLinkInput';
 import SortButton from './components/sortButton/SortButton';
@@ -7,25 +7,30 @@ import addIcon from '../../assets/folder/add.svg';
 import SearchBar from '../../components/searchBar/SearchBar';
 
 export default function FolderPage() {
+  const [category, setCategory] = useState('전체');
+
   return (
     <div className="folder-container">
       <header className="folder-header">
         <AddLinkInput />
       </header>
-      <section className="folder-content-section">
+      <main className="folder-main">
         <SearchBar />
-        <div className="folder-sort-add-buttons-container">
-          <div className="folder-sort-buttons-container">
-            {SORT_BUTTON_NAME.map((item) => (
-              <SortButton>{item}</SortButton>
-            ))}
+        <section className="folder-content-section">
+          <div className="folder-sort-add-buttons-container">
+            <div className="folder-sort-buttons-container">
+              {SORT_BUTTON_NAME.map((item) => (
+                <SortButton onClick={setCategory}>{item}</SortButton>
+              ))}
+            </div>
+            <button type="button" className="folder-add-button">
+              폴더 추가
+              <img src={addIcon} alt="add-icon" />
+            </button>
           </div>
-          <button type="button" className="folder-add-button">
-            폴더 추가
-            <img src={addIcon} alt="add-icon" />
-          </button>
-        </div>
-      </section>
+          <h1 className="folder-category">{category}</h1>
+        </section>
+      </main>
     </div>
   );
 }
--- src/pages/folder/components/sortButton/SortButton.js
+++ src/pages/folder/components/sortButton/SortButton.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './sortButton.css';
 
-export default function SortButton({ children }) {
+export default function SortButton({ children, onClick }) {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleSortButtonClick = () => {
+    onClick(children);
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <button type="button" className="sort-button">
+    <button
+      type="button"
+      className={isClicked ? 'sort-button clicked' : 'sort-button'}
+      onClick={handleSortButtonClick}
+    >
       {children}
     </button>
   );
