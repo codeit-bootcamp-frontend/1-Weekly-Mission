ad034f67bbe9b5fcecc2898090321263bb924fd9
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-27 11:07:01 +0900

    Feat: 글로벌 스타일 설정

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
 import Header from "./Header";
 import Main from "./Main";
 import Footer from "./Footer";
+import Globalstyle from "./GlobalStyle";
 
 function App() {
   
   return (
     <>
+      <Globalstyle />
       <Header />
       <Main />
       <Footer />
