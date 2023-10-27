cc44307a8b13e75673e1587fa89c1df3d1c3ef29
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-27 22:45:36 +0900

    feat(WholdData.js): 페이지이동기능끗

--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -3,18 +3,20 @@ import NavSec from "../components/nav/NavSec";
 import Header from "../components/header/Header";
 import SearchBar from "../components/searchBar/SearchBar";
 import Menubar from "../components/menuBar/Menubar";
-import LinksData from "../components/linksdata/LinksData";
 import WholeData from "../components/linksdata/WholeData";
 
 import useFetch from "../hooks/useFetch";
 import Landing from "../components/landing/Landing";
 import LocaleContext from "../contexts/LocaleContext";
+import { useParams } from "react-router-dom";
 
-export default function FolderPage({ userId }) {
+export default function FolderPage() {
   const [data, isLoading] = useFetch(
     "https://bootcamp-api.codeit.kr/api/users/1/folders"
   );
 
+  // const { folderId } = useParams();
+  // console.log(folderId);
   const result = data?.data;
   const obj = {};
   let obj_keys = [];
@@ -41,8 +43,8 @@ export default function FolderPage({ userId }) {
         <SearchBar />
         <Menubar data={data} isLoading={isLoading} />
         {/* <Landing data={data} isLoading={isLoading} key={key} /> */}
+
         <WholeData />
-        {/* <LinksData /> */}
       </div>
     </LocaleContext.Provider>
   );
