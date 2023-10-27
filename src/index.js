5c08d9283421c923f348b5208c1357aa1777c34b
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-28 00:06:23 +0900

    Design: 글로벌 스타일 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import Globalstyle from "./styles/GlobalStyles";
 import Main from "./Main";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<Main />);
+root.render(
+  <>
+    <Globalstyle />
+    <Main />
+  </>
+);
