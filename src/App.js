33a3121e912179575f1165bd54af20652c1b70d8
HellenHong <hongjw030@naver.com>
2023-10-28 15:17:20 +0900

    refactor(App): Delete useless codes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
+import { Outlet } from "react-router-dom";
 import { useEffect, useState } from "react";
 import Nav from "./components/Nav/Nav";
-import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
-import SearchBar from "./components/SearchBar/SearchBar";
-import CardList from "./components/Card/Card";
 import useAsync from "./hooks/useAsync";
 
 import { getUser, getFolder } from "../src/api/api";
@@ -15,26 +13,15 @@ const INITIAL_USER = {
   userProfileImage: "",
 };
 
-const INITIAL_FOLDER = {
-  folderName: "",
-  folderOwnerrName: "",
-  folderOwnerProfileImage: "",
-};
-
 function App() {
   const [userValues, setUserValues] = useState(INITIAL_USER);
-  const [folderValues, setFolderValues] = useState(INITIAL_FOLDER);
 
   const [isUserLoading, userLoadingError, getUserAsync] = useAsync(getUser);
-  const [isFolderLoading, folderLoadingError, getFolderAsync] =
-    useAsync(getFolder);
-
-  const [cardList, setCardList] = useState([]);
+  useAsync(getFolder);
 
   const loadUser = async () => {
     const userResult = await getUserAsync();
-    const folderResult = await getFolderAsync();
-    if (!folderResult || !userResult) return;
+    if (!userResult) return;
 
     const {
       name: userName = "",
@@ -49,18 +36,6 @@ function App() {
       };
       return { ...prevValues, ...newValues };
     });
-
-    if (!folderResult.folder) return;
-    const { name = "", owner = null, links = "" } = folderResult.folder;
-    setFolderValues((prevValues) => {
-      const newValues = {
-        folderName: name,
-        folderOwnerName: owner?.name,
-        folderOwnerProfileImage: owner?.profileImageSource,
-      };
-      return { ...prevValues, ...newValues };
-    });
-    setCardList(links);
   };
 
   useEffect(() => {
@@ -76,23 +51,8 @@ function App() {
       {isUserLoading && <p> 로그인 중...</p>}
       {userLoadingError?.message && <span>{userLoadingError.message}</span>}
 
-      <Header
-        folderOwnerProfile={folderValues.folderOwnerProfileImage}
-        folderName={folderValues.folderName}
-        folderOwnerName={folderValues.folderOwnerName}
-      />
-
       <section className="section">
-        <div className="search-section">
-          <SearchBar />
-        </div>
-        <div className="card-section">
-          {isFolderLoading && <p> 유저 정보를 받아오는 중...</p>}
-          {folderLoadingError?.message && (
-            <span>{folderLoadingError.message}</span>
-          )}
-          <CardList cardList={cardList} />
-        </div>
+        <Outlet />
       </section>
 
       <Footer />
