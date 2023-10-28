635ff31245727442913e7c2e80177ad06d6fcb33
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-28 20:05:39 +0900

    feat(Article.js): 화면줄어들면은 글자 바꾸는구현

--- src/components/article/Article.js
+++ src/components/article/Article.js
@@ -1,18 +1,42 @@
-import React, { useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import styles from "./Article.module.css";
 import titleImage from "../../assets/images/title.svg";
 
 export default function Article() {
+  const [isMobile, setIsMobile] = useState(false);
   const [title, setTitle] = useState("유용한글");
+
+  const handleSize = () => {
+    if (window.innerWidth > 768 && window.innerWidth < 1124) {
+      setTitle("전체");
+      setIsMobile(true);
+    } else {
+      setTitle("유용한글");
+      setIsMobile(false);
+    }
+  };
+
+  useEffect(() => {
+    if (window.innerWidth > 768 && window.innerWidth < 1124) {
+      setTitle("전체");
+    }
+
+    window.addEventListener("resize", handleSize);
+    return () => {
+      window.removeEventListener("resize", handleSize);
+    };
+  }, []);
 
   return (
     <div className={styles.container}>
       <p className={styles.title}>{title}</p>
-      <img
-        className={styles.images__container}
-        src={titleImage}
-        alt={title}
-      ></img>
+      {!isMobile && (
+        <img
+          className={styles.images__container}
+          src={titleImage}
+          alt={title}
+        ></img>
+      )}
     </div>
   );
 }
