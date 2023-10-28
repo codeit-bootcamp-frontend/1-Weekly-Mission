41960408e6677036a52e4af4d13364eea19d1301
wecaners <wecaners@naver.com>
2023-10-28 15:58:46 +0900

    Feat: add getUserFolders function

--- src/api/folder.js
+++ src/api/folder.js
@@ -1,14 +1,25 @@
 import { BASE_URL, USERS_ENDPOINT } from './services/endpoints';
 
-const getUserLinks = async () => {
+export const getUserFolders = async () => {
   let result;
   try {
-    const response = await fetch(`${BASE_URL}${USERS_ENDPOINT}/1/links`);
+    const response = await fetch(`${BASE_URL}${USERS_ENDPOINT}/1/folders`);
+    result = await response.json();
+  } catch (error) {
+    console.log(error);
+  }
+  return result;
+};
+
+export const getUserLinks = async (folderId) => {
+  let result;
+  try {
+    const response = await fetch(
+      `${BASE_URL}${USERS_ENDPOINT}/1/links?folderId=${folderId}`,
+    );
     result = await response.json();
   } catch (error) {
     console.log(error.message);
   }
   return result;
 };
-
-export default getUserLinks;
--- src/pages/folder/FolderPage.js
+++ src/pages/folder/FolderPage.js
@@ -2,20 +2,25 @@ import React, { useEffect, useState } from 'react';
 import './folderPage.css';
 import AddLinkInput from './components/addLinkInput/AddLinkInput';
 import SortButton from './components/sortButton/SortButton';
-import SORT_BUTTON_NAME from './constant';
 import addIcon from '../../assets/folder/add.svg';
 import SearchBar from '../../components/searchBar/SearchBar';
-import getUserLinks from '../../api/folder';
+import { getUserFolders, getUserLinks } from '../../api/folder';
 import Card from '../../components/card/Card';
 
 export default function FolderPage() {
-  const [category, setCategory] = useState('전체');
+  const [links, setLinks] = useState([]);
   const [folders, setFolders] = useState([]);
+
+  useEffect(async () => {
+    const result = await getUserFolders();
+    const { data } = result;
+    setFolders(data);
+  }, []);
 
   useEffect(async () => {
     const result = await getUserLinks();
     const { data } = result;
-    setFolders(data);
+    setLinks(data);
   }, []);
 
   return (
@@ -28,20 +33,24 @@ export default function FolderPage() {
         <section className="folder-content-section">
           <div className="folder-sort-add-buttons-container">
             <div className="folder-sort-buttons-container">
-              {SORT_BUTTON_NAME.map((item) => (
-                <SortButton onClick={setCategory}>{item}</SortButton>
-              ))}
+              <SortButton>전체</SortButton>
+              {folders &&
+                folders.map((item) => (
+                  <div key={item.id}>
+                    <SortButton>{item.name}</SortButton>
+                  </div>
+                ))}
             </div>
             <button type="button" className="folder-add-button">
               폴더 추가
               <img src={addIcon} alt="add-icon" />
             </button>
           </div>
-          <h1 className="folder-category">{category}</h1>
+          <h1 className="folder-category">카테고리</h1>
         </section>
         <div className="links-container">
-          {folders &&
-            folders.map((item) => (
+          {links &&
+            links.map((item) => (
               <div key={item.id}>
                 <Card linkInfo={item} />
               </div>
