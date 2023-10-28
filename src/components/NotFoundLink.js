4c5ec1bbfc557fc9cba9b217f1781b59bd2c4b24
NIX <lsc58461@naver.com>
2023-10-28 14:54:50 +0900

    feat: NotFoundLink 컴포넌트 구현

---
+++ src/components/NotFoundLink.js
@@ -0,0 +1,11 @@
+import styles from "../styles/NotFoundLink.module.css";
+
+const NotFoundLink = () => {
+  return (
+    <div className={styles.NotFoundLink}>
+      <p className={styles.description}>저장된 링크가 없습니다.</p>
+    </div>
+  );
+};
+
+export default NotFoundLink;
