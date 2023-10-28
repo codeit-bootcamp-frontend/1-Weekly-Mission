a9b3aa099d2a8a812f040bd59a55c419721c3bbc
손지은 <wiseag@naver.com>
2023-10-28 17:53:54 +0900

    refactor(Card) : css 가독성 좋게 적용

--- src/component/Card.js
+++ src/component/Card.js
@@ -3,8 +3,9 @@ import { formatDate, prettyFormatTimeDiff } from "../util/useDate";
 import kebabImg from "../assets/img/kebab.png";
 import noImg from "../assets/img/no-image.svg";
 import style from "./Card.module.css";
+import clsx from "clsx";
 
-function Card({ card }) {
+function Card({ card: { title, description, url, imageSource, createdAt } }) {
   const [hover, setHover] = useState(false);
 
   const handleMouseOver = () => {
@@ -14,37 +15,28 @@ function Card({ card }) {
     setHover(false);
   };
 
-  const hoverImage = hover ? style.hoverImage : "";
-  const hoverBg = hover ? style.hoverBgColor : "";
-
-  const imageStyle = `${style.image} ${hoverImage}`;
-  const bgColorStyle = `${style.container} ${hoverBg}`;
-
-  const bgImg = {
-    backgroundImage: `url(${card.imageSource || noImg})`,
-  };
-
   return (
     <div
-      className={bgColorStyle}
+      className={clsx(style.container, { [style.hoverBgColor]: hover })}
       onMouseOver={handleMouseOver}
       onMouseOut={handleMouseOut}
     >
-      <a className={style.card} href={card.url}>
-        <div className={imageStyle} style={bgImg}></div>
+      <a className={style.card} href={url}>
+        <div
+          className={clsx(style.image, { [style.hoverImage]: hover })}
+          style={{ backgroundImage: `url(${imageSource || noImg})` }}
+        ></div>
         <div className={style.explanation}>
           <div className={style.header}>
-            <div>{prettyFormatTimeDiff(new Date(card.createdAt))}</div>
+            <div>{prettyFormatTimeDiff(new Date(createdAt))}</div>
             <img src={kebabImg} alt="쩜쩜쩜" />
           </div>
           <div className={style.text}>
-            <div>{card.title}</div>
-            <div>{card.description}</div>
+            <div>{title}</div>
+            <div>{description}</div>
           </div>
 
-          <div className={style.footer}>
-            {formatDate(new Date(card.createdAt))}
-          </div>
+          <div className={style.footer}>{formatDate(new Date(createdAt))}</div>
         </div>
       </a>
     </div>
