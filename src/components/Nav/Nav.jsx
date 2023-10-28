1fcec3c3e1523d1148ebed8c82ea2c2493b3f482
sunny-son <thstkd92@gmail.com>
2023-10-28 13:32:46 +0900

    feat : Folder 페이지에서는 nav바 고정 해제 적용

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,9 +1,14 @@
 import styled from 'styled-components';
 import Navbar from './Navbar';
+import { useLocation } from 'react-router-dom';
+
 
 function Nav() {
+  const location = useLocation();
+  const path = location.pathname;
+
   return (
-    <NavBlock>
+    <NavBlock $path={path}>
       <Navbar />
     </NavBlock>
   );
@@ -14,9 +19,9 @@ export default Nav;
 const NavBlock = styled.nav`
   display: flex;
   justify-content: center;
-  position: sticky;
+  $position: ${props => (props.path === '/folder' ? 'static' : 'sticky')};
   top: 0;
   width: 100%;
   background: #f0f6ff;
   z-index: 1;
-`
+`;
