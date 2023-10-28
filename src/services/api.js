2f3158dbeac898dad098ba28baef622f3f1b8fca
Hyeyoon <foryou9097@gmail.com>
2023-10-28 20:13:24 +0900

    fix:(Shared/pc)에러구문 추가, 페이지 추가

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,17 @@
 export async function getFolderData() {
   const res = await fetch("https://bootcamp-api.codeit.kr/api/sample/folder");
   const body = await res.json();
+  if (!res.ok) {
+    throw new Error("FOLDER DATA ERROR");
+  }
   return body;
 }
 
 export async function getUserData() {
   const res = await fetch("https://bootcamp-api.codeit.kr/api/sample/user");
   const body = await res.json();
+  if (!res.ok) {
+    throw new Error("USER DATA ERROR");
+  }
   return body;
 }
