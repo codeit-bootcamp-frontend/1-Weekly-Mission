1a5f9950cbd5d0a8e7c2d5774c2b8e882476ce66
HellenHong <hongjw030@naver.com>
2023-10-28 18:00:39 +0900

    feat(LinkAddInput): Create LinkAddInput component

---
+++ src/components/LinkAddInput/LinkAddInput.js
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import "./LinkAddInput.style.css";
+import linkIcon from "../../assets/link-icon.svg";
+
+function LinkAddInput() {
+  const [keyword, setKeyword] = useState("");
+  const handleKeywordChange = (e) => setKeyword(e.target.value);
+
+  return (
+    <form>
+      <div className="add-link-form">
+        <img src={linkIcon} className="add-link-icon" />
+        <input
+          name="linkAdd-input"
+          value={keyword}
+          onChange={handleKeywordChange}
+          placeholder="링크를 추가해보세요"
+          className="add-link-input"
+        ></input>
+        <button className="add-link-button" type="submit">
+          추가하기
+        </button>
+      </div>
+    </form>
+  );
+}
+
+export default LinkAddInput;
