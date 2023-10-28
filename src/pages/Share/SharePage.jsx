68fd17affaee91ef2dc79629469bded736a81b4f
qooktree1 <qooktree@gmail.com>
2023-10-28 15:20:30 +0900

    rename sharePage component 이름 수정

---
+++ src/pages/Share/SharePage.jsx
@@ -0,0 +1,35 @@
+import {
+  useFetchSampleFolder,
+  useFetchUserProfileSample,
+} from "../../apis/fetch"
+import Footer from "../../components/Footer/Footer"
+import Navbar from "../../components/Navbar/Navbar"
+import ShareContainer from "../../containers/Share/ShareContainer"
+import * as S from "../styles.js"
+
+const SharePage = () => {
+  const userProfile = useFetchUserProfileSample()
+  const sampleFolder = useFetchSampleFolder()
+  const userLoading = userProfile?.loading
+  const folderLoading = sampleFolder?.loading
+
+  let userData, folderData
+  if (!userLoading && !folderLoading) {
+    userData = userProfile?.data
+    folderData = sampleFolder?.data
+  }
+
+  if (userData && folderData) {
+    return (
+      <>
+        <S.StyledHeader>
+          <Navbar userData={userData} />
+        </S.StyledHeader>
+        <ShareContainer shareData={folderData} />
+        <Footer />
+      </>
+    )
+  }
+}
+
+export default SharePage
