2f5a92a4017e80a65918fc6c7e6ff50a02b71ab2
HellenHong <hongjw030@naver.com>
2023-10-28 15:37:37 +0900

    refactor(*) Change path

--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -1,19 +1,10 @@
 import { useEffect, useState } from "react";
-import Nav from "../components/Nav/Nav";
-import Header from "../components/Header/Header";
-import Footer from "../components/Footer/Footer";
 import SearchBar from "../components/SearchBar/SearchBar";
 import CardList from "../components/Card/Card";
 import useAsync from "../hooks/useAsync";
 
-import { getUser, getFolder } from "../api/api";
+import { getFolder } from "../api/api";
 import "../App.style.css";
-
-const INITIAL_USER = {
-  userName: "",
-  userEmail: "",
-  userProfileImage: "",
-};
 
 const INITIAL_FOLDER = {
   folderName: "",
@@ -22,45 +13,14 @@ const INITIAL_FOLDER = {
 };
 
 function FolderPage() {
-  const [userValues, setUserValues] = useState(INITIAL_USER);
-  const [folderValues, setFolderValues] = useState(INITIAL_FOLDER);
-
-  const [isUserLoading, userLoadingError, getUserAsync] = useAsync(getUser);
   const [isFolderLoading, folderLoadingError, getFolderAsync] =
     useAsync(getFolder);
 
   const [cardList, setCardList] = useState([]);
 
   const loadUser = async () => {
-    const userResult = await getUserAsync();
     const folderResult = await getFolderAsync();
-    if (!folderResult || !userResult) return;
-
-    const {
-      name: userName = "",
-      email = "",
-      profileImageSource = "",
-    } = userResult;
-    setUserValues((prevValues) => {
-      const newValues = {
-        userName: userName,
-        userEmail: email,
-        userProfileImage: profileImageSource,
-      };
-      return { ...prevValues, ...newValues };
-    });
-
-    if (!folderResult.folder) return;
-    const { name = "", owner = null, links = "" } = folderResult.folder;
-    setFolderValues((prevValues) => {
-      const newValues = {
-        folderName: name,
-        folderOwnerName: owner?.name,
-        folderOwnerProfileImage: owner?.profileImageSource,
-      };
-      return { ...prevValues, ...newValues };
-    });
-    setCardList(links);
+    setCardList(folderResult?.folder?.links);
   };
 
   useEffect(() => {
@@ -69,19 +29,6 @@ function FolderPage() {
 
   return (
     <>
-      <Nav
-        userEmail={userValues.userEmail}
-        userProfile={userValues.userProfileImage}
-      />
-      {isUserLoading && <p> 로그인 중...</p>}
-      {userLoadingError?.message && <span>{userLoadingError.message}</span>}
-
-      <Header
-        folderOwnerProfile={folderValues.folderOwnerProfileImage}
-        folderName={folderValues.folderName}
-        folderOwnerName={folderValues.folderOwnerName}
-      />
-
       <section className="section">
         <div className="search-section">
           <SearchBar />
@@ -94,8 +41,6 @@ function FolderPage() {
           <CardList cardList={cardList} />
         </div>
       </section>
-
-      <Footer />
     </>
   );
 }
