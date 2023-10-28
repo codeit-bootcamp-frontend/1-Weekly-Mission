6e44c700a43af720f56d18adc8d27f7767254314
Jyophie <jyophie@gmail.com>
2023-10-28 17:42:34 +0900

    Feat: 옵션 영역 추가

--- src/components/Option.js
+++ src/components/Option.js
@@ -1,5 +1,43 @@
-const Option = () => {
-  return <div></div>;
+import shareImg from "../img/share.svg";
+import penImg from "../img/pen.svg";
+import deleteImg from "../img/delete.svg";
+import "./Option.css";
+
+const Option = ({ currentFolder }) => {
+  const show = currentFolder.id !== "";
+
+  const share = {
+    src: shareImg,
+    name: "공유",
+  };
+  const rename = {
+    src: penImg,
+    name: "이름 변경",
+  };
+  const deleteFolder = {
+    src: deleteImg,
+    name: "삭제",
+  };
+
+  const options = [share, rename, deleteFolder];
+
+  return (
+    <div className="option-container">
+      <h1>{currentFolder.name}</h1>
+      {show ? (
+        <div className="option-wrapper">
+          {options.map((option) => (
+            <div className="option-box">
+              <img src={option.src} alt="공유" />
+              <div>{option.name}</div>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 };
 
 export default Option;
