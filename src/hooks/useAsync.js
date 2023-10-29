695404b562e071c244775061f09126a2ae0ff592
NIX <lsc58461@naver.com>
2023-10-29 17:40:16 +0900

    feat(useAsync): async func 2개 받아오게 변경

--- src/hooks/useAsync.js
+++ src/hooks/useAsync.js
@@ -7,8 +7,9 @@ import { useCallback, useState } from "react";
  * @returns {[boolean, Function]} - 배열로 반환되며 첫 번째 요소는 실행 상태(pending)를 나타내는 불리언 값이고,
  * 두 번째 요소는 비동기 함수를 래핑한 함수입니다.
  */
-const useAsync = (asyncFunction) => {
+const useAsync = (asyncFunction, asyncFunction2) => {
   const [pending, setPending] = useState(false);
+  const [pending2, setPending2] = useState(false);
 
   const wrappedFunction = useCallback(
     async (...args) => {
@@ -24,7 +25,25 @@ const useAsync = (asyncFunction) => {
     [asyncFunction]
   );
 
-  return [pending, wrappedFunction];
+  const wrappedFunction2 = useCallback(
+    async (...args) => {
+      try {
+        setPending2(true);
+        return await asyncFunction2(...args);
+      } catch (error) {
+        console.log(error);
+      } finally {
+        setPending2(false);
+      }
+    },
+    [asyncFunction2]
+  );
+
+  if (!asyncFunction2) {
+    return [pending, wrappedFunction];
+  }
+
+  return [pending, pending2, wrappedFunction, wrappedFunction2];
 };
 
 export default useAsync;
