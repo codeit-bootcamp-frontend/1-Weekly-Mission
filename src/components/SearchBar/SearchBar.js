6984c582f3a2d28386b298afff55e400f4509ae9
HellenHong <hongjw030@naver.com>
2023-10-29 13:27:34 +0900

    refactor(searchBar): Refactor searchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,18 +1,27 @@
-import "./SearchBar.style.css";
-import "../../styles/reset.css";
+/* 검색어를 입력해 seach 하기 위한 인풋 컴포넌트 */
+
+/* css 모듈 방식 적용
+ */
+
+import styles from "./SearchBar.module.css";
 import { useState } from "react";
 import searchIcon from "../../assets/search-icon.svg";
+
 function SearchBar() {
   const [text, setText] = useState("");
   const handleTitleChange = (e) => {
     setText(e.target.value);
   };
   return (
-    <div className="search-form-container">
-      <form className="search-form">
-        <img className="search-icon" src={searchIcon} alt="search icon" />
+    <div className={styles["search-form-container"]}>
+      <form className={styles["search-form"]}>
+        <img
+          className={styles["search-icon"]}
+          src={searchIcon}
+          alt="search icon"
+        />
         <input
-          className="search-bar"
+          className={styles["search-bar"]}
           value={text}
           placeholder="검색어를 입력하세요."
           onChange={handleTitleChange}
