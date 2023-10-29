7722b0d36caf9a33eb57584e502d9e4b3be587f2
kanglocal <develocal25@gmail.com>
2023-10-29 16:43:16 +0900

    refactor : nav, footer제거

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -35,16 +35,6 @@ function HomePage() {
   return (
     <>
       <header>
-        <nav>
-          <a href="../.." className="nav-logo">
-            <img src={logoImg} alt="Linkbrary 로고" />
-          </a>
-
-          <div className="user-profile">
-            <img src={user.profileImageSource} alt="프로필 이미지" />
-            <span>{user.email}</span>
-          </div>
-        </nav>
         <div className="folder-info">
           <div className="folder-owner">
             <div className="owner-profile">
@@ -73,42 +63,6 @@ function HomePage() {
           ))}
         </div>
       </main>
-
-      <footer>
-        <div className="footer-container">
-          <span>©codeit - 2023</span>
-          <div className="footer-menu">
-            <a href="/pages/privacy/privacy.html">Privacy Policy</a>
-            <a href="/pages/faq/faq.html">FAQ</a>
-          </div>
-          <div className="sns-icons">
-            <a
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={snsIconF} alt="Linkbrary의 facebook 바로가기" />
-            </a>
-            <a
-              href="https://twitter.com/?lang=ko"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={snsIconT} alt="Linkbrary의 twiter 바로가기" />
-            </a>
-            <a href="https://www.youtube.com/" target="_blank" rel="noreferrer">
-              <img src={snsIconY} alt="Linkbrary의 youtube 바로가기" />
-            </a>
-            <a
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              <img src={snsIconI} alt="Linkbrary의 instagram 바로가기" />
-            </a>
-          </div>
-        </div>
-      </footer>
     </>
   );
 }
