6031a142a3669de1b4059794b54d623a29bf9d32
sunny-son <thstkd92@gmail.com>
2023-10-29 16:27:34 +0900

    style : 테블릿 사이즈 미디어 쿼리사용하여 폴더 추가 버튼 생성

--- src/components/Folder/FolderCategory.jsx
+++ src/components/Folder/FolderCategory.jsx
@@ -3,6 +3,7 @@ import FolderCategoryButton from './FolderCategoryButton';
 import { Fragment, useCallback, useEffect, useState } from 'react';
 import useAsync from '../../hooks/useAsync';
 import { getFolders } from '../../api/api';
+import add_icon from '../../assets/svg/add-folder.svg';
 
 const ENTIRE_CATEGORY = {
   id: 0,
@@ -30,18 +31,23 @@ function FolderCategory({ onGetCategory }) {
   }, [handleLoad]);
 
   return (
-    <FolderCategoryStyle>
-      {categories.map((category) => {
-        return (
-          <Fragment key={category.id}>
-            <FolderCategoryButton category={category} onGetCategory={onGetCategory}></FolderCategoryButton>
-            {foldersLoadingError?.message && <span>{foldersLoadingError.message}</span>}
-          </Fragment>
-        );
-      })
-      }
-    </FolderCategoryStyle>
-    
+    <FolderCategoryContainer>
+      <FolderCategoryStyle>
+        {categories.map((category) => {
+          return (
+            <Fragment key={category.id}>
+              <FolderCategoryButton category={category} onGetCategory={onGetCategory}></FolderCategoryButton>
+              {foldersLoadingError?.message && <span>{foldersLoadingError.message}</span>}
+            </Fragment>
+          );
+        })
+        }
+      </FolderCategoryStyle>
+      <FolderAddButton>
+        <FolderAddName>폴더 추가</FolderAddName>
+        <FolderAddIcon src={add_icon} alt='폴더 추가 아이콘' />
+      </FolderAddButton>
+    </FolderCategoryContainer>
   );
 }
 
@@ -54,4 +60,38 @@ const FolderCategoryStyle = styled.div`
   column-gap: 0.8rem;
   width: 100%;
   flex-wrap: wrap;
+`;
+
+const FolderCategoryContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  align-items: center;
+`;
+
+const FolderAddButton = styled.button`
+  display: none;
+  
+  @media (min-width: 768px) {
+    display: flex;
+    align-items: flex-start;
+    gap: 0.4rem;  
+  }
+`;
+
+const FolderAddName = styled.p`
+  color: #6d6afe;
+  text-align: center;
+  font-family: Pretendard, sans-serif;
+  font-size: 1.6rem;
+  font-style: normal;
+  font-weight: 500;
+  line-height: normal;
+  letter-spacing: -0.3px;
+  white-space: nowrap;
+`;
+
+const FolderAddIcon = styled.img`
+  width: 1.6rem;
+  height: 1.6rem;
 `;
