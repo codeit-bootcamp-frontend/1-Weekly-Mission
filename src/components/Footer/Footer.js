5b63316cc0b0098cf08894b1abbea9ed298a0b62
HellenHong <hongjw030@naver.com>
2023-10-29 12:58:19 +0900

    refactor(footer): Refactor footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,5 +1,4 @@
-import "./Footer.style.css";
-import "../../styles/reset.css";
+import styles from "./Footer.module.css";
 import twitterIcon from "../../assets/akar-icons_twitter-fill.svg";
 import facebookIcon from "../../assets/akar-icons_facebook-fill.svg";
 import youtubeIcon from "../../assets/akar-icons_youtube-fill.svg";
@@ -7,27 +6,41 @@ import instagramIcon from "../../assets/akar-icons_instagram-filled.svg";
 
 function Footer() {
   return (
-    <footer>
-      <div className="content">
-        <div className="footer logo">©Codeit - 2023</div>
-        <div className="footer link">
-          <a href="./">Privacy Policy </a>
-          <a href="./">FAQ</a>
-        </div>
-        <div className="footer sns">
-          <a href="https://www.facebook.com/" target="_blank">
-            <img src={facebookIcon} alt="페이스북 아이콘" />
-          </a>
-          <a href="https://www.twitter.com/" target="_blank">
-            <img src={twitterIcon} alt="트위터 아이콘" />
-          </a>
-          <a href="https://www.youtube.com/" target="_blank">
-            <img src={youtubeIcon} alt="유튜브 아이콘" />
-          </a>
-          <a href="https://www.instagram.com/" target="_blank">
-            <img src={instagramIcon} alt="인스타그램 아이콘" />
-          </a>
-        </div>
+    <footer className={styles["content"]}>
+      <div className={`${styles.footer} ${styles.logo}`}>©Codeit - 2023</div>
+      <div className={`${styles.footer} ${styles.link}`}>
+        <a href="./">Privacy Policy </a>
+        <a href="./">FAQ</a>
+      </div>
+      <div className={`${styles.footer} ${styles.sns}`}>
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={facebookIcon} alt="페이스북 아이콘" />
+        </a>
+        <a
+          href="https://www.twitter.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={twitterIcon} alt="트위터 아이콘" />
+        </a>
+        <a
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={youtubeIcon} alt="유튜브 아이콘" />
+        </a>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noreferrer noopener"
+        >
+          <img src={instagramIcon} alt="인스타그램 아이콘" />
+        </a>
       </div>
     </footer>
   );
