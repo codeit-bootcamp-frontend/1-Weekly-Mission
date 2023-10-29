1479556096f503237c52231fb1c237d2b7d34beb
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-29 20:32:55 +0900

    Feat: 프로필 데이터 불러오기(미해결)

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,46 +4,56 @@ import { Link } from "react-router-dom";
 import logoImg from "../assets/logo.svg";
 import './Nav.css';
 import Button from "./Button";
+import { getUser } from "../api";
 
 
-// function Profile({ userData }) {
-//   const a = userData.data;
-//   console.log(a);
-//   const b = a["id"];
-//   console.log(b);
-  
-//   console.log(data);
-  
-//   const { data } = userData;
-//   console.log(data);
+function Profile({userData}) {
+  console.log(userData);
+  // const email = userData.email;
+  // const image = userData.image;
+  // console.log(email);
 
+  return (
+    <div className="profile">
+      {/* <img className="profile-img" src={image} alt="프로필"/>
+      <span className="profile-email">{email}</span> */}
+    </div>
+  );
+}
 
-//   return (
-//     <div className="profile">
-//       <img className="profile-img" src={profileImageSource} alt="프로필"/>
-//       <span className="profile-email">{email}</span>
-//     </div>
-//   );
-// }
 
 function Nav() {
-  // const [userData, setUserData] = useState({}); 
 
-  // const handleLoad = async () => {
-  //   const nextUserData = await getUser();
-  //   setUserData(nextUserData);
-  //   console.log(userData);
-  // };
+  // 프로필 데이터 가져오기 
+  const [userData, setUserData] = useState(null); 
+  const [isLoading, setIsLoading] = useState(false);
+  
+  const handleLoad = async () => {
+    try {
+      const response = await getUser();
+      const data = response.data[0];
+      const email = data.email;
+      const image = data.image_source;
+      // console.log(email);
+      // console.log(image);
+      const nextUserData = { email, image };
+      console.log(nextUserData);
+      setUserData(nextUserData);
+      setIsLoading(true);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
 
-  // useEffect(() => {
-  //   handleLoad();
-  // }, []);
+  useEffect(() => {
+    handleLoad();
+  }, []);
 
   return (
     <div className="nav-container">
       <Link to="/"><img className="logo-img" src={logoImg} alt="Linkbrary Logo"/></Link>
-      {/* <Profile userData={userData} /> */}
-      <Button>로그인</Button>
+      {(!isLoading) ? <Profile userData={userData} /> : <Button>로그인</Button>}
     </div>
   );
 }
