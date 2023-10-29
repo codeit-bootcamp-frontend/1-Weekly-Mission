507763fd381bc361e2d7eec1d5f55f96d21fd0ac
NIX <lsc58461@naver.com>
2023-10-29 22:03:00 +0900

    refactor(Footer): Link 태그로 변경

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styles from "../styles/Footer.module.css";
 import facebookImg from "../assets/facebook.svg";
 import twitterImg from "../assets/twitter.svg";
@@ -10,26 +11,26 @@ const Footer = () => {
       <div className={styles.container}>
         <span className={styles.copyright}>©codeit - 2023</span>
         <div className={styles.links}>
-          <a className={styles.link} href="privacy.html">
+          <Link className={styles.link} to="/privacy">
             Privacy Policy
-          </a>
-          <a className={styles.link} href="faq.html">
+          </Link>
+          <Link className={styles.link} to="/faq">
             FAQ
-          </a>
+          </Link>
         </div>
         <div className={styles.sns_links}>
-          <a href="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
+          <Link to="https://www.facebook.com/" target="_blank" rel="noopener noreferrer">
             <img src={facebookImg} alt="facebook 바로가기" />
-          </a>
-          <a href="https://twitter.com/" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link to="https://twitter.com/" target="_blank" rel="noopener noreferrer">
             <img src={twitterImg} alt="twitter 바로가기" />
-          </a>
-          <a href="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link to="https://www.youtube.com/" target="_blank" rel="noopener noreferrer">
             <img src={youtubeImg} alt="youtube 바로가기" />
-          </a>
-          <a href="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
+          </Link>
+          <Link to="https://www.instagram.com/" target="_blank" rel="noopener noreferrer">
             <img src={instagramImg} alt="instagram 바로가기" />
-          </a>
+          </Link>
         </div>
       </div>
     </footer>
