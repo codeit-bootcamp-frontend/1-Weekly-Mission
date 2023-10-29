d2a1f8d78b946ca531808ed3391205dd55950c93
NIX <lsc58461@naver.com>
2023-10-29 22:24:32 +0900

    feat(CardList): link 데이터 없을 때 NotFoundLink 렌더링 구현

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,6 +1,7 @@
 import styles from "../styles/CardList.module.css";
 import CardItem from "./CardItem";
 import CardItemSkeleton from "./CardItemSkeleton";
+import NotFoundLink from "./NotFoundLink";
 
 const CardList = ({ folderData, isLoading }) => {
   if (isLoading) {
@@ -16,6 +17,7 @@ const CardList = ({ folderData, isLoading }) => {
       </ul>
     );
   }
+
   if (folderData && folderData.folder) {
     const { folder } = folderData;
     const { links } = folder;
@@ -32,6 +34,7 @@ const CardList = ({ folderData, isLoading }) => {
   }
   if (folderData && folderData.data) {
     const { data } = folderData;
+    if (data.length === 0) return <NotFoundLink />;
 
     return (
       <ul className={styles.CardList}>
