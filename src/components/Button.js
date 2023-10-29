4e5807d878d480caf632883a6f4dfe2cd939acc4
DESKTOP-HKBDD45\dkssu <summerkim0825@gmail.com>
2023-10-29 12:47:22 +0900

    Refactor: 버튼 컴포넌트 수정

--- src/components/Button.js
+++ src/components/Button.js
@@ -4,7 +4,8 @@ const StyledButton = styled.button`
   width: 128px;
   padding: 16px 20px;
   border-radius: 8px;
-  background-color: linear-gradient(91deg, #6D6AFE 0.12%, #6AE3FE 101.84%));
+  background: var(--color-background);
+  color: var(--color-white);
   font-size: 18px;
   font-style: normal;
   font-weight: 600;
