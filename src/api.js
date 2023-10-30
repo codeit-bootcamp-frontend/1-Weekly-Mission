bbb9b7320f2456b80c4c7d1fa9b498dbefc1b7db
summerkimm <summerkim0825@gmail.com>
2023-10-30 22:17:42 +0900

    Refactor: API 에러 발생 시 console.log 출력

--- src/api.js
+++ src/api.js
@@ -16,7 +16,7 @@ export async function getFolder() {
   const response = await fetch(`${BASE_URL}/users/1/folders`);
 
   if (!response.ok) {
-    throw new Error("데이터를 불러오는데 실패했습니다.");
+    console.log("데이터를 불러오는데 실패했습니다.");
   } 
   const result = await response.json();
   return result;
