5f4001124fa4f22ecbdaa9fa61c06936575af9db
summerkimm <summerkim0825@gmail.com>
2023-10-30 22:08:43 +0900

    Refactor: 에러 발생 시 console.log 로 표시

--- src/api.js
+++ src/api.js
@@ -5,7 +5,7 @@ export async function getUser() {
   const response = await fetch(`${BASE_URL}/users/1`);
   
   if (!response.ok) {
-    throw new Error("데이터를 불러오는데 실패했습니다.");
+    console.log("데이터를 불러오는데 실패했습니다.");
   } 
   const result = await response.json();
   return result;
