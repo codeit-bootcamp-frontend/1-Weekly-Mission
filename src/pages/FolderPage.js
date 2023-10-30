6e115a3b7e57dad983bf14131c0931b5e175b043
Seoyoung Min <seoyoungiyo@gmail.com>
2023-10-30 22:53:54 +0900

    design: 링크가 없는 폴더 클릭시 노출 문구 스타일 수정

--- src/pages/FolderPage.js
+++ src/pages/FolderPage.js
@@ -1,12 +1,22 @@
 import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet";
 import { useParams } from "react-router-dom";
+import styled from "styled-components";
 import { getFolderLinks, getFolders } from "../api";
 import Gnb from "../component/Gnb";
 import SearchBar from "../component/SearchBar";
 import FolderList from "../component/FolderList";
 import CardSection from "../component/CardSection";
 import "../assets/css/SharedPage.css";
+
+const StyledDiv = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 66.25rem;
+  height: 6.25rem;
+  line-height: 1.5rem;
+`;
 
 function FolderPage() {
   const { folderId } = useParams();
@@ -42,9 +52,9 @@ function FolderPage() {
 
       <section className="section">
         <SearchBar size="large" />
-        <FolderList folders={folders} />
+        {(links && links.length === 0) || <FolderList folders={folders} />}
         {links && links.length === 0 ? (
-          <h1> 저장된 링크가 없습니다. </h1>
+          <StyledDiv> 저장된 링크가 없습니다 📭 </StyledDiv>
         ) : (
           <CardSection data={links} />
         )}
