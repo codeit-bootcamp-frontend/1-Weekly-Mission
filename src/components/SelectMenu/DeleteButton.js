fbf5cc1931c902ca6df4b6c91b6c57a0611025d4
oauch <lgm9515@naver.com>
2023-10-31 13:55:33 +0900

    Refactor(mentor): SelectMenu 스타일드 태그 컴포넌트 밖으로

--- src/components/SelectMenu/DeleteButton.js
+++ src/components/SelectMenu/DeleteButton.js
@@ -1,23 +1,20 @@
 import { styled } from 'styled-components';
 
-function DeleteButton() {
-  const Delete = styled.button`
-    box-shadow: 0px 2px 8px 0px rgba(51, 50, 54, 0.1);
-    border: 0;
-    background-color: #fff;
-    padding: 7px 12px;
-    font-size: 14px;
+const Delete = styled.button`
+  box-shadow: 0px 2px 8px 0px rgba(51, 50, 54, 0.1);
+  border: 0;
+  background-color: #fff;
+  padding: 7px 12px;
+  font-size: 14px;
 
-    &:hover {
-      background-color: var(--grayE7);
-      color: var(--primary);
-    }
-  `;
-  return (
-    <>
-      <Delete>삭제하기</Delete>
-    </>
-  );
+  &:hover {
+    background-color: var(--grayE7);
+    color: var(--primary);
+  }
+`;
+
+function DeleteButton() {
+  return <Delete>삭제하기</Delete>;
 }
 
 export default DeleteButton;
