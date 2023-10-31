65d724ef280d9715910b2c11878dd5ea107b2c6e
hanseulhee <3021062@gmail.com>
2023-11-01 03:20:23 +0900

    feat: footer links 상수화, styled-components 적용

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,36 +1,80 @@
+import { social } from "constants/link";
 import { Link } from "react-router-dom";
-import styles from "styles/modules/footer.module.css";
+import styled from "styled-components";
 
 function Footer() {
   return (
-    <footer>
-      <div className={styles.wrapper}>
-        <span className={styles.logo}>©codeit - 2023</span>
-        <div className={styles.linkToPage}>
-          <Link to="/privacy.html" className={styles.link}>
-            Privacy Policy
-          </Link>
-          <Link to="/faq.html" className={styles.link}>
-            FAQ
-          </Link>
-        </div>
-        <div className={styles.socialIcon}>
-          <Link to="https://www.facebook.com/?locale=ko_KR">
-            <img src="assets/images/facebook.svg" alt="facebookLogo" />
-          </Link>
-          <Link to="https://www.twitter.com/">
-            <img src="assets/images/twitter.svg" alt="twitterLogo" />
-          </Link>
-          <Link to="https://www.youtube.com/">
-            <img src="assets/images/youtube.svg" alt="youtubeLogo" />
-          </Link>
-          <Link to="https://www.instagram.com/">
-            <img src="assets/images/instagram.svg" alt="instagramLogo" />
-          </Link>
-        </div>
-      </div>
-    </footer>
+    <StyledFooter>
+      <StyledWrapper>
+        <StyledLogo>©codeit - 2023</StyledLogo>
+        <StyledLinkToPage>
+          <StyledLink to="/">Privacy Policy</StyledLink>
+          <StyledLink to="/">FAQ</StyledLink>
+        </StyledLinkToPage>
+        <StyledSocialIcon>
+          {social.map((media) => (
+            <Link to={media.link} key={media.icon}>
+              <img src={media.icon} alt="socialIcon" />
+            </Link>
+          ))}
+        </StyledSocialIcon>
+      </StyledWrapper>
+    </StyledFooter>
   );
 }
 
 export default Footer;
+
+const StyledFooter = styled.footer`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 16rem;
+  padding-top: 3.2rem;
+  background-color: #111322;
+  min-height: 100%;
+`;
+
+const StyledWrapper = styled.div`
+  bottom: 0;
+  display: grid;
+  grid-template: "logo linkToPage socialIcon";
+  justify-content: space-between;
+  width: 100%;
+  max-width: 192rem;
+  height: fit-content;
+  padding: 0 10.4rem;
+
+  @media (max-width: 767px) and (min-width: 375px) {
+    grid-template:
+      "linkToPage socialIcon"
+      "logo .";
+    padding: 0 2rem;
+    gap: 5rem;
+  }
+`;
+
+const StyledLogo = styled.span`
+  grid-area: logo;
+  font-size: 1.6rem;
+  color: #676767;
+`;
+
+const StyledLinkToPage = styled.div`
+  grid-area: linkToPage;
+  display: flex;
+  column-gap: 3rem;
+  padding-right: 1.8rem;
+`;
+
+const StyledSocialIcon = styled.div`
+  grid-area: socialIcon;
+  display: flex;
+  column-gap: 1.2rem;
+  height: 2rem;
+`;
+
+const StyledLink = styled(Link)`
+  font-size: 1.6rem;
+  color: #cfcfcf;
+`;
