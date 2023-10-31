a50ead388ef39e28d834249a4c53161d6f45eb86
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-10-31 12:33:28 +0900

    refactor(folderMenu.js): 사용할필요없는 FolderMenu안에서 useEffect제거

--- src/components/menuBar/FolderMenu.js
+++ src/components/menuBar/FolderMenu.js
@@ -3,23 +3,18 @@ import styles from "./FolderMenu.module.css";
 import titleImage from "../../assets/images/title.svg";
 import LocaleContext from "../../contexts/LocaleContext";
 
+// APP에서 뿌려줌
 export default function FolderMenu({ folderId }) {
-  const [isSelected, setIsSelected] = useState(false);
-  const [letter, setLetter] = useState("전체");
-  const [num, setNum] = useState(folderId);
-  const localeValue = useContext(LocaleContext);
+  // const [isSelected, setIsSelected] = useState(false);
+  // const [letter, setLetter] = useState("전체");
+  // const [num, setNum] = useState(folderId);
 
-  useEffect(() => {
-    setNum(folderId || "전체");
-
-    if (localeValue[folderId]) {
-      setLetter(localeValue[folderId]["folderName"] || "전체");
-      setIsSelected(true);
-    } else {
-      setLetter("전체");
-      setIsSelected(false);
-    }
-  }, [folderId]);
+  const localeValue = useContext(LocaleContext); // obj
+  const folder = localeValue[folderId];
+  // folder의 타입이 undefined가 아니면은 isSelected = ture, undefined이면은 false
+  const isSelected = typeof folder !== "undefined";
+  // optional chaining 사용 , folder의 folderName키가 undefined, 즉없으면은 전체를 나오게한다
+  const letter = folder?.folderName || "전체";
 
   return (
     <div className={styles.container}>
