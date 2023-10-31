3feeea1101e8350852584e80d2130be87205d6ad
qooktree1 <qooktree@gmail.com>
2023-10-31 15:12:28 +0900

    remove: 사용하지 않는 css-modules 파일들 삭제

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,11 +1,11 @@
-import * as S from "./styles"
+import * as S from './styles';
 
 const Button = ({ size, text }) => {
   return (
     <S.CTA size={size}>
       <span>{text}</span>
     </S.CTA>
-  )
-}
+  );
+};
 
-export default Button
+export default Button;
