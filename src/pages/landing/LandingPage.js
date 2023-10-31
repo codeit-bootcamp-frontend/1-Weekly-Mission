935c95e2e02db726529a25cf12e8ee5ac8213265
kanglocal <develocal25@gmail.com>
2023-11-01 05:45:12 +0900

    refactor : LandingSection에 개별 section을 만들던 로직을 삭제하고 template컴포넌트에서 children을 받아오도록 수정

--- src/pages/landing/LandingPage.js
+++ src/pages/landing/LandingPage.js
@@ -2,13 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import '../../styles/global.css';
 import './LandingPage.css';
-import { titleImg } from '../../constants/landingImages';
 import {
-  ManageSection,
-  SaveSection,
-  SearchSection,
-  ShareSection,
-} from '../../components/landingSection/LandingSection';
+  manageImg,
+  saveImg,
+  searchImg,
+  shareImg,
+  titleImg,
+} from '../../constants/landingImages';
+import { LandingSectionTemplate } from '../../components/landingSection/LandingSection';
 
 function LandingPage() {
   return (
@@ -26,10 +27,53 @@ function LandingPage() {
         </div>
       </header>
       <article>
-        <SaveSection />
-        <ManageSection />
-        <ShareSection />
-        <SearchSection />
+        <LandingSectionTemplate sectionName="save">
+          <h2>
+            <span className="font-gradation">원하는 링크</span>를<br />
+            저장하세요.
+          </h2>
+          <p>
+            나중에 읽고 싶은 글, 다시 보고 싶은 영상,
+            <br />
+            사고 싶은 옷, 기억하고 싶은 모든 것을
+            <br />한 공간에 저장하세요.
+          </p>
+          <img src={saveImg} alt="저장기능 설명 사진" />
+        </LandingSectionTemplate>
+        <LandingSectionTemplate sectionName="manage">
+          <h2>
+            링크를 폴더로
+            <br />
+            <span className="font-gradation">관리</span>하세요
+          </h2>
+          <p>
+            나만의 폴더를 무제한으로 만들고
+            <br />
+            다양하게 활용할 수 있습니다.
+          </p>
+          <img src={manageImg} alt="폴더관리기능 설명 사진" />
+        </LandingSectionTemplate>
+        <LandingSectionTemplate sectionName="share">
+          <h2>
+            저장한 링크를 <br />
+            <span className="font-gradation">공유</span>해 보세요.요
+          </h2>
+          <p>
+            여러 링크를 폴더에 담고 공유할 수 있습니다. <br />
+            가족, 친구, 동료들에게 쉽고 빠르게 링크를 <br />
+            공유해 보세요.
+          </p>
+          <img src={shareImg} alt="공유기능 설명 사진" />
+        </LandingSectionTemplate>
+        <LandingSectionTemplate sectionName="search">
+          <h2>
+            저장한 링크를
+            <br />
+            <span className="font-gradation">검색</span>해 보세요.
+          </h2>
+          <p>중요한 정보들을 검색으로 쉽게 찾아보세요.</p>
+          <img src={searchImg} alt="검색기능 설명 이미지" />
+        </LandingSectionTemplate>
       </article>
     </>
   );
