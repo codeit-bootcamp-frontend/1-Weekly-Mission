e6433ee30ea296cf11431ca8f9cccd7033ac3689
ayden <wpfekdml@naver.com>
2023-11-02 13:37:50 +0900

    feat(useAsync.js) : w9에 필요해질 거 같아서 추가

---
+++ src/hooks/useAsync.js
@@ -0,0 +1,30 @@
+import { useState, useEffect } from 'react';
+
+//
+function useAsync(asyncFunction, initialArgs = [], deps = [], skip = false) {
+  const [data, setData] = useState(null);
+  const [pending, setPending] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async (...initialArgs) => {
+    setPending(true);
+    setError(null);
+    try {
+      const fetchedData = await asyncFunction(...initialArgs);
+      setData(fetchedData);
+    } catch (error) {
+      setError(error);
+    } finally {
+      setPending(false);
+    }
+  };
+
+  useEffect(() => {
+    if (skip) return;
+    fetchData(initialArgs);
+  }, deps);
+
+  return { data, pending, error, fetchData };
+}
+
+export default useAsync;
