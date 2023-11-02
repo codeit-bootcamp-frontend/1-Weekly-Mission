ec8ea45558dfb8844b41d4be188ade7945d21312
ParkSohyunee <sohyunee016@gmail.com>
2023-11-02 15:47:12 +0900

    feat: Kebabmenu - 케밥메뉴 옵션에 따라 Modal 창 띄우기 구현

--- src/components/Kebabmenu.js
+++ src/components/Kebabmenu.js
@@ -1,8 +1,13 @@
 import { useState } from "react";
+import { createPortal } from "react-dom";
+
 import styled from "styled-components";
 import kebabIcon from "assets/kebab.svg";
+import ModalContainer from "./modal/ModalContainer";
+import AddFolder from "./modal/AddFolder";
+import DeleteFolder from "./modal/DeleteFolder";
 
-const Modal = styled.div`
+const PopoverMenu = styled.div`
   position: absolute;
   top: 20px;
   right: 0;
@@ -16,7 +21,7 @@ const Modal = styled.div`
   box-shadow: 0px 2px 8px 0px rgba(51, 50, 54, 0.1);
 `;
 
-const Button = styled.button`
+const DeleteOption = styled.button`
   padding: 7px 12px;
   font-size: 0.875rem;
   color: var(--color-text);
@@ -25,28 +30,48 @@ const Button = styled.button`
   cursor: pointer;
 `;
 
-const AddButton = styled(Button)`
+const AddOption = styled(DeleteOption)`
   color: var(--color-primary);
   background: var(--color-primary-bg);
 `;
 
 export default function KebabMenu() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenMenu, setIsOpenMenu] = useState(false);
+  const [iseOpenModal, setIsOpenModal] = useState(false);
+  const [selectedOption, setSelectedOption] = useState("");
 
-  const handleModalToggle = (e) => {
+  const handleMenuToggle = (e) => {
     e.preventDefault();
-    setIsOpen((prev) => !prev);
+    setIsOpenMenu((prev) => !prev);
+  };
+
+  const handleModalOpen = (e) => {
+    e.preventDefault();
+    setIsOpenModal(true);
+    setSelectedOption(e.target.id);
   };
 
   return (
     <>
-      <img src={kebabIcon} onClick={handleModalToggle} alt="memu" />
-      {isOpen && (
-        <Modal>
-          <Button>삭제하기</Button>
-          <AddButton>폴더에 추가</AddButton>
-        </Modal>
+      <img src={kebabIcon} onClick={handleMenuToggle} alt="memu" />
+      {isOpenMenu && (
+        <PopoverMenu onClick={handleModalOpen}>
+          <DeleteOption id="deleteLink">삭제하기</DeleteOption>
+          <AddOption id="addLink">폴더에 추가</AddOption>
+        </PopoverMenu>
       )}
+
+      {iseOpenModal &&
+        createPortal(
+          <ModalContainer onClose={() => setIsOpenModal(false)}>
+            {selectedOption === "addLink" ? (
+              <AddFolder />
+            ) : (
+              <DeleteFolder currentFolderName="test" />
+            )}
+          </ModalContainer>,
+          document.getElementById("portal"),
+        )}
     </>
   );
 }
