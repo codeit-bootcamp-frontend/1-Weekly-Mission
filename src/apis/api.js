6d8bd89bd6ac89acf26704b065418feab64dfd0e
ayden <wpfekdml@naver.com>
2023-11-02 15:12:53 +0900

    refactor(api.js) : 햇갈릴 수 있는 파라미터 이름 변경

--- src/apis/api.js
+++ src/apis/api.js
@@ -1,7 +1,7 @@
 const APIpoint = 'https://bootcamp-api.codeit.kr/api/';
 
-async function requestAPI(url, body) {
-  const response = await fetch(`${APIpoint}${url}`, body);
+async function requestAPI(url, option) {
+  const response = await fetch(`${APIpoint}${url}`, option);
   const result = await response.json();
   return { response, result };
 }
