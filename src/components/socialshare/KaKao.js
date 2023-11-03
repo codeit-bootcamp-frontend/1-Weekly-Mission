5b156ff5e484005f6b3603eae7f58dd7d7fd5d19
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-03 21:52:50 +0900

    refactor(KaKao.js): 동적param생성

--- src/components/socialshare/KaKao.js
+++ src/components/socialshare/KaKao.js
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import kakaochat from "../../assets/images/kakaochat.svg";
+import { useParams } from "react-router-dom";
+
 export default function KaKao() {
   const jsKey = "e90c940cb2e88d388b74702a56c46c05";
   const { Kakao } = window;
@@ -14,15 +16,19 @@ export default function KaKao() {
 
   const shareKakao = () => {
     const userID = 1;
-    const folderID = 7;
-
-    Kakao.Share.sendCustom({
-      templateId: 100292,
-      templateArgs: {
-        userId: userID,
-        folderId: folderID,
-      },
-    });
+    let folderID;
+    const currentURL = window.location.pathname;
+    const loc = currentURL.indexOf("/folder/");
+    if (loc !== -1) {
+      folderID = currentURL.split("/")[2];
+      Kakao.Share.sendCustom({
+        templateId: 100292,
+        templateArgs: {
+          userId: userID,
+          folderId: folderID,
+        },
+      });
+    }
   };
 
   return (
