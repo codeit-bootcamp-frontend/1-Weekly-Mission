f594db9a8356504c87e83fcb09daa1cee3ff5591
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-04 08:51:57 +0900

    feat(Meta.js): meta공유기능

--- src/components/socialshare/Meta.js
+++ src/components/socialshare/Meta.js
@@ -1,10 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import metachat from "../../assets/images/metachat.svg";
 
 export default function Meta() {
-  const shareFaceBook = () => {
-    const local = window.location.href;
-    window.open(`http://www.facebook.com/sharer.php?u=${local}`);
+  const { FB } = window;
+  useEffect(() => {
+    // cleanup안하면은 에러터짐..
+    window.fbAsyncInit = function () {
+      FB.init({
+        appId: "1059767891829351",
+        xfbml: true,
+        cookie: true,
+        version: "v18.0",
+      });
+    };
+  }, []);
+  const shareMeta = () => {
+    FB.ui(
+      {
+        // display: "popup",
+        // method: "share",
+        // href: "https://developers.facebook.com/docs/",
+        method: "feed",
+        name: "Facebook Dialogs",
+        link: "https://developers.facebook.com/docs/dialogs/",
+      },
+      function (response) {
+        if (response.status === "connected") {
+          console.log("logged in!");
+        }
+      }
+    );
   };
-  return <img src={metachat} alt="meta_chat" onClick={shareFaceBook} />;
+
+  const handleShare = () => {
+    window.open(
+      "http://www.facebook.com/sharer.php?u=https://niceeey.netlify.app/"
+    );
+  };
+
+  return <img src={metachat} alt="meta" onClick={handleShare}></img>;
 }
