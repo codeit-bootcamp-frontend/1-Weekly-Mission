0cf0ad9055a330168e59eb6a8c112167d484933f
Nahyun-Kang <tarea202001@gmail.com>
2023-11-03 12:13:22 +0900

    Fix(Folder) : 클립보드 URL 잘못 복사하는 문제 해결

--- src/hooks/useShareUrl.js
+++ src/hooks/useShareUrl.js
@@ -1,13 +1,13 @@
-import {useLocation} from 'react-router-dom'
+import { useLocation } from "react-router-dom";
 
-const UseShareUrl = (userId, folderId) =>{
+const UseShareUrl = (userId, folderId) => {
   const location = useLocation().pathname;
-  const wholeUrl = window.location.href
-  console.log(wholeUrl)
-  const hostAddress = wholeUrl.replace(`${location}?`,'');
-  console.log(hostAddress)
+  const wholeUrl = window.location.href;
+  console.log(wholeUrl);
+  const hostAddress = wholeUrl.replace(`${location}`, "");
+  console.log(hostAddress);
   const shareURL = `${hostAddress}/shared?user=${userId}&folder=${folderId}`;
-  return shareURL
-}
+  return shareURL;
+};
 
-export default UseShareUrl
+export default UseShareUrl;
