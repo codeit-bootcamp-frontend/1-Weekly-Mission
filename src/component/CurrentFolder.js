bfb78a2b969bc0854b9dd272e7c86f7f6ed1971e
손지은 <wiseag@naver.com>
2023-11-05 03:19:06 +0900

    feat(CurrentFolder.js) : 폴더 이름 변경 모달, 폴더 삭제 모달 생성

--- src/component/CurrentFolder.js
+++ src/component/CurrentFolder.js
@@ -3,7 +3,22 @@ import shareIcon from "../assets/img/share.svg";
 import penIcon from "../assets/img/pen.svg";
 import style from "./CurrentFolder.module.css";
 import clsx from "clsx";
+import { useState } from "react";
+import Modal from "./Modal";
 function CurrentFolder({ folderId, folders }) {
+  const [isChangeNameModal, setIsChanageNameModal] = useState(false);
+  const [isDeleteModal, setIsDeleteNameModal] = useState(false);
+
+  const handleChangeNameClick = () => {
+    setIsChanageNameModal(true);
+  };
+  const handleDeleteClick = () => {
+    setIsDeleteNameModal(true);
+  };
+  const handleExitClick = () => {
+    setIsChanageNameModal(false);
+    setIsDeleteNameModal(false);
+  };
   const currentFolder = folders.filter((folder) => folder.id == folderId);
   const folderName = currentFolder.length ? currentFolder[0].name : "전체";
   console.log(folderId, folders);
@@ -19,15 +34,29 @@ function CurrentFolder({ folderId, folders }) {
           <img src={shareIcon} alt="공유하기" />
           공유
         </button>
-        <button>
+        <button onClick={handleChangeNameClick}>
           <img src={penIcon} alt="이름변경하기" />
           이름변경
         </button>
-        <button>
+        <button onClick={handleDeleteClick}>
           <img src={trashIcon} alt="삭제하기" />
           삭제
         </button>
       </div>
+      {isChangeNameModal && (
+        <Modal
+          title="폴더 이름 변경"
+          data={folderName}
+          onExitClick={handleExitClick}
+        />
+      )}
+      {isDeleteModal && (
+        <Modal
+          title="폴더 삭제"
+          data={folderName}
+          onExitClick={handleExitClick}
+        />
+      )}
     </div>
   );
 }
