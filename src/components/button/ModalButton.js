768ee9b51c966e74a69736be1927298df1629cff
ParkSohyunee <sohyunee016@gmail.com>
2023-11-04 13:29:03 +0900

    style: 모달버튼 반응형 스타일 수정

--- src/components/button/ModalButton.js
+++ src/components/button/ModalButton.js
@@ -1,5 +1,6 @@
 import styled from "styled-components";
 import { MainButton } from "./Button";
+import { breakPoints } from "styles/media";
 
 const ModalBasicButton = styled(MainButton)`
   padding: 1rem 1.25rem;
@@ -7,6 +8,10 @@ const ModalBasicButton = styled(MainButton)`
   font-size: 1rem;
 
   background: ${({ action }) => (action === "delete" ? "var(--color-red)" : "")};
+
+  @media only screen and (${breakPoints.mobile}) {
+    width: 100%;
+  }
 `;
 
 export default function ModalButton({ action, label }) {
