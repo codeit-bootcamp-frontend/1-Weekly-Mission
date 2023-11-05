be60e3c3f59faa3d2f005c83f5d78af5d4e503d4
ayden <wpfekdml@naver.com>
2023-11-06 06:57:12 +0900

    feat(Share.js) : 공유하기 버튼 기능 추가

--- src/modals/Share.js
+++ src/modals/Share.js
@@ -1,14 +1,57 @@
 import styles from './Share.module.css';
-import shareKakao from '../assets/images/share_kakao.png';
+import shareKakaoTalk from '../assets/images/share_kakao.png';
 import shareFacebook from '../assets/images/share_facebook.png';
 import shareLink from '../assets/images/share_link.png';
 import ModalHeader from '../components/ModalHeader/ModalHeader';
+import { useEffect } from 'react';
 
 function Share({ children }) {
+  const url = '';
+  const userID = '';
+  const folderID = '';
+
+  const { Kakao } = window;
+
+  useEffect(() => {
+    Kakao.cleanup();
+    Kakao.init('315e98490c74ca221a6889ab7d1bbd56');
+  }, [Kakao]);
+
+  const shareKakao = () => {
+    Kakao.Share.sendDefault({
+      objectType: 'text',
+      text: `Linkbrary`,
+      link: {
+        mobileWebUrl: `${url}/shared?user=${userID}&folder=${folderID}`,
+        webUrl: `${url}/shared?user=${userID}&folder=${folderID}`,
+      },
+    });
+  };
+
   const shareItems = [
-    { img: shareKakao, sub: '카카오톡', onClick: '' },
-    { img: shareFacebook, sub: '페이스북', onClick: '' },
-    { img: shareLink, sub: '링크 복사', onClick: '' },
+    {
+      img: shareKakaoTalk,
+      sub: '카카오톡',
+      onClick: shareKakao /* 도메인 등록이 필요하다고 함 */,
+    },
+    {
+      img: shareFacebook,
+      sub: '페이스북',
+      onClick: () => {
+        window.open(
+          `http://www.facebook.com/sharer.php?u=${url}/shared?user=${userID}&folder=${folderID}`
+        );
+      },
+    },
+    {
+      img: shareLink,
+      sub: '링크 복사',
+      onClick: () => {
+        navigator.clipboard.writeText(
+          `${url}/shared?user=${userID}&folder=${folderID}`
+        );
+      },
+    },
   ];
 
   return (
