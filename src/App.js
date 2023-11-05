9c185ef13dff1bd658dffb80cc6d9e1a28cfafc7
NIX <lsc58461@naver.com>
2023-11-05 17:46:43 +0900

    style: App으로 global 스타일 옮김

--- src/App.js
+++ src/App.js
@@ -1,15 +1,25 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./colors.css";
+import "./reset.css";
 import FolderPage from "./pages/FolderPage";
 import SharedPage from "./pages/SharedPage";
+import Layout from "./components/layout/Layout";
+import NotFoundLink from "./components/Card/NotFoundLink";
+import { UserIdProvider } from "./contexts/UersIdContext";
 
 const App = () => {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/folder" element={<FolderPage />} />
-        <Route path="/shared" element={<SharedPage />} />
-      </Routes>
-    </BrowserRouter>
+    <UserIdProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/folder" element={<FolderPage />} />
+            <Route path="/shared" element={<SharedPage />} />
+            <Route path="*" element={<NotFoundLink />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </UserIdProvider>
   );
 };
 
