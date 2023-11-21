57fffb8c4c19ab1f340096c3024eb1790748d805
Soeun Kim <summerkim0825@gmail.com>
2023-11-21 17:36:16 +0900

    Rename: 전체 폴더 구조 수정

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
-import Nav from "./Nav";
-import Footer from "./Footer";
+import Nav from "./components/Nav";
+import Footer from "./components/Footer";
 
 function App() {
   return (
