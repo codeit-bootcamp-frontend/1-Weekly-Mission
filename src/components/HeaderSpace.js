64f45e83df57d7eed23cc6458152200bf66f9408
고민혁 <yeppi9801@gmail.com>
2023-11-24 00:36:24 +0900

    feat: 스크롤 기능 구현 (임시)

--- src/components/HeaderSpace.js
+++ src/components/HeaderSpace.js
@@ -1,14 +1,39 @@
 import "../styles/navheader.css";
 import "../styles/reset.css";
 import linkAdd from "../images/link.svg";
-import { useState } from "react";
+import { useRef, useState, useEffect } from "react";
 
 function HeaderSpace({ profiles, openMAF }) {
   const [text, setText] = useState("");
+  const targetRef = useRef("");
+  // const [scroll, setScroll] = useState('')
+
+  window.addEventListener("scroll", (e) => {
+    if (window.scrollY > 300 && window.scrollY < 1100) {
+      document
+        .querySelector(".linkAdd-background")
+        .classList.add("show-bottom");
+    } else {
+      document
+        .querySelector(".linkAdd-background")
+        .classList.remove("show-bottom");
+    }
+  });
 
   const handleChangeText = (e) => setText(e.target.value);
   const handleTypingUrl = (e) => openMAF(e, text);
 
+  // const fixBottom = () => {
+  //   document.querySelector(".linkAdd-bar-wrapper").classList.add("show-bottom");
+  // };
+
+  // useEffect(() => {
+  //   let observer = new IntersectionObserver(fixBottom, {
+  //     threshold: 0.1,
+  //   });
+
+  //   observer.observe(targetRef.current);
+  // }, []);
   return (
     <>
       {profiles !== undefined ? (
@@ -28,26 +53,31 @@ function HeaderSpace({ profiles, openMAF }) {
           </div>
         </header>
       ) : (
-        <header className=" folder-header">
-          <div className="linkAdd-bar-wrapper">
-            <input
-              name="text"
-              value={text}
-              className="linkAdd-bar"
-              placeholder="링크를 추가해보세요"
-              onChange={handleChangeText}
-            />
+        <>
+          <header className=" folder-header">
+            <div className="linkAdd-background">
+              <div className="linkAdd-bar-wrapper">
+                <input
+                  name="text"
+                  value={text}
+                  className="linkAdd-bar"
+                  placeholder="링크를 추가해보세요"
+                  onChange={handleChangeText}
+                />
 
-            <img src={linkAdd} className="linkAdd-bar-image" alt=" " />
-            <button
-              onClick={handleTypingUrl}
-              className="cta cta-short"
-              href="/"
-            >
-              <span>추가하기</span>
-            </button>
-          </div>
-        </header>
+                <img src={linkAdd} className="linkAdd-bar-image" alt=" " />
+                <button
+                  onClick={handleTypingUrl}
+                  className="cta cta-short"
+                  href="/"
+                >
+                  <span>추가하기</span>
+                </button>
+              </div>
+            </div>
+          </header>
+          <div id="scroll-target"></div>
+        </>
       )}
     </>
   );
