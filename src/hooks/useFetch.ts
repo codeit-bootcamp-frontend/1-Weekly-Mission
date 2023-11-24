c610292ed1f30f5ef16c39e7cbb7b50226f06f2e
ParkSohyunee <sohyunee016@gmail.com>
2023-11-24 16:54:14 +0900

    커스텀 훅 useFetch 타입 지정

--- src/hooks/useFetch.ts
+++ src/hooks/useFetch.ts
@@ -1,14 +1,16 @@
 import { useState } from "react";
 
-export default function useFetch(asyncFunc) {
+export default function useFetch(asyncFunc: (...args: any[]) => Promise<any>) {
   const [isLoading, setIsLoading] = useState(true);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState<null | Error>(null);
 
-  const wrappedFunction = async (...args) => {
+  const wrappedFunction = async (...args: any[]) => {
     try {
       return await asyncFunc(...args);
     } catch (error) {
-      setError(error);
+      if (error instanceof Error) {
+        setError(error);
+      }
       return;
     } finally {
       setIsLoading(false);
