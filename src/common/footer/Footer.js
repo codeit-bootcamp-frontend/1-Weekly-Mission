41b7a287ab50d20089d5907a257aa911cc3a447c
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-26 08:06:52 +0900

    작업중

--- src/common/footer/Footer.js
+++ src/common/footer/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useRef, useState } from "react";
 
 import styles from "./Footer.module.css";
 
@@ -6,10 +6,39 @@ import { ReactComponent as InstagramIcon } from "../../assets/images/instagram.s
 import { ReactComponent as MetaIcon } from "../../assets/images/meta.svg";
 import { ReactComponent as TwitterIcon } from "../../assets/images/twitter.svg";
 import { ReactComponent as YoutubeIcon } from "../../assets/images/youtube.svg";
+import ObserverContext from "../../contexts/ObserverContext";
 
 export default function Footer() {
+  const {
+    footerRef: { footerRef, isFooterVisible, setIsFooterVisible },
+  } = useContext(ObserverContext);
+
+  const options = {
+    root: null,
+    rootMargin: "0px 0px 0px 0px",
+    threshold: 1,
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver((entries, observer) => {
+      const [IntersectionObserverEntry] = entries;
+
+      if (IntersectionObserverEntry.isIntersecting) {
+        setIsFooterVisible(IntersectionObserverEntry.isIntersecting);
+      }
+
+      if (!IntersectionObserverEntry.isIntersecting) {
+        setIsFooterVisible(IntersectionObserverEntry.isIntersecting);
+      }
+    }, options);
+
+    if (footerRef.current) {
+      observer.observe(footerRef.current);
+    }
+  }, []);
+
   return (
-    <div className={styles.container}>
+    <div className={styles.container} ref={footerRef}>
       <span className={styles.codeit}>©codeit - 2023</span>
       <div className={styles.policy}>
         <span>Privacy Policy</span>
