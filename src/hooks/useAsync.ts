8d383f90e8a7fa3f62e7d10cb34aac3edb8a0e96
sunny-son <thstkd92@gmail.com>
2023-11-25 14:48:27 +0900

    refactor : useAsync 타입 추가

--- src/hooks/useAsync.ts
+++ src/hooks/useAsync.ts
@@ -1,15 +1,15 @@
 import { useCallback, useState } from 'react';
 
-function useAsync(asyncFunction) {
+function useAsync(asyncFunction: Promise<any>) {
   const [pending, setPending] = useState(false);
   const [error, setError] = useState(null);
 
-  const wrappedFunction = useCallback(async (...args) => {
+  const wrappedFunction = useCallback(async (...args: any[]) => {
     setPending(true);
     setError(null);
     try {
-      return await asyncFunction(...args);
-    } catch (error) {
+      return (await asyncFunction)(...args);
+    } catch (error: any) {
       setError(error);
     } finally {
       setPending(false);
