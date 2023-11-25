99c26fc474bf504be942403785873683c32b4923
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-26 07:21:38 +0900

    observer만드는중

--- src/components/button/HeaderButton.js
+++ src/components/button/HeaderButton.js
@@ -1,10 +1,11 @@
-import React, { useEffect, useRef } from "react";
+import React, { useContext, useEffect, useRef } from "react";
 import styles from "./HeaderButton.module.css";
 import linkImage from "../../assets/images/link.svg";
 import HeaderModal from "../../common/modal/HeaderModal";
 import { useState } from "react";
+import ObserverContext from "../../contexts/ObserverContext";
 
-export default function HeaderButton({ observerRef }) {
+export default function HeaderButton() {
   const [openModal, setOpenModal] = useState(false);
   const [inputText, setInputText] = useState("");
 
@@ -16,47 +17,39 @@ export default function HeaderButton({ observerRef }) {
     setInputText(e.target.value);
   };
 
-  const ref = useRef(null);
-
-  const [isVisible, setIsvisible] = useState(true);
-
   const options = {
     root: null,
     rootMargin: "0px 0px 0px 0px",
     threshold: 0,
   };
-
-  useEffect(() => {
-    const observer = new IntersectionObserver((entries, observer) => {
-      const [IntersectionObserverEntry] = entries;
-
-      setIsvisible(IntersectionObserverEntry.isIntersecting);
-    }, options);
-
-    if (ref.current) {
-      observer.observe(ref.current);
-    }
-  }, [ref, isVisible]);
-
-  console.log(isVisible);
+  const {
+    headerRef: { headerRef, isHeaderVisible, setIsHeadervisible },
+    footerRef: { isFooterVisible },
+  } = useContext(ObserverContext);
 
   return (
-    <div ref={ref} className={styles.container}>
-      <div className={styles.button__container}>
-        <div className={styles.input__container}>
-          <img className={styles.image} src={linkImage} alt="링크이미지" />
-          <input
-            className={styles.input}
-            placeholder="링크를 추가해보세요"
-            onChange={handleInput}
-          />
+    <div ref={headerRef}>
+      <div
+        className={`${styles.container} ${
+          isHeaderVisible ? styles.sticky : ""
+        }`}
+      >
+        <div className={styles.button__container}>
+          <div className={styles.input__container}>
+            <img className={styles.image} src={linkImage} alt="링크이미지" />
+            <input
+              className={styles.input}
+              placeholder="링크를 추가해보세요"
+              onChange={handleInput}
+            />
+          </div>
+          <button className={styles.add__button} onClick={handleModal}>
+            추가하기
+          </button>
+          {openModal && (
+            <HeaderModal setterFunc={setOpenModal} inputLink={inputText} />
+          )}
         </div>
-        <button className={styles.add__button} onClick={handleModal}>
-          추가하기
-        </button>
-        {openModal && (
-          <HeaderModal setterFunc={setOpenModal} inputLink={inputText} />
-        )}
       </div>
     </div>
   );
