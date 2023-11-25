a7d24bfc1ff876b7e6d8f0acf295a953001fe8a8
ParkSohyunee <sohyunee016@gmail.com>
2023-11-25 15:49:16 +0900

    Feat(FolderContainer): 폴더페이지 검색바 기능 구현

--- src/components/inputs/Searchbar.tsx
+++ src/components/inputs/Searchbar.tsx
@@ -1,11 +1,16 @@
+import { ChangeEvent } from "react";
 import * as S from "./SearchbarStyle";
 import searchIcon from "assets/Search.svg";
 
-export default function Searchbar() {
+interface SearchbarProps {
+  handleOnChangeInput: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export default function Searchbar({ handleOnChangeInput }: SearchbarProps) {
   return (
     <S.Search>
       <S.Icon src={searchIcon} alt="search" />
-      <S.Input type="text" placeholder="링크를 검색해 보세요." />
+      <S.Input onChange={handleOnChangeInput} type="text" placeholder="링크를 검색해 보세요." />
     </S.Search>
   );
 }
--- src/pages/folder/FolderContainer.tsx
+++ src/pages/folder/FolderContainer.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect, useState } from "react";
+import { ChangeEvent, useContext, useEffect, useState } from "react";
 import useFetch from "hooks/useFetch";
 
 import * as S from "./FolderStyle";
@@ -22,16 +22,45 @@ const USER_ID = 1;
 
 export default function Folder() {
   const [links, setLinks] = useState<LinkData[]>([]);
+  const [filteredLinks, setFilteredLinks] = useState<LinkData[]>([]);
   const [folders, setFolders] = useState<FolderData[]>([]);
   const [selected, setSelected] = useState(DEFAULT);
   const [selectedFolderId, setSelectedFolderId] = useState("");
   const [isOpenModal, setIsOpenModal] = useState(false);
   const [addLinkValue, setAddLinkValue] = useState("");
+  const [keyword, setKeyword] = useState("");
 
   const { isLoading, error, wrappedFunction: getLinksAsyncFunc } = useFetch(getAllLinks);
   const { error: errorFolder, wrappedFunction: getFoldersAsyncFunc } = useFetch(getAllFolders);
 
   const { handleFolderUpdate } = useContext(FolderContext);
+
+  /**
+   * 검색바 구현 todo
+   * 1. keyword 상태 useState
+   * 2. onChange / onKeyUp 이벤트시 setState에 저장
+   * 3. state가 변할때마다 필터하는 로직 구현하기
+   * 4. 리팩토링 - 디바운싱
+   * 5. share 컴포넌트랑 공유할 수 있도록 재사용성 고려
+   * 6. x 버튼 - 클릭시 로직 구현하기
+   */
+
+  const handleOnChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+    const searchedLinks = checkMatchedAllLinks(e.target.value, links);
+    setFilteredLinks(searchedLinks.length !== 0 ? searchedLinks : links);
+  };
+
+  const checkMatchedAllLinks = (keyword: string, links: LinkData[]) => {
+    const filteredLinks = links.filter((link) => {
+      return (
+        (link.title && link.title.includes(keyword)) ||
+        (link.description && link.description.includes(keyword)) ||
+        (link.url && link.url.includes(keyword))
+      );
+    });
+    return filteredLinks;
+  };
 
   const handleSelectedFolder = (category: string) => {
     setSelected(category);
@@ -49,6 +78,7 @@ export default function Folder() {
     const { data: folderData } = await getFoldersAsyncFunc(USER_ID);
 
     setLinks(linkData);
+    setFilteredLinks(linkData);
     setFolders(folderData);
     updateFolderList(folderData);
   };
@@ -84,7 +114,7 @@ export default function Folder() {
           <FolderHero onChangeAddLink={handleAddLink} addLinkValue={addLinkValue} />
         </S.HeroContainer>
         <S.Contents>
-          <Searchbar />
+          <Searchbar handleOnChangeInput={handleOnChangeInput} />
           <S.MenuContainer>
             <Categories
               categories={[DEFAULT, ...folderNames]}
@@ -105,7 +135,7 @@ export default function Folder() {
                 <S.SubTitle>{selected}</S.SubTitle>
                 {selected !== DEFAULT && <Options selected={selected} />}
               </S.MenuContainer>
-              <CardList links={links} />
+              <CardList links={filteredLinks} />
             </>
           )}
         </S.Contents>
