89d32abc4010aa911cc3540d6087b151b197d375
ayden <wpfekdml@naver.com>
2023-11-26 21:21:22 +0900

    fix(styles) : 폴더 위치 정리

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Main from "./Main";
 
-import "./styles/reset.css";
-import "./styles/index.css";
+import "./assets/styles/reset.css";
+import "./assets/styles/index.css";
 import "./assets/fonts/pretendard/pretendard.css";
 
 const rootElement = document.getElementById("root");
