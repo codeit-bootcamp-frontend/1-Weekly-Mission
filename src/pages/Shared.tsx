9846fcd4293b80678a9a1e40085efcc3f0d8bbf2
sunny-son <thstkd92@gmail.com>
2023-11-26 13:35:37 +0900

    refactor : Shared Page 타입 추가

--- src/pages/Shared.tsx
+++ src/pages/Shared.tsx
@@ -1,22 +1,37 @@
 import SharedProfile from '../components/Shared/SharedProfile';
 import { useCallback, useEffect, useState } from 'react';
-import useAsync from '../hooks/useAsync';
 import { getShared } from '../api/api';
 import SharedMain from '../components/Shared/SharedMain';
+import { CardProps } from '../components/Card/CardList';
+
+type Shared = {
+  name: string;
+  owner?: {
+    profileImageSource: string;
+    name: string;
+    folderName: string;
+  }
+  links: CardProps[];
+}
+
+const INIT_SHARED_PROFILE = {
+  avatarUrl: '',
+  ownerName: '',
+  folderName: '',
+}
 
 function Shared() {
-  const [sharedProfile, setSharedProfile] = useState(null);
-  const [cards, setCards] = useState([]);
-  const [, sharedLoadingError, getSharedAsync] = useAsync(getShared);
+  const [sharedProfile, setSharedProfile] = useState(INIT_SHARED_PROFILE);
+  const [cards, setCards] = useState<CardProps[]>([]);
 
   const handleLoad = useCallback(
     async () => {
-      const result = await getSharedAsync();
+      const result = await getShared();
       if (!result) {
         return;
       }
 
-      const { name, owner, links: cards } = { ...result.folder };
+      const { name, owner, links: cards }: Shared = { ...result.folder };
 
       setSharedProfile({
         avatarUrl: owner?.profileImageSource ?? '',
@@ -24,7 +39,7 @@ function Shared() {
         folderName: name,
       });
       setCards(cards);
-    }, [getSharedAsync],
+    }, [],
   );
 
   useEffect(() => {
@@ -34,7 +49,7 @@ function Shared() {
   return (
     <>
       <SharedProfile sharedProfile={sharedProfile} />
-      <SharedMain cards={cards} cardsLoadingError={sharedLoadingError} />
+      <SharedMain cards={cards} />
     </>
   )
 }
