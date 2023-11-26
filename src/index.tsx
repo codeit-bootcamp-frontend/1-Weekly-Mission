7024209435e8f8ebe72d9f4f7918e165729cefb2
HellenHong <hongjw030@naver.com>
2023-11-26 15:05:32 +0900

    Fix: 더이상 사용되지 않는 메소드 수정

--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,11 @@
-import ReactDOM from "react-dom";
+import React from "react";
+import * as ReactDOM from "react-dom/client";
 import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootDiv = document.getElementById("root");
+if (rootDiv) {
+  const root = ReactDOM.createRoot(rootDiv);
+  root.render(<App />);
+  reportWebVitals();
+}
