a2b0de5c65957f3958d9e2e8c4c82d1d124ddec0
Soeun Kim <summerkim0825@gmail.com>
2023-11-26 19:38:35 +0900

    Refactor: Mainpage 초기화

--- src/pages/MainPage/MainPage.tsx
+++ src/pages/MainPage/MainPage.tsx
@@ -1,13 +1,6 @@
-// import Input from 'components/Input';
-// import Card from 'components/CardList/Card';
-// import SelectMenu from 'components/SelectMenu/SelectMenu';
-import ModalEdit from 'components/Modal/ModalEdit';
-import Button from 'components/Button';
-
 function MainPage() {
   return (
     <>
-      <Button>button</Button>
     </>
   );
 }
