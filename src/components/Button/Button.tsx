f00b3ca7b4e165f2290ade257d1a6e52bd64271b
Soeun Kim <summerkim0825@gmail.com>
2023-11-26 19:44:08 +0900

    Refactor: Button 컴포넌트 type 수정

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,16 +1,12 @@
 import * as S from "./Button.style";
 
 type ButtonProps = {
-  className: string;
   children: React.ReactNode;
-  onClick: () => void;
 };
 
 function Button(props: ButtonProps) {
   return (
     <S.Button
-      className={props.className}
-      onClick={props.onClick}
     >
       {props.children}
     </S.Button>
