1ab1ab3111adbc4642ee750abac094e46003d15a
lsc58461 <lsc58461@naver.com>
2023-11-28 13:01:51 +0900

    fix: 제대로 필터링 안되던 문제

--- src/components/Search/Search.tsx
+++ src/components/Search/Search.tsx
@@ -30,13 +30,13 @@ const Search = ({ linksListData, onChange }: Props) => {
       value.trim() === ""
         ? linksListData.data // 검색어가 없을 때 전체 리스트를 유지
         : linksListData.data.filter((link) =>
-            Object.values(link).some((value) =>
-              value && typeof value === "string"
-                ? value.toLowerCase().includes(value.toLowerCase())
+            Object.values(link).some((val) =>
+              val && typeof val === "string"
+                ? val.toLowerCase().includes(value.toLowerCase())
                 : false
             )
           );
-
+    console.log(value, filteredData);
     onChange(filteredData.length > 0 ? { data: filteredData } : undefined);
   };
 
