a1d3b7e48bcf6180f9c520af344358a118f25185
hanseulhee <3021062@gmail.com>
2023-11-29 20:09:13 +0900

    apply @tanstack/react-query

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,25 @@
+import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
+import Footer from "src/components/Footer";
+import Nav from "src/components/Nav";
+import { queryClient } from "src/libs/apis/queryClient";
 import GlobalStyle from "src/styles/GlobalStyle";
 import theme from "src/styles/Theme/theme";
 import { ThemeProvider } from "styled-components";
 
-export default function App({ Component, pageProps }: AppProps) {
+function App({ Component, pageProps }: AppProps) {
   return (
-    <ThemeProvider theme={theme}>
-      <GlobalStyle />
-      <Component {...pageProps} />
-    </ThemeProvider>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Nav />
+        <Component {...pageProps} />
+        <Footer />
+      </ThemeProvider>
+    </QueryClientProvider>
   );
 }
+
+export default App;
