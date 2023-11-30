897d43eb11bab410e0432c4267879bb0f61c41b2
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-11-30 13:52:35 +0900

    refactor(share.ts):코드리뷰반영

--- api/share.ts
+++ api/share.ts
@@ -1,25 +1,16 @@
-export type getUserResponse = {
-  id: number;
-  name: string;
-  email: string;
-  profileImageSource: string;
-};
+const baseUrl = new URL("https://bootcamp-api.codeit.kr");
+const getUrl = (path: string) => new URL(path, baseUrl);
 
-export const getShareUserData = async (): Promise<
-  [Response, getUserResponse]
-> => {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/user"
-  );
-
+export const getShareUserData = async () => {
+  const requestUrl = getUrl("/api/sample/user");
+  const response = await fetch(requestUrl);
   const jsonData = await response.json();
   return [response, jsonData];
 };
 
-export const getShareData = async (): Promise<[Response, string]> => {
-  const response = await fetch(
-    "https://bootcamp-api.codeit.kr/api/sample/folder"
-  );
+export const getShareData = async () => {
+  const requestUrl = getUrl("/api/sample/folder");
+  const response = await fetch(requestUrl);
   const jsonData = await response.json();
   return [response, jsonData];
 };
