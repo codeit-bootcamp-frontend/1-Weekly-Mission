820ff71d63128aa52e65df69f35a4eb256f5c01e
ParkSohyunee <sohyunee016@gmail.com>
2023-12-01 15:20:04 +0900

    Fix(Loading): 로딩스피너 라이브러리 dynamic import 적용 - SSR시 style가 변경되는 경고 해결

--- components/Loading.tsx
+++ components/Loading.tsx
@@ -1,4 +1,4 @@
-import { GridLoader } from "react-spinners";
+import dynamic from "next/dynamic";
 import styled from "styled-components";
 
 const Container = styled.div`
@@ -7,10 +7,14 @@ const Container = styled.div`
   justify-content: center;
 `;
 
+const DynamicLoader = dynamic(() => import("react-spinners/GridLoader"), {
+  ssr: false,
+});
+
 export default function Loading() {
   return (
     <Container>
-      <GridLoader color="#6d6afe" size={20} speedMultiplier={1} />
+      <DynamicLoader color="#6d6afe" size={20} speedMultiplier={1} />
     </Container>
   );
 }
