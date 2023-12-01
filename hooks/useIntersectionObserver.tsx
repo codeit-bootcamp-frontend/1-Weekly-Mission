5c2cc6c75202763e1b124a88225df450cf0755fd
ParkSohyunee <sohyunee016@gmail.com>
2023-12-01 20:49:45 +0900

    Feat: Observer custom hook

---
+++ hooks/useIntersectionObserver.tsx
@@ -0,0 +1,30 @@
+import { useLayoutEffect, useRef, useState } from "react";
+
+export const useIntersectionObserver = <T extends HTMLElement>(
+  once: boolean = false,
+  options?: IntersectionObserverInit,
+) => {
+  const ref = useRef<T>(null);
+  const [isIntersecting, setIsIntersecting] = useState(false);
+
+  useLayoutEffect(() => {
+    if (!ref.current) return;
+
+    const observer = new IntersectionObserver((entries: IntersectionObserverEntry[]) => {
+      setIsIntersecting(entries[0].isIntersecting);
+      if (once && entries[0].isIntersecting) {
+        observer.unobserve(entries[0].target);
+      }
+    }, options);
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      observer.disconnect();
+    };
+  }, [options, once]);
+
+  return { ref, isIntersecting };
+};
