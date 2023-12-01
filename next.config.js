fc79a086892ed10827a5e55e6b89a5156b78aeae
youdame <112458620+youdame@users.noreply.github.com>
2023-12-01 22:26:42 +0900

    Feat : 폴더 이름 변경

--- next.config.js
+++ next.config.js
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-};
+
+const path = require("path");
 
 module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, "components/")],
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
