de6e3ec58a680fcfdff62ca3fa6f7371298404c1
wecaners <wecaners@naver.com>
2023-12-03 02:53:46 +0900

    feat: landing page 디자인 추가

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+import styles from "./index.module.css";
+import landingGif from "@/public/images/landing.gif";
+import Link from "next/link";
+
 export default function Home() {
-  return <div>랜딩페이지입니다.</div>;
+  return (
+    <div className={styles.container}>
+      <Image src={landingGif} alt="랜딩 움짤" />
+      <Link href={"/folder"} className="button button--piyo">
+        <button className={styles.button}>FolderPage로 이동하기! 🚀</button>
+      </Link>
+    </div>
+  );
 }
