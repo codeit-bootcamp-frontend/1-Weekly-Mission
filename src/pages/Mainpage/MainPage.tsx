b9cd43beaf8bb222c9252909e47e1bd329eb5744
arky02 <kyean07@naver.com>
2023-12-02 15:25:14 +0900

    next application

--- src/pages/Mainpage/MainPage.tsx
+++ src/pages/Mainpage/MainPage.tsx
@@ -1,5 +1,6 @@
 import Header from "../../components/Header/Header";
 import ReactModal from "react-modal";
+import Input from "../../components/TextInputs/Input";
 
 function MainPage() {
   ReactModal.setAppElement("#root");
@@ -7,6 +8,7 @@ function MainPage() {
   return (
     <>
       <Header />
+      <Input></Input>
     </>
   );
 }
