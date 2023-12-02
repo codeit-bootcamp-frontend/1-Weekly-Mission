993093b1722a8761c1f4b5e1f44d0040f7f12086
손지은 <wiseag@naver.com>
2023-12-02 18:51:11 +0900

    deploy test

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -5,6 +5,7 @@ import SocialSign from "@/src/components/Sign/SocialSign";
 function SignInPage() {
   return (
     <>
+      deploy test
       <SignHeader type="signin" />
       <SignIpnut type="email" label="이메일" htmlFor="email_input" />
       <SignIpnut type="password" label="비밀번호" htmlFor="pwd_input" />
