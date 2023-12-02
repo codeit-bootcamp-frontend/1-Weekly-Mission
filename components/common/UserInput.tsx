2dbc7d907565b6f0d0aba8951116d853e75176af
전수빈 <soobin08011812@gmail.com>
2023-12-02 21:54:57 +0900

    Feat: userInput 추가

---
+++ components/common/UserInput.tsx
@@ -0,0 +1,93 @@
+import Image from "next/image";
+import styled from "styled-components";
+import EyeOn from "@/public/assets/common/img_eyeOn.png";
+import EyeOff from "@/public/assets/common/img_eyeOff.png";
+import { useState } from "react";
+
+const obj = {
+  email: {
+    label: "이메일",
+    type: "email",
+  },
+  password: {
+    label: "비밀번호",
+    type: "password",
+  },
+  passwordConfirm: {
+    label: "이메일",
+    type: "password",
+  },
+};
+
+interface ILabelProps {
+  label: "email" | "password" | "passwordConfirm";
+}
+
+const UserInput = (props: ILabelProps) => {
+  const { label } = props;
+  const [isOn, setIsOn] = useState<boolean>(false);
+
+  return (
+    <InputContainer>
+      <label htmlFor={label}>{obj[label].label}</label>
+      <div className="inputBox">
+        <Input type={isOn ? "text" : obj[label].type} id={label + "Input"} />
+        {label !== "email" && (
+          <Image
+            src={isOn ? EyeOn : EyeOff}
+            width="16"
+            height="16"
+            alt="eyeImg"
+            onClick={() => setIsOn(!isOn)}
+          />
+        )}
+      </div>
+    </InputContainer>
+  );
+};
+
+export default UserInput;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+  width: 40rem;
+
+  label {
+    color: #000;
+    font-size: 1.4rem;
+    font-weight: 400;
+  }
+
+  .inputBox {
+    position: relative;
+
+    img {
+      position: absolute;
+      top: 2rem;
+      right: 1.5rem;
+      cursor: pointer;
+    }
+  }
+`;
+
+const Input = styled.input`
+  box-sizing: border-box;
+  border-radius: 0.8rem;
+  padding: 1.8rem 1.5rem;
+  background: var(--white);
+  border: 1px solid var(--gray20);
+  width: 40rem;
+  height: 6rem;
+  margin-bottom: 2.4rem;
+  color: var(--gray100);
+  font-size: 1.6rem;
+  font-weight: 400;
+  line-height: 2.4rem;
+
+  &:focus {
+    border: 1px solid var(--primary);
+    outline: none;
+  }
+`;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,14 @@
+import UserInput from "@/components/common/UserInput";
+
 export default function Home() {
   return (
-    <>
+    <div
+      style={{
+        marginTop: "10rem",
+      }}
+    >
       <div>main</div>
-    </>
+      <UserInput label="email" />
+    </div>
   );
 }
