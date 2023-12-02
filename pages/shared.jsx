dda2340baf33d0208a4387a56c3c7552bfba5f56
손지은 <wiseag@naver.com>
2023-12-02 19:07:10 +0900

    style(shared.jsx) : css 수정

--- pages/shared.jsx
+++ pages/shared.jsx
@@ -4,6 +4,7 @@ import Search from "@/src/components/Search/Search";
 import SharedPageCards from "@/src/components/Cards/SharedPageCards";
 import style from "./shared.module.css";
 import FolderInfo from "@/src/components/FolderInfo/FolderInfo";
+import { Nav, Footer } from "@/src/containers";
 
 function SharedPage() {
   const [folderInfo, setFolderInfo] = useState({});
@@ -27,17 +28,24 @@ function SharedPage() {
   }, []);
 
   return (
-    <div className={style.root}>
+    <>
+      <Nav />
+
       <FolderInfo
         userName={folderInfo?.owner?.name}
         folderName={folderInfo?.name}
         profileImageSource={folderInfo?.owner?.profileImageSource}
       />
-      <div className={style.section}>
-        <Search />
-        <SharedPageCards cards={cards} />
+      <div className={style.root}>
+        <div className={style.section}>
+          <Search />
+          <div className={style.folderSection}>
+            <SharedPageCards cards={cards} />
+          </div>
+        </div>
       </div>
-    </div>
+      <Footer />
+    </>
   );
 }
 export default SharedPage;
