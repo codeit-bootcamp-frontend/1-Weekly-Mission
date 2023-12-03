de0df8057f188c6794a53f7b1d6068be347987f2
Haneul-Kim <4188989@naver.com>
2023-12-03 20:56:46 +0900

    fix (Navigation) : useRef -> js변수 변경(두번 클릭하는 문제)

--- components/Nav/Navigation.tsx
+++ components/Nav/Navigation.tsx
@@ -1,6 +1,6 @@
 import Avatar from "@/components/Nav/Avatar/Avatar";
 import Logo from "@/components/Nav/Avatar/Logo";
-import { Nav, StyledLink } from "@/components/Nav/Navigation.styled";
+import { Background, Nav, StyledLink } from "@/components/Nav/Navigation.styled";
 import { useEffect, useRef } from "react";
 
 interface Props {
@@ -8,24 +8,22 @@ interface Props {
   $page?: string;
 }
 
-export default function Navigation({ id, $page = "" }: Props) {
-  const locate = useRef("");
+let locate = "/signin";
 
+export default function Navigation({ id, $page = "" }: Props) {
   useEffect(() => {
     const accessToken = sessionStorage.getItem("accessToken");
     if (accessToken) {
-      locate.current = "/folder";
+      locate = "/folder";
       return;
     }
-    locate.current = "/signin";
-    return;
   }, []);
   return (
     <>
-      {/* <Background /> */}
+      <Background />
       <Nav $page={$page}>
         <Logo src="/logo.svg" alt="링크브러리 홈화면으로 이동" />
-        {$page === "/" ? <StyledLink href={locate.current}>로그인</StyledLink> : <Avatar id={id} />}
+        {$page === "/" ? <StyledLink href={locate}>로그인</StyledLink> : <Avatar id={id} />}
       </Nav>
     </>
   );
