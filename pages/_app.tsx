81e89787dd4daa506cf4324882ee2aa198fc31fc
aowjarkwk <bulls1rose@naver.com>
2023-12-03 15:58:42 +0900

    Fix: 메타 태그 수정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -20,16 +20,22 @@ export default function App({ Component, pageProps }: AppProps) {
         <meta name="description" content="세상의 모든 정보를 쉽게 저장하고 관리해 보세요" />
 
         <meta property="og:type" content="website" />
-        <meta property="og:url" content="a1b2c3d4zzzzzznextts.vercel.app" />
+        <meta property="og:url" content="https://a1b2c3d4zzzzzznextts.vercel.app" />
         <meta property="og:title" content="Linkbrary" />
         <meta property="og:description" content="세상의 모든 정보를 쉽게 저장하고 관리해 보세요" />
-        <meta property="og:image" content="a1b2c3d4zzzzzznextts.vercel.app/images/image-for-meta-tag-test.jpg" />
+        <meta
+          property="og:image"
+          content="https://a1b2c3d4zzzzzznextts.vercel.app/images/image-for-meta-tag-test.jpg"
+        />
 
         <meta property="twitter:card" content="summary_large_image" />
-        <meta property="twitter:url" content="a1b2c3d4zzzzzznextts.vercel.app" />
+        <meta property="twitter:url" content="https://a1b2c3d4zzzzzznextts.vercel.app" />
         <meta property="twitter:title" content="Linkbrary" />
         <meta property="twitter:description" content="세상의 모든 정보를 쉽게 저장하고 관리해 보세요" />
-        <meta property="twitter:image" content="a1b2c3d4zzzzzznextts.vercel.app/images/image-for-meta-tag-test.jpg" />
+        <meta
+          property="twitter:image"
+          content="https://a1b2c3d4zzzzzznextts.vercel.app/images/image-for-meta-tag-test.jpg"
+        />
       </Head>
       {useLayout ? (
         <UserProvider>
