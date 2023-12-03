ed8098630c733749fe9a65ffbab1fbfc0248e24a
yunajoe <86518113+yunajoe@users.noreply.github.com>
2023-12-03 17:28:33 +0900

    type명시중

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -1,12 +1,20 @@
 import React from "react";
-import Folder from "../folder/Folder";
-import HeaderButton from "../button/HeaderButton";
-type HeaderProps = {
-  data?: Folder;
-  isLoading?: boolean;
-};
 
-export default function Header({ data, isLoading }: HeaderProps) {
+import HeaderButton from "../button/HeaderButton";
+import { FolderContents } from "@/api/share";
+import Folder from "../folder/Folder";
+// type HeaderProps = {
+//   data?: Folder;
+//   isLoading?: boolean;
+// };
+
+export default function Header({
+  data,
+  isLoading,
+}: {
+  data: { folder: FolderContents };
+  isLoading: boolean;
+}) {
   if (data) {
     return <div>{!isLoading && <Folder items={data} />}</div>;
   }
