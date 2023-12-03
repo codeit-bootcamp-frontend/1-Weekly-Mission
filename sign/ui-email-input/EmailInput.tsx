402f9a0d34a72fd2a4472121534e7a15afe99cea
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-03 22:53:25 +0900

    input 컴포넌트

---
+++ sign/ui-email-input/EmailInput.tsx
@@ -0,0 +1,26 @@
+import { isEmailValid } from '../utils';
+import React, { useState } from 'react';
+import styles from './EmailInput.module.scss';
+import classNames from 'classnames';
+const cx = classNames.bind(styles);
+
+const EmailInput = () => {
+  const [value, setValue] = useState('');
+  const isError = value !== '' && !isEmailValid(value);
+  let classType = isError ? 'input-error' : 'input-default';
+  console.log('classType', classType);
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  return (
+    <input
+      className={cx('inputField')}
+      placeholder=""
+      value={value}
+      onChange={handleInputChange}
+    />
+  );
+};
+
+export default EmailInput;
---
+++ sign/ui-password-input/PasswordInput.tsx
@@ -0,0 +1,54 @@
+import { useState } from 'react';
+import styles from './PasswordInput.module.scss';
+import { ReactComponent as EyeOff } from './eye-off.svg';
+import { ReactComponent as EyeOn } from './eye-on.svg';
+import { ChangeEventHandler } from 'react';
+import classNames from 'classnames';
+const cx = classNames.bind(styles);
+
+type PasswordInputProps = {
+  type: string;
+  value: string;
+  onChange?: ChangeEventHandler<HTMLInputElement> | undefined;
+  className?: string;
+  isPasswordSame: boolean;
+};
+
+const PasswordInput = ({
+  type,
+  value,
+  onChange,
+  isPasswordSame,
+}: PasswordInputProps) => {
+  const [showPassword, setShowPassword] = useState(false);
+  const [inputType, setInputType] = useState(type);
+
+  const togglePasswordVisibility = () => {
+    setShowPassword(!showPassword);
+    setInputType(showPassword ? 'password' : 'text');
+  };
+
+  const isError = value && !isPasswordSame;
+  let classType = !isError ? 'input-default' : 'input-error';
+  console.log(classType);
+  return (
+    <>
+      <input
+        className={cx('input-default')}
+        placeholder=""
+        value={value}
+        onChange={onChange}
+        type={inputType}
+      />
+      <button
+        className="eye-button"
+        type="button"
+        onClick={togglePasswordVisibility}
+      >
+        {showPassword ? <EyeOn /> : <EyeOff />}
+      </button>
+    </>
+  );
+};
+
+export default PasswordInput;
