5e06a3e436fd1b77ff14f726eb908f08aa9baa5b
oauch <lgm9515@naver.com>
2023-12-03 15:46:29 +0900

    feat: input 메시지 상수화

---
+++ src/constants/input.ts
@@ -0,0 +1,18 @@
+// 타입
+export const INPUT_TYPE = {
+  email: {
+    type: "email",
+    placeholder: "이메일을 입력해주세요",
+    errorMsg: "이메일을 다시 입력해주세요",
+  },
+  password: {
+    type: "password",
+    placeholder: "비밀번호를 입력해주세요",
+    errorMsg: "비밀번호를 다시 입력해주세요",
+  },
+  passwordCheck: {
+    type: "passwordCheck",
+    placeholder: "비밀번호를 한번 더 입력해주세요",
+    errorMsg: "비밀번호가 일치하지 않습니다.",
+  },
+};
