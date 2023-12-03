932ae6f3d02412d6eb418086ffc1ed7b36a488d5
heewon <hw7666@sogang.ac.kr>
2023-12-03 21:56:44 +0900

    🐛 Fix(all) : 배포 시 오류 수정

--- pages/folder.tsx
+++ pages/folder.tsx
@@ -3,6 +3,7 @@ import Header from '@/components/common/Header';
 import Footer from '@/components/common/Footer';
 import AddLinkBar from '@/components/folder/AddLinkBar';
 import FolderContent from '@/components/folder/FolderContent';
+import Script from 'next/script';
 
 export default function Folder() {
   return (
@@ -10,6 +11,11 @@ export default function Folder() {
       <Head>
         <title>folder | Linkbrary</title>
       </Head>
+      <Script
+        src="https://t1.kakaocdn.net/kakao_js_sdk/2.4.0/kakao.min.js"
+        integrity="sha384-mXVrIX2T/Kszp6Z0aEWaA8Nm7J6/ZeWXbL8UpGRjKwWe56Srd/iyNmWMBhcItAjH"
+        crossOrigin="anonymous"
+      ></Script>
       <Header page="folder" />
       <AddLinkBar />
       <FolderContent />
