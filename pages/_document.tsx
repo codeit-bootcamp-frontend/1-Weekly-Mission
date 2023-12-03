041147f6c81e27764dcd64721e194047da5fe3f2
qooktree1 <qooktree@gmail.com>
2023-12-03 18:04:22 +0900

    Feat: _app.js, _document.js 에 styled-components 및 카카오 공유를 위한 설정 완료

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,54 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Fragment } from "react";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import { ServerStyleSheet } from "styled-components";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const sheet = new ServerStyleSheet();
+    const originalRenderPage = ctx.renderPage;
+
+    try {
+      ctx.renderPage = () =>
+        originalRenderPage({
+          enhanceApp: (App) => (props) =>
+            sheet.collectStyles(<App {...props} />),
+        });
+
+      const initialProps = await Document.getInitialProps(ctx);
+      return {
+        ...initialProps,
+        styles: [
+          <Fragment key="1">
+            {initialProps.styles}
+            {sheet.getStyleElement()}
+          </Fragment>,
+        ],
+      };
+    } finally {
+      sheet.seal();
+    }
+  }
+
+  render() {
+    return (
+      <Html lang="ko">
+        <Head />
+        <script
+          defer
+          src="https://developers.kakao.com/sdk/js/kakao.min.js"
+        ></script>
+        <body>
+          <Main />
+          <NextScript />
+          <div id="modal-root"></div>
+        </body>
+      </Html>
+    );
+  }
 }
