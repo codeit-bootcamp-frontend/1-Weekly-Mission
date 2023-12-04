01c98ac31f2cd08f202715bbc19700c047692331
thisisthewa2 <thisisthewa2@gmail.com>
2023-12-04 21:13:31 +0900

    ✅ feat: next js 마이그레이션

--- user/data-access-user/useGetUser.ts
+++
@@ -1,9 +0,0 @@
-import { useAsync } from "sharing/util";
-import { axiosInstance } from "sharing/util";
-import { UserRawData } from "user/type";
-
-export const useGetUser = () => {
-  const getUser = () => axiosInstance.get<{ data: UserRawData }>("sample/user");
-  const { loading, error, data } = useAsync(getUser);
-  return { loading, error, data: data?.data ?? null };
-};
