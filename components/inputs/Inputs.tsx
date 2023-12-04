ebce5758d8f21cfcd6d6098977af82b378320149
ParkSohyunee <sohyunee016@gmail.com>
2023-12-04 10:57:01 +0900

    Feat(Inputs): 이벤트 로직 수정

--- components/inputs/Inputs.tsx
+++ components/inputs/Inputs.tsx
@@ -1,20 +1,31 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, FocusEvent } from "react";
 import * as S from "./InputStyle";
 
-export default function Input({ type = "password" }) {
+export default function Input({ type = "text" }) {
   const defaultType = type;
-  const [text, setText] = useState("");
-  const [errormsg, setErrormsg] = useState("");
+  const [inputValue, setInputValue] = useState("");
+  const [errorMessage, setErrorMessage] = useState("");
   const [inputType, setInputType] = useState(type);
   const [iconUrl, setIconUrl] = useState("/assets/eye-off.svg");
 
-  const handleText = (e: ChangeEvent<HTMLInputElement>) => {
-    const newText = e.target.value;
-    setText(newText);
-    if (!newText) setErrormsg("내용을 작성해주세요.");
-    else {
-      setErrormsg("");
+  /**
+   * todo
+   * input 컴포넌트, container 컴포넌트 분리
+   * input - onChange 값을 저장
+   * container - UI 요소 (text/pwd 분기 등)
+   */
+
+  const handleInputValue = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputValue(e.target.value);
+    checkInputValidation(e.target.value);
+  };
+
+  const checkInputValidation = (value: string) => {
+    if (!value) {
+      setErrorMessage("내용을 작성해주세요,");
+      return;
     }
+    setErrorMessage("");
   };
 
   const handleChangeType = () => {
@@ -27,13 +38,18 @@ export default function Input({ type = "password" }) {
     }
   };
 
+  /** 추후 form 제출시 이벤트 */
+  const handleFousOutInput = (e: FocusEvent<HTMLDivElement>) => {
+    checkInputValidation(inputValue);
+  };
+
   return (
-    <S.Container>
+    <S.Container onBlur={handleFousOutInput}>
       <S.InputWrapper>
         <S.Input
           type={inputType}
-          value={text}
-          onChange={handleText}
+          value={inputValue}
+          onChange={handleInputValue}
           placeholder="내용 입력"
           autoComplete="off"
         />
@@ -41,7 +57,7 @@ export default function Input({ type = "password" }) {
           <S.EyeIcon src={iconUrl} alt="비밀번호 보기 아이콘" onClick={handleChangeType} />
         )}
       </S.InputWrapper>
-      {errormsg && <S.ErrorMessage>{errormsg}</S.ErrorMessage>}
+      {errorMessage && <S.ErrorMessage>{errorMessage}</S.ErrorMessage>}
     </S.Container>
   );
 }
