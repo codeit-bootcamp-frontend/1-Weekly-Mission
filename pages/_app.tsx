3013ea4e249cef736d778224f1ad3155f8507215
ParkSohyunee <sohyunee016@gmail.com>
2023-12-04 14:50:32 +0900

    Style: 사용하지 않는 코드 및 주석 삭제

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,12 @@
 import "@/styles/globals.css";
 
 import type { AppProps } from "next/app";
-import Layout from "@/components/layout/Layout";
 import { FolderContextProvider } from "@/context/FolderContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    // <Layout>
     <FolderContextProvider>
       <Component {...pageProps} />
     </FolderContextProvider>
-    // </Layout>
   );
 }
