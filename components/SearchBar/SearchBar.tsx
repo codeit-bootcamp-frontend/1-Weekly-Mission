b8c3310c4870110a93816bb2cc5862c93de51dc5
ayden <wpfekdml@naver.com>
2023-12-05 16:07:01 +0900

    refactor(SearchBar.tsx) : 검색 결과 안내 문구와 통합

--- components/SearchBar/SearchBar.tsx
+++ components/SearchBar/SearchBar.tsx
@@ -1,34 +1,49 @@
-import { ChangeEvent, MouseEvent } from "react";
+import { ChangeEvent, Dispatch, MouseEvent, SetStateAction } from "react";
 import styles from "./SearchBar.module.css";
 import Image from "next/image";
 
 interface Props {
-  value: string;
-  onChange: (e: ChangeEvent<HTMLInputElement>) => void;
-  onClick: (e: MouseEvent<HTMLButtonElement>) => void;
+  searchBar: {
+    values: string;
+    handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+    setValues: Dispatch<SetStateAction<string>>;
+  };
 }
 
-function SearchBar({ value, onChange, onClick }: Props) {
+function SearchBar({ searchBar }: Props) {
+  const handleClick = (e: MouseEvent) => {
+    e.preventDefault();
+    searchBar.setValues("");
+  };
+
   return (
-    <article className={styles.root}>
-      <div className={styles.searchIconContainer}>
-        <Image fill className={styles.icon} src="/images/icon/common-icons/Search.svg" alt="" />
-      </div>
-      <form className={styles.form}>
-        <input
-          onChange={onChange}
-          value={value}
-          type="text"
-          className={styles.input}
-          placeholder="링크를 검색해 보세요"
-        ></input>
-        {value && (
-          <button className={styles.closeIconContainer} type="button" onClick={onClick}>
-            <Image fill className={styles.icon} src="/images/icon/common-icons/closeIcon.svg" alt="" />
-          </button>
-        )}
-      </form>
-    </article>
+    <>
+      <article className={styles.root}>
+        <div className={styles.searchIconContainer}>
+          <Image fill className={styles.icon} src="/images/icon/common-icons/Search.svg" alt="" />
+        </div>
+        <form className={styles.form}>
+          <input
+            onChange={searchBar.handleChange}
+            value={searchBar.values}
+            type="text"
+            className={styles.input}
+            placeholder="링크를 검색해 보세요"
+          ></input>
+          {searchBar.values && (
+            <button className={styles.closeIconContainer} type="button" onClick={handleClick}>
+              <Image fill className={styles.icon} src="/images/icon/common-icons/closeIcon.svg" alt="" />
+            </button>
+          )}
+        </form>
+      </article>
+      {searchBar.values && (
+        <p className={styles.searchResult}>
+          {searchBar.values}
+          <span className={styles.searchResult}>로 검색한 결과입니다</span>
+        </p>
+      )}
+    </>
   );
 }
 
