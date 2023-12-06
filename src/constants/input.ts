947dc5cb4c0fda624e8645cd27ea0b607342cba8
oauch <lgm9515@naver.com>
2023-12-06 12:07:25 +0900

    feat: input 에러메시지 상수 추가

--- src/constants/input.ts
+++ src/constants/input.ts
@@ -3,16 +3,17 @@ export const INPUT_TYPE = {
   email: {
     type: "email",
     placeholder: "이메일을 입력해주세요",
-    errorMsg: "이메일을 다시 입력해주세요",
+    errorMsg1: "이메일을 다시 입력해주세요",
+    errorMsg2: "올바른 이메일 주소가 아닙니다.",
   },
   password: {
     type: "password",
     placeholder: "비밀번호를 입력해주세요",
-    errorMsg: "비밀번호를 다시 입력해주세요",
+    errorMsg1: "비밀번호를 다시 입력해주세요",
   },
   passwordCheck: {
     type: "passwordCheck",
     placeholder: "비밀번호를 한번 더 입력해주세요",
-    errorMsg: "비밀번호가 일치하지 않습니다.",
+    errorMsg1: "비밀번호가 일치하지 않습니다.",
   },
 };
