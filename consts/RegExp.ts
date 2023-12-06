1f18d89073de18d8444b192b419f4d044a05c6cb
K-Y-Hoo <bookopen1@gmail.com>
2023-12-06 22:28:41 +0900

    feat: 로그인 페이지 요구사항 기능 구현

---
+++ consts/RegExp.ts
@@ -0,0 +1,1 @@
+export const EMAIL_REGEX = /^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/;
---
+++ libs/getSignIn.ts
@@ -0,0 +1,27 @@
+import axios from "@/libs/axios";
+
+interface TokenData {
+  accessToken: string;
+  refreshToken: string;
+}
+
+interface Result {
+  data: TokenData;
+}
+
+export default async function getSignIn(email: string, password: string) {
+  let result: Result | undefined;
+  await axios
+    .post(`/sign-in`, {
+      email: email,
+      password: password,
+    })
+    .then((response) => {
+      console.log(response.data);
+      result = response.data;
+    })
+    .catch((error) => {
+      console.log(error.response);
+    });
+  return result;
+}
