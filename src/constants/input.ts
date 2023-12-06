d042e56b289b3c49584180ea796e01d661684a9c
oauch <lgm9515@naver.com>
2023-12-06 14:04:13 +0900

    refactor: placeholder, errorMsg 상수 내용 변경

--- src/constants/input.ts
+++ src/constants/input.ts
@@ -2,18 +2,20 @@
 export const INPUT_TYPE = {
   email: {
     type: "email",
-    placeholder: "이메일을 입력해주세요",
-    errorMsg1: "이메일을 다시 입력해주세요",
+    placeholder: "이메일을 입력해주세요.",
+    errorMsg1: "이메일을 다시 입력해주세요.",
     errorMsg2: "올바른 이메일 주소가 아닙니다.",
   },
   password: {
     type: "password",
-    placeholder: "비밀번호를 입력해주세요",
-    errorMsg1: "비밀번호를 다시 입력해주세요",
+    signInPlaceholder: "비밀번호를 입력해주세요.",
+    signUpPlaceholder: "영문, 숫자를 조합해 8자 이상 입력해 주세요.",
+    errorMsg1: "비밀번호를 다시 입력해주세요.",
+    errorMsg2: "비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.",
   },
   passwordCheck: {
     type: "passwordCheck",
-    placeholder: "비밀번호를 한번 더 입력해주세요",
+    placeholder: "비밀번호와 일치하는 값을 입력해 주세요.",
     errorMsg1: "비밀번호가 일치하지 않습니다.",
   },
 };
