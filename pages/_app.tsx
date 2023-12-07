b691ccb796874d6b7504909da11048ac64f625b4
Jyophie <jyophie@gmail.com>
2023-12-07 22:52:51 +0900

    Feat: Pretendard 폰트 적용

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "@/styles/reset.css";
 import type { AppProps } from "next/app";
-import { fontFace } from "next/font";
+import localFont from "next/font/local";
 
-const Pretendard = fontFace({
-  src: "url(https://cdn.jsdelivr.net/gh/orioncactus/pretendard/dist/web/static/pretendard.css)",
+const Pretendard = localFont({
+  src: "../public/font/PretendardVariable.woff2",
 });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={Pretendard.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
