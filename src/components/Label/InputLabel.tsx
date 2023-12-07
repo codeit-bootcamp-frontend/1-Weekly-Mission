f6073940fa426cbbab4501e7a68e8125d8b8aafb
hanseulhee <3021062@gmail.com>
2023-12-08 08:12:50 +0900

    label component

---
+++ src/components/Label/InputLabel.tsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+interface Props {
+  htmlFor: string;
+  label: LabelTypes;
+}
+
+function InputLabel({ htmlFor, label }: Props) {
+  return <StyledLabel htmlFor={htmlFor}>{label}</StyledLabel>;
+}
+
+export default InputLabel;
+
+const StyledLabel = styled.label`
+  font-size: 13px;
+  margin: 10px 0;
+`;
