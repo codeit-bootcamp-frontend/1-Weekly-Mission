042098cab244fddb9d4f781994c18b67d35c7198
ParkSohyunee <sohyunee016@gmail.com>
2023-12-08 09:33:52 +0900

    Feat: SWR설치 및 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,17 @@ import "styles/globals.css";
 
 import type { AppProps } from "next/app";
 import { FolderContextProvider } from "@/context/FolderContext";
+import { SWRConfig } from "swr";
+import { DOMAIN_URL } from "@/common/constants";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <FolderContextProvider>
-      <Component {...pageProps} />
-    </FolderContextProvider>
+    <SWRConfig
+      value={{ fetcher: (url: string) => fetch(`${DOMAIN_URL}${url}`).then((res) => res.json()) }}
+    >
+      <FolderContextProvider>
+        <Component {...pageProps} />
+      </FolderContextProvider>
+    </SWRConfig>
   );
 }
