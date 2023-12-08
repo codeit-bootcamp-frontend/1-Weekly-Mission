8ba87cc18c70c04a25f6438195284e86d08e53e6
Seoyoung <seoyoungiyo@gmail.com>
2023-12-08 13:44:55 +0900

    style: eslint 설정 변경 및 기타라이브러리 설치
    react-hook-form, classnames

--- src/component/sign/Input.tsx
+++ src/component/sign/Input.tsx
@@ -1,15 +1,15 @@
-import { ReactElement, useEffect, useState } from "react";
-import { InputProps } from "@/types/Component";
-import useToggle from "@/src/hooks/useToggle";
-import useInput from "@/src/hooks/useInput";
-import { IconEyeON, IconEyeOff } from "@/public/svgs";
-import styles from "./Input.module.css";
+import { ReactElement, useEffect, useState } from 'react';
+import { InputProps } from '@/types/Component';
+import useToggle from '@/src/hooks/useToggle';
+import useInput from '@/src/hooks/useInput';
+import { IconEyeON, IconEyeOff } from '@/public/svgs';
+import styles from './Input.module.css';
 
 export default function Input({ type, onBlur }: InputProps): ReactElement {
   const { isOn, toggle } = useToggle(false);
-  const { value, handleInputChange } = useInput("");
+  const { value, handleInputChange } = useInput('');
   const [showError, setShowError] = useState(false);
-  const [error, setError] = useState("내용을 다시 작성해주세요");
+  const [error, setError] = useState('내용을 다시 작성해주세요');
 
   return (
     <>
@@ -18,13 +18,13 @@ export default function Input({ type, onBlur }: InputProps): ReactElement {
           className={styles.Input}
           placeholder="내용 입력"
           value={value}
-          type={isOn ? "text" : "password"}
+          type={isOn ? 'text' : 'password'}
           onChange={handleInputChange}
           onBlur={onBlur}
         />
         {isOn
-          ? type === "password" && <IconEyeON onClick={toggle} />
-          : type === "password" && <IconEyeOff onClick={toggle} />}
+          ? type === 'password' && <IconEyeON onClick={toggle} />
+          : type === 'password' && <IconEyeOff onClick={toggle} />}
       </div>
       {showError && <span className={styles.Error}>{error}</span>}
     </>
