dcee1a56df0308eb85f179063059edcd020a60a3
ParkSohyunee <sohyunee016@gmail.com>
2023-12-08 17:01:29 +0900

    Design(Landing): Next.js built-in 컴포넌트로 변경(이미지,링크)

--- src/components/landing/index.tsx
+++ src/components/landing/index.tsx
@@ -1,3 +1,6 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import Layout from "@/components/layout/Layout";
 import styles from "./index.module.css";
 
@@ -11,12 +14,18 @@ export default function Landing() {
           쉽게 저장하고 관리해 보세요
         </h1>
         <div className={styles.hero__link}>
-          <a href="signup.html">
-            <button className={styles.hero__link__btn}>링크 추가하기</button>
-          </a>
+          <Link href="/signup" className={styles.hero__link__btn}>
+            링크 추가하기
+          </Link>
         </div>
         <div>
-          <img src="images/landing.svg" alt="hero" className={styles.hero__image} />
+          <Image
+            className={styles.hero__image}
+            src="images/landing.svg"
+            alt="hero"
+            width={100}
+            height={100}
+          />
         </div>
       </section>
       <section>
@@ -29,7 +38,13 @@ export default function Landing() {
               나중에 읽고 싶은 글, 다시 보고 싶은 영상, 사고 싶은 옷, 기억하고 싶은 모든 것을 한
               공간에 저장하세요.
             </p>
-            <img className={styles.content__image} src="images/contents1.svg" alt="contents" />
+            <Image
+              className={styles.content__image}
+              src="images/contents1.svg"
+              alt="contents"
+              width={100}
+              height={100}
+            />
           </li>
           <li className={styles.content}>
             <h2 className={styles.content__info__title}>
@@ -39,7 +54,13 @@ export default function Landing() {
             <p className={styles.content__info__description}>
               나만의 폴더를 무제한으로 만들고 다양하게 활용할 수 있습니다.
             </p>
-            <img className={styles.content__image} src="images/contents2.svg" alt="contents" />
+            <Image
+              className={styles.content__image}
+              src="images/contents2.svg"
+              alt="contents"
+              width={100}
+              height={100}
+            />
           </li>
           <li className={styles.content}>
             <h2 className={styles.content__info__title}>
@@ -50,7 +71,13 @@ export default function Landing() {
               여러 링크를 폴더에 담고 공유할 수 있습니다. 가족, 친구, 동료들에게 쉽고 빠르게 링크를
               공유해 보세요.
             </p>
-            <img className={styles.content__image} src="images/contents3.svg" alt="contents" />
+            <Image
+              className={styles.content__image}
+              src="images/contents3.svg"
+              alt="contents"
+              width={100}
+              height={100}
+            />
           </li>
           <li className={styles.content}>
             <h2 className={styles.content__info__title}>
@@ -60,7 +87,13 @@ export default function Landing() {
             <p className={styles.content__info__description}>
               중요한 정보들을 검색으로 쉽게 찾아보세요.
             </p>
-            <img className={styles.content__image} src="images/contents4.svg" alt="contents" />
+            <Image
+              className={styles.content__image}
+              src="images/contents4.svg"
+              alt="contents"
+              width={100}
+              height={100}
+            />
           </li>
         </ul>
       </section>
