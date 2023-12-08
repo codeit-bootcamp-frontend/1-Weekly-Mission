8ca69ce9c3bb9d7c4858ad613280920b5d2113ee
hyejungan <wjddk582@naver.com>
2023-12-08 22:42:58 +0900

    login layout add use react-hook-form

--- components/Input/input.style.js
+++
@@ -1,6 +0,0 @@
-import styled from 'styled-components';
-
-export const Input = styled.input`
-  border : ${({active}) => active ? '#6D6AFE' : '#CCD5E3'};
-  
-`
--- public/portal.ts
+++
@@ -1,9 +0,0 @@
-import { ReactNode } from "react";
-import reactDOM from "react-dom";
-
-const ModalPortal = ({children} : {children : ReactNode}) => {
-  const el = document.getElementById("modal") as HTMLDivElement;
-  return reactDOM.createPortal(children, el);
-}
-
-export default ModalPortal;
