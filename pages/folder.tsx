2206ceb6f5e998d3a44811e512897960c1e88c60
oauch <lgm9515@naver.com>
2023-12-08 17:34:59 +0900

    feat: 커스텀 훅 적용

--- pages/folder.tsx
+++ pages/folder.tsx
@@ -1,6 +1,4 @@
 import { useRouter } from "next/router";
-import { getAllFolder, getFolderLinks, getUserFolder } from "pages/api/api";
-import { useEffect, useState } from "react";
 import CardList from "src/components/CardList/CardList";
 import FolderList from "src/components/FolderList/FolderList";
 import Footer from "src/components/Footer/Footer";
@@ -13,16 +11,17 @@ import NotFoundLink from "src/components/NotFoundLink/NotFoundLink";
 import Search from "src/components/Search/Search";
 import ResultSearch from "src/components/Search/SearchResult";
 import Title from "src/components/Title/Title";
+import { useFolderAndCardInfo } from "src/hook/Folder/useFolderAndCardInfo";
+import { useFolderName } from "src/hook/Folder/useFolderName";
 import { useModal } from "src/hook/Folder/useModal";
-import isEmpty from "src/utils/isEmpty";
-import { filterCardsSearch } from "src/utils/searchFilterCards";
 
 function Folder() {
-  const [folders, setFolders] = useState<[]>([]);
-  const [isFunctionButtonShow, setIsFunctionButtonShow] = useState(false);
-  const [cards, setCards] = useState<Card[]>([]);
-  const [folderName, setFolderName] = useState("");
-  const [searchResult, setSearchResult] = useState("");
+  const router = useRouter();
+  const initFolderId = router.query.folderId as string;
+  const { folderName, folders, isFunctionButtonShow } =
+    useFolderName(initFolderId);
+  const { cards, searchResult, setSearchResult } =
+    useFolderAndCardInfo(initFolderId);
   const {
     isModalOpen,
     modalButtonContent,
@@ -33,56 +32,6 @@ function Folder() {
     showShareKakao,
     showShareLinkCopy,
   } = useModal(folderName);
-
-  const router = useRouter();
-  const initFolderId = router.query.folderId as string;
-
-  const folderInfo = async (folderId: string) => {
-    const introResult = await getUserFolder();
-
-    const currentId = introResult.filter(
-      (data: { id: number }) => data.id === Number(folderId)
-    );
-
-    let folderName;
-    if (currentId.length === 0) {
-      folderName = "전체";
-      setIsFunctionButtonShow(false);
-    } else {
-      folderName = currentId[0].name;
-      setIsFunctionButtonShow(true);
-    }
-
-    setFolderName(folderName);
-    setFolders(introResult);
-  };
-
-  const folderAndCardInfo = async (folderId: string) => {
-    const introResult = isEmpty(folderId)
-      ? await getAllFolder()
-      : await getFolderLinks(folderId as string);
-    if (!introResult) return;
-
-    if (searchResult) {
-      const filterCard = filterCardsSearch(introResult, searchResult);
-      setCards(filterCard);
-    }
-
-    if (!searchResult) {
-      setCards(introResult);
-    }
-  };
-
-  useEffect(() => {
-    folderInfo(initFolderId);
-    folderAndCardInfo(initFolderId);
-
-    return () => setCards([]);
-  }, [initFolderId]);
-
-  useEffect(() => {
-    folderAndCardInfo(initFolderId);
-  }, [searchResult]);
 
   return (
     <>
