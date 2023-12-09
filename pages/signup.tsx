90a7e877d918808bcd5f0ab17e92485e39845f18
heewon <hw7666@sogang.ac.kr>
2023-12-09 19:38:06 +0900

    ✨ Feat(signup) : input 유효성 검사 기능 추가

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -1,17 +1,48 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import styled from 'styled-components';
+import { useForm } from 'react-hook-form';
 import Logo from '@/components/common/Logo';
 import Button from '@/components/common/Button';
 import Google from '@/public/assets/images/social_google.svg';
 import Kakao from '@/public/assets/images/social_kakao.svg';
 import InputBox from '@/components/common/Input/InputBox';
-import { validateEmail, validatePassword } from '@/lib/utils/validations';
+
+const emailRules = {
+  required: '이메일을 입력해주세요.',
+  pattern: {
+    value: /[0-9a-zA-Z]*@[0-9a-zA-Z]*\.[a-zA-Z]{2,3}$/i,
+    message: '올바른 이메일 주소가 아닙니다.',
+  },
+};
+
+const pwRules = {
+  required: '비밀번호를 입력해주세요.',
+  pattern: {
+    value: /^(?=.*[a-zA-Z])(?=.*[0-9]).{8,}$/,
+    message: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
+  },
+};
 
 export default function SignUp() {
+  const {
+    register,
+    handleSubmit,
+    setError,
+    formState: { errors },
+  } = useForm({ mode: 'onBlur' });
+
+  function onValidSignUp(data: any): void {
+    if (data.password !== data.pwCheck) {
+      setError('pwCheck', { message: '비밀번호가 일치하지 않아요.' }, { shouldFocus: true });
+    } else {
+      alert('성공~!');
+    }
+  }
+
   return (
     <Background>
-      <Container>
+      <Container onSubmit={handleSubmit(onValidSignUp)}>
         <Header>
           <Logo width={210.6} height={38} />
           <Signin>
@@ -19,9 +50,14 @@ export default function SignUp() {
             <GoSignIn href={'/signin'}>로그인 하기</GoSignIn>
           </Signin>
         </Header>
-        <InputBox type="이메일" placeholder="이메일을 입력해 주세요." validationFunc={validateEmail} />
-        <InputBox type="비밀번호" placeholder="영문, 숫자를 조합해 8자 이상 입력해 주세요." validationFunc={validatePassword} />
-        <InputBox type="비밀번호 확인" placeholder="비밀번호와 일치하는 값을 입력해 주세요." validationFunc={validatePassword} />
+        <InputBox type="이메일" placeholder="이메일을 입력해 주세요." register={register('email', emailRules)} error={errors.email} />
+        <InputBox
+          type="비밀번호"
+          placeholder="영문, 숫자를 조합해 8자 이상 입력해 주세요."
+          register={register('password', pwRules)}
+          error={errors.password}
+        />
+        <InputBox type="비밀번호 확인" placeholder="비밀번호와 일치하는 값을 입력해 주세요." register={register('pwCheck')} error={errors.pwCheck} />
         <Button>회원가입</Button>
         <SocialBox>
           다른 방식으로 가입하기
@@ -48,7 +84,7 @@ const Background = styled.div`
   background-color: var(--bg);
 `;
 
-const Container = styled.div`
+const Container = styled.form`
   width: 400px;
   margin: 0 auto;
 
