00a18a18763d0f0d021637889bbfb8656f4cb12c
BeMatthewsong <songww1997@naver.com>
2023-12-09 16:26:51 +0900

    fix: useForm에 타입 명시 및 속성 추가

--- pages/signin/index.tsx
+++ pages/signin/index.tsx
@@ -6,20 +6,33 @@ import Link from 'next/link';
 import { SnsLogin } from '@/src/sign/ui-sns/SnsLogin';
 import { FormInput } from '@/src/sign/ui-form-input/FormInput';
 import { FormButton } from '@/src/sign/ui-form-button/FormButton';
-import { useForm } from 'react-hook-form';
+import { SubmitHandler, useForm } from 'react-hook-form';
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
 
 const cx = classNames.bind(styles);
+
+interface FormInput {
+  email: string;
+  password: string;
+}
 
 const Signin = () => {
   const {
     register,
     handleSubmit,
     formState: { errors },
-  } = useForm();
+  } = useForm<FormInput>();
+  const router = useRouter();
 
-  const onSubmit = (data: any) => {
-    console.log(data);
+  const onBlur = () => {
+    // validate? react-hook-form에서 포커스아웃했을 때 어떻게 유효성 검사해야 할까요?
   };
+
+  const onSubmit = handleSubmit((data: FormInput) => {
+    console.log(data);
+  });
+
   return (
     <SignLayout
       logo={
@@ -33,35 +46,37 @@ const Signin = () => {
         </p>
       }
       form={
-        <form onSubmit={handleSubmit(onSubmit)}>
+        <form onSubmit={onSubmit}>
           <FormInput
             label="이메일"
             type="text"
+            placeholder="이메일을 입력해주세요"
             {...register('email', {
               required: '이메일은 필수 입력 항목입니다.',
               pattern: {
                 value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i,
-                message: '유효한 이메일 주소를 입력하세요.',
+                message: '올바른 이메일 주소가 아닙니다/',
               },
             })}
+            error={errors.email} // error의 email 부분에 값이 있다면 true 반환, 그렇지 않으면 false 반환
           />
-          {errors.email && <p>{errors.email.message as string}</p>}
           <FormInput
             label="비밀번호"
             type="password"
+            placeholder="비밀번호를 입력해주세요"
             {...register('password', {
-              required: '비밀번호는 필수 입력 항목입니다.',
+              required: '비밀번호를 입력해주세요.',
               minLength: {
                 value: 6,
-                message: '비밀번호는 최소 6자 이상이어야 합니다.',
+                message: '비밀번호를 6자 이상 입력해주세요.',
               },
               pattern: {
                 value: /^(?=.*[!@#$%^&*])/,
-                message: '유효한 이메일 주소를 입력하세요.',
+                message: '특수 기호를 넣어주세요.',
               },
             })}
+            error={errors.password}
           />
-          {errors.password && <p>{errors.password.message as string}</p>}
           <button>
             <FormButton>로그인</FormButton>
           </button>
--- src/sign/ui-form-input/FormInput.tsx
+++ src/sign/ui-form-input/FormInput.tsx
@@ -5,16 +5,20 @@ import styles from '@/src/sign/ui-form-input/FormInput.module.scss';
 type FormInputProps = {
   type: HTMLInputAttribute;
   label: ReactNode;
+  error: string | undefined | FieldError;
+  placeholder: string;
 };
 
 const cx = classNames.bind(styles);
 
-export const FormInput = ({ type, label }: FormInputProps) => {
+export const FormInput = ({ type, label, error, placeholder }: FormInputProps) => {
   return (
     <div className={cx('sign-input-box')}>
       <label className={cx('sign-input-label')}>{label}</label>
-      <input id="email" className={cx('sign-input')} type={type} />
-      <p id="email-error-message" className={cx('error-message')}></p>
+      <input id="email" className={cx('sign-input', { error: error })} type={type} placeholder={placeholder} />
+      <p id="email-error-message" className={cx('error-message')}>
+        {error?.message}
+      </p>
     </div>
   );
 };
