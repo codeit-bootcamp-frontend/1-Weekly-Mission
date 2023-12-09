74c15b1af31de8760822a83a42f33ac13ae484d7
Geon <geonulim@gmail.com>
2023-12-10 01:19:09 +0900

    refactor: validation 로직 분리

--- src/containers/Auth/Signin.tsx
+++ src/containers/Auth/Signin.tsx
@@ -1,27 +1,11 @@
-import { ChangeEvent, InputHTMLAttributes, useState } from 'react';
+import { ChangeEvent, FocusEvent, SyntheticEvent, useState } from 'react';
 import Button from '@/components/Button';
 import Header from './components/Header';
 import InputContainer from './components/InputContainer';
 import Social from './components/Social';
 import useRequest from '@/hooks/useRequest';
 import { useRouter } from 'next/router';
-
-export const ERROR_MESSAGES = {
-  email: {
-    emptyInput: '이메일을 입력해주세요.',
-    invalidInput: '올바른 이메일 주소가 아닙니다.',
-    invalidLogin: '이메일을 확인해주세요.',
-    unavailableEmail: '이미 사용 중인 이메일입니다.',
-  },
-  password: {
-    emptyInput: '비밀번호를 입력해주세요.',
-    invalidInput: '비밀번호는 영문, 숫자 조합 8자 이상 입력해 주세요.',
-    invalidLogin: '비밀번호를 확인해주세요.',
-  },
-  passwordCheck: {
-    invalidInput: '비밀번호가 일치하지 않아요.',
-  },
-};
+import { ERROR_MESSAGES, validateEmail, validatePassword } from './validation';
 
 interface Signin {
   data: {
@@ -57,7 +41,16 @@ function Signin() {
     },
   });
 
-  const onSignin = async () => {
+  const onSignin = async (e: SyntheticEvent) => {
+    e.preventDefault();
+    const newEmailErrorMessage = validateEmail(email);
+    const newPasswordErrorMessage = validatePassword(password, router.pathname);
+    if (newEmailErrorMessage || newPasswordErrorMessage) {
+      setEmailErrorMessage(newEmailErrorMessage);
+      setPasswordErrorMessage(newPasswordErrorMessage);
+      return;
+    }
+
     const { data: accessToken, error } = await signin();
     if (error) {
       setEmailErrorMessage(ERROR_MESSAGES.email.invalidLogin);
@@ -67,23 +60,32 @@ function Signin() {
     router.push('/folder');
   };
 
-  const clearErrorMessage = () => {
-    setEmailErrorMessage('');
-    setPasswordErrorMessage('');
+  const onEmailBlur = (e: FocusEvent<HTMLInputElement>) => {
+    const errorMessage = validateEmail(e.target.value);
+    setEmailErrorMessage(errorMessage);
+  };
+
+  const onPasswordBlur = (e: FocusEvent<HTMLInputElement>) => {
+    const errorMessage = validatePassword(e.target.value, router.pathname);
+    setPasswordErrorMessage(errorMessage);
   };
 
   return (
     <div className='flex h-screen w-screen items-center justify-center bg-background'>
       <main className='flex w-[40rem] flex-col items-center justify-center gap-30pxr'>
         <Header />
-        <form noValidate className='flex w-full flex-col gap-18pxr'>
+        <form
+          onSubmit={onSignin}
+          noValidate
+          className='flex w-full flex-col gap-18pxr'
+        >
           <InputContainer
             id='email'
             type='email'
             placeholder='codeit@codeit.com'
             errorMessage={emailErrorMessage}
             onChange={onEmailChange}
-            clearErrorMessage={clearErrorMessage}
+            onBlur={onEmailBlur}
           >
             이메일
           </InputContainer>
@@ -93,11 +95,11 @@ function Signin() {
             placeholder='• • • • • • • •'
             errorMessage={passwordErrorMessage}
             onChange={onPasswordChange}
-            clearErrorMessage={clearErrorMessage}
+            onBlur={onPasswordBlur}
           >
             비밀번호
           </InputContainer>
-          <Button size='lg' type='button' onClick={onSignin}>
+          <Button size='lg' type='submit'>
             로그인
           </Button>
         </form>
