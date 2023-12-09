311fdbb76ea72c19531f986f203577a14e7bbf0f
Soeun Kim <summerkim0825@gmail.com>
2023-12-09 21:32:38 +0900

    Feat: signin 페이지 react-hook-form 적용

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -4,31 +4,123 @@ import Image from 'next/image';
 import logoImage from "@/public/images/logo.svg";
 import InputForm from "@/components/InputForm";
 import Button from "@/components/Button";
+import { useForm } from 'react-hook-form';
+import { ChangeEvent, useState } from 'react';
+import googleIcon from '@/public/images/googleIcon.svg';
+import kakaotalkIcon from '@/public/images/kakaotalkIcon.svg';
+import eyeOn from '@/public/images/eyeOn.svg';
+import eyeOff from '@/public/images/eyeOff.svg';
+
+interface InputForm {
+  email: string;
+  password: string;
+}
 
 export default function SignIn() {
+  const [isError, setIsError] = useState(false);
+  const [showPassword, setShowPassword] = useState(false);
+
+  const { 
+    register, 
+    handleSubmit: onSubmit,
+    formState:{ errors },
+  } = useForm<InputForm>({
+    mode: "onBlur",
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const handleVisibility = (e: React.MouseEvent<HTMLElement>) => {
+    e.preventDefault();
+    setIsError((prev) => !prev);
+    setShowPassword((prev) => !prev);
+  }
+
+  const handleSubmit = (data: InputForm) => {
+    console.log(data);
+  }
 
   return (
     <S.Container>
       <S.Wrapper>
         <Image src={logoImage} width={210} height={38} alt="로고이미지" />
         <S.Header>
-          회원이 아니신가요? <S.LinkText><Link href="/signup">회원 가입하기</Link></S.LinkText>
+          회원이 아니신가요? <S.LinkText>
+            <Link href="/signup">회원 가입하기</Link>
+          </S.LinkText>
         </S.Header>
-        <S.SignInForm>
+        <S.SignInForm onSubmit={onSubmit(handleSubmit)}>
           <S.SignInContainer>
-            <InputForm
-              name="이메일"
-              type="email"
-              placeholder="이메일을 입력해 주세요"
-            />
-            <InputForm
-              name="비밀번호"
-              type="password"
-              placeholder="비밀번호가 일치하지 않아요"
-            />
+            <S.InputContainer>
+              <S.Label htmlFor="email">이메일</S.Label>
+              <S.Input
+                id="email"
+                type="email"
+                placeholder="이메일을 입력해주세요"
+                {...register("email", {
+                  required: "이메일을 입력해주세요.",
+                  pattern: {
+                    value: /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i,
+                    message: "올바른 이메일 주소가 아닙니다",
+                  },
+                })}
+              />
+              {errors.email ? <S.ErrorText>{errors.email.message}</S.ErrorText> : null}
+            </S.InputContainer>
+            <S.InputContainer>
+              <S.Label htmlFor="password">비밀번호</S.Label>
+              <S.PasswordContainer>
+                <S.Input
+                  id="password"
+                  type={
+                    showPassword ? "text" : "password"
+                  }
+                  placeholder="비밀번호를 입력해 주세요"
+                  {...register("password", {
+                    required: "비밀번호를 입력해주세요."},
+                  )}
+                />
+                
+                  <S.VisibleButton onClick={handleVisibility}>
+                    {isError ? (
+                      <Image fill src={eyeOn} alt="비밀번호 보이기" />
+                    ) : (
+                      <Image fill src={eyeOff} alt="비밀번호 숨기기" />
+                    )}
+                  </S.VisibleButton>
+              </S.PasswordContainer>
+              {errors.password ? <S.ErrorText>{errors.password.message}</S.ErrorText> : null}
+            </S.InputContainer>
           </S.SignInContainer>
           <Button>로그인</Button>
         </S.SignInForm>
+        <S.SnsContainer>
+          <S.SnsText>소셜 로그인</S.SnsText>
+          <S.SnsIconContainer>
+            <S.SnsIconWrapper
+              href="https://www.google.com"
+              target="_blank"
+            >
+              <Image
+                fill 
+                src={googleIcon} 
+                alt="구글 페이지로 이동"
+              />
+            </S.SnsIconWrapper>
+            <S.SnsIconWrapper
+              href="https://www.kakaocorp.com/page"
+              target="_blank"
+            >
+              <Image
+                fill 
+                src={kakaotalkIcon} 
+                alt="카카오톡으로 이동"
+              />
+            </S.SnsIconWrapper>
+          </S.SnsIconContainer>
+        </S.SnsContainer>
       </S.Wrapper>
     </S.Container>
   );
--- styles/SignIn.styled.ts
+++ styles/SignIn.styled.ts
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import { COLORS } from './palettes';
+import Link from 'next/link';
 
 export const Container = styled.div`
   display: flex;
@@ -46,6 +47,31 @@ export const SignInForm = styled.form`
   flex-direction: column;
 `;
 
+export const SignInContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 2.4rem;
+  margin: 3rem 0;
+`;
+
+export const InputContainer = styled.div`
+  width: 100%;
+`;
+
+export const Label = styled.label`
+  display: block;
+  margin-bottom: 1.2rem;
+  font-size: 1.4rem;
+
+  /* Linkbrary/body2-regular */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
 export const Input = styled.input`
   width: 100%;
   padding: 1.8rem 1.5rem;
@@ -55,21 +81,62 @@ export const Input = styled.input`
   border-radius: 0.8rem;
 `;
 
-export const SignInContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  gap: 2.4rem;
-  margin: 30px 0;
-`;
-
 export const PasswordContainer = styled.div`
-  border: 1px solid #000;
   display: flex;
+  align-items: center;
 `;
 
 export const VisibleButton = styled.button`
-  border: 1px solid #000;
+  width: 1.6rem;
+  height: 1.6rem;
+  position: relative;
   margin-left: -30px;
+`;
+
+export const ErrorText = styled.p`
+  color: ${COLORS.RED};
+  margin-top: 6px;
+
+  /* Linkbrary/body2-regular */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const SnsContainer = styled.div`
+  border: 1px solid ${COLORS.GRAY_300};
+  width: 100%;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 12px 24px;
+  border-radius: 0.8rem;
+  margin-top: 3.2rem;
+`;
+
+export const SnsText = styled.div`
+  color: ${COLORS.GRAY_600};
+
+  /* Linkbrary/body2-regular */
+  font-family: Pretendard;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+`;
+
+export const SnsIconContainer = styled.div`
+  display: flex;
+  align-items: flex-start;
+  gap: 16px;
+`;
+
+export const SnsIconWrapper = styled(Link)`
+  width: 4.2rem;
+  height: 4.2rem;
+  position: relative;
+  cursor: pointer;
 `;
 
