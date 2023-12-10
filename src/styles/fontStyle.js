8e2246dde63004eff3b1cd39e7ddac88069b6f97
arky02 <kyean07@naver.com>
2023-12-10 15:34:00 +0900

    feat(fontStyle)

---
+++ src/styles/fontStyle.js
@@ -0,0 +1,30 @@
+import { css } from "styled-components";
+
+export const FONT_STYLE = {
+  BODY1_REGULAR: css`
+    /* Linkbrary/body1-regular */
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: 24px; /* 150% */
+  `,
+
+  BODY1_SEMIBOLD: css`
+    /* Linkbrary/body 1-semibold */
+    font-family: Pretendard;
+    font-size: 16px;
+    font-style: normal;
+    font-weight: 600;
+    line-height: normal;
+  `,
+
+  BODY2_REGULAR: css`
+    /* Linkbrary/body2-regular */
+    font-family: Pretendard;
+    font-size: 14px;
+    font-style: normal;
+    font-weight: 400;
+    line-height: normal;
+  `,
+};
