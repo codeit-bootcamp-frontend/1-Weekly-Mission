c66ecff7b3e46c5ce23d554e6977a82ff67974c4
asusia <asusia1111@gmail.com>
2023-12-10 16:55:37 +0900

    chore: API 주소 환경 변수로 설정

--- src/api/apiRequest.ts
+++ src/api/apiRequest.ts
@@ -1,7 +1,7 @@
 import axios, { AxiosRequestConfig } from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://bootcamp-api.codeit.kr/api/',
+  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
 });
 
 const apiRequest = async (options: AxiosRequestConfig) => {
