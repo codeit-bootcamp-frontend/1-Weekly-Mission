b9ea7ababc8e97faebe3e824fce191011c366e9e
hanseulhee <3021062@gmail.com>
2023-12-10 16:26:15 +0900

    modify FormProvider name

--- src/components/FormProvider.tsx
+++ src/components/FormProvider.tsx
@@ -1,17 +1,15 @@
 import { DevTool } from "@hookform/devtools";
 import { PropsWithChildren } from "react";
 import { FormProvider, useForm } from "react-hook-form";
+import { FormValuesType } from "src/types/FormValue";
 
 function RegisterFormProvider({ children }: PropsWithChildren) {
-  const methods = useForm({ mode: "onChange" });
-
-  const submit = () => {
-    console.log("슬희");
-  };
+  const methods = useForm<FormValuesType>({ mode: "onSubmit" });
+  const onSubmit = (data: FormValuesType) => console.log(data);
 
   return (
     <FormProvider {...methods}>
-      <form onSubmit={methods.handleSubmit(submit)}>{children}</form>
+      <form onSubmit={methods.handleSubmit(onSubmit)}>{children}</form>
       <DevTool control={methods.control} />
     </FormProvider>
   );
