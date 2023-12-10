ba2432c7ffcc14393d175c30f5d7aa83ae218a66
HellenHong <hongjw030@naver.com>
2023-12-10 22:14:19 +0900

    Refactor: 필요한 속성 추가

--- src/components/sign/Input/Input.tsx
+++ src/components/sign/Input/Input.tsx
@@ -9,20 +9,29 @@ export default function Input({
   label = "",
   type = "text",
   placeholder = "내용을 입력해주세요.",
+  onChange,
+  onBlur,
+  value,
+  hasError = false,
+  errorMsg = "잘못되었습니다!",
 }: {
   id: string;
   label: string;
   type: inputType;
   placeholder?: string;
+  onChange?: any;
+  onBlur?: any;
+  value: string;
+  hasError: boolean;
+  errorMsg?: string;
 }) {
   const defaultType = type;
-  const [text, setText] = useState("");
   const [inputType, setInputType] = useState(type);
   const [iconUrl, setIconUrl] = useState<eyeIconUrl>("/icons/eye-close.svg");
 
   const handleText = (e: ChangeEvent<HTMLInputElement>) => {
     const newText = e.target.value;
-    setText(newText);
+    onChange(newText);
   };
 
   const handleChangeInputType = () => {
@@ -41,14 +50,16 @@ export default function Input({
         {label}
         <input
           id={id}
-          // data-status={errormsg ? "isError" : "isOk"}
+          data-status={hasError ? "isError" : "isOk"}
           type={inputType}
           className={styles.input}
           placeholder={placeholder}
-          value={text}
+          value={value}
           onChange={handleText}
+          onBlur={onBlur}
         />
       </label>
+      {hasError && <div className={styles["error-msg"]}>{errorMsg}</div>}
       {defaultType === "password" && (
         <img
           className={styles.icon}
