388563e200bf55c76efcc0e69e02a4829fd4609f
ParkSohyunee <sohyunee016@gmail.com>
2023-12-15 21:11:12 +0900

    Feat(ShareContainer): 공유페이지 dynamic routing 페이지로 변경

--- pages/shared/[id]/ShareContainer.tsx
+++ pages/shared/[id]/ShareContainer.tsx
@@ -1,5 +1,6 @@
 import { ChangeEvent, useEffect, useState } from "react";
 import useSWR from "swr";
+import { useRouter } from "next/router";
 import * as S from "./ShareContainerStyles";
 
 import Layout from "@/components/layout/Layout";
@@ -10,21 +11,22 @@ import Loading from "@/components/Loading";
 
 import { LinkData, SharedFolderData } from "@/types/folder";
 
-/** @TODO 폴더id 가져오기 */
-const TEST_FOLDER_ID = 16;
-
 export default function Share() {
+  const router = useRouter();
+  const folderId = router.query.id;
+
   const [links, setLinks] = useState<LinkData[]>([]);
   const [filteredLinks, setFilteredLinks] = useState<LinkData[]>([]);
   const [keyword, setKeyword] = useState("");
 
   const { data, isLoading, error } = useSWR<{ data: SharedFolderData[] }>(
-    `/api/folders/${TEST_FOLDER_ID}`,
+    `/api/folders/${folderId}`,
   );
   const { data: linksData } = useSWR<{ data: LinkData[] }>(
-    `/api/users/${data?.data[0].user_id}/links?folderId=${TEST_FOLDER_ID}`,
+    `/api/users/${data?.data[0].user_id}/links?folderId=${folderId}`,
   );
 
+  /** @TODO 검색바 관련 로직 분리해보기(folder에서도 사용하므로) */
   const handleOnChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
     if (linksData?.data) {
       setKeyword(e.target.value);
