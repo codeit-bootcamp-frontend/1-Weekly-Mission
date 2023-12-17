bad1cebefe0d6f72b4a0d637f8c33a3acc279d1a
James Song <skdmlrh12@gmail.com>
2023-12-17 15:09:15 +0900

    (refactor: api.ts): sign-in, sign-up fetch 함수 삭제

--- lib/utils/api.ts
+++ lib/utils/api.ts
@@ -46,25 +46,6 @@ export async function handleCopyClipBoard(text: string) {
   }
 }
 
-export async function requestSignIn(email = "", password = "") {
-  const formData = JSON.stringify({
-    email: email,
-    password: password,
-  });
-  const response = await fetch(`${BASE_URL}/sign-in`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: formData,
-  });
-  if (!response.ok) {
-    throw new Error("로그인에 실패했습니다");
-  }
-  const body = await response.json();
-  return body;
-}
-
 export async function validateOverlapEmail(email = "") {
   const formData = JSON.stringify({
     email: email,
@@ -78,29 +59,6 @@ export async function validateOverlapEmail(email = "") {
   });
   if (!response.ok) {
     throw new Error("이미 존재하는 이메일입니다.");
-  }
-  const body = await response.json();
-  return body;
-}
-
-export async function requestSignUp(email = "", password = "") {
-  const formData = JSON.stringify({
-    email: email,
-    password: password,
-  });
-  const response = await fetch(`${BASE_URL}/sign-up`, {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: formData,
-  });
-  if (!response.ok) {
-    const body = await response.json();
-    const {
-      error: { message },
-    } = body;
-    throw new Error(message);
   }
   const body = await response.json();
   return body;
