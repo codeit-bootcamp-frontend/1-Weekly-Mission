943c35092a97831abdd105380dd32d71c688570e
HellenHong <hongjw030@naver.com>
2023-12-17 22:26:20 +0900

    Refactor: api 수정

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -4,6 +4,45 @@ export const axiosInstance = axios.create({
   baseURL: "https://bootcamp-api.codeit.kr/api/",
   headers: {
     "Content-Type": "application/json",
+    withCredentials: true,
   },
-  withCredentials: true,
 });
+
+axiosInstance.interceptors.request.use((config) => {
+  if (!config.headers) return config;
+  const accessToken = localStorage.getItem("accessToken") || "";
+  if (accessToken && config.headers) {
+    config.headers["Authorization"] = accessToken;
+  }
+  return config;
+});
+
+axiosInstance.interceptors.response.use(
+  (res) => {
+    return res;
+  },
+  async (error) => {
+    if (error.config && error.response && error.response.status === 401) {
+      error.config._retry = true;
+      const refreshtoken = localStorage.getItem("refreshToken");
+      error.config.headers.RefreshToken = `${refreshtoken}`;
+      return axios
+        .get(`${process.env.NEXT_PUBLIC_API_URL}/auth/me`, {
+          headers: {
+            RefreshToken: `${refreshtoken}`,
+            "Content-Type": "application/json",
+            withCredentials: true,
+          },
+        })
+        .then(async (res) => {
+          if (res.status === 200 && res.data.accessToken) {
+            localStorage.setItem("accessToken", res.data.accessToken);
+            const accesstoken = localStorage.getItem("accessToken");
+            error.config.headers["Authorization"] = `${accesstoken}`;
+            return axiosInstance(error.config);
+          }
+        });
+    }
+    return Promise.reject(error);
+  },
+);
--- src/api/getSignUp.tsx
+++ src/api/getSignUp.tsx
@@ -1,16 +1,11 @@
-// BUG - 왜 instance 쓰면 안됨????
 import { axiosInstance } from "./axiosInstance";
-import axios from "axios";
 
 export default async function getSignUp(email = "", password = "") {
   try {
-    const response = await axios.post(
-      "https://bootcamp-api.codeit.kr/api/sign-up",
-      {
-        email: email,
-        password: password,
-      },
-    );
+    const response = await axiosInstance.post("/sign-up", {
+      email: email,
+      password: password,
+    });
     return response.data;
   } catch (e) {
     return;
