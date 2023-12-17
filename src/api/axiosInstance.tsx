db04824ef94dd412c03e0be83156ac60e186b072
HellenHong <hongjw030@naver.com>
2023-12-17 21:29:49 +0900

    Refactor: api 코드 수정

--- src/api/axiosInstance.tsx
+++ src/api/axiosInstance.tsx
@@ -2,5 +2,8 @@ import axios from "axios";
 
 export const axiosInstance = axios.create({
   baseURL: "https://bootcamp-api.codeit.kr/api/",
+  headers: {
+    "Content-Type": "application/json",
+  },
   withCredentials: true,
 });
--- src/api/getSignin.tsx
+++ src/api/getSignin.tsx
@@ -1,13 +1,19 @@
+// BUG - 왜 instance 쓰면 안됨????
+import axios from "axios";
 import { axiosInstance } from "./axiosInstance";
 
 export default async function getSignIn(email = "", password = "") {
   try {
-    const response = await axiosInstance.post(`sign-in`, {
-      email: email,
-      password: password,
-    });
+    const response = await axios.post(
+      "https://bootcamp-api.codeit.kr/api/sign-in",
+      {
+        email: email,
+        password: password,
+      },
+    );
     return response.data;
   } catch (e) {
+    console.log(e);
     return;
   }
 }
