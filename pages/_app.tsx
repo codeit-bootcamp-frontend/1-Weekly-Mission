1fb4802b78e904d015b6f52a3c06ceb8ae436e87
wecaners <wecaners@naver.com>
2024-01-12 00:00:59 +0900

    refactor: 로그인 react query로 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,25 +2,42 @@ import Footer from "@/components/layout/footer/Footer";
 import Nav from "@/components/layout/nav/Nav";
 import { AuthProvider } from "@/context/AuthProvider";
 import "@/styles/global.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import type { AppProps } from "next/app";
 import { useRouter } from "next/router";
+import { useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
   const router = useRouter();
   const isAuthPage =
     router.pathname === "/signin" || router.pathname === "/signup";
 
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      })
+  );
+
   return (
-    <AuthProvider>
-      {isAuthPage ? (
-        <Component {...pageProps} />
-      ) : (
-        <>
-          <Nav />
+    <QueryClientProvider client={queryClient}>
+      <AuthProvider>
+        {isAuthPage ? (
           <Component {...pageProps} />
-          <Footer />
-        </>
-      )}
-    </AuthProvider>
+        ) : (
+          <>
+            <Nav />
+            <Component {...pageProps} />
+            <ReactQueryDevtools initialIsOpen={false} />
+            <Footer />
+          </>
+        )}
+      </AuthProvider>
+    </QueryClientProvider>
   );
 }
