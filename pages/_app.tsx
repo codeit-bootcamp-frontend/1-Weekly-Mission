ed532f1778436781be1ea19379624784fbad7d54
ParkSohyunee <sohyunee016@gmail.com>
2024-01-12 15:53:40 +0900

    Chore: 리액트쿼리 라이브러리 설치 및 설정

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,18 +1,19 @@
 import "styles/globals.css";
 
 import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { FolderContextProvider } from "@/context/FolderContext";
-import { SWRConfig } from "swr";
-import { DOMAIN_URL } from "@/common/constants";
+
+const queryClent = new QueryClient();
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <SWRConfig
-      value={{ fetcher: (url: string) => fetch(`${DOMAIN_URL}${url}`).then((res) => res.json()) }}
-    >
+    <QueryClientProvider client={queryClent}>
       <FolderContextProvider>
         <Component {...pageProps} />
       </FolderContextProvider>
-    </SWRConfig>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 }
