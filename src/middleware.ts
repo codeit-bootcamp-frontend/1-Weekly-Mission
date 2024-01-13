a6a3647bc69e021898e83b97f865186f3d23b210
HellenHong <hongjw030@naver.com>
2024-01-14 01:27:36 +0900

    Feat: 미들웨어 추가

---
+++ src/middleware.ts
@@ -0,0 +1,16 @@
+import type { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
+
+const REQUIRED_AUTH = ["/folder", "/folder/*"];
+const NOT_REQUIRED_AUTH = ["/", "/signin", "/signup"];
+
+export async function middleware(request: NextRequest) {
+  const jwt = request.cookies.get("accessToken");
+  if (!jwt && REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL(`/signin`, request.url));
+  }
+  if (jwt && NOT_REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
+    return NextResponse.redirect(new URL(`/folder`, request.url));
+  }
+  return NextResponse.next();
+}
