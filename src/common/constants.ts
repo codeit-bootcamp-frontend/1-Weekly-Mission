fd9be93cf8e82725e597f1fef6f70aa6936cfd8b
ParkSohyunee <sohyunee016@gmail.com>
2024-01-13 09:50:32 +0900

    Feat(Layout): 변경된 api로 유저데이터 연동

--- src/common/constants.ts
+++ src/common/constants.ts
@@ -1,2 +1,3 @@
 export const TEST_USER_ID = 1;
-export const DOMAIN_URL = "https://bootcamp-api.codeit.kr";
+// export const DOMAIN_URL = "https://bootcamp-api.codeit.kr";
+export const DOMAIN_URL = "https://bootcamp-api.codeit.kr/api/linkbrary/v1";
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -13,13 +13,11 @@ interface LayoutProps {
 }
 
 export default function Layout({ children, footerRef }: LayoutProps) {
-  const { data, isLoading, error } = useSWR(`/api/users/${TEST_USER_ID}`); // 로그인 기능 배우면 userID 수정하기
-
-  if (error) console.log(error);
+  const { data, isLoading } = useSWR(`/users/${TEST_USER_ID}`); // 로그인 기능 배우면 userID 수정하기
 
   return (
     <section className={styles.container}>
-      <Header user={data?.data[0]} isLoading={isLoading} />
+      <Header user={data?.[0]} isLoading={isLoading} />
       <section className={styles.body}>{children}</section>
       <Footer ref={footerRef} />
     </section>
