0352c15baf4bf426c08dbd28f4c44b3f6611fa7e
ParkSohyunee <sohyunee016@gmail.com>
2024-01-14 20:41:27 +0900

    Fix(App): SWR로 설정 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,18 @@
 import "styles/globals.css";
 
 import type { AppProps } from "next/app";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { FolderContextProvider } from "@/context/FolderContext";
-
-const queryClent = new QueryClient();
+import { SWRConfig } from "swr";
+import { DOMAIN_URL } from "@/common/constants";
+import { FolderContextProvider } from "@/context/SelectedFolderContext";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <QueryClientProvider client={queryClent}>
+    <SWRConfig
+      value={{ fetcher: (url: string) => fetch(`${DOMAIN_URL}${url}`).then((res) => res.json()) }}
+    >
       <FolderContextProvider>
         <Component {...pageProps} />
       </FolderContextProvider>
-      <ReactQueryDevtools initialIsOpen={false} />
-    </QueryClientProvider>
+    </SWRConfig>
   );
 }
