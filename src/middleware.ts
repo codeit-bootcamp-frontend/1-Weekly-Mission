f6cbbcd696e94cfceaf9995c0b401f121023f1ef
HellenHong <hongjw030@naver.com>
2024-01-15 23:53:32 +0900

    Refactor: folders 하위 페이지에서도 라우팅 작동하게 코드 수정

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,16 +1,15 @@
 import type { NextRequest } from "next/server";
 import { NextResponse } from "next/server";
 
-const REQUIRED_AUTH = ["/folder", "/folder/*"];
 const NOT_REQUIRED_AUTH = ["/signin", "/signup"];
 
 export async function middleware(request: NextRequest) {
   const jwt = request.cookies.get("accessToken");
-  if (!jwt && REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
+  if (!jwt && request.nextUrl.pathname.startsWith("/folders")) {
     return NextResponse.redirect(new URL(`/signin`, request.url));
   }
   if (jwt && NOT_REQUIRED_AUTH.includes(request.nextUrl.pathname)) {
-    return NextResponse.redirect(new URL(`/folder`, request.url));
+    return NextResponse.redirect(new URL(`/folders`, request.url));
   }
   return NextResponse.next();
 }
